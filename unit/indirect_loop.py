70a97a9a71e45b01c40bbca4f51c66866b60c435
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-07-10 11:44:37 +0100

    Add test-case for indirect reductions

--- unit/indirect_loop.py
+++ unit/indirect_loop.py
@@ -21,7 +21,7 @@ class IndirectLoopTest(unittest.TestCase):
         self._elems = op2.Set(nelems, "elems")
         self._input_x = numpy.array(range(nelems), dtype=numpy.uint32)
         self._x = op2.Dat(self._elems,  1, self._input_x, numpy.uint32, "x")
-
+        self._g = op2.Global(1, 0, numpy.uint32, "natural_sum")
         self._input_11 = numpy.array(range(nelems), dtype=numpy.uint32)
         random.shuffle(self._input_11)
         self._11_elems = op2.Map(self._itset_11, self._elems, 1, self._input_11, "11_elems")
@@ -42,6 +42,13 @@ class IndirectLoopTest(unittest.TestCase):
         kernel_rw = "void kernel_rw(unsigned int* x) { (*x) = (*x) + 1; }\n"
         l = op2.par_loop(op2.Kernel(kernel_rw, "kernel_rw"), self._itset_11, self._x(self._11_elems(0), op2.RW))
         self.assertTrue(sum(self._x.value) == nelems * (nelems + 1) / 2);
+
+    def test_global_inc(self):
+        kernel_global_inc = "void kernel_global_inc(unsigned int *x, unsigned int *inc) { (*x) = (*x) + 1; (*inc) += (*x); }"
+        l = op2.par_loop(op2.Kernel(kernel_global_inc, "kernel_global_inc"),
+                         self._itset_11, self._x(self._11_elems(0), op2.RW),
+                         self._g(op2.INC))
+        self.assertTrue(self._g.data[0] == nelems * (nelems + 1) / 2)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(IndirectLoopTest)
 unittest.TextTestRunner(verbosity=0).run(suite)
