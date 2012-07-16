a7025c512422e31740cef26fb306236d6b2ee39b
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-07-16 11:40:45 +0100

    init unit tests

--- unit/test_api.py
+++ unit/test_api.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pyop2 import op2
 
 class TestAPI:
@@ -7,6 +9,17 @@ class TestAPI:
 
     _backend = 'sequential'
 
+    def test_noninit(self):
+        "RuntimeError should be raised when using op2 before calling init."
+        with pytest.raises(RuntimeError):
+            op2.Set(1)
+
     def test_init(self):
+        "init should correctly set the backend."
         op2.init(self._backend)
         assert op2.backends.get_backend() == 'pyop2.'+self._backend
+
+    def test_double_init(self):
+        "init should only be callable once."
+        with pytest.raises(RuntimeError):
+            op2.init(self._backend)
