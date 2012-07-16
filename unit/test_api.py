2aab4cb278b27c0d214e9374936ac9461146b97b
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-07-16 13:18:24 +0100

    Use funcargs for Set unit tests

--- unit/test_api.py
+++ unit/test_api.py
@@ -3,12 +3,17 @@ import pytest
 from pyop2 import op2
 from pyop2 import sequential
 
+def pytest_funcarg__set(request):
+    return op2.Set(5, 'foo')
+
 class TestUserAPI:
     """
     User API Unit Tests
     """
 
     _backend = 'sequential'
+
+    ## Init unit tests
 
     def test_noninit(self):
         "RuntimeError should be raised when using op2 before calling init."
@@ -25,30 +30,29 @@ class TestUserAPI:
         with pytest.raises(RuntimeError):
             op2.init(self._backend)
 
+    ## Set unit tests
+
     def test_set_illegal_size(self):
         "Set size should be int"
         with pytest.raises(sequential.SizeTypeError):
             op2.Set('foo')
 
     def test_set_illegal_name(self):
-        "Set name should be int"
+        "Set name should be string"
         with pytest.raises(sequential.NameTypeError):
             op2.Set(1,2)
 
-    def test_set_size(self):
+    def test_set_size(self, set):
         "Set constructor should correctly set the size"
-        s = op2.Set(5)
-        assert s.size == 5
+        assert set.size == 5
 
-    def test_set_repr(self):
+    def test_set_repr(self, set):
         "Set repr should have the expected format"
-        s = op2.Set(5, 'foo')
-        assert repr(s) == "Set(5, 'foo')"
+        assert repr(set) == "Set(5, 'foo')"
 
-    def test_set_str(self):
+    def test_set_str(self, set):
         "Set string representation should have the expected format"
-        s = op2.Set(5, 'foo')
-        assert str(s) == "OP2 Set: foo with size 5"
+        assert str(set) == "OP2 Set: foo with size 5"
 
     # FIXME: test Set._lib_handle
 
