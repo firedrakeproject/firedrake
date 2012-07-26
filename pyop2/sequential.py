886a0c79308b99f543cebc2c798ee7be7eb5d2bb
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-07-26 20:35:45 +0100

    Fix typo in argument checking of Mat __call__

--- pyop2/sequential.py
+++ pyop2/sequential.py
@@ -371,7 +371,7 @@ class Mat(DataCarrier):
         for p_map, s_map in zip(path_maps, sparsity_maps):
             if p_map._dataset != s_map._dataset:
                 raise SetValueError("Invalid data set for map %s (is %s, should be %s)" \
-                        % (map._name, a_map._dataset._name, s_map.dataset._name))
+                        % (map._name, p_map._dataset._name, s_map.dataset._name))
         return self._arg_type(data=self, map=path_maps, access=access, idx=path_idxs)
 
     @property
