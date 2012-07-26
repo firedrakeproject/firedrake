79bd4668bc101b99349ccfc16db480f7c5aed887
gmarkall <grm08@doc.ic.ac.uk>
2012-07-26 12:35:06 +0100

    Convert matrix tests to py.test

--- unit/test_matrices.py
+++ unit/test_matrices.py
@@ -1,9 +1,14 @@
-import unittest
+import pytest
 import numpy
 
 from pyop2 import op2
-# Initialise OP2
-op2.init(backend='sequential')
+
+def setup_module(module):
+    # Initialise OP2
+    op2.init(backend='sequential')
+
+def teardown_module(module):
+    op2.exit()
 
 # Data type
 valuetype = numpy.float64
@@ -13,35 +18,57 @@ NUM_ELE   = 2
 NUM_NODES = 4
 NUM_DIMS  = 2
 
-class MatricesTest(unittest.TestCase):
+class TestMatrices:
     """
-
     Matrix tests
 
     """
 
-    def setUp(self):
+    def pytest_funcarg__nodes(cls, request):
+        return op2.Set(NUM_NODES, "nodes")
+
+    def pytest_funcarg__elements(cls, request):
+        return op2.Set(NUM_ELE, "elements")
+
+    def pytest_funcarg__elem_node(cls, request):
+        elements = request.getfuncargvalue('elements')
+        nodes = request.getfuncargvalue('nodes')
         elem_node_map = numpy.asarray([ 0, 1, 3, 2, 3, 1 ], dtype=numpy.uint32)
+        return op2.Map(elements, nodes, 3, elem_node_map, "elem_node")
+
+    def pytest_funcarg__mat(cls, request):
+        elem_node = request.getfuncargvalue('elem_node')
+        sparsity = op2.Sparsity(elem_node, elem_node, 1, "sparsity")
+        return request.cached_setup(
+                setup=lambda: op2.Mat(sparsity, 1, valuetype, "mat"),
+                scope='session')
+
+    def pytest_funcarg__coords(cls, request):
+        nodes = request.getfuncargvalue('nodes')
         coord_vals = numpy.asarray([ (0.0, 0.0), (2.0, 0.0),
                                      (1.0, 1.0), (0.0, 1.5) ],
                                    dtype=valuetype)
+        return op2.Dat(nodes, 2, coord_vals, valuetype, "coords")
+
+    def pytest_funcarg__f(cls, request):
+        nodes = request.getfuncargvalue('nodes')
         f_vals = numpy.asarray([ 1.0, 2.0, 3.0, 4.0 ], dtype=valuetype)
+        return op2.Dat(nodes, 1, f_vals, valuetype, "f")
+
+    def pytest_funcarg__b(cls, request):
+        nodes = request.getfuncargvalue('nodes')
         b_vals = numpy.asarray([0.0]*NUM_NODES, dtype=valuetype)
+        return request.cached_setup(
+                setup=lambda: op2.Dat(nodes, 1, b_vals, valuetype, "b"),
+                scope='session')
+
+    def pytest_funcarg__x(cls, request):
+        nodes = request.getfuncargvalue('nodes')
         x_vals = numpy.asarray([0.0]*NUM_NODES, dtype=valuetype)
+        return op2.Dat(nodes, 1, x_vals, valuetype, "x")
 
-        self._nodes = op2.Set(NUM_NODES, "nodes")
-        self._elements = op2.Set(NUM_ELE, "elements")
-        self._elem_node = op2.Map(self._elements, self._nodes, 3, elem_node_map,
-                                  "elem_node")
-
-        sparsity = op2.Sparsity(self._elem_node, self._elem_node, 1, "sparsity")
-        self._mat = op2.Mat(sparsity, 1, valuetype, "mat")
-        self._coords = op2.Dat(self._nodes, 2, coord_vals, valuetype, "coords")
-        self._f = op2.Dat(self._nodes, 1, f_vals, valuetype, "f")
-        self._b = op2.Dat(self._nodes, 1, b_vals, valuetype, "b")
-        self._x = op2.Dat(self._nodes, 1, x_vals, valuetype, "x")
-
-        kernel_mass = """
+    def pytest_funcarg__mass(cls, request):
+        kernel_code = """
 void mass(double* localTensor, double* c0[2], int i_r_0, int i_r_1)
 {
   const double CG1[3][6] = { {  0.09157621, 0.09157621, 0.81684757,
@@ -93,10 +120,12 @@ void mass(double* localTensor, double* c0[2], int i_r_0, int i_r_1)
     ST0 += CG1[i_r_0][i_g] * CG1[i_r_1][i_g] * (c_q0[i_g][0][0] * c_q0[i_g][1][1] + -1 * c_q0[i_g][0][1] * c_q0[i_g][1][0]);
     localTensor[0] += ST0 * w[i_g];
   };
-}
-"""
+}"""
+        return op2.Kernel(kernel_code, "mass")
 
-        kernel_rhs = """
+    def pytest_funcarg__rhs(cls, request):
+
+        kernel_code = """
 void rhs(double** localTensor, double* c0[2], double* c1[1])
 {
   const double CG1[3][6] = { {  0.09157621, 0.09157621, 0.81684757,
@@ -157,38 +186,14 @@ void rhs(double** localTensor, double* c0[2], double* c1[1])
       localTensor[i_r_0][0] += ST1 * w[i_g];
     };
   };
-}
-"""
+}"""
+        return op2.Kernel(kernel_code, "rhs")
 
-        self._mass = op2.Kernel(kernel_mass, "mass")
-        self._rhs = op2.Kernel(kernel_rhs, "rhs")
-
-    def tearDown(self):
-        del self._nodes
-        del self._elements
-        del self._elem_node
-        del self._mat
-        del self._coords
-        del self._f
-        del self._b
-        del self._x
-        del self._mass
-        del self._rhs
-
-    def _assemble_mass(self):
-        op2.par_loop(self._mass, self._elements(3,3),
-                     self._mat((self._elem_node(op2.i(0)), self._elem_node(op2.i(1))), op2.INC),
-                     self._coords(self._elem_node, op2.READ))
-
-    def _assemble_rhs(self):
-        op2.par_loop(self._rhs, self._elements,
-                     self._b(self._elem_node, op2.INC),
-                     self._coords(self._elem_node, op2.READ),
-                     self._f(self._elem_node, op2.READ))
-
-    @unittest.expectedFailure
-    def test_assemble(self):
-        #self._assemble_mass()
+    @pytest.mark.xfail
+    def test_assemble(self, mass, mat, coords, elements, elem_node):
+        op2.par_loop(mass, elements(3,3),
+                     mat((elem_node(op2.i(0)), elem_node(op2.i(1))), op2.INC),
+                     coords(elem_node, op2.READ))
 
         expected_vals = [(0.25, 0.125, 0.0, 0.125),
                          (0.125, 0.291667, 0.0208333, 0.145833),
@@ -196,27 +201,25 @@ void rhs(double** localTensor, double* c0[2], double* c1[1])
                          (0.125, 0.145833, 0.0208333, 0.291667) ]
         expected_matrix = numpy.asarray(expected_vals, dtype=valuetype)
         # Check that the matrix values equal these values, somehow.
-        assertTrue(False)
+        assert False
 
-    @unittest.expectedFailure
-    def test_rhs(self):
-        # Assemble the RHS here, so if solve fails we know whether the RHS
-        # assembly went wrong or something to do with the solve.
-        #self._assemble_rhs()
-        #print self._b.data
-        assertTrue(False)
+    def test_rhs(self, rhs, elements, b, coords, f, elem_node):
+        op2.par_loop(rhs, elements,
+                     b(elem_node, op2.INC),
+                     coords(elem_node, op2.READ),
+                     f(elem_node, op2.READ))
 
-    @unittest.expectedFailure
-    def test_solve(self):
-        # Assemble matrix and RHS and solve.
-        self._assemble_mass()
-        self._assemble_rhs()
-        print "RHS:"
-        print self._b.data
-        op2.solve(self._mat, self._b, self._x)
-        print "solution: "
-        print self._x.data
-        assertTrue(False)
+        expected = numpy.asarray([[0.9999999523522115], [1.3541666031724144],
+                                  [0.2499999883507239], [1.6458332580869566]],
+                                  dtype=valuetype)
+        eps = 1.e-12
+        assert all(abs(b.data-expected)<eps)
 
-suite = unittest.TestLoader().loadTestsFromTestCase(MatricesTest)
-unittest.TextTestRunner(verbosity=0).run(suite)
+    def test_solve(self, mat, b, x, f):
+        op2.solve(mat, b, x)
+        eps = 1.e-12
+        assert all(abs(x.data-f.data)<eps)
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
