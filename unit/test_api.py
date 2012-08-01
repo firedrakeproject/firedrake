f551206a69ffc198f3c467fdfcbea2f65068b2ab
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-08-01 11:01:42 +0100

    Access API tests take backend parameter to fix test order

--- unit/test_api.py
+++ unit/test_api.py
@@ -110,12 +110,12 @@ class TestAccessAPI:
     """
 
     @pytest.mark.parametrize("mode", sequential.Access._modes)
-    def test_access(self, mode):
+    def test_access(self, mode, backend):
         "Access repr should have the expected format."
         a = sequential.Access(mode)
         assert repr(a) == "Access('%s')" % mode
 
-    def test_illegal_access(self):
+    def test_illegal_access(self, backend):
         "Illegal access modes should raise an exception."
         with pytest.raises(exceptions.ModeValueError):
             sequential.Access('ILLEGAL_ACCESS')
