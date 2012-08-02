28ac91069cd1a756304353057ad647b00119dca3
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-08-02 12:28:50 +0100

    Update unit test documentation, add README.rst

--- unit/conftest.py
+++ unit/conftest.py
@@ -31,72 +31,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""
-Auto-parametrization of test cases
-==================================
-
-Passing the parameter 'backend' to any test case will auto-parametrise
-that test case for all selected backends. By default all backends from
-the backends dict in the backends module are selected. Backends for
-which the dependencies are not installed are thereby automatically
-skipped. Tests execution is grouped per backend and op2.init() and
-op2.exit() for a backend are only called once per test session.
-
-Selecting for which backend to run
-==================================
-
-The default backends can be overridden by passing the
-`--backend=<string>` parameter on test invocation. Passing it multiple
-times runs the tests for all the given backends.
-
-Skipping backends on a per-test basis
-=====================================
-
-To skip a particular backend in a test case, pass the 'skip_<backend>'
-parameter to the test function, where '<backend>' is any valid backend
-string.
-
-Skipping backends on a module or class basis
-============================================
-
-You can supply a list of backends to skip for all tests in a given
-module or class with the ``skip_backends`` attribute in the module or
-class scope:
-
-    # module test_foo.py
-
-    # All tests in this module will not run for the CUDA backend
-    skip_backends = ['cuda']
-
-    class TestFoo:
-        # All tests in this class will not run for the CUDA and OpenCL
-        # backends
-        skip_backends = ['opencl']
-
-Selecting backends on a module or class basis
-=============================================
-
-Not passing the parameter 'backend' to a test case will cause it to
-only run once for the backend that is currently initialized, which is
-not always safe.
-
-You can supply a list of backends for which to run all tests in a given
-module or class with the ``backends`` attribute in the module or class
-scope:
-
-    # module test_foo.py
-
-    # All tests in this module will only run for the CUDA and OpenCL
-    # backens
-    backends = ['cuda', 'opencl']
-
-    class TestFoo:
-        # All tests in this class will only run for the CUDA backend
-        backends = ['sequential', 'cuda']
-
-This set of backends to run for will be further restricted by the
-backends selected via command line parameters if applicable.
-"""
+"""Global test configuration."""
 
 import pytest
 
