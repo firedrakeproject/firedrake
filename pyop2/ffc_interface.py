000745716164f86dbba8ef227150998cf969cfa8
gmarkall <grm08@doc.ic.ac.uk>
2012-08-09 18:07:34 +0100

    Provide FFC interface for safely generating code that can be passed to backends.

---
+++ pyop2/ffc_interface.py
@@ -0,0 +1,73 @@
+# This file is part of PyOP2
+#
+# PyOP2 is Copyright (c) 2012, Imperial College London and
+# others. Please see the AUTHORS file in the main source directory for
+# a full list of copyright holders.  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * The name of Imperial College London or that of other
+#       contributors may not be used to endorse or promote products
+#       derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTERS
+# ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""Provides the interface to FFC for compiling a form, and transforms the FFC-
+generated code in order to make it suitable for passing to the backends."""
+
+from ffc import default_parameters, compile_form as ffc_compile_form
+import re
+
+def compile_form(form, name):
+    """Compile a form using FFC and return an OP2 kernel"""
+
+    ffc_parameters = default_parameters()
+    ffc_parameters['write_file'] = False
+    ffc_parameters['format'] = 'pyop2'
+
+    code = ffc_compile_form(form, prefix=name, parameters=ffc_parameters)
+    return comment_remover(continuation_remover(code))
+
+def comment_remover(text):
+    """Remove all C- and C++-style comments from a string."""
+    # Reference: http://stackoverflow.com/questions/241327/python-snippet-to-remove-c-and-c-comments
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return ""
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
+
+def continuation_remover(text):
+    """Remove the trailing backslashes from a string."""
+    def replacer(match):
+        return match.group(0)[0:-1]
+    pattern = re.compile(
+        r'.*\\$',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
