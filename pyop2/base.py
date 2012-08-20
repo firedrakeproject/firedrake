d26d967089b1a2753c10bb60508a23fc552400c5
David A Ham <david.ham@imperial.ac.uk>
2012-08-20 14:34:09 +0100

    Document the Access constants.

--- pyop2/base.py
+++ pyop2/base.py
@@ -59,11 +59,22 @@ class Access(object):
         return "Access('%s')" % self._mode
 
 READ  = Access("READ")
+"""The :class:`DataCarrier` is accessed read-only."""
+
 WRITE = Access("WRITE")
+"""The :class:`DataCarrier` is accessed write-only, and OP2 is not required to handle write conflicts."""
+
 RW    = Access("RW")
+"""The :class:`DataCarrier` is accessed for reading and writing, and OP2 is not required to handle write conflicts."""
+
 INC   = Access("INC")
+"""The kernel computes increments to be summed onto :class:`DataCarrier`. OP2 is responsible for managing the write conflicts caused."""
+
 MIN   = Access("MIN")
+"""The kernel contributes to a reduction into a :class:`Global` using a ``min`` operation. OP2 is responsible for reducing over the different kernel invocations."""
+
 MAX   = Access("MAX")
+"""The kernel contributes to a reduction into a :class:`Global` using a ``max`` operation. OP2 is responsible for reducing over the different kernel invocations."""
 
 # Data API
 
