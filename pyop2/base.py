3203b946b3d26f7644b731a995e2c02c1cb71ee4
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-09-20 12:28:02 +0100

    Use discard when removing a Const
    This way, we don't have to check that the Const is in the list of
    known Const objects first.

--- pyop2/base.py
+++ pyop2/base.py
@@ -447,8 +447,7 @@ class Const(DataCarrier):
         """Remove this Const object from the namespace
 
         This allows the same name to be redeclared with a different shape."""
-        if self in Const._defs:
-            Const._defs.remove(self)
+        Const._defs.discard(self)
 
     def _format_for_c(self):
         d = {'type' : self.ctype,
