1004936cacbcb5bf47d3b3c0257611960ad8b658
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2013-01-31 17:22:17 +0000

    Remove restriction that Dats must have data associated with them
    par_loops are collective and therefore if one iterates over the
    boundary ids in a mesh, some of the processes may have Dats with no
    data associated with them.  So only raise an error if the size of the
    Dat is zero when the Dat's dataset size is non-zero.

--- pyop2/base.py
+++ pyop2/base.py
@@ -597,8 +597,8 @@ class Dat(DataCarrier):
     @property
     def data(self):
         """Numpy array containing the data values."""
-        if len(self._data) is 0:
-            raise RuntimeError("Illegal access: No data associated with this Dat!")
+        if self.dataset.total_size > 0 and self._data.size == 0:
+            raise RuntimeError("Illegal access: no data associated with this Dat!")
         maybe_setflags(self._data, write=True)
         self.needs_halo_update = True
         return self._data
@@ -606,8 +606,8 @@ class Dat(DataCarrier):
     @property
     def data_ro(self):
         """Numpy array containing the data values.  Read-only"""
-        if len(self._data) is 0:
-            raise RuntimeError("Illegal access: No data associated with this Dat!")
+        if self.dataset.total_size > 0 and self._data.size == 0:
+            raise RuntimeError("Illegal access: no data associated with this Dat!")
         maybe_setflags(self._data, write=False)
         return self._data
 
