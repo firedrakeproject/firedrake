OP2/PyOP2

Pack sends/receives into buffers before sending
Because we're using asynchronous comms, we can't pass a python object
exposing the buffer interface, because the buffer might have
disappeared by the time we actually receive the data.  This is the
same problem as passing fortran array slices into Isend/Irecv
pairs (although at least we don't segfault).  So pack into temporary
buffers and unpack after actually receiving everything.

pyop2/base.py
@@ -557,7 +557,9 @@ class Dat(DataCarrier):
         self._dirty = False
         # FIXME: Use correct communicator
         self._send_reqs = [None]*MPI_COMM.size
+        self._send_buf = [None]*MPI_COMM.size
         self._recv_reqs = [None]*MPI_COMM.size
+        self._recv_buf = [None]*MPI_COMM.size
         # so that we can tag halo exchanges for each Dat uniquely
         # FIXME: This requires that Dats are declared /in the same
         # order/ on all MPI processes and hence have the same id, we
@@ -631,7 +633,8 @@ class Dat(DataCarrier):
                 # 0 previously) or if there are no elements to send
                 self._send_reqs[dest] = MPI.REQUEST_NULL
                 continue
-            self._send_reqs[dest] = halo.comm.Isend(self._data[ele],
+            self._send_buf[dest] = self._data[ele]
+            self._send_reqs[dest] = halo.comm.Isend(self._send_buf[dest],
                                                     dest=dest, tag=self._id)
         for source,ele in enumerate(halo.receives):
             if ele.size == 0:
@@ -639,7 +642,8 @@ class Dat(DataCarrier):
                 # to receive
                 self._recv_reqs[source] = MPI.REQUEST_NULL
                 continue
-            self._recv_reqs[source] = halo.comm.Irecv(self._data[ele],
+            self._recv_buf[source] = self._data[ele]
+            self._recv_reqs[source] = halo.comm.Irecv(self._recv_buf[source],
                                                       source=source, tag=self._id)
 
     def halo_exchange_end(self):
@@ -647,6 +651,11 @@ class Dat(DataCarrier):
             return
         MPI.Request.Waitall(self._recv_reqs)
         MPI.Request.Waitall(self._send_reqs)
+        self._send_buf = [None]*MPI_COMM.size
+        for source, buf in enumerate(self._recv_buf):
+            if buf is not None:
+                self._data[self.dataset.halo.receives[source]] = buf
+        self._recv_buf = [None]*MPI_COMM.size
 
     def zero(self):
         """Zero the data associated with this :class:`Dat`"""

firedrakeproject/firedrake

pyop2/base.py
@@ -561,7 +561,9 @@ class Dat(DataCarrier):
         self._needs_halo_update = False
         # FIXME: Use correct communicator
         self._send_reqs = [None]*PYOP2_COMM.size
+        self._send_buf = [None]*PYOP2_COMM.size
         self._recv_reqs = [None]*PYOP2_COMM.size
+        self._recv_buf = [None]*PYOP2_COMM.size
         # so that we can tag halo exchanges for each Dat uniquely
         # FIXME: This requires that Dats are declared /in the same
         # order/ on all MPI processes and hence have the same id, we
@@ -636,7 +638,8 @@ class Dat(DataCarrier):
                 # 0 previously) or if there are no elements to send
                 self._send_reqs[dest] = MPI.REQUEST_NULL
                 continue
-            self._send_reqs[dest] = halo.comm.Isend(self._data[ele],
+            self._send_buf[dest] = self._data[ele]
+            self._send_reqs[dest] = halo.comm.Isend(self._send_buf[dest],
                                                     dest=dest, tag=self._id)
         for source,ele in enumerate(halo.receives):
             if ele.size == 0:
@@ -644,7 +647,8 @@ class Dat(DataCarrier):
                 # to receive
                 self._recv_reqs[source] = MPI.REQUEST_NULL
                 continue
-            self._recv_reqs[source] = halo.comm.Irecv(self._data[ele],
+            self._recv_buf[source] = self._data[ele]
+            self._recv_reqs[source] = halo.comm.Irecv(self._recv_buf[source],
                                                       source=source, tag=self._id)
 
     def halo_exchange_end(self):
@@ -652,6 +656,11 @@ class Dat(DataCarrier):
             return
         MPI.Request.Waitall(self._recv_reqs)
         MPI.Request.Waitall(self._send_reqs)
+        self._send_buf = [None]*PYOP2_COMM.size
+        for source, buf in enumerate(self._recv_buf):
+            if buf is not None:
+                self._data[self.dataset.halo.receives[source]] = buf
+        self._recv_buf = [None]*PYOP2_COMM.size
 
     def zero(self):
         """Zero the data associated with this :class:`Dat`"""