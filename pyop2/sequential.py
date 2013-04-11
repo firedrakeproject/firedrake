18b611b1068806e7a492ca71a826b4b8674b0e37
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-04-11 18:49:51 +0100

    Break sequential code generation into more manageable chunks

--- pyop2/sequential.py
+++ pyop2/sequential.py
@@ -53,8 +53,28 @@ def par_loop(kernel, it_space, *args):
     ParLoop(kernel, it_space, *args).compute()
 
 class ParLoop(petsc_base.ParLoop):
+
+    wrapper = """
+              void wrap_%(kernel_name)s__(PyObject *_start, PyObject *_end, %(wrapper_args)s %(const_args)s) {
+                int start = (int)PyInt_AsLong(_start);
+                int end = (int)PyInt_AsLong(_end);
+                %(wrapper_decs)s;
+                %(tmp_decs)s;
+                %(const_inits)s;
+                for ( int i = start; i < end; i++ ) {
+                  %(vec_inits)s;
+                  %(itspace_loops)s
+                  %(ind)s%(zero_tmps)s;
+                  %(ind)s%(kernel_name)s(%(kernel_args)s);
+                  %(ind)s%(addtos_vector_field)s;
+                  %(itspace_loop_close)s
+                  %(addtos_scalar_field)s;
+                }
+              }
+              """
+
     def compute(self):
-        _fun = self.generate_code()
+        _fun = self.build()
         _args = [0, 0]          # start, stop
         for arg in self.args:
             if arg._is_mat:
@@ -100,7 +120,7 @@ class ParLoop(petsc_base.ParLoop):
             if arg._is_mat:
                 arg.data._assemble()
 
-    def generate_code(self):
+    def build(self):
 
         key = self._cache_key
         _fun = petsc_base._parloop_cache.get(key)
@@ -110,71 +130,7 @@ class ParLoop(petsc_base.ParLoop):
 
         from instant import inline_with_numpy
 
-        def itspace_loop(i, d):
-            return "for (int i_%d=0; i_%d<%d; ++i_%d) {" % (i, i, d, i)
-
-        def c_const_arg(c):
-            return 'PyObject *_%s' % c.name
-
-        def c_const_init(c):
-            d = {'name' : c.name,
-                 'type' : c.ctype}
-            if c.cdim == 1:
-                return '%(name)s = ((%(type)s *)(((PyArrayObject *)_%(name)s)->data))[0]' % d
-            tmp = '%(name)s[%%(i)s] = ((%(type)s *)(((PyArrayObject *)_%(name)s)->data))[%%(i)s]' % d
-            return ';\n'.join([tmp % {'i' : i} for i in range(c.cdim)])
-
-        args = self.args
-        _wrapper_args = ', '.join([arg.c_wrapper_arg() for arg in args])
-
-        _tmp_decs = ';\n'.join([arg.tmp_decl(self._it_space.extents) for arg in args if arg._is_mat])
-        _wrapper_decs = ';\n'.join([arg.c_wrapper_dec() for arg in args])
-
-        _const_decs = '\n'.join([const._format_declaration() for const in Const._definitions()]) + '\n'
-
-        _kernel_user_args = [arg.c_kernel_arg() for arg in args]
-        _kernel_it_args   = ["i_%d" % d for d in range(len(self._it_space.extents))]
-        _kernel_args = ', '.join(_kernel_user_args + _kernel_it_args)
-        _vec_inits = ';\n'.join([arg.c_vec_init() for arg in args \
-                                 if not arg._is_mat and arg._is_vec_map])
-
-        nloops = len(self._it_space.extents)
-        _itspace_loops = '\n'.join(['  ' * i + itspace_loop(i,e) for i, e in enumerate(self._it_space.extents)])
-        _itspace_loop_close = '\n'.join('  ' * i + '}' for i in range(nloops - 1, -1, -1))
-
-        _addtos_vector_field = ';\n'.join([arg.c_addto_vector_field() for arg in args \
-                                           if arg._is_mat and arg.data._is_vector_field])
-        _addtos_scalar_field = ';\n'.join([arg.c_addto_scalar_field() for arg in args \
-                                           if arg._is_mat and arg.data._is_scalar_field])
-
-        _zero_tmps = ';\n'.join([arg.c_zero_tmp() for arg in args if arg._is_mat])
-
-        if len(Const._defs) > 0:
-            _const_args = ', '
-            _const_args += ', '.join([c_const_arg(c) for c in Const._definitions()])
-        else:
-            _const_args = ''
-        _const_inits = ';\n'.join([c_const_init(c) for c in Const._definitions()])
-        wrapper = """
-                  void wrap_%(kernel_name)s__(PyObject *_start, PyObject *_end, %(wrapper_args)s %(const_args)s) {
-                    int start = (int)PyInt_AsLong(_start);
-                    int end = (int)PyInt_AsLong(_end);
-                    %(wrapper_decs)s;
-                    %(tmp_decs)s;
-                    %(const_inits)s;
-                    for ( int i = start; i < end; i++ ) {
-                      %(vec_inits)s;
-                      %(itspace_loops)s
-                      %(ind)s%(zero_tmps)s;
-                      %(ind)s%(kernel_name)s(%(kernel_args)s);
-                      %(ind)s%(addtos_vector_field)s;
-                      %(itspace_loop_close)s
-                      %(addtos_scalar_field)s;
-                    }
-                  }
-                  """
-
-        if any(arg._is_soa for arg in args):
+        if any(arg._is_soa for arg in self.args):
             kernel_code = """
             #define OP2_STRIDE(a, idx) a[idx]
             inline %(code)s
@@ -184,22 +140,9 @@ class ParLoop(petsc_base.ParLoop):
             kernel_code = """
             inline %(code)s
             """ % {'code' : self._kernel.code }
-        indent = lambda t, i: ('\n' + '  ' * i).join(t.split('\n'))
-        code_to_compile = dedent(wrapper) % {
-            'ind': '  ' * nloops,
-            'kernel_name': self._kernel.name,
-            'wrapper_args': _wrapper_args,
-            'wrapper_decs': indent(_wrapper_decs, 1),
-            'const_args': _const_args,
-            'const_inits': indent(_const_inits, 1),
-            'tmp_decs': indent(_tmp_decs, 1),
-            'itspace_loops': indent(_itspace_loops, 2),
-            'itspace_loop_close': indent(_itspace_loop_close, 2),
-            'vec_inits': indent(_vec_inits, 2),
-            'zero_tmps': indent(_zero_tmps, 2 + nloops),
-            'kernel_args': _kernel_args,
-            'addtos_vector_field': indent(_addtos_vector_field, 2 + nloops),
-            'addtos_scalar_field': indent(_addtos_scalar_field, 2)}
+        code_to_compile = dedent(self.wrapper) % self.generate_code()
+
+        _const_decs = '\n'.join([const._format_declaration() for const in Const._definitions()]) + '\n'
 
         # We need to build with mpicc since that's required by PETSc
         cc = os.environ.get('CC')
@@ -220,6 +163,67 @@ class ParLoop(petsc_base.ParLoop):
 
         petsc_base._parloop_cache[key] = _fun
         return _fun
+
+    def generate_code(self):
+
+        def itspace_loop(i, d):
+            return "for (int i_%d=0; i_%d<%d; ++i_%d) {" % (i, i, d, i)
+
+        def c_const_arg(c):
+            return 'PyObject *_%s' % c.name
+
+        def c_const_init(c):
+            d = {'name' : c.name,
+                 'type' : c.ctype}
+            if c.cdim == 1:
+                return '%(name)s = ((%(type)s *)(((PyArrayObject *)_%(name)s)->data))[0]' % d
+            tmp = '%(name)s[%%(i)s] = ((%(type)s *)(((PyArrayObject *)_%(name)s)->data))[%%(i)s]' % d
+            return ';\n'.join([tmp % {'i' : i} for i in range(c.cdim)])
+
+        _wrapper_args = ', '.join([arg.c_wrapper_arg() for arg in self.args])
+
+        _tmp_decs = ';\n'.join([arg.tmp_decl(self._it_space.extents) for arg in self.args if arg._is_mat])
+        _wrapper_decs = ';\n'.join([arg.c_wrapper_dec() for arg in self.args])
+
+        _kernel_user_args = [arg.c_kernel_arg() for arg in self.args]
+        _kernel_it_args   = ["i_%d" % d for d in range(len(self._it_space.extents))]
+        _kernel_args = ', '.join(_kernel_user_args + _kernel_it_args)
+        _vec_inits = ';\n'.join([arg.c_vec_init() for arg in self.args \
+                                 if not arg._is_mat and arg._is_vec_map])
+
+        nloops = len(self._it_space.extents)
+        _itspace_loops = '\n'.join(['  ' * i + itspace_loop(i,e) for i, e in enumerate(self._it_space.extents)])
+        _itspace_loop_close = '\n'.join('  ' * i + '}' for i in range(nloops - 1, -1, -1))
+
+        _addtos_vector_field = ';\n'.join([arg.c_addto_vector_field() for arg in self.args \
+                                           if arg._is_mat and arg.data._is_vector_field])
+        _addtos_scalar_field = ';\n'.join([arg.c_addto_scalar_field() for arg in self.args \
+                                           if arg._is_mat and arg.data._is_scalar_field])
+
+        _zero_tmps = ';\n'.join([arg.c_zero_tmp() for arg in self.args if arg._is_mat])
+
+        if len(Const._defs) > 0:
+            _const_args = ', '
+            _const_args += ', '.join([c_const_arg(c) for c in Const._definitions()])
+        else:
+            _const_args = ''
+        _const_inits = ';\n'.join([c_const_init(c) for c in Const._definitions()])
+
+        indent = lambda t, i: ('\n' + '  ' * i).join(t.split('\n'))
+        return {'ind': '  ' * nloops,
+                'kernel_name': self._kernel.name,
+                'wrapper_args': _wrapper_args,
+                'wrapper_decs': indent(_wrapper_decs, 1),
+                'const_args': _const_args,
+                'const_inits': indent(_const_inits, 1),
+                'tmp_decs': indent(_tmp_decs, 1),
+                'itspace_loops': indent(_itspace_loops, 2),
+                'itspace_loop_close': indent(_itspace_loop_close, 2),
+                'vec_inits': indent(_vec_inits, 2),
+                'zero_tmps': indent(_zero_tmps, 2 + nloops),
+                'kernel_args': _kernel_args,
+                'addtos_vector_field': indent(_addtos_vector_field, 2 + nloops),
+                'addtos_scalar_field': indent(_addtos_scalar_field, 2)}
 
 def _setup():
     pass
