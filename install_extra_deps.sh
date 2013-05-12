02ed645e907777af49e2579b35901a440b1e9659
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-05-12 23:25:35 +0100

    Use pip requirements.txt instead of bash script for additional dependencies

--- install_extra_deps.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-pip install hg+https://bitbucket.org/khinsen/scientificpython
-pip install "codepy>=2013.1"
-LDFLAGS=$TOX_LDFLAGS pip install git+git://github.com/inducer/pycuda.git#egg=pycuda
-pip install "pyopencl>=2012.1"
-C_INCLUDE_PATH=$TOX_C_INCLUDE_PATH pip install "h5py>=2.0.0"
-PETSC_CONFIGURE_OPTIONS="--with-fortran-interfaces=1 --with-c++-support" pip install petsc
-pip install hg+https://bitbucket.org/mapdes/petsc4py#egg=petsc4py
