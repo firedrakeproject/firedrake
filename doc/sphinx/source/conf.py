068a8c4ec72669c523d107425d5a925dd4762197
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-05-16 17:42:35 +0100

    sphinx doc: autodoc undocumented and private members

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -27,6 +27,7 @@ sys.path.insert(0, os.path.abspath('../../..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath']
+autodoc_default_flags = ['members', 'undoc-members', 'private-members']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
