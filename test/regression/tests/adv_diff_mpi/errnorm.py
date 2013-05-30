20f3761f8cb78119b464c59fcb5821252f642b7d
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-05-30 20:14:18 +0100

    Compatibility fix: with does not support multiple arguments in 2.6

--- test/regression/tests/adv_diff_mpi/errnorm.py
+++ test/regression/tests/adv_diff_mpi/errnorm.py
@@ -1,5 +1,6 @@
 from math import log, sqrt
 
 def convergence(filename1, filename2):
-    with open(filename1) as f1, open(filename2) as f2:
-        return log(sqrt(float(f1.read())) / sqrt(float(f2.read())), 2)
+    with open(filename1) as f1:
+        with open(filename2) as f2:
+            return log(sqrt(float(f1.read())) / sqrt(float(f2.read())), 2)
