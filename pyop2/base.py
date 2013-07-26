9239bd8255a66c708d41acf0993aed7201ad796a
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2013-07-26 16:28:59 +0100

    RHS Test in firedrake now returns 0.

--- pyop2/base.py
+++ pyop2/base.py
@@ -588,11 +588,6 @@ class DataSet(object):
         return getattr(self._set, name)
 
     @property
-    def total_size(self):
-        """Total set size, including halo elements."""
-        return self.set._inh_size * self.layers
-
-    @property
     def dim(self):
         """The shape tuple of the values for each element of the set."""
         return self._dim
