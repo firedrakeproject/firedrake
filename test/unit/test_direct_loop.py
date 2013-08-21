88df5f07d3e2b41cb121beafdd6f9cf6e4ccf254
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-08-21 12:04:18 +0100

    Docstrings for direct loop unit tests

--- test/unit/test_direct_loop.py
+++ test/unit/test_direct_loop.py
@@ -87,41 +87,39 @@ class TestDirectLoop:
         return op2.Dat(delems2, [xarray(), xarray()], numpy.uint32, "x", soa=True)
 
     def test_wo(self, backend, elems, x):
-        kernel_wo = """
-void kernel_wo(unsigned int* x) { *x = 42; }
-"""
+        """Set a Dat to a scalar value with op2.WRITE."""
+        kernel_wo = """void kernel_wo(unsigned int* x) { *x = 42; }"""
         op2.par_loop(op2.Kernel(kernel_wo, "kernel_wo"),
                      elems, x(op2.IdentityMap, op2.WRITE))
         assert all(map(lambda x: x == 42, x.data))
 
     def test_rw(self, backend, elems, x):
-        kernel_rw = """
-void kernel_rw(unsigned int* x) { (*x) = (*x) + 1; }
-"""
+        """Increment each value of a Dat by one with op2.RW."""
+        kernel_rw = """void kernel_rw(unsigned int* x) { (*x) = (*x) + 1; }"""
         op2.par_loop(op2.Kernel(kernel_rw, "kernel_rw"),
                      elems, x(op2.IdentityMap, op2.RW))
         assert sum(x.data) == nelems * (nelems + 1) / 2
 
     def test_global_inc(self, backend, elems, x, g):
-        kernel_global_inc = """
-void kernel_global_inc(unsigned int* x, unsigned int* inc) { (*x) = (*x) + 1; (*inc) += (*x); }
-"""
+        """Increment each value of a Dat by one and a Global at the same time."""
+        kernel_global_inc = """void kernel_global_inc(unsigned int* x, unsigned int* inc) {
+          (*x) = (*x) + 1; (*inc) += (*x);
+        }"""
         op2.par_loop(op2.Kernel(kernel_global_inc, "kernel_global_inc"),
                      elems, x(op2.IdentityMap, op2.RW), g(op2.INC))
         assert g.data[0] == nelems * (nelems + 1) / 2
 
     def test_global_inc_init_not_zero(self, backend, elems, g):
-        k = """
-void k(unsigned int* inc) { (*inc) += 1; }
-"""
+        """Increment a global initialized with a non-zero value."""
+        k = """void k(unsigned int* inc) { (*inc) += 1; }"""
         g.data[0] = 10
         op2.par_loop(op2.Kernel(k, 'k'), elems, g(op2.INC))
         assert g.data[0] == elems.size + 10
 
     def test_global_max_dat_is_max(self, backend, elems, x, g):
-        k_code = """
-        void k(unsigned int *x, unsigned int *g) {
-        if ( *g < *x ) { *g = *x; }
+        """Verify that op2.MAX reduces to the maximum value."""
+        k_code = """void k(unsigned int *x, unsigned int *g) {
+          if ( *g < *x ) { *g = *x; }
         }"""
         k = op2.Kernel(k_code, 'k')
 
@@ -129,9 +127,10 @@ void k(unsigned int* inc) { (*inc) += 1; }
         assert g.data[0] == x.data.max()
 
     def test_global_max_g_is_max(self, backend, elems, x, g):
-        k_code = """
-        void k(unsigned int *x, unsigned int *g) {
-        if ( *g < *x ) { *g = *x; }
+        """Verify that op2.MAX does not reduce a maximum value smaller than the
+        Global's initial value."""
+        k_code = """void k(unsigned int *x, unsigned int *g) {
+          if ( *g < *x ) { *g = *x; }
         }"""
 
         k = op2.Kernel(k_code, 'k')
@@ -143,9 +142,9 @@ void k(unsigned int* inc) { (*inc) += 1; }
         assert g.data[0] == nelems * 2
 
     def test_global_min_dat_is_min(self, backend, elems, x, g):
-        k_code = """
-        void k(unsigned int *x, unsigned int *g) {
-        if ( *g > *x ) { *g = *x; }
+        """Verify that op2.MIN reduces to the minimum value."""
+        k_code = """void k(unsigned int *x, unsigned int *g) {
+          if ( *g > *x ) { *g = *x; }
         }"""
         k = op2.Kernel(k_code, 'k')
         g.data[0] = 1000
@@ -154,9 +153,10 @@ void k(unsigned int* inc) { (*inc) += 1; }
         assert g.data[0] == x.data.min()
 
     def test_global_min_g_is_min(self, backend, elems, x, g):
-        k_code = """
-        void k(unsigned int *x, unsigned int *g) {
-        if ( *g > *x ) { *g = *x; }
+        """Verify that op2.MIN does not reduce a minimum value larger than the
+        Global's initial value."""
+        k_code = """void k(unsigned int *x, unsigned int *g) {
+          if ( *g > *x ) { *g = *x; }
         }"""
 
         k = op2.Kernel(k_code, 'k')
@@ -167,30 +167,37 @@ void k(unsigned int* inc) { (*inc) += 1; }
         assert g.data[0] == 10
 
     def test_global_read(self, backend, elems, x, h):
+        """Increment each value of a Dat by the value of a Global."""
         kernel_global_read = """
-void kernel_global_read(unsigned int* x, unsigned int* h) { (*x) += (*h); }
-"""
+        void kernel_global_read(unsigned int* x, unsigned int* h) {
+          (*x) += (*h);
+        }"""
         op2.par_loop(op2.Kernel(kernel_global_read, "kernel_global_read"),
                      elems, x(op2.IdentityMap, op2.RW), h(op2.READ))
         assert sum(x.data) == nelems * (nelems + 1) / 2
 
     def test_2d_dat(self, backend, elems, y):
-        kernel_2d_wo = """
-void kernel_2d_wo(unsigned int* x) { x[0] = 42; x[1] = 43; }
-"""
+        """Set both components of a vector-valued Dat to a scalar value."""
+        kernel_2d_wo = """void kernel_2d_wo(unsigned int* x) {
+          x[0] = 42; x[1] = 43;
+        }"""
         op2.par_loop(op2.Kernel(kernel_2d_wo, "kernel_2d_wo"),
                      elems, y(op2.IdentityMap, op2.WRITE))
         assert all(map(lambda x: all(x == [42, 43]), y.data))
 
     def test_2d_dat_soa(self, backend, elems, soa):
-        kernel_soa = """
-void kernel_soa(unsigned int * x) { OP2_STRIDE(x, 0) = 42; OP2_STRIDE(x, 1) = 43; }
-"""
+        """Set both components of a vector-valued Dat in SoA order to a scalar
+        value."""
+        kernel_soa = """void kernel_soa(unsigned int * x) {
+          OP2_STRIDE(x, 0) = 42; OP2_STRIDE(x, 1) = 43;
+        }"""
         op2.par_loop(op2.Kernel(kernel_soa, "kernel_soa"),
                      elems, soa(op2.IdentityMap, op2.WRITE))
         assert all(soa.data[:, 0] == 42) and all(soa.data[:, 1] == 43)
 
     def test_soa_should_stay_c_contigous(self, backend, elems, soa):
+        """Verify that a Dat in SoA order remains C contiguous after being
+        written to in a par_loop."""
         k = "void dummy(unsigned int *x) {}"
         assert soa.data.flags['C_CONTIGUOUS']
         op2.par_loop(op2.Kernel(k, "dummy"), elems,
@@ -198,6 +205,7 @@ void kernel_soa(unsigned int * x) { OP2_STRIDE(x, 0) = 42; OP2_STRIDE(x, 1) = 43
         assert soa.data.flags['C_CONTIGUOUS']
 
     def test_parloop_should_set_ro_flag(self, backend, elems, x):
+        """Assert that a par_loop locks each Dat argument for writing."""
         kernel = """void k(unsigned int *x) { *x = 1; }"""
         x_data = x.data
         op2.par_loop(op2.Kernel(kernel, 'k'),
@@ -205,7 +213,8 @@ void kernel_soa(unsigned int * x) { OP2_STRIDE(x, 0) = 42; OP2_STRIDE(x, 1) = 43
         with pytest.raises((RuntimeError, ValueError)):
             x_data[0] = 1
 
-    def test_host_write_works(self, backend, elems, x, g):
+    def test_host_write(self, backend, elems, x, g):
+        """Increment a global by the values of a Dat."""
         kernel = """void k(unsigned int *x, unsigned int *g) { *g += *x; }"""
         x.data[:] = 1
         g.data[:] = 0
@@ -219,13 +228,15 @@ void kernel_soa(unsigned int * x) { OP2_STRIDE(x, 0) = 42; OP2_STRIDE(x, 1) = 43
                      x(op2.IdentityMap, op2.READ), g(op2.INC))
         assert g.data[0] == 2 * nelems
 
-    def test_zero_1d_dat_works(self, backend, x):
+    def test_zero_1d_dat(self, backend, x):
+        """Zero a Dat."""
         x.data[:] = 10
         assert (x.data == 10).all()
         x.zero()
         assert (x.data == 0).all()
 
-    def test_zero_2d_dat_works(self, backend, y):
+    def test_zero_2d_dat(self, backend, y):
+        """Zero a vector-valued Dat."""
         y.data[:] = 10
         assert (y.data == 10).all()
         y.zero()
