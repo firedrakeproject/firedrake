ce3d4a0f1caed6f4262dd72c78f7edaf5772df65
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-09-01 19:19:12 +0100

    Add a brief description to the pyop2 package

--- pyop2/__init__.py
+++ pyop2/__init__.py
@@ -1,3 +1,10 @@
+"""
+PyOP2 is a library for parallel computations on unstructured meshes and
+delivers performance-portability across a range of platforms:
+
+* multi-core CPU (sequential, OpenMP, OpenCL and MPI)
+* GPU (CUDA and OpenCL)
+"""
 
 from op2 import *
 from version import __version__, __version_info__  # noqa: we just want to expose these
