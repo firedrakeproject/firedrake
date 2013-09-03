7ef335db02e015387e8834d2f7ecef2da375b5c1
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-09-03 14:04:12 +0100

    Implement adv_diff regression test in pytest

--- test/regression/test_regression.py
+++ test/regression/test_regression.py
@@ -1,3 +1,4 @@
+import numpy as np
 from os.path import join, dirname, abspath, exists
 from os import mkdir
 import pytest
@@ -9,3 +10,21 @@ def meshdir():
     if not exists(d):
         mkdir(d)
     return lambda m: join(d, m)
+
+
+@pytest.fixture
+def meshes(meshdir):
+    from demo.meshes.generate_mesh import generate_meshfile
+    m = [(meshdir('a'), 20), (meshdir('b'), 40), (meshdir('c'), 80), (meshdir('d'), 160)]
+    for name, layers in m:
+        if not all(exists(name + ext) for ext in ['.edge', '.ele', '.node']):
+            generate_meshfile(name, layers)
+    return m
+
+
+def test_adv_diff(backend, meshes):
+    from demo.adv_diff import main, parser
+    res = np.array([np.sqrt(main(vars(parser.parse_args(['-m', name, '-r']))))
+                    for name, _ in meshes])
+    convergence = np.log2(res[:len(meshes) - 1] / res[1:])
+    assert all(convergence > [1.5, 1.85, 1.95])
