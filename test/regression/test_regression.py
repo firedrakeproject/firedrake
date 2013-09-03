7dd7039ec54cac8aef2895f863aaaad3b2c8166b
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-09-03 14:53:55 +0100

    No more need to create meshes dir for regression tests

--- test/regression/test_regression.py
+++ test/regression/test_regression.py
@@ -1,15 +1,11 @@
 import numpy as np
 from os.path import join, dirname, abspath, exists
-from os import mkdir
 import pytest
 
 
 @pytest.fixture(scope='session')
 def meshdir():
-    d = join(dirname(abspath(__file__)), 'meshes')
-    if not exists(d):
-        mkdir(d)
-    return lambda m: join(d, m)
+    return lambda m='': join(join(dirname(abspath(__file__)), 'meshes'), m)
 
 
 @pytest.fixture
