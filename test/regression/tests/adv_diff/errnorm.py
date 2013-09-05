720c985bed6bd50460f9feac245eafbcc3cbfdc1
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-09-05 10:42:35 +0100

    Remove old regression test harness

--- test/regression/tests/adv_diff/errnorm.py
+++
@@ -1,6 +0,0 @@
-from math import sqrt
-
-
-def errnorm(filename):
-    with open(filename, "r") as f:
-        return sqrt(float(f.read()))
--- test/regression/tests/adv_diff_mpi/errnorm.py
+++
@@ -1,7 +0,0 @@
-from math import log, sqrt
-
-
-def convergence(filename1, filename2):
-    with open(filename1) as f1:
-        with open(filename2) as f2:
-            return log(sqrt(float(f1.read())) / sqrt(float(f2.read())), 2)
