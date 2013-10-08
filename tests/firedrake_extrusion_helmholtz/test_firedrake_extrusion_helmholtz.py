a0ece57e8f79b639845cabf6f3635a97d75af266
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2013-10-08 12:45:45 +0100

    Helmholtz extruded test.

--- tests/firedrake_extrusion_helmholtz/test_firedrake_extrusion_helmholtz.py
+++ tests/firedrake_extrusion_helmholtz/test_firedrake_extrusion_helmholtz.py
@@ -15,46 +15,67 @@ and the analytical solution
 from firedrake import *
 import sys
 
-power = int(sys.argv[1])
-# Create mesh and define function space
-m = UnitSquareMesh(2 ** power, 2 ** power)
-layers = 2 ** int(sys.argv[1]) + 1
 
-# Populate the coordinates of the extruded mesh by providing the
-# coordinates as a field.
-extrusion_kernel = """
-void extrusion_kernel(double *xtr[], double *x[], int* j[])
-{
-    //Only the Z-coord is increased, the others stay the same
-    xtr[0][0] = x[0][0];
-    xtr[0][1] = x[0][1];
-    xtr[0][2] = %(height)s*j[0][0];
-}""" % {'height': str(1.0 / 2 ** int(sys.argv[1]))}
+def helmholtz(test_mode, pwr=None):
+    if pow is None:
+        power = int(sys.argv[1])
+    else:
+        power = pwr
+    # Create mesh and define function space
+    m = UnitSquareMesh(2 ** power, 2 ** power)
+    layers = 2 ** power + 1
 
-mesh = ExtrudedMesh(m, layers, extrusion_kernel)
-V = FunctionSpace(mesh, "Lagrange", 1, vfamily="Lagrange", vdegree=1)
+    # Populate the coordinates of the extruded mesh by providing the
+    # coordinates as a field.
+    extrusion_kernel = """
+    void extrusion_kernel(double *xtr[], double *x[], int* j[])
+    {
+        //Only the Z-coord is increased, the others stay the same
+        xtr[0][0] = x[0][0];
+        xtr[0][1] = x[0][1];
+        xtr[0][2] = %(height)s*j[0][0];
+    }""" % {'height': str(1.0 / 2 ** power)}
 
-# Define variational problem
-u = TrialFunction(V)
-v = TestFunction(V)
-f = Function(V)
-f.interpolate(
-    Expression("(1+12*pi*pi)*cos(x[0]*pi*2)*cos(x[1]*pi*2)*cos(x[2]*pi*2)"))
-a = (dot(grad(v), grad(u)) + v * u) * dx
-L = f * v * dx
+    mesh = ExtrudedMesh(m, layers, extrusion_kernel)
 
-# Compute solution
-A = assemble(a)
-b = assemble(L)
-x = Function(V)
-solve(a == L, x)
+    V = FunctionSpace(mesh, "Lagrange", 1, vfamily="Lagrange", vdegree=1)
 
-# Analytical solution
-f.interpolate(Expression("cos(x[0]*pi*2)*cos(x[1]*pi*2)*cos(x[2]*pi*2)"))
+    # Define variational problem
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    f = Function(V)
+    f.interpolate(
+        Expression("(1+12*pi*pi)*cos(x[0]*pi*2)*cos(x[1]*pi*2)*cos(x[2]*pi*2)"))
+    a = (dot(grad(v), grad(u)) + v * u) * dx
+    L = f * v * dx
 
-print sqrt(assemble((x - f) * (x - f) * dx))
+    # Compute solution
+    assemble(a)
+    assemble(L)
+    x = Function(V)
+    solve(a == L, x)
 
-# Save solution in VTK format
-#file = File("helmholtz.pvd")
-#file << x
-#file << f
+    # Analytical solution
+    f.interpolate(Expression("cos(x[0]*pi*2)*cos(x[1]*pi*2)*cos(x[2]*pi*2)"))
+
+    if not test_mode:
+        print sqrt(assemble((x - f) * (x - f) * dx))
+        # Save solution in VTK format
+        file = File("helmholtz.pvd")
+        file << x
+        file << f
+    else:
+        return sqrt(assemble((x - f) * (x - f) * dx))
+
+
+def run_test(test_mode=False):
+    result = []
+    for i in range(1, 5):
+        result.append(helmholtz(test_mode, pwr=i))
+    return result
+
+if __name__ == "__main__":
+
+    result = run_test()
+    for i, res in enumerate(result):
+        print "Helmholtz convergence values %r: %r" % (i + 1, res)
