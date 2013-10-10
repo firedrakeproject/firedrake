df61abd7509b06139b5254243dd5471effdfe842
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-10-10 10:42:08 +0100

    Convert identity test to pytest

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -1,9 +1,9 @@
 import firedrake
 import numpy as np
-mesh = firedrake.UnitCubeMesh(1, 1, 1)
 
 
 def identity(family, degree):
+    mesh = firedrake.UnitCubeMesh(1, 1, 1)
     fs = firedrake.FunctionSpace(mesh, family, degree)
 
     f = firedrake.Function(fs)
@@ -23,18 +23,8 @@ def identity(family, degree):
     return np.max(np.abs(out.dat.data - f.dat.data))
 
 
-def run_test():
+def test_firedrake_identity():
     family = "Lagrange"
     degree = range(1, 5)
-
-    error = []
-    for d in degree:
-        error.append(identity(family, d))
-    return error
-
-
-if __name__ == "__main__":
-
-    error = run_test()
-    for i, err in enumerate(error):
-        print "Inf norm error in identity for Lagrange %r: %r" % (i + 1, err)
+    error = np.array([identity(family, d) for d in degree])
+    assert (error < np.array([1.0e-13, 1.0e-6, 1.0e-6, 1.0e-5])).all()
