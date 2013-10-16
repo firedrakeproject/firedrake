c1df97a1c77be7e62ff4a045a0c693929beb8cbd
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2013-10-16 16:53:34 +0100

    Add test solving identity problem on VectorFunctionSpace

--- tests/test_identity.py
+++ tests/test_identity.py
@@ -25,14 +25,38 @@ def identity(family, degree):
     return np.max(np.abs(out.dat.data - f.dat.data))
 
 
+def vector_identity(family, degree):
+    mesh = firedrake.UnitSquareMesh(2, 2)
+    fs = firedrake.VectorFunctionSpace(mesh, family, degree)
+    f = firedrake.Function(fs)
+    out = firedrake.Function(fs)
+    u = firedrake.TrialFunction(fs)
+    v = firedrake.TestFunction(fs)
+
+    f.interpolate(firedrake.Expression(("x[0]", "x[1]")))
+    firedrake.solve(firedrake.inner(u, v)*firedrake.dx == firedrake.inner(f, v)*firedrake.dx, out)
+
+    return np.max(np.abs(out.dat.data - f.dat.data))
+
+
 def run_test():
     family = "Lagrange"
     degree = range(1, 5)
     return np.array([identity(family, d) for d in degree])
 
 
+def run_vector_test():
+    family = "Lagrange"
+    degree = range(1, 5)
+    return np.array([vector_identity(family, d) for d in degree])
+
+
 def test_firedrake_identity():
     assert (run_test() < np.array([1.0e-13, 1.0e-6, 1.0e-6, 1.0e-5])).all()
+
+
+def test_vector_identity():
+    assert (run_vector_test() < 1e-6).all()
 
 
 @pytest.mark.parallel
