fd5e19b88bfb8554336f4ef69de4308df66c1026
David A Ham <david.ham@imperial.ac.uk>
2013-10-19 13:09:02 +0100

    Initial definition of the DirichletBC class.

---
+++ python/firedrake/bcs.py
@@ -0,0 +1,25 @@
+# A module implementing strong (Dirichlet) boundary conditions.
+
+
+class DirichletBC(object):
+
+    def __init__(self, V, g, sub_domain):
+        '''Implementation of a strong Dirichlet boundary condition.
+
+        :arg V: the :class:`FunctionSpace` on which the boundary condition
+            should be applied.
+        :arg g: the :class:`GenericFunction` defining the boundary condition
+            values.
+        :arg sub_domain: the integer id of the boundary region over which the
+            boundary condition should be applied.
+        '''
+
+        self._function_space = V
+        self.function_arg = g
+        self.sub_domain = sub_domain
+
+    def function_space(self):
+        '''The :class:`FunctionSpace` on which this boundary condition should
+        be applied.'''
+
+        return self._function_space
