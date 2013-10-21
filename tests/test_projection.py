b069d7f4ebae86defb962096aefcfd7abae59440
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2013-10-21 09:46:12 +0100

    Add more tests of project functionality

--- tests/test_projection.py
+++ tests/test_projection.py
@@ -3,6 +3,34 @@ import pytest
 from firedrake import *
 
 
+def run_vector_valued_test(x, degree=1, family='RT'):
+    m = UnitSquareMesh(2 ** x, 2 ** x)
+    V = FunctionSpace(m, family, degree)
+    expr = ['cos(x[0]*pi*2)*sin(x[1]*pi*2)']*2
+    e = Expression(expr)
+    exact = Function(VectorFunctionSpace(m, 'CG', 5))
+    exact.interpolate(e)
+
+    # Solve to machine precision.
+    ret = project(e, V, solver_parameters={'ksp_type': 'preonly', 'pc_type': 'lu'})
+
+    return sqrt(assemble(inner((ret - exact), (ret - exact)) * dx))
+
+
+def run_vector_test(x, degree=1, family='CG'):
+    m = UnitSquareMesh(2 ** x, 2 ** x)
+    V = VectorFunctionSpace(m, family, degree)
+    expr = ['cos(x[0]*pi*2)*sin(x[1]*pi*2)']*2
+    e = Expression(expr)
+    exact = Function(VectorFunctionSpace(m, 'CG', 5))
+    exact.interpolate(e)
+
+    # Solve to machine precision.
+    ret = project(e, V, solver_parameters={'ksp_type': 'preonly', 'pc_type': 'lu'})
+
+    return sqrt(assemble(inner((ret - exact), (ret - exact)) * dx))
+
+
 def run_test(x, degree=1, family='CG'):
     m = UnitSquareMesh(2 ** x, 2 ** x)
     V = FunctionSpace(m, family, degree)
@@ -27,6 +55,85 @@ def test_convergence(degree, family, expected_convergence):
     l2_diff = np.array([run_test(x, degree, family) for x in range(2, 7)])
     conv = np.log2(l2_diff[:-1] / l2_diff[1:])
     assert (conv > expected_convergence).all()
+
+
+@pytest.mark.parametrize(('degree', 'family', 'expected_convergence'), [
+    (1, 'CG', 1.8),
+    (2, 'CG', 2.6),
+    (3, 'CG', 3.8),
+    (0, 'DG', 0.8),
+    (1, 'DG', 1.8),
+    (2, 'DG', 2.8)])
+def test_vector_convergence(degree, family, expected_convergence):
+    l2_diff = np.array([run_vector_test(x, degree, family) for x in range(2, 7)])
+    conv = np.log2(l2_diff[:-1] / l2_diff[1:])
+    assert (conv > expected_convergence).all()
+
+
+@pytest.mark.parametrize(('degree', 'family', 'expected_convergence'), [
+    (1, 'RT', 0.8),
+    (2, 'RT', 1.8),
+    (3, 'RT', 2.8),
+    (1, 'BDM', 1.8),
+    (2, 'BDM', 2.8),
+    (3, 'BDM', 3.8)])
+def test_vector_valued_convergence(degree, family, expected_convergence):
+    l2_diff = np.array([run_vector_valued_test(x, degree, family)
+                        for x in range(2, 7)])
+    conv = np.log2(l2_diff[:-1] / l2_diff[1:])
+    assert (conv > expected_convergence).all()
+
+
+def test_project_mismatched_rank():
+    m = UnitSquareMesh(2, 2)
+    V = FunctionSpace(m, 'CG', 1)
+    U = FunctionSpace(m, 'RT', 1)
+    v = Function(V)
+    u = Function(U)
+    ev = Expression('')
+    eu = Expression(('', ''))
+    with pytest.raises(RuntimeError):
+        project(v, U)
+    with pytest.raises(RuntimeError):
+        project(u, V)
+    with pytest.raises(RuntimeError):
+        project(ev, U)
+    with pytest.raises(RuntimeError):
+        project(eu, V)
+
+
+def test_project_mismatched_mesh():
+    m2 = UnitSquareMesh(2, 2)
+    m3 = UnitCubeMesh(2, 2, 2)
+
+    U = FunctionSpace(m2, 'CG', 1)
+    V = FunctionSpace(m3, 'CG', 1)
+
+    u = Function(U)
+    v = Function(V)
+
+    with pytest.raises(RuntimeError):
+        project(u, V)
+
+    with pytest.raises(RuntimeError):
+        project(v, U)
+
+
+def test_project_mismatched_shape():
+    m = UnitSquareMesh(2, 2)
+
+    U = VectorFunctionSpace(m, 'CG', 1, dim=3)
+    V = VectorFunctionSpace(m, 'CG', 1, dim=2)
+
+    u = Function(U)
+    v = Function(V)
+
+    with pytest.raises(RuntimeError):
+        project(u, V)
+
+    with pytest.raises(RuntimeError):
+        project(v, U)
+
 
 if __name__ == '__main__':
     import os
