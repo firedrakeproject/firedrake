197b266dc7ac46d5c357c4a11f934aeeb73e536a
David A Ham <david.ham@imperial.ac.uk>
2013-10-21 17:04:51 +0100

    Also test strong BCs via the "linear" solver route.

--- tests/test_poisson_strong_bcs.py
+++ tests/test_poisson_strong_bcs.py
@@ -40,12 +40,45 @@ def run_test(x, degree, parameters={}):
     return sqrt(assemble(dot(u - f, u - f) * dx))
 
 
+def run_test_linear(x, degree, parameters={}):
+    # Create mesh and define function space
+    mesh = UnitSquareMesh(2 ** x, 2 ** x)
+    V = FunctionSpace(mesh, "CG", degree)
+
+    # Define variational problem
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    a = dot(grad(v), grad(u)) * dx
+    L = v*0*dx
+
+    bcs = [DirichletBC(V, 0, 1),
+           DirichletBC(V, 42, 2)]
+
+    # Compute solution
+    u = Function(V)
+    solve(a == L, u, solver_parameters=parameters, bcs=bcs)
+
+    f = Function(V)
+    f.interpolate(Expression("42*x[1]"))
+
+    return sqrt(assemble(dot(u - f, u - f) * dx))
+
+
 @pytest.mark.parametrize(['params', 'degree'],
                          [(p, d)
                           for p in [{}, {'snes_type': 'ksponly', 'ksp_type': 'preonly', 'pc_type': 'lu'}]
                           for d in (1, 2)])
 def test_poisson_analytic(params, degree):
     assert (run_test(2, degree, parameters=params) < 1.e-9)
+
+
+@pytest.mark.parametrize(['params', 'degree'],
+                         [(p, d)
+                          for p in [{}, {'snes_type': 'ksponly', 'ksp_type': 'preonly', 'pc_type': 'lu'}]
+                          for d in (1, 2)])
+def test_poisson_analytic_linear(params, degree):
+    assert (run_test_linear(2, degree, parameters=params) < 1.e-9)
+
 
 if __name__ == '__main__':
     import os
