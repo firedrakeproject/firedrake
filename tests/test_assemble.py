5852b45da2f756d0c944924b51397c4c20bf27c6
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-10-25 12:37:40 +0100

    Test assembly of expressions on MixedFunctionSpaces

--- tests/test_assemble.py
+++ tests/test_assemble.py
@@ -1,44 +1,74 @@
 import pytest
-import firedrake as fd
+from firedrake import *
 
 
-@pytest.fixture
-def fs():
-    mesh = fd.UnitSquareMesh(5, 5)
-    return fd.FunctionSpace(mesh, "Lagrange", 1)
+@pytest.fixture(scope='module')
+def mesh():
+    return UnitSquareMesh(5, 5)
+
+
+@pytest.fixture(scope='module')
+def cg1(mesh):
+    return FunctionSpace(mesh, "Lagrange", 1)
+
+
+@pytest.fixture(scope='module')
+def cg1cg1(mesh):
+    CG1 = FunctionSpace(mesh, "CG", 1)
+    return CG1 * CG1
+
+
+@pytest.fixture(scope='module')
+def cg1dg0(mesh):
+    CG1 = FunctionSpace(mesh, "CG", 1)
+    DG0 = FunctionSpace(mesh, "DG", 0)
+    return CG1 * DG0
+
+
+@pytest.fixture(scope='module')
+def cg2dg1(mesh):
+    CG2 = FunctionSpace(mesh, "CG", 2)
+    DG1 = FunctionSpace(mesh, "DG", 1)
+    return CG2 * DG1
+
+
+@pytest.fixture(scope='module', params=['cg1', 'cg1cg1', 'cg1dg0', 'cg2dg1'])
+def fs(request, cg1, cg1cg1, cg1dg0, cg2dg1):
+    return {'cg1': cg1, 'cg1cg1': cg1cg1, 'cg1dg0': cg1dg0, 'cg2dg1': cg2dg1}[request.param]
 
 
 @pytest.fixture
 def f(fs):
-    f = fd.Function(fs, name="f")
-    f.interpolate(fd.Expression("x[0]"))
+    f = Function(fs, name="f")
+    f.interpolate(Expression("x[0]"))
     return f
 
 
 @pytest.fixture
 def one(fs):
-    one = fd.Function(fs, name="one")
-    one.interpolate(fd.Expression("1"))
+    one = Function(fs, name="one")
+    one.interpolate(Expression("1"))
     return one
 
 
 @pytest.fixture
 def M(fs):
-    uhat = fd.TrialFunction(fs)
-    v = fd.TestFunction(fs)
-    return uhat * v * fd.dx
+    uhat = TrialFunction(fs)
+    v = TestFunction(fs)
+    return inner(uhat, v) * dx
 
 
 def test_one_form(M, f):
-    one_form = fd.assemble(fd.action(M, f))
-    assert isinstance(one_form, fd.Function)
-    assert abs(sum(one_form.dat.data) - 0.5) < 1.0e-14
+    one_form = assemble(action(M, f))
+    assert isinstance(one_form, Function)
+    for d in one_form.dat:
+        assert abs(sum(d.data) - 0.5) < 1.0e-12
 
 
 def test_zero_form(M, f, one):
-    zero_form = fd.assemble(fd.action(fd.action(M, f), one))
+    zero_form = assemble(action(action(M, f), one))
     assert isinstance(zero_form, float)
-    assert abs(zero_form - 0.5) < 1.0e-14
+    assert abs(zero_form - 0.5 * np.prod(f.shape())) < 1.0e-12
 
 if __name__ == '__main__':
     import os
