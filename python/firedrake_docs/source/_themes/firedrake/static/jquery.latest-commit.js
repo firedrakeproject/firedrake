bf6bbcd12e09e60cf6481b556afc24b60e097501
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-10-29 12:35:43 +0000

    Query GitHub API for latest commit instead of showing RSS feed

---
+++ python/firedrake_docs/source/_themes/firedrake/static/jquery.latest-commit.js
@@ -0,0 +1,39 @@
+// Based on http://github.com/skulbuny/jquery.latest-commit.js by Sean Clayton
+jQuery(document).ready(function($){
+  $('.latest-commit').each(function(){ // Attach to any div/section/whatever with this class
+    var $container = $(this), $commit,
+      repo = $container.data('github'),
+      username = repo.split('/')[0],
+      repoName = repo.split('/')[1],
+      userUrl = "http://github.com/" + username, // Gets your user url
+      repoUrl = "http://github.com/" + username + '/' + repoName; // Gets your repo url
+      $commit = $(
+        '<div>' + // Needs to be wrapped.
+        // ADD DESIRED CLASSES TO HTML TAGS BELOW!
+        '<img class="commit-author-img" src="#" />' + // Commit author image
+        '<div>' +
+        '<p><a class="commit-link" href="#"></a></p>' + // First line of commit message
+        '<a class="commit-author" href="#" target="_blank"></a>' + // Link to commit author
+        ' authored at <span class="commit-date"></span>' + // Outputs the commit date
+        '</div>' +
+        '<pre class="commit"></pre>' + // Displays commit message
+        '</div>'
+      );
+    $commit.appendTo($container);
+    $.ajax({
+      url: 'https://api.github.com/repos/' + repo + '/commits/master', // Sets url to your commit index
+      dataType: 'jsonp',
+      success: function(results) {
+        var repo = results.data, date, pushed_at = 'unknown';
+        var commitUrl = repo.html_url; // Grabs URL of the commit
+        var fullsha = repo.sha; // Grabs SHA of Commit
+        var sha = fullsha.substring(0,10); // Truncates SHA to 10 characters, just like GitHub does
+        $commit.find('.commit-author-img').attr('src', repo.author.avatar_url); // Add commit author avatar image
+        $commit.find('.commit-link').attr('href',commitUrl).text(repo.commit.message.split("\n")[0]); // Adds link to commit and commit SHA
+        $commit.find('.commit-author').attr('href', repo.author.html_url).text(repo.commit.author.name); // Outputs commit author name
+        $commit.find('.commit-date').text(new Date(repo.commit.author.date).toLocaleString()); // Outputs commit date
+        $commit.find('.commit').text(repo.commit.message); // Outputs commit message
+      }
+    });
+  });
+});
--- python/firedrake_docs/source/index.rst
+++ python/firedrake_docs/source/index.rst
@@ -1,45 +1,10 @@
-.. Firedrake documentation master file, created by
-   sphinx-quickstart on Sat Jun 29 19:06:45 2013.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 .. sidebar:: Latest Firedrake activity on Github
 
   .. raw:: html
 
-    <script type="text/javascript">
-    <!--
-    rssmikle_url="https://github.com/firedrakeproject/firedrake/commits/master.atom";
-    rssmikle_frame_width="300";
-    rssmikle_frame_height="300";
-    rssmikle_target="_blank";
-    rssmikle_font="Arial, Helvetica, sans-serif";
-    rssmikle_font_size="12";
-    rssmikle_border="on";
-    rssmikle_css_url="http://firedrakeproject.org/fenics.css";
-    <!--rssmikle_css_url="http://view-source:http://www.firedrakeproject.org/_static/fenics.css";-->
-    rssmikle_title="on";
-    rssmikle_title_bgcolor="#0066FF";
-    rssmikle_title_color="#FFFFFF";
-    rssmikle_title_bgimage="http://";
-    rssmikle_item_bgcolor="#FFFFFF";
-    rssmikle_item_bgimage="http://";
-    rssmikle_item_title_length="555";
-    rssmikle_item_title_color="#666666";
-    rssmikle_item_border_bottom="on";
-    rssmikle_item_description="off";
-    rssmikle_item_description_length="150";
-    rssmikle_item_description_color="#666666";
-    rssmikle_item_date="gl1";
-    rssmikle_item_description_tag="off";
-    rssmikle_item_podcast="off";
-    //-->
-    </script>
-    <script type="text/javascript" src="http://widget.feed.mikle.com/js/rssmikle.js"></script>
-    <div style="font-size:10px; text-align:right;">
-    <a href="http://feed.mikle.com/" target="_blank" style="color:#CCCCCC;">FeedWind</a>
-    <!--Please display the above link in your web page according to Terms of Service.-->
-    </div>
-    <!-- end feedwind code -->
+    <div class="latest-commit" data-github="firedrakeproject/firedrake"></div>
+    <script type="text/javascript" src="_static/jquery.latest-commit.js"></script>
+
 
 Firedrake is an automated system for the portable solution of partial
 differential equations using the finite element method (FEM). Firedrake
