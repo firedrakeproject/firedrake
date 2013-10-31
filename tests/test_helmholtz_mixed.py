308998e845bedd743b9ca4e55309ea8b31b1cfd8
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-10-31 10:57:01 +0000

    Parametrize mixed Helmholtz: RT1+DG0, BDM1+DG0, BDFM2+DG1

--- tests/test_helmholtz_mixed.py
+++ tests/test_helmholtz_mixed.py
@@ -1,18 +1,19 @@
+import pytest
 from firedrake import *
 
 
-def helmholtz_mixed(x):
+def helmholtz_mixed(x, V1, V2):
     # Create mesh and define function space
     mesh = UnitSquareMesh(2**x, 2**x)
-    RT1 = FunctionSpace(mesh, "RT", 1)
-    P0 = FunctionSpace(mesh, "DG", 0)
-    W = RT1 * P0
+    V1 = FunctionSpace(mesh, *V1)
+    V2 = FunctionSpace(mesh, *V2)
+    W = V1 * V2
 
     # Define variational problem
     lmbda = 1
     u, p = TrialFunctions(W)
     v, q = TestFunctions(W)
-    f = Function(P0)
+    f = Function(V2)
 
     f.interpolate(Expression("(1+8*pi*pi)*sin(x[0]*pi*2)*sin(x[1]*pi*2)"))
     a = (p*q - q*div(u) + lmbda*inner(v, u) + div(v)*p) * dx
@@ -27,13 +28,17 @@ def helmholtz_mixed(x):
     return sqrt(assemble(dot(x[2] - f, x[2] - f) * dx))
 
 
-def test_firedrake_helmholtz():
+@pytest.mark.parametrize(('V1', 'V2', 'threshold'),
+                         [(('RT', 1), ('DG', 0), 1.9),
+                          (('BDM', 1), ('DG', 0), 1.9),
+                          (('BDFM', 2), ('DG', 1), 1.9)])
+def test_firedrake_helmholtz(V1, V2, threshold):
     import numpy as np
-    diff = np.array([helmholtz_mixed(i) for i in range(3, 7)])
+    diff = np.array([helmholtz_mixed(i, V1, V2) for i in range(3, 7)])
     print "l2 error norms:", diff
     conv = np.log2(diff[:-1] / diff[1:])
     print "convergence order:", conv
-    assert (np.array(conv) > 1.9).all()
+    assert (np.array(conv) > threshold).all()
 
 if __name__ == '__main__':
     import os
