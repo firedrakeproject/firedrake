e71b574ba2da0c273b7e57e11339774317ba4669
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2013-11-06 16:21:34 +0000

    Add more tests of preassembled Matrix boundary conditions

--- tests/test_poisson_strong_bcs.py
+++ tests/test_poisson_strong_bcs.py
@@ -15,6 +15,7 @@ This has the analytical solution
   u(x, y) = 42*x[1]
 """
 import pytest
+import numpy as np
 from firedrake import *
 
 
@@ -79,13 +80,42 @@ def run_test_preassembled(x, degree, parameters={}):
 
     u = Function(V)
 
-    A = assemble(a, bcs=bcs)
+    A = assemble(a)
+    b = assemble(L)
+    for bc in bcs:
+        bc.apply(A)
+        bc.apply(b)
+    solve(A, u, b, solver_parameters=parameters)
+
+    expected = Function(V)
+    expected.interpolate(Expression("42*x[1]"))
+
+    method_A = sqrt(assemble(dot(u - expected, u - expected) * dx))
+
+    A = assemble(a)
     b = assemble(L)
     solve(A, u, b, bcs=bcs, solver_parameters=parameters)
+    method_B = sqrt(assemble(dot(u - expected, u - expected) * dx))
 
-    f.interpolate(Expression("42*x[1]"))
+    A = assemble(a, bcs=bcs)
+    b = assemble(L, bcs=bcs)
+    solve(A, u, b, solver_parameters=parameters)
+    method_C = sqrt(assemble(dot(u - expected, u - expected) * dx))
 
-    return sqrt(assemble(dot(u - f, u - f) * dx))
+    A = assemble(a, bcs=bcs)
+    b = assemble(L)
+    solve(A, u, b, solver_parameters=parameters)
+    method_D = sqrt(assemble(dot(u - expected, u - expected) * dx))
+
+    A = assemble(a)
+    b = assemble(L)
+    # Don't actually need to apply the bcs to b explicitly since it's
+    # done in the solve if A has any.
+    for bc in bcs:
+        bc.apply(A)
+    solve(A, u, b, solver_parameters=parameters)
+    method_E = sqrt(assemble(dot(u - expected, u - expected) * dx))
+    return np.asarray([method_A, method_B, method_C, method_D, method_E])
 
 
 @pytest.mark.parametrize(['params', 'degree'],
@@ -109,7 +139,7 @@ def test_poisson_analytic_linear(params, degree):
                           for p in [{}, {'snes_type': 'ksponly', 'ksp_type': 'preonly', 'pc_type': 'lu'}]
                           for d in (1, 2)])
 def test_poisson_analytic_preassembled(params, degree):
-    assert (run_test_preassembled(2, degree, parameters=params) < 5.e-6)
+    assert (run_test_preassembled(2, degree, parameters=params) < 5.e-6).all()
 
 
 if __name__ == '__main__':
