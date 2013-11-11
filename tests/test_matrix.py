de9ee362b4c1f68147abba7a297ffba2b365ede5
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2013-11-11 13:04:11 +0000

    Add more tests of matrices with boundary conditions

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -54,6 +54,54 @@ def test_solve_forces_assemble(a, V):
     assert not (A._M.values == 0.0).all()
 
 
+def test_adding_bcs(a, V):
+    bc1 = DirichletBC(V, 0, 1)
+    A = assemble(a, bcs=[bc1])
+
+    for bc in A.bcs:
+        assert bc is bc1
+
+    bc2 = DirichletBC(V, 1, 1)
+    bc2.apply(A)
+    for bc in A.bcs:
+        assert bc is bc2
+
+    bc3 = DirichletBC(V, 1, 0)
+
+    bc3.apply(A)
+    assert A.bcs == set([bc2, bc3])
+
+
+def test_assemble_with_bcs(a, V):
+    bc1 = DirichletBC(V, 0, 1)
+    A = assemble(a, bcs=[bc1])
+
+    A.assemble()
+    assert A.assembled
+    assert not A._needs_reassembly
+
+    # Same subdomain, should not need reassembly
+    bc2 = DirichletBC(V, 1, 1)
+    bc2.apply(A)
+    assert A.assembled
+    assert not A._needs_reassembly
+
+    bc3 = DirichletBC(V, 1, 0)
+    A.bcs = bc3
+    assert A.assembled
+    assert A._needs_reassembly
+    A.assemble()
+    assert A.assembled
+    assert not A._needs_reassembly
+
+    bc2.apply(A)
+    assert A.assembled
+    assert A._needs_reassembly
+    A.assemble()
+    assert A.assembled
+    assert not A._needs_reassembly
+
+
 if __name__ == '__main__':
     import os
     pytest.main(os.path.abspath(__file__))
