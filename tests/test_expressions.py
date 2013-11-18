328b0bcc5fe4066dd4592f7361746fe7b0a22347
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-11-18 16:25:01 +0000

    Refactor expression tests
    * Split fs fixture into separate fixtures for scalar and vector/mixed
      function spaces
    * Use a factory to create functions on those function spaces
    * Parametrize test cases with the expression to test for and eval it

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -1,41 +1,43 @@
 import pytest
 
 import numpy as np
-import ufl
+import ufl  # noqa: used in eval'd expressions
 
 from firedrake import *
 from common import *
 
 
-@pytest.fixture(scope='module', params=['cg1', 'vcg1',
-                                        'cg1cg1', 'cg1cg1[0]', 'cg1cg1[1]',
-                                        'cg1vcg1', 'cg1vcg1[0]', 'cg1vcg1[1]',
-                                        'cg1dg0', 'cg1dg0[0]', 'cg1dg0[1]',
-                                        'cg2dg1', 'cg2dg1[0]', 'cg2dg1[1]'])
-def fs(request, cg1, vcg1, cg1cg1, cg1vcg1, cg1dg0, cg2dg1):
+@pytest.fixture(scope='module', params=['cg1', 'cg1cg1[0]', 'cg1cg1[1]',
+                                        'cg1vcg1[0]', 'cg1dg0[0]', 'cg1dg0[1]',
+                                        'cg2dg1[0]', 'cg2dg1[1]'])
+def sfs(request, cg1, vcg1, cg1cg1, cg1vcg1, cg1dg0, cg2dg1):
     return {'cg1': cg1,
-            'vcg1': vcg1,
-            'cg1cg1': cg1cg1,
             'cg1cg1[0]': cg1cg1[0],
             'cg1cg1[1]': cg1cg1[1],
-            'cg1vcg1': cg1vcg1,
             'cg1vcg1[0]': cg1vcg1[0],
-            'cg1vcg1[1]': cg1vcg1[1],
-            'cg1dg0': cg1dg0,
             'cg1dg0[0]': cg1dg0[0],
             'cg1dg0[1]': cg1dg0[1],
-            'cg2dg1': cg2dg1,
             'cg2dg1[0]': cg2dg1[0],
             'cg2dg1[1]': cg2dg1[1]}[request.param]
 
 
-@pytest.fixture(params=['assign', 'interpolate'])
-def functions(request, fs):
+@pytest.fixture(scope='module', params=['vcg1', 'cg1cg1', 'cg1vcg1',
+                                        'cg1vcg1[1]', 'cg1dg0', 'cg2dg1'])
+def mfs(request, cg1, vcg1, cg1cg1, cg1vcg1, cg1dg0, cg2dg1):
+    return {'vcg1': vcg1,
+            'cg1cg1': cg1cg1,
+            'cg1vcg1': cg1vcg1,
+            'cg1vcg1[1]': cg1vcg1[1],
+            'cg1dg0': cg1dg0,
+            'cg2dg1': cg2dg1}[request.param]
+
+
+def func_factory(fs, method):
     f = Function(fs, name="f")
     one = Function(fs, name="one")
     two = Function(fs, name="two")
     minusthree = Function(fs, name="minusthree")
-    if request.param == 'assign':
+    if method == 'assign':
         one.assign(1)
         two.assign(2)
         minusthree.assign(-3)
@@ -48,6 +50,16 @@ def functions(request, fs):
         two.interpolate(Expression("2"))
         minusthree.interpolate(Expression("-3"))
     return f, one, two, minusthree
+
+
+@pytest.fixture(params=['assign', 'interpolate'])
+def functions(request, sfs):
+    return func_factory(sfs, method=request.param)
+
+
+@pytest.fixture(params=['assign', 'interpolate'])
+def mfunctions(request, mfs):
+    return func_factory(mfs, method=request.param)
 
 
 @pytest.fixture
@@ -96,32 +108,32 @@ def idivtest(f, expr, x):
             to_bool(d == x for d in f.dat.data))
 
 
-@pytest.fixture(params=range(1, 12))
-def alltests(request, functions):
+common_tests = [
+    'exprtest(one + two, 3)',
+    'assigntest(f, one + two, 3)',
+    'iaddtest(f, two, 5)',
+    'iaddtest(f, 2, 7)',
+    'isubtest(f, 2, 5)',
+    'imultest(f, 2, 10)',
+    'idivtest(f, 2, 5)']
+
+scalar_tests = common_tests + [
+    'exprtest(ufl.ln(one), 0)',
+    'exprtest(two ** minusthree, 0.125)',
+    'exprtest(ufl.sign(minusthree), -1)',
+    'exprtest(one + two / two ** minusthree, 17)']
+
+
+@pytest.mark.parametrize('expr', scalar_tests)
+def test_scalar_expressions(expr, functions):
     f, one, two, minusthree = functions
-    tests = {
-        1: exprtest(one + two, 3),
-        2: assigntest(f, one + two, 3),
-        3: iaddtest(f, two, 5),
-        4: iaddtest(f, 2, 7),
-        5: isubtest(f, 2, 5),
-        6: imultest(f, 2, 10),
-        7: idivtest(f, 2, 5),
-    }
-    # Not all test cases work for vector function spaces
-    if isinstance(one.function_space(), FunctionSpace):
-        tests[8] = exprtest(ufl.ln(one), 0)
-        tests[9] = exprtest(two ** minusthree, 0.125)
-        tests[10] = exprtest(ufl.sign(minusthree), -1)
-        tests[11] = exprtest(one + two / two ** minusthree, 17)
-    try:
-        return tests[request.param]
-    except KeyError:
-        pytest.skip()
+    assert eval(expr)[2]
 
 
-def test_expressions(alltests):
-    assert alltests[2]
+@pytest.mark.parametrize('expr', common_tests)
+def test_mixed_expressions(expr, mfunctions):
+    f, one, two, minusthree = mfunctions
+    assert eval(expr)[2]
 
 
 def test_vf_assign_f(sf, vf):
