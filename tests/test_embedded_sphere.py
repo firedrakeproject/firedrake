da6435e9063da7b2326c96a1cf08fc0a71530671
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2013-12-13 20:17:04 +0000

    Test assembly of volume integrals on surface of sphere
    Integral of the cell area approximates the surface of the sphere.
    Integral of an extruded annulus approximates the appropriate volume as
    long as the layer height is very much less than the radius of the sphere
    since we assume the Jacobian is independent of the position in the
    element which is not actually the case.

---
+++ tests/test_embedded_sphere.py
@@ -0,0 +1,25 @@
+# Test that integrals over the surface of a sphere do the right thing
+import pytest
+
+from firedrake import *
+
+
+def integrate_sphere_area(radius=1, refinement=2):
+    mesh = IcosahedralSphereMesh(radius=radius, refinement_level=refinement)
+    fs = FunctionSpace(mesh, "CG", 1, name="fs")
+
+    f = Function(fs)
+
+    f.assign(1)
+
+    exact = 4*pi*radius**2
+    return np.abs(assemble(f * dx) - exact) / exact
+
+
+@pytest.mark.parametrize(('radius', 'refinement', 'error'),
+                         [(1, 2, 0.02),
+                          (10, 2, 0.02),
+                          (1, 5, 0.0003),
+                          (10, 5, 0.0003)])
+def test_surface_area_sphere(radius, refinement, error):
+    assert integrate_sphere_area(radius=radius, refinement=refinement) < error
