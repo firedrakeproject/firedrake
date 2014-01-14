7b9078808f7ebce9947b48a546262d197936e9b3
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-01-14 17:46:29 +0000

    Bump version to 0.8.3

--- pyop2/version.py
+++ pyop2/version.py
@@ -1,4 +1,4 @@
-__version_info__ = (0, 8, 2)
+__version_info__ = (0, 8, 3)
 __version__ = '.'.join(map(str, __version_info__))
 __compatible_ffc_version_info__ = (0, 4, 0)
 __compatible_ffc_version__ = '.'.join(map(str, __compatible_ffc_version_info__))
