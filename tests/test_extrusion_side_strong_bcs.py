4577db37395611c6b3c9dd5b46e95fac53a2a74e
Gheorghe-Teodor Bercea <gb308@doc.ic.ac.uk>
2014-01-14 14:58:07 +0000

    Change the strong bcs test case to use the initial expression.

--- tests/test_extrusion_side_strong_bcs.py
+++ tests/test_extrusion_side_strong_bcs.py
@@ -9,17 +9,18 @@ from firedrake import *
 def run_test(x, degree, parameters={}, test_mode=False):
     # Create mesh and define function space
     m = UnitSquareMesh(2, 2)
-    layers = 11
+    layers = 3
     mesh = ExtrudedMesh(m, layers, layer_height=1.0 / (layers - 1))
 
     # Define variational problem
     V = FunctionSpace(mesh, "CG", 1)
-    bcs = [DirichletBC(V, Expression("x[0]*x[0] - x[1]*x[1]"), "bottom"),
-           DirichletBC(V, Expression("x[0]*x[0] - x[1]*x[1] - 1"), "top"),
-           DirichletBC(V, Expression("-x[1]*x[1] - x[2]*x[2]"), 1),
-           DirichletBC(V, Expression("1 - x[1]*x[1] - x[2]*x[2]"), 2),
-           DirichletBC(V, Expression("x[0]*x[0] - x[2]*x[2]"), 3),
-           DirichletBC(V, Expression("x[0]*x[0] - 1 - x[2]*x[2]"), 4)]
+    exp = Expression('x[0]*x[0] - x[1]*x[1] - x[2]*x[2]')
+    bcs = [DirichletBC(V, exp, "bottom"),
+           DirichletBC(V, exp, "top"),
+           DirichletBC(V, exp, 1),
+           DirichletBC(V, exp, 2),
+           DirichletBC(V, exp, 3),
+           DirichletBC(V, exp, 4)]
 
     v = TestFunction(V)
     u = TrialFunction(V)
@@ -33,7 +34,7 @@ def run_test(x, degree, parameters={}, test_mode=False):
     out = Function(V)
 
     exact = Function(V)
-    exact.interpolate(Expression('x[0]*x[0] - x[1]*x[1] - x[2]*x[2]'))
+    exact.interpolate(exp)
 
     solve(a == L, out, bcs=bcs)
 
@@ -45,7 +46,6 @@ def run_test(x, degree, parameters={}, test_mode=False):
         file << out
         file = File("side-bcs-expected.pvd")
         file << exact
-    print res
     return res
 
 
