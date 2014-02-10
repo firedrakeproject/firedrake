c9d9e5e2ae612e568554c420edaff66128f59fcf
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-02-10 16:33:45 +0000

    Add tests on extruded interval with DG coordinates
    Compute the area of an extruded interval with a DG coordinate
    field (both periodic and non-periodic cases).

---
+++ tests/extrusion/test_extrusion_0_dg_coords.py
@@ -0,0 +1,49 @@
+from firedrake import *
+import pytest
+import ufl
+
+
+def test_extruded_interval_area():
+    m = UnitIntervalMesh(10)
+
+    DG = VectorFunctionSpace(m, 'DG', 1)
+    new_coords = project(m._coordinate_field, DG)
+    m._coordinates = new_coords.dat.data
+    m._coordinate_fs = new_coords.function_space()
+    m._coordinate_field = new_coords
+
+    ufl.dx._domain_data = m._coordinate_field
+    V = FunctionSpace(m, 'CG', 1)
+    u = Function(V)
+    u.assign(1)
+
+    assert abs(assemble(u*dx) - 1.0) < 1e-12
+
+    e = ExtrudedMesh(m, layers=5, layer_height=0.25)
+
+    V = FunctionSpace(e, 'CG', 1)
+    u = Function(V)
+    u.assign(1)
+
+    assert abs(assemble(u*dx) - 1.0) < 1e-12
+
+
+def test_extruded_periodic_interval_area():
+    m = PeriodicUnitIntervalMesh(10)
+
+    V = FunctionSpace(m, 'CG', 1)
+    u = Function(V)
+    u.assign(1)
+    assert abs(assemble(u*dx) - 1.0) < 1e-12
+
+    e = ExtrudedMesh(m, layers=5, layer_height=0.25)
+    V = FunctionSpace(e, 'CG', 1)
+    u = Function(V)
+    u.assign(1)
+
+    assert abs(assemble(u*dx) - 1.0) < 1e-12
+
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
