ba60f37518c8cc0ef0701e01403fb52e71eb0d81
dorugeber <a.mcrae12@imperial.ac.uk>
2014-02-14 15:18:17 +0000

    Laplace and Poisson tests for facets, using Neumann BCs

---
+++ tests/extrusion/test_poisson_neumann.py
@@ -0,0 +1,85 @@
+"""This demo program solves Poisson's equation
+
+  - div grad u(x, y, z) = f
+
+in a unit cube, with Dirichlet boundary conditions on 4 sides
+and Neumann boundary conditions (one explicit, one implicit)
+on the other 2, opposite, sides.
+"""
+
+import pytest
+from firedrake import *
+from tests.common import *
+
+
+@pytest.fixture(scope='module')
+def P2():
+    mesh = extmesh(4, 4, 4)
+    return FunctionSpace(mesh, "CG", 2)
+
+
+def test_bottom(P2):
+        u = TrialFunction(P2)
+        v = TestFunction(P2)
+
+        a = dot(grad(u), grad(v))*dx
+        L = -20*v*dx + 20*v*ds_b
+        bc_expr = Expression("10*(x[2]-1)*(x[2]-1)")
+        bcs = [DirichletBC(P2, bc_expr, 1),
+               DirichletBC(P2, bc_expr, 2),
+               DirichletBC(P2, bc_expr, 3),
+               DirichletBC(P2, bc_expr, 4)]
+
+        u = Function(P2)
+        solve(a == L, u, bcs)
+
+        u_exact = Function(P2)
+        u_exact.interpolate(bc_expr)
+
+        assert max(abs(u.dat.data - u_exact.dat.data)) < 1.0e-6
+
+
+def test_top(P2):
+        u = TrialFunction(P2)
+        v = TestFunction(P2)
+
+        a = dot(grad(u), grad(v))*dx
+        L = -20*v*dx + 20*v*ds_t
+        bc_expr = Expression("10*x[2]*x[2]")
+        bcs = [DirichletBC(P2, bc_expr, 1),
+               DirichletBC(P2, bc_expr, 2),
+               DirichletBC(P2, bc_expr, 3),
+               DirichletBC(P2, bc_expr, 4)]
+
+        u = Function(P2)
+        solve(a == L, u, bcs)
+
+        u_exact = Function(P2)
+        u_exact.interpolate(bc_expr)
+
+        assert max(abs(u.dat.data - u_exact.dat.data)) < 1.0e-6
+
+
+def test_topbottom(P2):
+        u = TrialFunction(P2)
+        v = TestFunction(P2)
+
+        a = dot(grad(u), grad(v))*dx
+        L = -20*v*dx + 10*v*ds_tb
+        bc_expr = Expression("10*(x[2]-0.5)*(x[2]-0.5)")
+        bcs = [DirichletBC(P2, bc_expr, 1),
+               DirichletBC(P2, bc_expr, 2),
+               DirichletBC(P2, bc_expr, 3),
+               DirichletBC(P2, bc_expr, 4)]
+
+        u = Function(P2)
+        solve(a == L, u, bcs)
+
+        u_exact = Function(P2)
+        u_exact.interpolate(bc_expr)
+
+        assert max(abs(u.dat.data - u_exact.dat.data)) < 1.0e-6
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
