0ed15e4098e3adc4c60e892e30b46b0929be59eb
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-02-27 13:14:03 +0000

    Add tests for FunctionSpace caching behaviour

--- tests/regression/test_function_spaces.py
+++ tests/regression/test_function_spaces.py
@@ -3,12 +3,43 @@ from firedrake import *
 from tests.common import *
 
 
+@pytest.fixture(scope='module')
+def mesh2():
+    return UnitSquareMesh(1, 1)
+
+
 @pytest.fixture(scope='module', params=['cg1cg1', 'cg1vcg1', 'cg1dg0', 'cg2dg1'])
 def fs(request, cg1cg1, cg1vcg1, cg1dg0, cg2dg1):
     return {'cg1cg1': cg1cg1,
             'cg1vcg1': cg1vcg1,
             'cg1dg0': cg1dg0,
             'cg2dg1': cg2dg1}[request.param]
+
+
+def test_function_space_cached(mesh):
+    "FunctionSpaces defined on the same mesh and element are cached."
+    assert FunctionSpace(mesh, "CG", 1) is FunctionSpace(mesh, "CG", 1)
+
+
+def test_function_space_different_mesh_differ(mesh, mesh2):
+    "FunctionSpaces defined on different meshes differ."
+    assert FunctionSpace(mesh, "CG", 1) is not FunctionSpace(mesh2, "CG", 1)
+
+
+def test_function_space_different_degree_differ(mesh):
+    "FunctionSpaces defined with different degrees differ."
+    assert FunctionSpace(mesh, "CG", 1) is not FunctionSpace(mesh, "CG", 2)
+
+
+def test_function_space_different_family_differ(mesh):
+    "FunctionSpaces defined with different element families differ."
+    assert FunctionSpace(mesh, "CG", 1) is not FunctionSpace(mesh, "DG", 1)
+
+
+def test_function_space_vector_function_space_differ(mesh):
+    """A FunctionSpace and a VectorFunctionSpace defined with the same
+    family and degree differ."""
+    assert FunctionSpace(mesh, "CG", 1) is not VectorFunctionSpace(mesh, "CG", 1)
 
 
 def test_indexed_function_space_index(fs):
