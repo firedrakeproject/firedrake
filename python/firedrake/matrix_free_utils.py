3de7b81ba86866a1143d48e12942cc7bfc79eaf9
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-03-03 15:16:52 +0000

    Extract diagonal via PyOP2 AST transformation

--- python/firedrake/matrix_free_utils.py
+++ python/firedrake/matrix_free_utils.py
@@ -1,4 +1,3 @@
-from pyop2 import op2
 from pyop2.ffc_interface import compile_form
 from solving import _do_assemble
 import core_types
@@ -18,45 +17,10 @@ def _matrix_diagonal(form, tensor=None, bcs=None):
     if test.function_space() != trial.function_space():
         raise RuntimeError("""It only makes sense to extract a matrix
         diagonal if the test and trial spaces are the same""")
-    kernels = compile_form(form, "a_kernels")
-
-    new_kernels = []
+    kernels = compile_form(form, "a_kernels", {"extract_diagonal": True})
 
     integrals = fd.preprocessed_form.integrals()
     coords = integrals[0].measure().domain_data()
-
-    coeffs = fd.original_coefficients
-    # FIXME: this should really be an FFC-level change.
-    for kernel, integral in zip(kernels, integrals):
-        args = []
-        arg_names = []
-        for i, c in enumerate(coeffs):
-            args.append("%s **arg%d" % (c.dat.ctype, i))
-            arg_names.append("arg%d" % i)
-        dt = integral.measure().domain_type()
-        if dt == "exterior_facet" or dt == "interior_facet":
-            args.append("unsigned int *local_facet")
-            arg_names.append("local_facet")
-
-        if len(args) == 0:
-            args = ""
-            arg_names = ""
-        else:
-            args = ", ".join([a for a in args]) + ","
-            arg_names = ", ".join([a for a in arg_names]) + ","
-        k = op2.Kernel("""
-        %(orig_kernel)s
-        inline void diag_%(orig_kernel_name)s(double A[1], double **coords, %(args)s int k) {
-            double t[1][1];
-            t[0][0] = 0;
-            %(orig_kernel_name)s(t, coords, %(arg_names)s k, k);
-            A[0] += t[0][0];
-        }
-        """ % {'orig_kernel': kernel.code,
-               'args': args,
-               'orig_kernel_name': kernel.name,
-               'arg_names': arg_names}, name='diag_%s' % kernel.name)
-        new_kernels.append(k)
 
     if tensor is None:
         out = core_types.Function(test.function_space())
@@ -67,7 +31,7 @@ def _matrix_diagonal(form, tensor=None, bcs=None):
     _do_assemble(result=result,
                  tensor=out.dat,
                  bcs=None,
-                 kernels=new_kernels,
+                 kernels=kernels,
                  integrals=integrals,
                  fd=fd,
                  m=test.function_space().mesh(),
