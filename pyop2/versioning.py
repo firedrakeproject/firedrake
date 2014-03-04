a2a18d74ee432b42d7ac2a75c1b450923ff92ea5
David A Ham <david.ham@imperial.ac.uk>
2014-03-04 16:32:01 +0000

    Conceptual documentation of versioning.

--- pyop2/versioning.py
+++ pyop2/versioning.py
@@ -1,3 +1,63 @@
+# This file is part of PyOP2
+#
+# PyOP2 is Copyright (c) 2012, Imperial College London and
+# others. Please see the AUTHORS file in the main source directory for
+# a full list of copyright holders.  All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * The name of Imperial College London or that of other
+#       contributors may not be used to endorse or promote products
+#       derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTERS
+# ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""This module implements the infrastructure required for versioning
+of data carrying objects (chiefly :class:`~pyop2.base.Dat`). Core
+functionality provided includes object version numbers and copy on
+write duplicates.
+
+Each data carrying object is equipped with a version number. This is
+incremented every time the value of the object is changed, whether
+this is by a :func:`~pyop2.base.par_loop` or through direct user access to a
+:attr:`data` attribute. Access to the :attr:`data_ro` read only data attribute does
+not increase the version number.
+
+Data carrying objects are also equipped with a :meth:`duplicate`
+method. From a user perspective, this is a deep copy of the original
+object. In the case of :class:`~pyop2.base.Dat` objects, this is implemented
+as a shallow copy along with a copy on write mechanism which causes
+the actual copy to occur if either the original or the copy is
+modified.  The delayed copy is implemented by immediately creating a
+copy :func:`~pyop2.base.par_loop` and, if lazy evaluation is enabled,
+enqueing it. This ensures that the dependency trace will cause all
+operations on which the copy depends to occur before the
+copy. Conversely, the dependency of the copy :class:`~pyop2.base.Dat` on the
+copying loop is artificially removed. This prevents the execution of
+the copy being triggered when the copy :class:`~pyop2.base.Dat` is
+read. Instead, writes to the original and copy :class:`~pyop2.base.Dat` are
+intercepted and execution of the copy :func:`~pyop2.base.par_loop` is forced
+at that point."""
+
 from copy import copy as shallow_copy
 import op2
 
