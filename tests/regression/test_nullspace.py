b43de8fd4ba64c8d5df12f936d1366412f7e7d7b
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-03-05 13:29:23 +0000

    Add test of nullspace for mixed poisson problem

--- tests/regression/test_nullspace.py
+++ tests/regression/test_nullspace.py
@@ -42,6 +42,55 @@ def test_nullspace_preassembled(V):
     assert sqrt(assemble((u - exact)*(u - exact)*dx)) < 5e-8
 
 
+def test_nullspace_mixed():
+    m = UnitSquareMesh(5, 5)
+    BDM = FunctionSpace(m, 'BDM', 1)
+    DG = FunctionSpace(m, 'DG', 0)
+    W = BDM * DG
+
+    sigma, u = TrialFunctions(W)
+    tau, v = TestFunctions(W)
+
+    a = (dot(sigma, tau) + div(tau)*u + div(sigma)*v)*dx
+
+    bcs = [DirichletBC(W.sub(0), (0, 0), (1, 2)),
+           DirichletBC(W.sub(0), (0, 1), (3, 4))]
+
+    w = Function(W)
+
+    f = Function(DG)
+    f.assign(0)
+    L = f*v*dx
+
+    # Null space is constant functions in DG and empty in BDM.
+    null_vec = Function(W)
+    null_vec.dat[1].data[:] = 1 / sqrt(W[1].dof_count)
+
+    nullspace = VectorSpaceBasis(vecs=[null_vec])
+
+    solve(a == L, w, bcs=bcs, nullspace=nullspace)
+
+    exact = Function(DG)
+    exact.interpolate(Expression('x[1] - 0.5'))
+
+    sigma, u = w.split()
+    assert sqrt(assemble((u - exact)*(u - exact)*dx)) < 1e-7
+
+    # Now using a Schur complement
+    nullspace = MixedVectorSpaceBasis([W.sub(0), VectorSpaceBasis(constant=True)])
+
+    w.assign(0)
+    solve(a == L, w, bcs=bcs, nullspace=nullspace,
+          solver_parameters={'pc_type': 'fieldsplit',
+                             'pc_fieldsplit_type': 'schur',
+                             'pc_fieldsplit_schur_fact_type': 'FULL',
+                             'fieldsplit_0_ksp_type': 'cg',
+                             'fieldsplit_1_ksp_type': 'cg'})
+
+    sigma, u = w.split()
+    assert sqrt(assemble((u - exact)*(u - exact)*dx)) < 5e-8
+
+
 if __name__ == '__main__':
     import os
     pytest.main(os.path.abspath(__file__))
