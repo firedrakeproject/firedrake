4f1cfa7cb3e06e6badfba346278ed0bab897159a
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-03-10 00:35:38 +0000

    Docs: fall back to SimpleHTTPServer if livereload not available

--- doc/sphinx/server.py
+++ doc/sphinx/server.py
@@ -2,15 +2,24 @@
 sphinx source directory for changes and rebuild the html documentation. Watch
 the pyop2 package directory for changes and rebuild the API documentation.
 
-Requires livereload_ ::
+Requires livereload_ (or falls back to SimpleHTTPServer) ::
 
   pip install git+https://github.com/lepture/python-livereload
 
 .. _livereload: https://github.com/lepture/python-livereload"""
 
-from livereload import Server
+try:
+    from livereload import Server
 
-server = Server()
-server.watch('source', 'make html')
-server.watch('../../pyop2', 'make apidoc')
-server.serve(root='build/html', open_url=True)
+    server = Server()
+    server.watch('source', 'make html')
+    server.watch('../../pyop2', 'make apidoc')
+    server.serve(root='build/html', open_url=True)
+except ImportError:
+    import SimpleHTTPServer
+    import SocketServer
+
+    PORT = 8000
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("build/html", PORT), Handler)
+    httpd.serve_forever()
