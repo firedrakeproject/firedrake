ac55a5ab0c1dbf6a656332b116f2c99a3a32fd1b
dorugeber <a.mcrae12@imperial.ac.uk>
2014-03-20 17:41:18 +0000

    Tidy existing tests, and un-xfail

--- tests/extrusion/test_extrusion_facet_integrals_2D.py
+++ tests/extrusion/test_extrusion_facet_integrals_2D.py
@@ -5,12 +5,23 @@ from firedrake import *
 from tests.common import *
 
 
-@pytest.mark.xfail(reason="waiting for extruded facet changes")
-@pytest.mark.parametrize('degree', [1, 2])
-def test_scalar_area(degree):
+@pytest.fixture(scope='module', params=[1, 2])
+def f(request):
     mesh = extmesh_2D(4, 4)
-    fspace = FunctionSpace(mesh, "CG", degree)
-    f = Function(fspace)
+    fspace = FunctionSpace(mesh, "CG", request.param)
+    return Function(fspace)
+
+
+@pytest.fixture(scope='module')
+def RT2():
+    mesh = extmesh_2D(4, 4)
+    U0 = FiniteElement("CG", "interval", 2)
+    U1 = FiniteElement("DG", "interval", 1)
+    W1 = HDiv(OuterProductElement(U1, U0)) + HDiv(OuterProductElement(U0, U1))
+    return FunctionSpace(mesh, W1)
+
+
+def test_scalar_area(f):
     f.assign(1)
     assert abs(assemble(f*ds_t) - 1.0) < 1e-7
     assert abs(assemble(f*ds_b) - 1.0) < 1e-7
@@ -22,12 +33,7 @@ def test_scalar_area(degree):
     assert abs(assemble(f('-')*dS_v) - 3.0) < 1e-7
 
 
-@pytest.mark.xfail(reason="waiting for extruded facet changes")
-@pytest.mark.parametrize('degree', [1, 2])
-def test_scalar_expression(degree):
-    mesh = extmesh_2D(4, 4)
-    fspace = FunctionSpace(mesh, "CG", degree)
-    f = Function(fspace)
+def test_scalar_expression(f):
     f.interpolate(Expression("x[1]"))
     assert abs(assemble(f*ds_t) - 1.0) < 1e-7
     assert abs(assemble(f*ds_b) - 0.0) < 1e-7
@@ -39,14 +45,8 @@ def test_scalar_expression(degree):
     assert abs(assemble(f('-')*dS_v) - 1.5) < 1e-7
 
 
-@pytest.mark.xfail(reason="waiting for extruded facet changes")
-def test_hcurl_area():
-    mesh = extmesh_2D(4, 4)
-    U0 = FiniteElement("CG", "interval", 1)
-    U1 = FiniteElement("DG", "interval", 0)
-    W1 = HCurl(OuterProductElement(U1, U0)) + HCurl(OuterProductElement(U0, U1))
-    fspace = FunctionSpace(mesh, W1)
-    f = project(Expression(("0.8", "0.6")), fspace)
+def test_hdiv_area(RT2):
+    f = project(Expression(("0.8", "0.6")), RT2)
     assert abs(assemble(dot(f, f)*ds_t) - 1.0) < 1e-7
     assert abs(assemble(dot(f, f)*ds_b) - 1.0) < 1e-7
     assert abs(assemble(dot(f, f)*ds_tb) - 2.0) < 1e-7
@@ -59,54 +59,22 @@ def test_hcurl_area():
     assert abs(assemble(dot(f('+'), f('-'))*dS_v) - 3.0) < 1e-7
 
 
-@pytest.mark.xfail(reason="waiting for extruded facet changes")
-def test_hdiv_area():
-    mesh = extmesh_2D(4, 4)
-    U0 = FiniteElement("CG", "interval", 1)
-    U1 = FiniteElement("DG", "interval", 0)
-    W1 = HDiv(OuterProductElement(U1, U0)) + HDiv(OuterProductElement(U0, U1))
-    fspace = FunctionSpace(mesh, W1)
-    f = project(Expression(("0.8", "0.6")), fspace)
-    assert abs(assemble(dot(f, f)*ds_t) - 1.0) < 1e-7
-    assert abs(assemble(dot(f, f)*ds_b) - 1.0) < 1e-7
-    assert abs(assemble(dot(f, f)*ds_tb) - 2.0) < 1e-7
-    assert abs(assemble(dot(f, f)*ds_v) - 2.0) < 1e-7
-    assert abs(assemble(dot(f('+'), f('+'))*dS_h) - 3.0) < 1e-7
-    assert abs(assemble(dot(f('-'), f('-'))*dS_h) - 3.0) < 1e-7
-    assert abs(assemble(dot(f('+'), f('-'))*dS_h) - 3.0) < 1e-7
-    assert abs(assemble(dot(f('+'), f('+'))*dS_v) - 3.0) < 1e-7
-    assert abs(assemble(dot(f('-'), f('-'))*dS_v) - 3.0) < 1e-7
-    assert abs(assemble(dot(f('+'), f('-'))*dS_v) - 3.0) < 1e-7
-
-
-@pytest.mark.xfail(reason="waiting for extruded facet changes")
-def test_exterior_horizontal_normals():
-    mesh = extmesh_2D(4, 4)
-    n = FacetNormal(mesh)
-    U0 = FiniteElement("CG", "interval", 1)
-    U1 = FiniteElement("DG", "interval", 0)
-    W1 = HDiv(OuterProductElement(U1, U0)) + HDiv(OuterProductElement(U0, U1))
-    fspace = FunctionSpace(mesh, W1)
-    f = project(Expression(("1.0", "0.0")), fspace)
+def test_exterior_horizontal_normals(RT2):
+    n = FacetNormal(RT2.mesh())
+    f = project(Expression(("1.0", "0.0")), RT2)
     assert abs(assemble(dot(f, n)*ds_t) - 0.0) < 1e-7
     assert abs(assemble(dot(f, n)*ds_b) - 0.0) < 1e-7
-    f = project(Expression(("0.0", "1.0")), fspace)
+    f = project(Expression(("0.0", "1.0")), RT2)
     assert abs(assemble(dot(f, n)*ds_t) - 1.0) < 1e-7
     assert abs(assemble(dot(f, n)*ds_b) - (-1.0)) < 1e-7
 
 
-@pytest.mark.xfail(reason="waiting for extruded facet changes")
-def test_exterior_vertical_normals():
-    mesh = extmesh_2D(4, 4)
-    n = FacetNormal(mesh)
-    U0 = FiniteElement("CG", "interval", 1)
-    U1 = FiniteElement("DG", "interval", 0)
-    W1 = HDiv(OuterProductElement(U1, U0)) + HDiv(OuterProductElement(U0, U1))
-    fspace = FunctionSpace(mesh, W1)
-    f = project(Expression(("1.0", "0.0")), fspace)
+def test_exterior_vertical_normals(RT2):
+    n = FacetNormal(RT2.mesh())
+    f = project(Expression(("1.0", "0.0")), RT2)
     assert abs(assemble(dot(f, n)*ds_v(1)) - (-1.0)) < 1e-7
     assert abs(assemble(dot(f, n)*ds_v(2)) - 1.0) < 1e-7
-    f = project(Expression(("0.0", "1.0")), fspace)
+    f = project(Expression(("0.0", "1.0")), RT2)
     assert abs(assemble(dot(f, n)*ds_v(1)) - 0.0) < 1e-7
     assert abs(assemble(dot(f, n)*ds_v(2)) - 0.0) < 1e-7
 
