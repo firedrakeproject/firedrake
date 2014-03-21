79561227cbc82700f2733ee7dfad0044e90cbcb9
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-03-21 15:08:03 +0000

    Add test assembling derivative of form with 0 block

---
+++ tests/regression/test_split.py
@@ -0,0 +1,22 @@
+import pytest
+from firedrake import *
+
+
+def test_assemble_split_derivative():
+    """Assemble the derivative of a form with a zero block."""
+    mesh = UnitSquareMesh(1, 1)
+    V1 = FunctionSpace(mesh, "BDM", 1, name="V")
+    V2 = FunctionSpace(mesh, "DG", 0, name="P")
+    W = V1 * V2
+
+    x = Function(W)
+    u, p = split(x)
+    v, q = TestFunctions(W)
+
+    F = (inner(u, v) + v[1]*p)*dx
+
+    assert assemble(derivative(F, x))
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
