41e2a1b24836ee1e042bcb5f46e2df9dad8b6c5b
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-03-22 16:48:42 +0000

    Make mat_utils purely a header file
    We basically just paste the code in anyway, so why not get the
    preprocessor to do it for us.

--- pyop2/mat_utils.h
+++ pyop2/mat_utils.h
@@ -2,9 +2,32 @@
 #define _MAT_UTILS_H
 
 #include <petscmat.h>
+#include <assert.h>
 
-void addto_scalar(Mat mat, const void *value, int row, int col, int insert);
-void addto_vector(Mat mat, const void* values, int nrows,
-                  const int *irows, int ncols, const int *icols, int insert);
+static inline void addto_scalar(Mat mat, const void *value, int row, int col, int insert)
+{
+  assert( mat && value);
+  // FIMXE: this assumes we're getting a PetscScalar
+  const PetscScalar * v = (const PetscScalar *)value;
+
+  if ( v[0] == 0.0 && !insert ) return;
+  MatSetValuesLocal( mat,
+                1, (const PetscInt *)&row,
+                1, (const PetscInt *)&col,
+                v, insert ? INSERT_VALUES : ADD_VALUES );
+}
+
+static inline void addto_vector(Mat mat, const void *values,
+                  int nrows, const int *irows,
+                  int ncols, const int *icols, int insert)
+{
+  assert( mat && values && irows && icols );
+  // FIMXE: this assumes we're getting a PetscScalar
+  MatSetValuesLocal( mat,
+                nrows, (const PetscInt *)irows,
+                ncols, (const PetscInt *)icols,
+                (const PetscScalar *)values,
+                insert ? INSERT_VALUES : ADD_VALUES );
+}
 
 #endif // _MAT_UTILS_H
