c7d1ab775f6d411c03efec31c721042966661d0b
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-16 17:00:52 +0100

    ExpressionSplitter: Simplify assignment to indexed LHS
    If the LHS is indexed, only build scalar operands i.e. tuples of
    length one and not the full size of the parent function space.

--- firedrake/assemble_expressions.py
+++ firedrake/assemble_expressions.py
@@ -278,23 +278,7 @@ class ExpressionSplitter(ReuseTransformer):
         if not isinstance(expr, AssignmentBase) and \
                 lhs.function_space() != rhs.function_space():
             raise ValueError("Operands of %r must have the same FunctionSpace" % expr)
-        self._function_space = lhs.function_space()
-        idx = lhs.function_space().index
-        # If it is assignment, and the lhs is indexed (w.sub(idx)) we
-        # split the lhs and put the expression in the index position
-        # but zero otherwise.
-        # Thus, for example, if w is on a 2-component mixed space:
-        # w.sub(0).assign(1)
-        # is transformed into:
-        # [Assign(w.sub(0), 1), Zero]
-        if isinstance(expr, AssignmentBase) and idx is not None:
-            ret = []
-            for i, _ in enumerate(lhs.function_space()._parent):
-                if i == idx:
-                    ret.append(expr.reconstruct(*[ops for ops in zip(*map(self.visit, (lhs, rhs)))][idx]))
-                else:
-                    ret.append(Zero())
-            return ret
+        self._fs = lhs.function_space()
         return [expr.reconstruct(*ops) for ops in zip(*map(self.visit, (lhs, rhs)))]
 
     def indexed(self, o, *operands):
@@ -322,54 +306,40 @@ class ExpressionSplitter(ReuseTransformer):
         if isinstance(o, types.Function):
             # A function must either be defined on the same function space
             # we're assigning to, in which case we split it into components
-            if o.function_space() == self._function_space:
-                idx = o.function_space().index
-                # Is the lhs an indexed function on a mixed space?  If
-                # so, splat it out so that we have the right number of
-                # components.
-                if idx is not None:
-                    return [o if idx == i else self._identity
-                            for i, _ in enumerate(o.function_space()._parent)]
+            if o.function_space() == self._fs:
                 return o.split()
             # Otherwise the function space must be indexed and we
             # return the Function for the indexed component and the
             # identity for this assignment for every other
-            else:
-                idx = o.function_space().index
-                vidx = self._function_space.index
-                if vidx is not None:
-                    # LHS is indexed
-                    if idx is not None:
-                        # RHS indexed, indexed RHS function space must match
-                        # indexed LHS function space.
-                        if self._function_space._fs != o.function_space()._fs:
-                            raise ValueError("Mismatching indexed function spaces")
-                    # RHS not indexed, RHS function space must match
-                    # indexed LHS function space
-                    elif self._function_space._fs != o.function_space():
-                        raise ValueError("Mismatching function spaces")
-                    # OK, everything checked out.  Return RHS in LHS
-                    # index slot in expression and identity otherwise.
-                    return [o if i == vidx else self._identity
-                            for i, _ in enumerate(self._function_space._parent)]
-                if idx is None:
-                    # LHS not indexed, RHS must be indexed and isn't
-                    raise ValueError("Coefficient %r is not indexed" % o)
-                # RHS indexed, parent function space must match LHS function space
-                if self._function_space != o.function_space()._parent:
+            idx = o.function_space().index
+            # LHS is indexed
+            if self._fs.index is not None:
+                # RHS indexed, indexed RHS function space must match
+                # indexed LHS function space.
+                if idx is not None and self._fs._fs != o.function_space()._fs:
+                    raise ValueError("Mismatching indexed function spaces")
+                # RHS not indexed, RHS function space must match
+                # indexed LHS function space
+                elif idx is None and self._fs._fs != o.function_space():
                     raise ValueError("Mismatching function spaces")
-                # Return RHS in index slot in expression and
-                # identity otherwise.
-                return [o if i == idx else self._identity
-                        for i, _ in enumerate(self._function_space)]
+                # OK, everything checked out. Return RHS
+                return (o,)
+            # LHS not indexed, RHS must be indexed and isn't
+            if idx is None:
+                raise ValueError("Coefficient %r is not indexed" % o)
+            # RHS indexed, parent function space must match LHS function space
+            if self._fs != o.function_space()._parent:
+                raise ValueError("Mismatching function spaces")
+            # Return RHS in index slot in expression and
+            # identity otherwise.
+            return tuple(o if i == idx else self._identity
+                         for i, _ in enumerate(self._fs))
         # We replicate ConstantValue and MultiIndex for each component
         elif isinstance(o, (types.Constant, ConstantValue, MultiIndex)):
-            idx = self._function_space.index
-            if idx is not None:
-                # If LHS is indexed, replicate for each component of
-                # the parent.
-                return [o for _ in self._function_space._parent]
-            return [o for _ in self._function_space]
+            # If LHS is indexed, only return a scalar result
+            if self._fs.index is not None:
+                return (o,)
+            return tuple(o for _ in self._fs)
         raise NotImplementedError("Don't know what to do with %r" % o)
 
     def product(self, o, *operands):
