edb35e563499502c0d9ddb057011f72a76e34b35
Fabio Luporini <f.luporini12@imperial.ac.uk>
2014-04-29 10:54:26 +0100

    Create basic AssemblyRewriter

--- pyop2/coffee/ast_optimizer.py
+++ pyop2/coffee/ast_optimizer.py
@@ -151,155 +151,13 @@ class AssemblyOptimizer(object):
         return (itspace_vrs, accessed_vrs)
 
     def generalized_licm(self):
-        """Perform loop-invariant code motion.
+        """Generalized loop-invariant code motion."""
 
-        Invariant expressions found in the loop nest are moved "after" the
-        outermost independent loop and "after" the fastest varying dimension
-        loop. Here, "after" means that if the loop nest has two loops i and j,
-        and j is in the body of i, then i comes after j (i.e. the loop nest
-        has to be read from right to left)
-
-        For example, if a sub-expression E depends on [i, j] and the loop nest
-        has three loops [i, j, k], then E is hoisted out from the body of k to
-        the body of i). All hoisted expressions are then wrapped within a
-        suitable loop in order to exploit compiler autovectorization.
-        """
-
-        def extract_const(node, expr_dep):
-            if isinstance(node, Symbol):
-                return (node.loop_dep, node.symbol not in written_vars)
-            if isinstance(node, Par):
-                return (extract_const(node.children[0], expr_dep))
-
-            # Traverse the expression tree
-            left, right = node.children
-            dep_left, invariant_l = extract_const(left, expr_dep)
-            dep_right, invariant_r = extract_const(right, expr_dep)
-
-            if dep_left == dep_right:
-                # Children match up, keep traversing the tree in order to see
-                # if this sub-expression is actually a child of a larger
-                # loop-invariant sub-expression
-                return (dep_left, True)
-            elif len(dep_left) == 0:
-                # The left child does not depend on any iteration variable,
-                # so it's loop invariant
-                return (dep_right, True)
-            elif len(dep_right) == 0:
-                # The right child does not depend on any iteration variable,
-                # so it's loop invariant
-                return (dep_left, True)
-            else:
-                # Iteration variables of the two children do not match, add
-                # the children to the dict of invariant expressions iff
-                # they were invariant w.r.t. some loops and not just symbols
-                if invariant_l and not isinstance(left, Symbol):
-                    expr_dep[dep_left].append(left)
-                if invariant_r and not isinstance(right, Symbol):
-                    expr_dep[dep_right].append(right)
-                return ((), False)
-
-        def replace_const(node, syms_dict):
-            if isinstance(node, Symbol):
-                return False
-            if isinstance(node, Par):
-                if node in syms_dict:
-                    return True
-                else:
-                    return replace_const(node.children[0], syms_dict)
-            # Found invariant sub-expression
-            if node in syms_dict:
-                return True
-
-            # Traverse the expression tree and replace
-            left = node.children[0]
-            right = node.children[1]
-            if replace_const(left, syms_dict):
-                node.children[0] = syms_dict[left]
-            if replace_const(right, syms_dict):
-                node.children[1] = syms_dict[right]
-            return False
-
-        # Find out all variables which are written to in this loop nest
-        written_vars = []
-        for s in self.asm_expr.keys():
-            if type(s) in [Assign, Incr]:
-                written_vars.append(s.children[0].symbol)
-
-        # Extract read-only sub-expressions that do not depend on at least
-        # one loop in the loop nest
-        ext_loops = []
-        for s, op in self.asm_expr.items():
-            expr_dep = defaultdict(list)
-            if isinstance(s, (Assign, Incr)):
-                typ = self.kernel_decls[s.children[0].symbol][0].typ
-                extract_const(s.children[1], expr_dep)
-
-            for dep, expr in expr_dep.items():
-                # 1) Determine the loops that should wrap invariant statements
-                # and where such for blocks should be placed in the loop nest
-                n_dep_for = None
-                fast_for = None
-                # Collect some info about the loops
-                for l in self.fors:
-                    if l.it_var() == dep[-1]:
-                        fast_for = fast_for or l
-                    if l.it_var() not in dep:
-                        n_dep_for = n_dep_for or l
-                    if l.it_var() == op[0][0]:
-                        op_loop = l
-                if not fast_for or not n_dep_for:
-                    continue
-
-                # Find where to put the new invariant for
-                pre_loop = None
-                for l in self.fors:
-                    if l.it_var() not in [fast_for.it_var(), n_dep_for.it_var()]:
-                        pre_loop = l
-                    else:
-                        break
-                if pre_loop:
-                    place = pre_loop.children[0]
-                    ofs = place.children.index(op_loop)
-                    wl = [fast_for]
-                else:
-                    place = self.pre_header
-                    ofs = place.children.index(self.fors[0])
-                    wl = [l for l in self.fors if l.it_var() in dep]
-
-                # 2) Create the new loop
-                sym_rank = tuple([l.size() for l in wl],)
-                syms = [Symbol("LI_%s_%s" % (wl[0].it_var(), i), sym_rank)
-                        for i in range(len(expr))]
-                var_decl = [Decl(typ, _s) for _s in syms]
-                for_rank = tuple([l.it_var() for l in reversed(wl)])
-                for_sym = [Symbol(_s.sym.symbol, for_rank) for _s in var_decl]
-                for_ass = [Assign(_s, e) for _s, e in zip(for_sym, expr)]
-                block = Block(for_ass, open_scope=True)
-                for l in wl:
-                    inv_for = For(dcopy(l.init), dcopy(l.cond),
-                                  dcopy(l.incr), block)
-                    block = Block([inv_for], open_scope=True)
-
-                # Update the lists of symbols accessed and of decls
-                self.sym.update([d.sym for d in var_decl])
-                lv = ast_plan.LOCAL_VAR
-                self.decls.update(dict(zip([d.sym.symbol for d in var_decl],
-                                           [(v, lv) for v in var_decl])))
-
-                # 3) Append the new node at the right level in the loop nest
-                new_block = var_decl + [inv_for] + place.children[ofs:]
-                place.children = place.children[:ofs] + new_block
-
-                # 4) Replace invariant sub-trees with the proper tmp variable
-                replace_const(s.children[1], dict(zip(expr, for_sym)))
-
-                # 5) Record invariant loops which have been hoisted out of
-                # the present loop nest
-                if not pre_loop:
-                    ext_loops.append(inv_for)
-
-        return ext_loops
+        nest = (self.fors, self.sym, self.decls)
+        parent = (self.pre_header, self.kernel_decls)
+        for expr in self.asm_expr.items():
+            ew = AssemblyRewriter(expr, nest, parent)
+            ew.licm()
 
     def slice_loop(self, slice_factor=None):
         """Perform slicing of the innermost loop to enhance register reuse.
@@ -431,3 +289,153 @@ class AssemblyOptimizer(object):
         if splittable:
             new_asm_expr.update(splittable)
         self.asm_expr = new_asm_expr
+
+
+class AssemblyRewriter(object):
+    """Rewrite assembly expressions according to the following expansion
+    rules."""
+
+    def __init__(self, expr, nest, parent):
+        self.expr, self.expr_info = expr
+        self.nest_loops, self.nest_syms, self.nest_decls = nest
+        self.parent, self.parent_decls = parent
+        self.hoisted = {}
+
+    def licm(self):
+        """Perform loop-invariant code motion.
+
+        Invariant expressions found in the loop nest are moved "after" the
+        outermost independent loop and "after" the fastest varying dimension
+        loop. Here, "after" means that if the loop nest has two loops i and j,
+        and j is in the body of i, then i comes after j (i.e. the loop nest
+        has to be read from right to left).
+
+        For example, if a sub-expression E depends on [i, j] and the loop nest
+        has three loops [i, j, k], then E is hoisted out from the body of k to
+        the body of i). All hoisted expressions are then wrapped within a
+        suitable loop in order to exploit compiler autovectorization. Note that
+        this applies to constant sub-expressions as well, in which case hoisting
+        after the outermost loop takes place."""
+
+        def extract_const(node, expr_dep):
+            if isinstance(node, Symbol):
+                return (node.loop_dep, node.symbol not in self.expr.children[0].symbol)
+            if isinstance(node, Par):
+                return (extract_const(node.children[0], expr_dep))
+
+            # Traverse the expression tree
+            left, right = node.children
+            dep_left, invariant_l = extract_const(left, expr_dep)
+            dep_right, invariant_r = extract_const(right, expr_dep)
+
+            if dep_left == dep_right:
+                # Children match up, keep traversing the tree in order to see
+                # if this sub-expression is actually a child of a larger
+                # loop-invariant sub-expression
+                return (dep_left, True)
+            elif len(dep_left) == 0:
+                # The left child does not depend on any iteration variable,
+                # so it's loop invariant
+                return (dep_right, True)
+            elif len(dep_right) == 0:
+                # The right child does not depend on any iteration variable,
+                # so it's loop invariant
+                return (dep_left, True)
+            else:
+                # Iteration variables of the two children do not match, add
+                # the children to the dict of invariant expressions iff
+                # they were invariant w.r.t. some loops and not just symbols
+                if invariant_l and not isinstance(left, Symbol):
+                    expr_dep[dep_left].append(left)
+                if invariant_r and not isinstance(right, Symbol):
+                    expr_dep[dep_right].append(right)
+                return ((), False)
+
+        def replace_const(node, syms_dict):
+            if isinstance(node, Symbol):
+                return False
+            if isinstance(node, Par):
+                if node in syms_dict:
+                    return True
+                else:
+                    return replace_const(node.children[0], syms_dict)
+            # Found invariant sub-expression
+            if node in syms_dict:
+                return True
+
+            # Traverse the expression tree and replace
+            left = node.children[0]
+            right = node.children[1]
+            if replace_const(left, syms_dict):
+                node.children[0] = syms_dict[left]
+            if replace_const(right, syms_dict):
+                node.children[1] = syms_dict[right]
+            return False
+
+        # Extract read-only sub-expressions that do not depend on at least
+        # one loop in the loop nest
+        expr_dep = defaultdict(list)
+        typ = self.parent_decls[self.expr.children[0].symbol][0].typ
+        extract_const(self.expr.children[1], expr_dep)
+
+        for dep, expr in expr_dep.items():
+            # 1) Determine the loops that should wrap invariant statements
+            # and where such for blocks should be placed in the loop nest
+            n_dep_for = None
+            fast_for = None
+            # Collect some info about the loops
+            for l in self.nest_loops:
+                if l.it_var() == dep[-1]:
+                    fast_for = fast_for or l
+                if l.it_var() not in dep:
+                    n_dep_for = n_dep_for or l
+                if l.it_var() == self.expr_info[0][0]:
+                    op_loop = l
+            if not fast_for or not n_dep_for:
+                continue
+
+            # Find where to put the new invariant for
+            pre_loop = None
+            for l in self.nest_loops:
+                if l.it_var() not in [fast_for.it_var(), n_dep_for.it_var()]:
+                    pre_loop = l
+                else:
+                    break
+            if pre_loop:
+                place = pre_loop.children[0]
+                ofs = place.children.index(op_loop)
+                wl = [fast_for]
+            else:
+                place = self.parent
+                ofs = place.children.index(self.nest_loops[0])
+                wl = [l for l in self.nest_loops if l.it_var() in dep]
+
+            # 2) Create the new loop
+            sym_rank = tuple([l.size() for l in wl],)
+            syms = [Symbol("LI_%s_%s" % (wl[0].it_var(), i), sym_rank)
+                    for i in range(len(expr))]
+            var_decl = [Decl(typ, _s) for _s in syms]
+            for_rank = tuple([l.it_var() for l in reversed(wl)])
+            for_sym = [Symbol(_s.sym.symbol, for_rank) for _s in var_decl]
+            for_ass = [Assign(_s, e) for _s, e in zip(for_sym, expr)]
+            block = Block(for_ass, open_scope=True)
+            for l in wl:
+                inv_for = For(dcopy(l.init), dcopy(l.cond),
+                              dcopy(l.incr), block)
+                block = Block([inv_for], open_scope=True)
+
+            # Update the lists of symbols accessed and of decls
+            self.nest_syms.update([d.sym for d in var_decl])
+            lv = ast_plan.LOCAL_VAR
+            self.nest_decls.update(dict(zip([d.sym.symbol for d in var_decl],
+                                        [(v, lv) for v in var_decl])))
+
+            # 3) Append the new node at the right level in the loop nest
+            new_block = var_decl + [inv_for] + place.children[ofs:]
+            place.children = place.children[:ofs] + new_block
+
+            # 4) Track hoisted symbols
+            self.hoisted.update(zip(for_sym, [(i, inv_for) for i in expr]))
+
+            # 5) Replace invariant sub-trees with the proper tmp variable
+            replace_const(self.expr.children[1], dict(zip(expr, for_sym)))
