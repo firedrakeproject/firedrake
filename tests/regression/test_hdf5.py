6a92d9086331d4387a45f081757a78d1e441ffcb
Michael Lange <michael.lange@imperial.ac.uk>
2014-04-30 16:04:34 +0100

    HDF5: Adding simple test for scalar output

---
+++ tests/regression/test_hdf5.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pytest
+import os
+
+from firedrake import *
+from tests.common import *
+
+try:
+    import h5py
+except ImportError:
+    h5py = None
+
+_h5file = os.path.join(os.curdir, "test_hdf5.h5")
+
+
+@pytest.fixture
+def filepath():
+    if os.path.exists(_h5file):
+        os.remove(_h5file)
+    return _h5file
+
+
+@pytest.mark.skipif("h5py is None", reason='h5py not available')
+def test_hdf5_scalar(mesh, filepath):
+    fs = FunctionSpace(mesh, "CG", 1)
+    x = Function(fs, name='xcoord')
+    x.interpolate(Expression("x[0]"))
+
+    h5file = File(filepath)
+    h5file << x
+
+    h5out = h5py.File(filepath, 'r')
+    xval = h5out['fields']['xcoord'][0, :, 0]
+    assert np.max(np.abs(xval - x.dat.data)) < 1e-6
