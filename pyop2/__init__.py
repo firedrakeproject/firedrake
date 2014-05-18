255bd197fcffa5b18a37a9390f30ddc561ff9998
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-18 22:44:51 +0100

    Install versioneer
    This file helps to compute a version number in source trees obtained
    from git-archive tarball (such as those provided by githubs
    download-from-tag feature). Distribution tarballs (build by setup.py
    sdist) and build directories (produced by setup.py build) will contain
    a much shorter file that just contains the computed version number.
    
    The PyOP2 package version now includes the git revision as returned by
      git describe --tags --dirty --always

--- pyop2/__init__.py
+++ pyop2/__init__.py
@@ -7,4 +7,8 @@ delivers performance-portability across a range of platforms:
 """
 
 from op2 import *
-from version import __version__, __version_info__  # noqa: we just want to expose these
+from version import __version_info__  # noqa: we just want to expose these
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
