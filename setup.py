99d5d0f041a0a65d9f3bbbef01669b5719affac4
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-05-20 15:31:41 +0100

    setup.py: Don't rely on PyOP2 already being installed

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from glob import glob
-from os import environ as env
-from pyop2.utils import get_petsc_dir
+from os import environ as env, path
+import sys
 import numpy as np
 import petsc4py
+
+
+def get_petsc_dir():
+    try:
+        petsc_arch = env.get('PETSC_ARCH', '')
+        petsc_dir = env['PETSC_DIR']
+        if petsc_arch:
+            return (petsc_dir, path.join(petsc_dir, petsc_arch))
+        return (petsc_dir,)
+    except KeyError:
+        try:
+            import petsc
+            return (petsc.get_petsc_dir(), )
+        except ImportError:
+            sys.exit("""Error: Could not find PETSc library.
+
+Set the environment variable PETSC_DIR to your local PETSc base
+directory or install PETSc from PyPI as described in the manual:
+
+http://firedrakeproject.org/obtaining_pyop2.html#petsc
+""")
 
 try:
     from Cython.Distutils import build_ext
