9d9078129093deb9adf53cd5bdd17ff86f1f2a06
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-07-10 15:00:34 +0100

    Test mixed space top/bottom bcs with mixed strong/weak bcs
    Solve two independent Poisson problems in a single mixed space applying
    strong bcs to the top and inhomogeneous weak bcs at the bottom in the
    first and vice versa in the second.  Tests that we don't have pollution
    of top/bottom bcs between the two spaces.

--- tests/extrusion/test_mixed_bcs.py
+++ tests/extrusion/test_mixed_bcs.py
@@ -48,6 +48,51 @@ def test_multiple_poisson_Pn(degree):
     assert assemble(inner(w - wexact, w - wexact)*dx) < 1e-8
 
 
+@pytest.mark.parametrize('degree', [1, 2, 3])
+def test_multiple_poisson_strong_weak_Pn(degree):
+    m = UnitSquareMesh(4, 4)
+    mesh = ExtrudedMesh(m, 4)
+
+    V = FunctionSpace(mesh, 'CG', degree)
+
+    W = V*V
+
+    w = Function(W)
+    u, p = TrialFunctions(W)
+    v, q = TestFunctions(W)
+
+    # Solve two independent Poisson problems with a strong boundary
+    # condition on the top and a weak condition on the bottom, and
+    # vice versa.
+    a = dot(grad(u), grad(v))*dx + dot(grad(p), grad(q))*dx
+    L = Constant(1)*v*ds_b + Constant(4)*q*ds_t
+
+    # BCs for first problem
+    bc0 = [DirichletBC(W[0], 10.0, "top")]
+    # BCs for second problem
+    bc1 = [DirichletBC(W[1], 2.0, "bottom")]
+
+    bcs = bc0 + bc1
+    solve(a == L, w, bcs=bcs,
+          # Operator is block diagonal, so we can just do block jacobi
+          # with lu on each block
+          solver_parameters={'ksp_type': 'cg',
+                             'pc_type': 'fieldsplit',
+                             'pc_fieldsplit_type': 'additive',
+                             'fieldsplit_ksp_type': 'preonly',
+                             'fieldsplit_0_pc_type': 'lu',
+                             'fieldsplit_1_pc_type': 'lu'})
+
+    wexact = Function(W)
+
+    u, p = wexact.split()
+
+    u.interpolate(Expression("11.0 - x[2]"))
+    p.interpolate(Expression("2.0 + 4*x[2]"))
+
+    assert assemble(inner(w - wexact, w - wexact)*dx) < 1e-8
+
+
 def test_stokes_taylor_hood():
     length = 10
     m = IntervalMesh(40, length)
