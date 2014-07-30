39cc8ba6ae786be393f7c0b268a9fc940f7ddf0f
David A Ham <david.ham@imperial.ac.uk>
2014-07-30 16:01:43 +0100

    Remove overcautious exception

--- pyop2/pyparloop.py
+++ pyop2/pyparloop.py
@@ -112,8 +112,6 @@ class ParLoop(base.ParLoop):
             else:
                 idx = e
             for arg in self.args:
-                if arg.access is base.INC:
-                    raise NotImplementedError("Need to think harder about this case")
                 if arg._is_global:
                     args.append(arg.data._data)
                 elif arg._is_direct:
