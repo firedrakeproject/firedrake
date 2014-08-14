8d4aa4564dd65a48d254ba219e2352879797cb48
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-08-14 23:34:52 +0100

    Parser: suppress backend argument instead of defaulting to sequential

--- pyop2/utils.py
+++ pyop2/utils.py
@@ -240,7 +240,7 @@ def parser(description=None, group=False):
     g = parser.add_argument_group(
         'pyop2', 'backend configuration options') if group else parser
 
-    g.add_argument('-b', '--backend', default='sequential',
+    g.add_argument('-b', '--backend', default=argparse.SUPPRESS,
                    choices=['sequential', 'openmp', 'opencl', 'cuda'],
                    help='select backend' if group else 'select pyop2 backend')
     g.add_argument('-d', '--debug', default=argparse.SUPPRESS,
