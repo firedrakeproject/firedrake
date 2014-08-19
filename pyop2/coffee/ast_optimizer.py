7e68c4130c597e48861b6c949670071cccb448c7
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-08-19 11:15:29 +0100

    Python 2.6 compatibility for OrderedDict

--- pyop2/coffee/ast_optimizer.py
+++ pyop2/coffee/ast_optimizer.py
@@ -31,7 +31,13 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from collections import defaultdict, OrderedDict
+try:
+    from collections import OrderedDict
+# OrderedDict was added in Python 2.7. Earlier versions can use ordereddict
+# from PyPI
+except ImportError:
+    from ordereddict import OrderedDict
+from collections import defaultdict
 import itertools
 from copy import deepcopy as dcopy
 
