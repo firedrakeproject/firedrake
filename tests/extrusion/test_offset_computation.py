a1bf4e65533bfff9dd76cce6d84d835dc0bf8487
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-10-23 15:17:51 +0100

    Add some (failing) parallel offset computations

--- tests/extrusion/test_offset_computation.py
+++ tests/extrusion/test_offset_computation.py
@@ -47,6 +47,30 @@ def test_offset_enriched():
             [2, 2, 2, 2, 2, 2, 1, 1]).all()
 
 
+def run_offset_parallel():
+    m = UnitSquareMesh(20, 20)
+    m = ExtrudedMesh(m, layers=1)
+
+    V = FunctionSpace(m, 'CG', 2)
+
+    offset = V.exterior_facet_boundary_node_map("topological").offset
+
+    offsets = op2.MPI.comm.allgather(offset)
+    assert all((o == offset).all() for o in offsets)
+
+
+@pytest.mark.xfail
+@pytest.mark.parallel(nprocs=6)
+def test_offset_parallel_indexerror():
+    run_offset_parallel()
+
+
+@pytest.mark.xfail
+@pytest.mark.parallel(nprocs=2)
+def test_offset_parallel_allsame():
+    run_offset_parallel()
+
+
 if __name__ == '__main__':
     import os
     pytest.main(os.path.abspath(__file__))
