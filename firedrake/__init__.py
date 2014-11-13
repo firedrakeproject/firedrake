3c7523a3a9ea020629ea115447f32d1dc5ceedf6
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-11-13 19:22:10 +0000

    Wrap removal of ufl.Expr.__del__ in try except
    Stops sphinx barfing.

--- firedrake/__init__.py
+++ firedrake/__init__.py
@@ -16,7 +16,10 @@ del petsc
 # To get around this, since the default __del__ on Expr is just
 # "pass", we just remove the method from the definition of Expr.
 import ufl
-del ufl.core.expr.Expr.__del__
+try:
+    del ufl.core.expr.Expr.__del__
+except AttributeError:
+    pass
 del ufl
 from ufl import *
 from pyop2.logger import set_log_level, info_red, info_green, info_blue, log  # noqa
