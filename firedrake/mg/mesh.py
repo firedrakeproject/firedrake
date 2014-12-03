30f1aa3cde16fef823d0444851120821bc991afb
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2014-12-03 15:21:20 +0000

    Add MeshHierarchy and ExtrudedMeshHierarchy
    The former takes an existing mesh and refines it uniformly a specified
    number of levels.  The latter builds a sequence of extruded meshes on an
    existing mesh hierarchy.

---
+++ firedrake/mg/mesh.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+import firedrake.mesh as mesh
+from firedrake.mg import impl
+
+
+__all__ = ["MeshHierarchy", "ExtrudedMeshHierarchy"]
+
+
+class MeshHierarchy(object):
+    def __init__(self, m, refinement_levels, reorder=None):
+        """Build a hierarchy of meshes by uniformly refining a coarse mesh.
+
+        :arg m: the coarse :class:`~.Mesh` to refine
+        :arg refinement_levels: the number of levels of refinement
+        :arg reorder: optional flag indicating whether to reorder the
+             refined meshes.
+        """
+        m._plex.setRefinementUniform(True)
+        dm_hierarchy = []
+
+        cdm = m._plex
+        fpoint_ises = []
+        for i in range(refinement_levels):
+            rdm = cdm.refine()
+            fpoint_ises.append(cdm.createCoarsePointIS())
+            # Remove interior facet label (re-construct from
+            # complement of exterior facets).  Necessary because the
+            # refinement just marks points "underneath" the refined
+            # facet with the appropriate label.  This works for
+            # exterior, but not marked interior facets
+            rdm.removeLabel("interior_facets")
+            # Remove vertex (and edge) points from labels on exterior
+            # facets.  Interior facets will be relabeled in Mesh
+            # construction below.
+            impl.filter_exterior_facet_labels(rdm)
+            rdm.removeLabel("op2_core")
+            rdm.removeLabel("op2_non_core")
+            rdm.removeLabel("op2_exec_halo")
+            rdm.removeLabel("op2_non_exec_halo")
+
+            dm_hierarchy.append(rdm)
+            cdm = rdm
+            # Fix up coords if refining embedded circle or sphere
+            if hasattr(m, '_circle_manifold'):
+                coords = cdm.getCoordinatesLocal().array.reshape(-1, 2)
+                scale = m._circle_manifold / np.linalg.norm(coords, axis=1).reshape(-1, 1)
+                coords *= scale
+            elif hasattr(m, '_icosahedral_sphere'):
+                coords = cdm.getCoordinatesLocal().array.reshape(-1, 3)
+                scale = m._icosahedral_sphere / np.linalg.norm(coords, axis=1).reshape(-1, 1)
+                coords *= scale
+
+        hierarchy = [m] + [mesh.Mesh(dm, dim=m.ufl_cell().geometric_dimension(),
+                                     distribute=False, reorder=reorder)
+                           for i, dm in enumerate(dm_hierarchy)]
+        self._hierarchy = tuple(hierarchy)
+
+    def __iter__(self):
+        for m in self._hierarchy:
+            yield m
+
+    def __len__(self):
+        return len(self._hierarchy)
+
+    def __getitem__(self, idx):
+        return self._hierarchy[idx]
+
+
+class ExtrudedMeshHierarchy(MeshHierarchy):
+    def __init__(self, mesh_hierarchy, layers, kernel=None, layer_height=None,
+                 extrusion_type='uniform', gdim=None):
+        self._base_hierarchy = mesh_hierarchy
+        hierarchy = [mesh.ExtrudedMesh(m, layers, kernel=kernel,
+                                       layer_height=layer_height,
+                                       extrusion_type=extrusion_type,
+                                       gdim=gdim)
+                     for m in mesh_hierarchy]
+        self._hierarchy = tuple(hierarchy)
