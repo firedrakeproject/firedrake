5197a6fb405580904a20a8dd027c0a1d0ad504e7
Doru Rathgeber <a.mcrae12@imperial.ac.uk>
2015-01-05 14:56:56 +0000

    Add some (weak) tests for FacetElement and InteriorElement

---
+++ tests/regression/test_facet_elements.py
@@ -0,0 +1,24 @@
+import pytest
+from firedrake import *
+from tests.common import *
+
+
+def test_all_dofs_on_facets(mesh):
+    V_elt = FiniteElement("BDM", triangle, 1)
+    # BDM has all dofs on facets, so these should be the same
+    W1_elt = FacetElement(V_elt)
+    V = FunctionSpace(mesh, V_elt)
+    W1 = FunctionSpace(mesh, W1_elt)
+    f = Function(V).assign(1)
+    g = Function(W1).assign(1)
+    n = FacetNormal(mesh)
+
+    assert abs(assemble(dot(f, f)*dx) - assemble(dot(g, g)*dx)) < 1e-14
+    assert abs(assemble(dot(f, f)*ds) - assemble(dot(g, g)*ds)) < 1e-14
+    assert abs(assemble(dot(f, n)*ds) - assemble(dot(g, n)*ds)) < 1e-14
+    assert abs(assemble(dot(f('+'), n('+'))*dS) - assemble(dot(g('+'), n('+'))*dS)) < 1e-14
+
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
