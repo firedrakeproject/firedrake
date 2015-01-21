cd13e45eb4fd5b79cea0bb764dcfb97a92036432
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-01-21 16:50:05 +0000

    add (non-mixed) Helmholtz on sphere test

--- tests/regression/test_helmholtz_sphere.py
+++ tests/regression/test_helmholtz_sphere.py
@@ -5,7 +5,27 @@ import numpy as np
 from firedrake import *
 
 
-def run_helmholtz_sphere(r):
+def run_helmholtz_sphere(MeshClass, r):
+    m = MeshClass(refinement_level=r)
+    m.init_cell_orientations(Expression(('x[0]', 'x[1]', 'x[2]')))
+    V = FunctionSpace(m, 'CG', 2)
+
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    f = Function(V).interpolate(Expression("x[0]*x[1]*x[2]"))
+
+    a = (dot(grad(v), grad(u)) + v * u) * dx
+    L = f * v * dx
+
+    u = Function(V)
+    solve(a == L, u)
+
+    f.interpolate(Expression("x[0]*x[1]*x[2]/13.0"))
+    return errornorm(f, u, degree_rise=0)
+
+
+def run_helmholtz_mixed_sphere(r):
     m = UnitIcosahedralSphereMesh(refinement_level=r)
     m.init_cell_orientations(Expression(('x[0]', 'x[1]', 'x[2]')))
     V = FunctionSpace(m, 'RT', 1)
@@ -29,8 +49,17 @@ def run_helmholtz_sphere(r):
     return errornorm(f, u, degree_rise=0)
 
 
-def test_helmholtz_sphere():
-    errors = [run_helmholtz_sphere(r) for r in range(1, 5)]
+@pytest.mark.parametrize('MeshClass', [UnitIcosahedralSphereMesh, UnitCubedSphereMesh])
+def test_helmholtz_sphere(MeshClass):
+    errors = [run_helmholtz_sphere(MeshClass, r) for r in range(1, 5)]
+    errors = np.asarray(errors)
+    l2conv = np.log2(errors[:-1] / errors[1:])
+
+    assert (l2conv > 1.7).all()
+
+
+def test_helmholtz_mixed_sphere():
+    errors = [run_helmholtz_mixed_sphere(r) for r in range(1, 5)]
     errors = np.asarray(errors)
     l2conv = np.log2(errors[:-1] / errors[1:])
 
