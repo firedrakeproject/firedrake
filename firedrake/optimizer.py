832f8d9307acf44961238353afe9f9542be9f94c
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-02-23 17:40:42 +0000

    Append extra maps for hard fusion

--- firedrake/optimizer.py
+++ firedrake/optimizer.py
@@ -7,7 +7,14 @@ def slope(mesh, debug=False):
         * Mesh coordinates
         * All available maps binding sets of mesh components
     """
+    # Add coordinates
     if debug:
         coords = mesh.coordinates.dat
         slope_python.set_debug_mode('VERY_LOW', (coords.dataset.set.name,
-                                                 coords._data, coords.shape[1]))
+                                                 coords._data,
+                                                 coords.shape[1]))
+    # Add any known maps potentially useful for fusion of cell and facet integrals
+    slope_python.HardFusion.add_maps([('cell_to_interior_facet',
+                                       mesh.interior_facets.set.name,
+                                       mesh.cell_set.name,
+                                       mesh.interior_facets.facet_cell)])
