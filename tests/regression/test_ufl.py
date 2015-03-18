8ab12c4decc7be77acedd3c6dfdc1bea9ef2d7ab
Andrew McRae <a.mcrae12@imperial.ac.uk>
2015-03-18 10:53:19 +0000

    update test accordingly

--- tests/regression/test_ufl.py
+++ tests/regression/test_ufl.py
@@ -10,12 +10,12 @@ def test_cellsize_1d(n):
 
 @pytest.mark.parametrize('n', [1, 3, 16])
 def test_cellsize_2d(n):
-    assert abs(assemble(CellSize(UnitSquareMesh(n, n))*dx) - sqrt(2)/n) < 1e-14
+    assert abs(assemble(CellSize(UnitSquareMesh(n, n))*dx) - 1.0/n) < 1e-14
 
 
 @pytest.mark.parametrize('n', [1, 3, 16])
 def test_cellsize_3d(n):
-    assert abs(assemble(CellSize(UnitCubeMesh(n, n, n))*dx) - sqrt(3)/n) < 5e-14
+    assert abs(assemble(CellSize(UnitCubeMesh(n, n, n))*dx) - 1.0/n) < 5e-14
 
 if __name__ == '__main__':
     import os
