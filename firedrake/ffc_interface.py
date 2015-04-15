1b72bcf1156fa8a2448a3557a56a9a22201f0d8c
Miklós Homolya <m.homolya14@imperial.ac.uk>
2015-04-15 16:37:22 +0100

    re-apply pull request #474

--- firedrake/ffc_interface.py
+++ firedrake/ffc_interface.py
@@ -206,8 +206,13 @@ class FFCKernel(DiskCached):
                 kernels.append((Kernel(Root(incl + [_kernel]), '%s_%s_integral_0_%s' %
                                        (name, it.integral_type(), it.subdomain_id()), opts, inc),
                                 needs_orientations))
-            self.kernels = tuple(kernels)
-            self._empty = False
+            # Sometimes FFC returns an empty list without raising
+            # EmptyIntegrandError, catch that here.
+            if len(kernels) == 0:
+                self._empty = True
+            else:
+                self.kernels = tuple(kernels)
+                self._empty = False
         except EmptyIntegrandError:
             # FFC noticed that the integrand was zero and simplified
             # it, catch this here and set a flag telling us to ignore
