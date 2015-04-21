7c8cb748b31f59c69d268b38581f1b8ef8a11778
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2015-04-21 17:03:12 +0100

    utils: Use pyop2 cached_property decorator

--- firedrake/utils.py
+++ firedrake/utils.py
@@ -1,5 +1,6 @@
 # Some generic python utilities not really specific to our work.
 from decorator import decorator
+from pyop2.utils import cached_property
 
 
 # after https://micheles.googlecode.com/hg/decorator/documentation.html and
@@ -22,26 +23,6 @@ def _memoize(func, obj, *args, **kw):
 
 def memoize(f):
     return decorator(_memoize, f)
-
-
-# from http://www.toofishes.net/blog/python-cached-property-decorator/
-class cached_property(object):
-
-    '''A read-only @property that is only evaluated once. The value is cached
-    on the object itself rather than the function or class; this should prevent
-    memory leakage.'''
-
-    def __init__(self, fget, doc=None):
-        self.fget = fget
-        self.__doc__ = doc or fget.__doc__
-        self.__name__ = fget.__name__
-        self.__module__ = fget.__module__
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        obj.__dict__[self.__name__] = result = self.fget(obj)
-        return result
 
 
 _current_uid = 0
