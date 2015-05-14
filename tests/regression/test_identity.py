5db069ee0972b53cd1cc6df57b8bba56ad284342
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2015-05-14 22:04:56 +0100

    Another identity test, this time involving a non-standard tensor shape.

--- tests/regression/test_identity.py
+++ tests/regression/test_identity.py
@@ -53,6 +53,20 @@ def tensor_identity(family, degree):
     return norm(assemble(f - out))
 
 
+def tensor_identity_nonstandard_shape(family, degree):
+    mesh = UnitSquareMesh(2, 2)
+    fs = TensorFunctionSpace(mesh, family, degree, shape=(2, 3))
+    f = Function(fs)
+    out = Function(fs)
+    u = TrialFunction(fs)
+    v = TestFunction(fs)
+
+    f.interpolate(Expression([("x[0]", "x[1]", "x[0]*x[1]"), ("x[0]", "x[1]", "x[0]*x[1]")]))
+    solve(inner(u, v)*dx == inner(f, v)*dx, out)
+
+    return norm(assemble(f - out))
+
+
 def run_test():
     family = "Lagrange"
     degree = range(1, 5)
@@ -71,6 +85,12 @@ def run_tensor_test():
     return np.array([tensor_identity(family, d) for d in degree])
 
 
+def run_tensor_test_nonstandard_shape():
+    family = "Lagrange"
+    degree = range(1, 5)
+    return np.array([tensor_identity_nonstandard_shape(family, d) for d in degree])
+
+
 def test_identity():
     assert (run_test() < 1e-6).all()
 
@@ -81,6 +101,10 @@ def test_vector_identity():
 
 def test_tensor_identity():
     assert (run_tensor_test() < 1e-6).all()
+
+
+def test_tensor_identity_nonstandard_shape():
+    assert (run_tensor_test_nonstandard_shape() < 1e-6).all()
 
 
 @pytest.mark.parallel
