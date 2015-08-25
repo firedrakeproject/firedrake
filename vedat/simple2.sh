26ec7e2f85eef2a813c13b213acb6795d32d87c5
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-08-25 14:03:32 +0100

    strip legacy scripts

--- vedat/simple2.sh
+++ vedat/simple2.sh
@@ -1,10 +1,3 @@
-python generate.py
-make clean
-make 
-#cd ${HOME}/Documents/diderot-git/examples/simple_d2s
-#make clean
-#make 
-#cd /Users/chariseechiw/firedrake/vedat
-python simple2.py
+py.test -v tests/regression/test_visualise.py
 unu quantize -b 8 -i x.nrrd -o tmpsine.png
 eog tmpsine.png
