5ff24d6587176890653edddff6637de80cac3a26
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-08-25 14:28:57 +0100

    move diderot binaries out of firedrake/

--- firedrake/visualise.py
+++ firedrake/visualise.py
@@ -8,8 +8,8 @@ __all__ = ['simple_d2s']
 
 def simple_d2s(f, size_u, size_v, steps):
     p_cf = cFunction(f)
-    init_file = path.join(path.dirname(__file__), 'simple_d2s_init.o')
-    diderot_file = path.join(path.dirname(__file__), 'simple_d2s.o')
+    init_file = path.join(path.dirname(__file__), '../diderot/simple_d2s_init.o')
+    diderot_file = path.join(path.dirname(__file__), '../diderot/simple_d2s.o')
     call = make_c_evaluate(f, "callDiderot2_step", ldargs=[init_file, diderot_file, "-lteem"])
 
     return call(p_cf, size_u, size_v, ctypes.c_float(steps))
