2be86b4d9a0826efa6d411d87f314e1b8afa5ebe
Colin Cotter <colin.cotter@imperial.ac.uk>
2015-09-03 10:42:01 +0100

    A test for Taylor DG.

---
+++ tests/regression/test_taylor.py
@@ -0,0 +1,20 @@
+import pytest
+from firedrake import *
+import numpy
+
+#Test the 1D Taylor DG elements from FIAT
+
+def test_Taylor():
+    mesh = UnitIntervalMesh(10)
+    VT = FunctionSpace(mesh,"TDG",2)
+    V1 = FunctionSpace(mesh,"DG",2)
+    V0 = FunctionSpace(mesh,"DG",0)
+    v = Function(V1).interpolate(Expression("sin(2*pi*x[0])"))
+    v0 = Function(V0).project(v)
+    vt = Function(VT).project(v)
+    
+    assert(numpy.abs(v0.dat.data-vt.dat.data[::3]).max()<1.0e-10)
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
