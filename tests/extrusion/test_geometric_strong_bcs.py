1b86b8a0dda53828d7deff897e813598a072fc88
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2015-10-06 11:08:49 +0100

    Test geometric bcs on extruded meshes for DG advection

---
+++ tests/extrusion/test_geometric_strong_bcs.py
@@ -0,0 +1,40 @@
+import pytest
+from firedrake import *
+
+
+@pytest.mark.parametrize(('degree'), range(4))
+@pytest.mark.parametrize('direction', ['left', 'right', 'up', 'down'])
+def test_dg_advection(degree, direction):
+    m = UnitIntervalMesh(10)
+    m = ExtrudedMesh(m, layers=10)
+    V = FunctionSpace(m, "DG", degree)
+    t = Function(V)
+    v = TestFunction(V)
+
+    d = {'left': ((1, 0), 1),
+         'right': ((-1, 0), 2),
+         'up': ((0, 1), 'bottom'),
+         'down': ((0, -1), 'top')}
+
+    vec, bc_domain = d[direction]
+    u = Constant(vec)
+
+    def upw(t, un):
+        return t('+')*un('+') - t('-')*un('-')
+
+    n = FacetNormal(m)
+
+    un = 0.5 * (dot(u, n) + abs(dot(u, n)))
+
+    F = -dot(grad(v), u) * t * dx + dot(jump(v), upw(t, un)) * (dS_v + dS_h) + dot(v, un*t) * (ds_tb + ds_v)
+
+    bc = DirichletBC(V, 1., bc_domain, method="geometric")
+
+    solve(F == 0, t, bcs=bc)
+
+    assert errornorm(Constant(1.0), t) < 1.e-12
+
+
+if __name__ == "__main__":
+    import os
+    pytest.main(os.path.abspath(__file__))
