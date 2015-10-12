07c22951c1d1e23b060f1a1a30a835a6505238b6
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-10-12 18:54:38 +0100

    tiling: Append maps describing mesh topology

--- firedrake/optimizer.py
+++ firedrake/optimizer.py
@@ -13,9 +13,18 @@ def slope(mesh, debug=False):
     except ImportError:
         return
 
+    coords = mesh.coordinates.dat
+
     # Add coordinates
     if debug:
-        coords = mesh.coordinates.dat
         slope_python.set_debug_mode('VERY_LOW', (coords.dataset.set.name,
                                                  coords.data_ro,
                                                  coords.shape[1]))
+
+    # Add available maps describing the mesh topology
+    # 1) cells to nodes map
+    slope_maps = []
+    maps = [mesh.coordinates.cell_node_map(),
+            mesh.coordinates.interior_facet_node_map()]
+    slope_maps = [(m.name, m.iterset.name, m.toset.name, m.values) for m in maps]
+    slope_python.set_mesh_maps(slope_maps)
