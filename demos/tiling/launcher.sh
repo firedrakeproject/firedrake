524cb699261341c61b7aac6c5d821043ac339169
Fabio Luporini <f.luporini12@imperial.ac.uk>
2015-10-12 09:51:42 +0100

    tiling: Add non-tiled tests to launcher

--- demos/tiling/launcher.sh
+++ demos/tiling/launcher.sh
@@ -5,11 +5,14 @@ export KMP_AFFINITY=scatter
 
 for b in "SEQUENTIAL" "OMP"
 do
+    export SLOPE_BACKEND=$b
     for m in "(300.0,150.0,2.5)" "(600.0,300.0,2.5)"
     do
+        # Non-tiled tests
+        python wave_elastic.py --output 20 --mesh-size $m --num-unroll 0
+        # Tiled tests
         for ts in 100 200 300 400 500 600 700 800 1000 1500
         do
-            export SLOPE_BACKEND=$b
             python wave_elastic.py --output 20 --mesh-size $m --tile-size $ts
         done
     done
