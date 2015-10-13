95e4ebe4ea7851b22f3b43db4796a23b55d3f5cb
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-10-13 13:39:14 +0100

    Revert "Merge pull request #564 from firedrakeproject/prefork-travis-fixes"
    This reverts commit 99f860df2f08f9cc712089a1a22a096edd434f03, reversing
    changes made to 6039437ec49d8f7763e5fcbd2ce04d28429c97ac.
    
    Conflicts:
    	firedrake/__init__.py

--- firedrake/__init__.py
+++ firedrake/__init__.py
@@ -1,9 +1,6 @@
 # Must happen first, to ensure prefork server is up and running.
 from __future__ import absolute_import
-from pyop2_utils import enable_mpi_prefork
-enable_mpi_prefork()
-del enable_mpi_prefork
-
+from pyop2 import op2                                           # noqa
 # Ensure petsc is initialised by us before anything else gets in there.
 import firedrake.petsc as petsc
 del petsc
@@ -31,7 +28,6 @@ from ufl import *
 from pyop2.logger import set_log_level, info_red, info_green, info_blue, log  # noqa
 from pyop2.logger import debug, info, warning, error, critical  # noqa
 from pyop2.logger import DEBUG, INFO, WARNING, ERROR, CRITICAL  # noqa
-from pyop2 import op2                                           # noqa
 
 from firedrake.assemble import *
 from firedrake.bcs import *
