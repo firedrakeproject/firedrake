7ac228e9aa86bcd576969d03c6a60793061b09ad
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-10-15 16:59:24 +0100

    test case: point in cell query extruded

--- tests/extrusion/test_point_eval_cells_extrusion.py
+++ tests/extrusion/test_point_eval_cells_extrusion.py
@@ -3,17 +3,19 @@ import numpy as np
 import pytest
 
 from firedrake import *
+from tests.common import longtest
 
 cwd = abspath(dirname(__file__))
 
 
 @pytest.fixture(params=[False, True])
-def mesh1d(request):
+def mesh2d(request):
     periodic = request.param
     if periodic:
-        return PeriodicUnitIntervalMesh(16)
+        m = PeriodicUnitIntervalMesh(16)
     else:
-        return UnitIntervalMesh(16)
+        m = UnitIntervalMesh(16)
+    return ExtrudedMesh(m, 12)
 
 
 @pytest.fixture(params=[('cg', False),
@@ -21,41 +23,32 @@ def mesh1d(request):
                         pytest.mark.xfail(('dg', False)),
                         pytest.mark.xfail(('dg', True)),
                         # TODO: generate mesh from .geo file
-                        ('file', '../t11_tria.msh'),
-                        ('file', '../t11_quad.msh')])
-def mesh2d(request):
+                        longtest(('file', '../t11_tria.msh')),
+                        longtest(('file', '../t11_quad.msh'))])
+def mesh3d(request):
     if request.param[0] == 'cg':
-        return UnitSquareMesh(12, 12, quadrilateral=request.param[1])
+        m = UnitSquareMesh(12, 12, quadrilateral=request.param[1])
     elif request.param[0] == 'dg':
-        return PeriodicUnitSquareMesh(12, 12, quadrilateral=request.param[1])
+        m = PeriodicUnitSquareMesh(12, 12, quadrilateral=request.param[1])
     elif request.param[0] == 'file':
         meshfile = join(cwd, request.param[1])
-        return Mesh(meshfile)
+        m = Mesh(meshfile)
+    return ExtrudedMesh(m, 12)
 
 
 @pytest.fixture
-def mesh3d():
-    return UnitCubeMesh(8, 8, 8)
-
-
-@pytest.fixture
-def circle_mesh():
-    return CircleManifoldMesh(16)
+def cylinder_mesh():
+    m = CircleManifoldMesh(16)
+    return ExtrudedMesh(m, 12)
 
 
 @pytest.fixture(params=['triangle', 'quadrilateral'])
-def sphere_mesh(request):
+def spherical_shell_mesh(request):
     if request.param == 'triangle':
-        return UnitIcosahedralSphereMesh(3)
+        m = UnitIcosahedralSphereMesh(3)
     elif request.param == 'quadrilateral':
-        return UnitCubedSphereMesh(4)
-
-
-@pytest.fixture
-def func1d(mesh1d):
-    V = FunctionSpace(mesh1d, "CG", 2)
-    f = Function(V).interpolate(Expression("x[0]*(1.0 - x[0])"))
-    return f
+        m = UnitCubedSphereMesh(4)
+    return ExtrudedMesh(m, 12, layer_height=1.0/12, extrusion_type='radial')
 
 
 @pytest.fixture
@@ -72,12 +65,6 @@ def func3d(mesh3d):
     return f
 
 
-def test_1d(func1d):
-    assert np.allclose(0.09, func1d([0.1]))
-    assert np.allclose(0.25, func1d([0.5]))
-    assert np.allclose(0.24, func1d([0.6]))
-
-
 def test_2d(func2d):
     assert np.allclose(0.0000, func2d([0.10, 0.20]))
     assert np.allclose(0.4794, func2d([0.98, 0.94]))
@@ -91,19 +78,18 @@ def test_3d(func3d):
 
 
 @pytest.mark.xfail(run=False)
-def test_circle(circle_mesh):
-    f = func2d(circle_mesh)
-    assert np.allclose(+0.2500, f([0.70710678118, +0.70710678118]))
-    assert np.allclose(-0.5000, f([0.00000000000, -1.00000000000]))
-    assert np.allclose(-0.4352, f([0.36000000000, -0.64000000000]))
+def test_cylinder(cylinder_mesh):
+    f = func3d(cylinder_mesh)
+    assert np.allclose(0.00, f([0.70710678118, +0.70710678118, 0.0]))
+    assert np.allclose(0.25, f([0.00000000000, -1.00000000000, 0.5]))
+    assert np.allclose(0.68, f([0.36000000000, -0.64000000000, 1.0]))
 
 
-@pytest.mark.xfail(run=False)
-def test_sphere(sphere_mesh):
-    f = func3d(sphere_mesh)
-    assert np.allclose(+0.1666666666, f([+0.57735026919, 0.57735026919, +0.57735026919]))
-    assert np.allclose(-0.4870627924, f([-0.40000000000, 0.59160797831, +0.70000000000]))
-    assert np.allclose(+0.2304000000, f([-0.36000000000, 0.00000000000, -0.64000000000]))
+def test_spherical_shell(spherical_shell_mesh):
+    f = func3d(spherical_shell_mesh)
+    assert np.allclose(+0.2400000000, f([+0.69282032302, 0.69282032302, +0.69282032302]))
+    assert np.allclose(-1.5780834474, f([-0.72000000000, 1.06489436096, +1.26000000000]))
+    assert np.allclose(+0.5184000000, f([-0.54000000000, 0.00000000000, -0.96000000000]))
 
 
 if __name__ == '__main__':
