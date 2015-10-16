c4f883d65357edbaf69612cc9bd62443dfca91d9
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-10-16 11:31:24 +0100

    split spatialindex.pyx

--- firedrake/spatialindex.pyx
+++ firedrake/spatialindex.pyx
@@ -1,50 +1,8 @@
-from libc.stdint cimport uint8_t, uint32_t, int64_t, uintptr_t
-from libcpp.string cimport string
-
-ctypedef uint8_t byte
-ctypedef int64_t id_type
-
-cdef extern from "spatialindex/SpatialIndex.h" namespace "SpatialIndex":
-     cdef cppclass IStorageManager
-
-     cdef cppclass IShape:
-         pass
-
-     cdef cppclass Region(IShape):
-         Region() except +
-         Region(double *pLow, double *pHigh, uint32_t dimension) except +
-
-     cdef cppclass ISpatialIndex:
-         void insertData(uint32_t len, byte *pData, IShape& shape, id_type identifier)
-
-
-cdef extern from "spatialindex/SpatialIndex.h" namespace "SpatialIndex::StorageManager":
-     cdef IStorageManager *createNewMemoryStorageManager() except +
-
-
-cdef extern from "spatialindex/SpatialIndex.h" namespace "SpatialIndex::RTree":
-     cdef ISpatialIndex *returnRTree(IStorageManager&, PropertySet&) except +
-
-
-cdef extern from "spatialindex/SpatialIndex.h" namespace "Tools":
-     cdef enum VariantType:
-         VT_ULONG
-
-     union VariantValue:
-         uint32_t ulVal
-
-     cdef cppclass Variant:
-         Variant() except +
-
-         VariantType m_varType
-         VariantValue m_val
-
-     cdef cppclass PropertySet:
-         PropertySet() except +
-         void setProperty(string, Variant&)
-
-
+cimport numpy as np
 import ctypes
+from libc.stdint cimport uintptr_t
+
+include "spatialindex.pxi"
 
 cdef class SpatialIndex(object):
     cdef IStorageManager *storage_manager
@@ -73,9 +31,6 @@ cdef class SpatialIndex(object):
     def ctypes(self):
         return ctypes.c_void_p(<uintptr_t> self.spatial_index)
 
-
-# import numpy as np
-cimport numpy as np
 
 def from_regions(np.ndarray[np.float64_t, ndim=2, mode="c"] regions_lo,
                  np.ndarray[np.float64_t, ndim=2, mode="c"] regions_hi):
