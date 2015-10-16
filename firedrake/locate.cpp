94668f4d2d149afc61486e035ed575fae566ba73
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-10-16 11:16:16 +0100

    refactor locate.cpp

--- firedrake/locate.cpp
+++ firedrake/locate.cpp
@@ -18,7 +18,8 @@ struct SearchResult {
 
 class Visitor : public IVisitor {
 public:
-	Visitor(struct Function *f, double *x, inside_predicate p, void *data) : m_f(f), m_x(x), m_p(p), m_data(data) {}
+	Visitor(struct Function *f, double *x, inside_predicate p, void *data)
+		: m_f(f), m_x(x), m_p(p), m_data(data) {}
 
 	virtual void visitNode(const INode&);
 	virtual void visitData(const IData&);
@@ -40,6 +41,7 @@ void Visitor::visitData(const IData& in)
 {
 	int cell = in.getIdentifier();
 	if ((*m_p)(m_data, m_f, cell, m_x)) {
+		// Do not use exceptions for flow control
 		throw SearchResult(cell);
 	}
 }
@@ -58,23 +60,22 @@ extern "C" int locate_cell(struct Function *f,
 			   void *data_)
 {
 	if (f->sidx) {
-		SpatialIndex::ISpatialIndex *spatial_index = reinterpret_cast<SpatialIndex::ISpatialIndex *>(f->sidx);
+		SpatialIndex::ISpatialIndex *spatial_index =
+			reinterpret_cast<SpatialIndex::ISpatialIndex *>(f->sidx);
 
 		Visitor visitor(f, x, try_candidate, data_);
 		Point point(x, dim);
 
+		// Do not use exceptions for flow control
 		try {
 			spatial_index->pointLocationQuery(point, visitor);
 		} catch (const SearchResult& ex) {
 			return ex.cell;
 		}
 	} else {
-		int c;
-		for (c = 0; c < f->n_cols * f->n_layers; c++) {
-			if ((*try_candidate)(data_, f, c, x)) {
+		for (int c = 0; c < f->n_cols * f->n_layers; c++)
+			if ((*try_candidate)(data_, f, c, x))
 				return c;
-			}
-		}
 	}
 	return -1;
 }
