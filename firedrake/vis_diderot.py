61c6a407fd7f94f348a9f460e4ef482aa0f2124f
Charisee Chiw <charisee.chiw@gmail.com>
2015-11-11 01:06:38 -0600

    2-d scalar files

--- firedrake/vis_diderot.py
+++ firedrake/vis_diderot.py
@@ -32,71 +32,30 @@ __all__ = ['simple_d2s']
 #           i.e. [resU,resV]*stepSize
 ######################################## simple #########################################################
 
-# file         - fem/examples/d2s/simple_lerp
+# file         - fem/basic/basic_d2s
 # calls        - callDiderot2_lerp()
 # field#k(2)[] - 2d scalar field
-def simple2_lerp(name,f, resU, resV, physicalx0,physicalxn,physicaly0,physicalyn,type):
+def basic_d2s_lerp(name,f, resU, resV, physicalx0,physicalxn,physicaly0,physicalyn,type):
     
     p_cf = cFunction(f)
-    init_file = path.join(path.dirname(__file__), '../diderot/simple_lerp_init.o')
-    diderot_file = path.join(path.dirname(__file__), '../diderot/simple_lerp.o')
-    call = make_c_evaluate(f, "callDiderot2_lerp", ldargs=[init_file, diderot_file, "-lteem"])
+    init_file = path.join(path.dirname(__file__), '../diderot/store/basic_d2s_lerp_init.o')
+    diderot_file = path.join(path.dirname(__file__), '../diderot/store/basic_d2s_lerp.o')
+    call = make_c_evaluate(f, "callDiderot", ldargs=[init_file, diderot_file, "-lteem"])
     
     return call(ctypes.c_char_p(name),type,p_cf, resU, resV, physicalx0,physicalxn,physicaly0,physicalyn)
+
 
 # file          - fem/examples/d2s/simple_sample
 # calls         - callDiderot2_step()
 # field#k(2)[]  - 2d scalar field
-def simple2_sample(name,f, resU, resV, stepSize,type):
+def basic_d2s_sample(name,f, resU, resV, stepSize,type):
     p_cf = cFunction(f)
-    init_file = path.join(path.dirname(__file__), '../diderot/simple_sample_init.o')
-    diderot_file = path.join(path.dirname(__file__), '../diderot/simple_sample.o')
-    call = make_c_evaluate(f, "callDiderot2_step", ldargs=[init_file, diderot_file, "-lteem"])
+    init_file = path.join(path.dirname(__file__), '../diderot/store/basic_d2s_sample_init.o')
+    diderot_file = path.join(path.dirname(__file__),'../diderot/store/basic_d2s_sample.o')
+    call = make_c_evaluate(f, "callDiderot", ldargs=[init_file, diderot_file, "-lteem"])
     
     return call(ctypes.c_char_p(name),type,p_cf, resU, resV, ctypes.c_float(stepSize))
 
-# file         - fem/examples/d3s/simple
-# calls        - callDiderot3()
-# field#k(3)[] - 3d scalar field
-def simple3(name,f, resU, resV,resW, physicalx0,physicalxn,physicaly0,physicalyn,physicalz0,physicalzn ):
-    p_cf = cFunction(f)
-    init_file = path.join(path.dirname(__file__), '../diderot/simple_init.o')
-    diderot_file = path.join(path.dirname(__file__), '../diderot/simple.o')
-    call = make_c_evaluate(f, "callDiderot3", ldargs=[init_file, diderot_file, "-lteem"])
-    
-    return call(ctypes.c_char_p(name),p_cf, resU, resV, resW,physicalx0,physicalxn,physicaly0,physicalyn,physicalz0,physicalzn)
 
-
-
-########################################## iso ##########################################################
-# Note- Look at .diderot file and rewrite
-# file          - fem/examples/d2s/iso_sample
-# call          - callDiderot2_iso
-# field#k(2)[]  - 2d scalar field
-# stepsMax:int  - max number of steps
-# isovalue:float- isovalue
-def iso_sample(name,f, resU, resV, stepSize,stepsMax,isovalue,epsilon):
-    p_cf = cFunction(f)
-    init_file = path.join(path.dirname(__file__), '../diderot/iso_sample_init.o')
-    diderot_file = path.join(path.dirname(__file__), '../diderot/iso_sample.o')
-    call = make_c_evaluate(f, "callDiderot2_iso", ldargs=[init_file, diderot_file, "-lteem"])
-    
-    return call(ctypes.c_char_p(name),p_cf, resU, resV, ctypes.c_float(stepSize),stepsMax,ctypes.c_float(isovalue),ctypes.c_float(epsilon))
-
-
-
-########################################## Mips ##########################################################
-# Maximum Intensity Projection
-# file          - fem/examples/d3s/mip
-# call          - callDiderot3_noCam()
-# field#k(3)[]  - 3d scalar field
-# steps:float   - rayStep
-def mip(name,f, resU, resV, steps):
-    p_cf = cFunction(f)
-    init_file = path.join(path.dirname(__file__), '../diderot/mip_init.o')
-    diderot_file = path.join(path.dirname(__file__), '../diderot/mip.o')
-    call = make_c_evaluate(f, "callDiderot3_noCam", ldargs=[init_file, diderot_file, "-lteem"])
-    
-    return call(ctypes.c_char_p(name),p_cf, resU, resV, ctypes.c_float(steps))
 
 
