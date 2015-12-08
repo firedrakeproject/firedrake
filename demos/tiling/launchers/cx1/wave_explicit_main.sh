5bac56d083f5159df60e30204acb7c96625cd20c
Fabio Luporini <fl1612@imperial.ac.uk>
2015-12-08 14:40:34 +0000

    cx1: Fix launcher scripts

--- demos/tiling/launchers/cx1/wave_explicit_main.sh
+++ demos/tiling/launchers/cx1/wave_explicit_main.sh
@@ -1,9 +1,14 @@
 LAUNCHERS=$FIREDRAKE_DIR/demos/tiling/launchers/cx1
 
-qsub -v NNODES=1 $LAUNCHERS/launcher_wave_explicit.sh
-qsub -v NNODES=2 $LAUNCHERS/launcher_wave_explicit.sh
-qsub -v NNODES=4 $LAUNCHERS/launcher_wave_explicit.sh
-qsub -v NNODES=8 $LAUNCHERS/launcher_wave_explicit.sh
-qsub -v NNODES=12 $LAUNCHERS/launcher_wave_explicit.sh
-qsub -v NNODES=16 $LAUNCHERS/launcher_wave_explicit.sh
-qsub -v NNODES=20 $LAUNCHERS/launcher_wave_explicit.sh
+if [ "$1" == "singlenode" ]
+then
+    echo "Executing single node experiments"
+    qsub -l walltime=10:00:00 -l select=1:ncpus=20:mem=16gb:icib=true -q pqcdt $LAUNCHERS/wave_explicit.pbs
+else
+    echo "Executing multi node experiments..."
+    for i in 1 2 4 8 12 16 20
+    do
+        echo "nodes="$i"..."
+        qsub -l walltime=10:00:00 -l select=$i:ncpus=20:mem=16gb:icib=true -q pqcdt $LAUNCHERS/wave_explicit.pbs
+    done
+fi
