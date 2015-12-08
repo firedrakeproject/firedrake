634d0b1057c38bac147fc3ab999b1bc578da2617
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-12-08 14:55:45 +0000

    test 1-forms and 2-forms as well

--- tests/regression/test_subdomain.py
+++ tests/regression/test_subdomain.py
@@ -4,7 +4,7 @@ import pytest
 from firedrake import *
 
 
-def test_box_1d():
+def test_box_1d_0form():
     mesh = UnitIntervalMesh(4)
     x = mesh.coordinates
     f = Function(FunctionSpace(mesh, 'CG', 1))
@@ -19,6 +19,40 @@ def test_box_1d():
 
     sd = make_subdomain_data(x[0] > 0.5)
     assert np.allclose(0.375, assemble(f*dx(subdomain_data=sd)))
+
+
+def test_box_1d_1form():
+    mesh = UnitIntervalMesh(4)
+    x = mesh.coordinates
+    v = TestFunction(FunctionSpace(mesh, 'CG', 1))
+
+    whole = assemble(v*dx).dat.data
+
+    sd = make_subdomain_data(x[0] < 0.5)
+    half_1 = assemble(v*dx(subdomain_data=sd)).dat.data
+
+    sd = make_subdomain_data(x[0] > 0.5)
+    half_2 = assemble(v*dx(subdomain_data=sd)).dat.data
+
+    assert np.allclose(whole, half_1 + half_2)
+
+
+def test_box_1d_2form():
+    mesh = UnitIntervalMesh(4)
+    x = mesh.coordinates
+    V = FunctionSpace(mesh, 'CG', 1)
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    whole = assemble(u*v*dx).M.values
+
+    sd = make_subdomain_data(x[0] < 0.5)
+    half_1 = assemble(u*v*dx(subdomain_data=sd)).M.values
+
+    sd = make_subdomain_data(x[0] > 0.5)
+    half_2 = assemble(u*v*dx(subdomain_data=sd)).M.values
+
+    assert np.allclose(whole, half_1 + half_2)
 
 
 if __name__ == '__main__':
