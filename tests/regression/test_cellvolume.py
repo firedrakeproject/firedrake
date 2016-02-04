d967cb364af17054a99ff1cf388eac11da954705
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-02-04 15:12:22 +0000

    Add some tests for claimed fixes

---
+++ tests/regression/test_cellvolume.py
@@ -0,0 +1,42 @@
+import pytest
+import numpy as np
+from firedrake import *
+
+
+@pytest.fixture(params=["interval",
+                        "triangle",
+                        pytest.mark.xfail(reason="CellVolume transformation not in UFL")("quadrilateral"),
+                        "tetrahedron"])
+def cell(request):
+    return request.param
+
+
+@pytest.fixture
+def mesh(cell):
+    if cell == "interval":
+        return UnitIntervalMesh(1)
+    if cell == "triangle":
+        return UnitTriangleMesh()
+    if cell == "quadrilateral":
+        return UnitSquareMesh(1, 1, quadrilateral=True)
+    if cell == "tetrahedron":
+        return UnitTetrahedronMesh()
+
+
+@pytest.fixture
+def expect(cell):
+    return {"interval": 1.0,
+            "triangle": 1.0/2.0,
+            "quadrilateral": 1.0,
+            "tetrahedron": 1.0/6.0}[cell]
+
+
+@pytest.mark.parametrize("exponent",
+                         [1, 0.5])
+def test_cell_volume(exponent, mesh, expect):
+    assert np.allclose(assemble((CellVolume(mesh)**exponent)*dx), expect**(exponent + 1))
+
+
+if __name__ == "__main__":
+    import os
+    pytest.main(os.path.abspath(__file__))
---
+++ tests/regression/test_manual_quadrature.py
@@ -0,0 +1,26 @@
+import pytest
+import numpy as np
+from firedrake import *
+
+
+def test_manual_quadrature():
+    mesh = UnitSquareMesh(4, 4)
+    V = FunctionSpace(mesh, "CG", 3)
+    f = Function(V).interpolate(Expression("pow(x[0], 3)"))
+
+    f1 = f*dx(degree=3)
+    f2 = f*dx(degree=1)
+    f3 = f*dx
+
+    assert np.allclose(assemble(f1), assemble(f3))
+
+    assert np.allclose(assemble(f1), 0.25)
+
+    assert np.allclose(assemble(f2), 0.244791666666)
+
+    assert np.allclose(assemble(f1) + assemble(f2), assemble(f1 + f2))
+
+
+if __name__ == "__main__":
+    import os
+    pytest.main(os.path.abspath(__file__))
