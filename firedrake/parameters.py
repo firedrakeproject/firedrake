e923be584b22a4e9d2af2102fd333ae5a396abc1
Fabio Luporini <f.luporini12@imperial.ac.uk>
2016-02-08 21:21:39 +0000

    DROP ME: default to coffee O3

--- firedrake/parameters.py
+++ firedrake/parameters.py
@@ -52,7 +52,7 @@ parameters.add(Parameters("assembly_cache",
                           max_misses=3))
 
 parameters.add(Parameters("coffee",
-                          O2=True))
+                          O3=True))
 
 # Default to the values of PyOP2 configuration dictionary
 pyop2_opts = Parameters("pyop2_options",
