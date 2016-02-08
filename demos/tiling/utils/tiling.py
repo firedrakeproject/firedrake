15ad3fb0ebfc31deb31e908d67afad3bb5797fd5
Fabio Luporini <f.luporini12@imperial.ac.uk>
2016-02-08 21:07:45 +0000

    tiling: Change automatic computation of sdepth

--- demos/tiling/utils/tiling.py
+++ demos/tiling/utils/tiling.py
@@ -1,3 +1,5 @@
+import math
+
 from pyop2.mpi import MPI
 
 
@@ -12,7 +14,7 @@ def calculate_sdepth(num_solves, num_unroll, extra_halo):
     :arg num_unroll: unroll factor for the loop chain
     :arg extra_halo: to expose the nonexec region to the tiling engine
     """
-    if MPI.parallel:
-        return 1 + num_solves*num_unroll + extra_halo
+    if MPI.parallel and num_unroll > 0:
+        return (int(math.ceil(num_solves/2.0)) or 1) + extra_halo
     else:
         return 1
