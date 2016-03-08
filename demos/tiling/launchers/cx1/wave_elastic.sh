34ee5e3cf7b5dcba5cae4ab7339e897575b6b30c
Fabio Luporini <fl1612@imperial.ac.uk>
2016-03-08 12:37:50 +0000

    cx1: launcher: increase requested wall time

--- demos/tiling/launchers/cx1/wave_elastic.sh
+++ demos/tiling/launchers/cx1/wave_elastic.sh
@@ -3,12 +3,12 @@ LAUNCHERS=$FIREDRAKE_DIR/demos/tiling/launchers/cx1/pbs
 if [ "$1" == "singlenode" ]
 then
     echo "Executing single node experiments"
-    qsub -l walltime=24:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
+    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
 else
     echo "Executing multi node experiments..."
     for i in 1 2 4 8 12 16 20
     do
         echo "nodes="$i"..."
-        qsub -l walltime=24:00:00 -l select=$i:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
+        qsub -l walltime=72:00:00 -l select=$i:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
     done
 fi
