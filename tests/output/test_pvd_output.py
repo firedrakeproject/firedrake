dadc82971ef07877c5dff405ca43227f2acc9048
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-04-11 09:02:32 +0100

    Add test of parallel output

--- tests/output/test_pvd_output.py
+++ tests/output/test_pvd_output.py
@@ -23,6 +23,12 @@ def test_can_save_coordinates(mesh, pvd):
     pvd.write(mesh.coordinates)
 
 
+@pytest.mark.xfail(reason="Broken call to get_vtu_name")
+@pytest.mark.parallel
+def test_can_save_coordinates_parallel(mesh, pvd):
+    pvd.write(mesh.coordinates)
+
+
 @pytest.mark.parametrize("typ",
                          ["vector", "tensor", "tensor-3"])
 def test_bad_shape(typ, mesh, pvd):
