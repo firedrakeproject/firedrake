7e8dd2b0629c4c574929805186bcd86029846e13
David Ham <David.Ham@imperial.ac.uk>
2016-05-12 17:11:18 +0100

    Cause the __del__ method to always work

--- firedrake/variational_solver.py
+++ firedrake/variational_solver.py
@@ -160,7 +160,10 @@ class NonlinearVariationalSolver(object):
 
     @property
     def parameters(self):
-        return self._parameters
+        try:
+            return self._parameters
+        except AttributeError:
+            return {}
 
     @parameters.setter
     def parameters(self, val):
