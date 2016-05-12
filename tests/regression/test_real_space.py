d6606fa9b6f451151eae4beb637d5954b507fbdf
David Ham <David.Ham@imperial.ac.uk>
2016-05-12 14:09:53 +0100

    Assembly now working

--- tests/regression/test_real_space.py
+++ tests/regression/test_real_space.py
@@ -68,16 +68,32 @@ def test_real_mixed_one_form_assembly():
     np.testing.assert_almost_equal(A.dat.data[1],
                                    AA.dat.data)
 
-# def test_real_mixed_two_form_assembly():
-#     mesh = UnitIntervalMesh(3)
-#     rfs = FunctionSpace(mesh, "Real", 0)
-#     cgfs = FunctionSpace(mesh, "CG", 1)
+def test_real_mixed_two_form_assembly():
+    mesh = UnitIntervalMesh(3)
+    rfs = FunctionSpace(mesh, "Real", 0)
+    cgfs = FunctionSpace(mesh, "CG", 1)
 
-#     mfs = cgfs*rfs
-#     u, p = TrialFunctions(mfs)
-#     v, q = TestFunctions(mfs)
+    mfs = cgfs*rfs
+    u, p = TrialFunctions(mfs)
+    v, q = TestFunctions(mfs)
 
-#     m = assemble(u*v*dx + p*q*dx).M.handle.getPythonContext().data
+    m = assemble(u*v*dx + p*q*dx + u*q*dx + p*v*dx)
+
+    qq = TestFunction(rfs)
+    vv = TestFunction(cgfs)
+    uu = TrialFunction(cgfs)
+
+    m00 = assemble(uu*vv*dx)
+    np.testing.assert_almost_equal(m00.M.values,
+                                   m.M.blocks[0][0].values)
+    m01 = assemble(uu*qq*dx)
+    np.testing.assert_almost_equal(m01.M.values.T,
+                                   m.M.blocks[0][1].values)
+    np.testing.assert_almost_equal(m01.M.values,
+                                   m.M.blocks[1][0].values)
+    np.testing.assert_almost_equal(np.array([[1.]]),
+                                   m.M.blocks[1][1].values)
+
 
 if __name__ == '__main__':
     import os
