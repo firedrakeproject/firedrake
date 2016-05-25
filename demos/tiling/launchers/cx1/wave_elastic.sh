f9bc72d427752561aa9376f1395fef25277e26fd
Fabio Luporini <fl1612@imperial.ac.uk>
2016-05-25 18:38:32 +0100

    cx1: Update launcher script

--- demos/tiling/launchers/cx1/wave_elastic.sh
+++ demos/tiling/launchers/cx1/wave_elastic.sh
@@ -3,9 +3,9 @@ LAUNCHERS=$FIREDRAKE_DIR/demos/tiling/launchers/cx1/pbs/wave_elastic/
 if [ "$1" == "singlenode" ]
 then
     echo "Executing single node experiments: Haswell"
-    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic_haswell.pbs
+    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
     echo "Executing single node experiments: Ivy Bridge"
-    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic_ivy.pbs
+    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic.pbs
 else
     echo "Executing multi node experiments..."
     for i in 1 2 4 8 12 16 20
