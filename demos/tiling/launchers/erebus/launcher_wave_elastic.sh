45fbc1aa2f2c74a45b101527afa31385cd9aa544
Fabio Luporini <f.luporini12@imperial.ac.uk>
2016-05-25 17:02:44 +0100

    tiling: Restructure benchmarking script

--- demos/tiling/launchers/erebus/launcher_wave_elastic.sh
+++ demos/tiling/launchers/erebus/launcher_wave_elastic.sh
@@ -2,33 +2,47 @@
 
 rm -rf /data/output/
 
-OPTS="--output 100 --flatten True"
-TILE_OPTS="--extra-halo 1 --fusion-mode only_tile"
+OPTS="--output 1000 --flatten True --nocache True"
+TILE_OPTS="--extra-halo 1 --fusion-mode only_tile --glb-maps True --coloring default"
 
 export OMP_NUM_THREADS=1
 export SLOPE_BACKEND=SEQUENTIAL
 
-#for poly in 1 2 3
-for poly in 2
+# Tile sizes for each poly order
+declare -a ts_p1=(150 230 310 400)
+declare -a ts_p2=(70 140 200 300)
+declare -a ts_p3=(40 70 100)
+declare -a ts_p4=(30 60 90)
+
+# Partition modes for each poly order
+declare -a part_p1=("chunk")
+declare -a part_p2=("chunk")
+declare -a part_p3=("chunk" "metis")
+declare -a part_p4=("metis")
+
+for poly in 1 2 3 4
 do
     output_file="output_p"$poly".txt"
+    rm -f $output_file
+    touch $output_file
     echo "Polynomial order "$poly
-    #for MESH in "--mesh-size (300.0,150.0,0.8)"
-    for MESH in "--mesh-file /data/meshes/domain_h07.msh --h 0.7"
+    for MESH in "--mesh-size (300.0,150.0,0.8)" "--mesh-file /data/meshes/domain_h08.msh --h 0.8"
     do
-        rm -f $output_file
-        touch $output_file
         echo "    Running "$MESH
         echo "        Untiled ..."
-        mpirun --bind-to-core -np 4 python wave_elastic.py --poly-order $poly $MESH $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
-        for p in "metis"
+        PROGRAM="python wave_elastic.py --poly-order $poly $MESH $OPTS --num-unroll 0"
+        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $MESH $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
+        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $MESH $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
+        part_p="part_p$poly[*]"
+        for p in ${!part_p}
         do
-            for sm in 4 8
+            for em in 2 3 4 5
             do
-                for ts in 40 70 150 300
+                ts_p="ts_p$poly[*]"
+                for ts in ${!ts_p}
                 do
-                    echo "        Tiled (pm="$p", ts="$ts") ..."
-                    mpirun --bind-to-core -np 4 python wave_elastic.py --poly-order $poly $MESH $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --split-mode $sm $TILE_OPTS 1>> $output_file 2>> $output_file
+                    echo "        Tiled (pm="$p", ts="$ts", em="$em") ..."
+                    mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $MESH $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --explicit-mode $em $TILE_OPT 1>> $output_file 2>> $output_file
                 done
             done
         done
