ab160a39caadb3908b1e4a70e4d984aafb4fed1d
David Ham <David.Ham@imperial.ac.uk>
2016-06-04 23:55:20 +0100

    Test real space solve

--- tests/regression/test_real_space.py
+++ tests/regression/test_real_space.py
@@ -96,6 +96,38 @@ def test_real_mixed_two_form_assembly():
                                    m.M.blocks[1][1].values)
 
 
+@pytest.mark.parallel
+def test_real_mixed_solve():
+    def poisson(resolution):
+        mesh = IntervalMesh(resolution, pi)
+        rfs = FunctionSpace(mesh, "Real", 0)
+        cgfs = FunctionSpace(mesh, "CG", 1)
+
+        mfs = cgfs*rfs
+
+        v, q = TestFunctions(mfs)
+
+        phi = Function(mfs)
+        f = Function(mfs)
+        x = SpatialCoordinate(mesh)
+
+        f0, _ = f.split()
+
+        f0.interpolate(cos(x[0]))
+
+        f0, _ = split(f)
+
+        phi_0, phi_1 = split(phi)
+
+        residual_form = (dot(grad(phi_0), grad(v)) + phi_1*v + phi_0*q - f0*v) * dx
+
+        solve(residual_form == 0, phi)
+
+        return sqrt(assemble((f - phi)**2*dx))
+
+    assert ln(poisson(50)/poisson(100))/ln(2) > 1.99
+
+
 if __name__ == '__main__':
     import os
     pytest.main(os.path.abspath(__file__))
