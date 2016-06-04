1f11e2129773dd4b9947fd604474e78b917ddec9
Fabio Luporini <fl1612@imperial.ac.uk>
2016-06-04 11:25:35 +0100

    cx1: Enhance launcher script

--- demos/tiling/launchers/cx1/wave_elastic.sh
+++ demos/tiling/launchers/cx1/wave_elastic.sh
@@ -2,12 +2,23 @@ LAUNCHERS=$FIREDRAKE_DIR/demos/tiling/launchers/cx1/pbs/wave_elastic/
 
 if [ "$1" == "singlenode" ]
 then
-    echo "Executing single node experiments: Haswell"
-    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
-    echo "Executing single node experiments: Ivy Bridge"
-    qsub -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic.pbs
+    echo "Executing single node experiments: Haswell (20 cores, pqcdt)"
+    qsub -v polys=1 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=2 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=3 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=4 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:icib=true -q pqcdt $LAUNCHERS/wave_elastic.pbs
+    echo "Executing single node experiments: Haswell (16 cores, defaultqueue)"
+    qsub -v polys=1 -l walltime=72:00:00 -l select=1:ncpus=16:mem=32gb:haswell=true $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=2 -l walltime=72:00:00 -l select=1:ncpus=16:mem=32gb:haswell=true $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=3 -l walltime=72:00:00 -l select=1:ncpus=16:mem=32gb:haswell=true $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=4 -l walltime=72:00:00 -l select=1:ncpus=16:mem=32gb:haswell=true $LAUNCHERS/wave_elastic.pbs
+    echo "Executing single node experiments: Ivy Bridge (20 cores, defaultqueue)"
+    qsub -v polys=1 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=2 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=3 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic.pbs
+    qsub -v polys=4 -l walltime=72:00:00 -l select=1:ncpus=20:mem=32gb:ivyb=true $LAUNCHERS/wave_elastic.pbs
 else
-    echo "Executing multi node experiments..."
+    echo "Executing multi node experiments: Haswell (with {1, 2, 4, 8, 12, 16, 20}x20 cores, pqcdt)"
     for i in 1 2 4 8 12 16 20
     do
         echo "nodes="$i"..."
