f67ce7e26fe26b5d7454dcf24d12996f5435d95f
Fabio Luporini <f.luporini12@imperial.ac.uk>
2016-06-06 12:26:29 +0100

    tiling: Changes to benchmark script

--- demos/tiling/launchers/erebus/launcher_wave_elastic.sh
+++ demos/tiling/launchers/erebus/launcher_wave_elastic.sh
@@ -2,29 +2,40 @@
 
 rm -rf /data/output/
 
-OPTS="--output 1000 --flatten True --nocache True"
-TILE_OPTS="--fusion-mode only_tile --coloring default --glb-maps True"
+OPTS="--output 5000 --flatten True --nocache True"
+TILE_OPTS="--fusion-mode only_tile --coloring default"
 
 export OMP_NUM_THREADS=1
 export SLOPE_BACKEND=SEQUENTIAL
 
+# The execution modes
+declare -a em_all=(1 2 3 4 5 6)
+
+# Extra options for each mode
+declare -a opts_em1=("" "--glb-maps True")
+declare -a opts_em2=("" "--glb-maps True")
+declare -a opts_em3=("" "--glb-maps True")
+declare -a opts_em4=("" "--extra-halo 1" "--glb-maps True" "--glb-maps True --extra-halo 1")
+declare -a opts_em5=("" "--extra-halo 1" "--glb-maps True" "--glb-maps True --extra-halo 1")
+declare -a opts_em6=("" "--extra-halo 1" "--glb-maps True" "--glb-maps True --extra-halo 1")
+
 # Tile sizes for each poly order
-declare -a ts_p1=(150 230 310 400)
+declare -a ts_p1=(140 250 320 400)
 declare -a ts_p2=(70 140 200 300)
-declare -a ts_p3=(40 70 100)
-declare -a ts_p4=(30 60 90)
+declare -a ts_p3=(45 60 75)
+declare -a ts_p4=(20 45 70)
 
 # Partition modes for each poly order
-declare -a part_p1=("chunk")
-declare -a part_p2=("chunk")
+declare -a part_p1=("chunk" "metis")
+declare -a part_p2=("chunk" "metis")
 declare -a part_p3=("chunk" "metis")
 declare -a part_p4=("chunk" "metis")
 
 # Meshes for each poly order
-declare -a mesh_p1=("--mesh-size (300.0,150.0,0.8)" "--mesh-file /data/meshes/domain_h08.msh --h 0.8")
-declare -a mesh_p2=("--mesh-size (300.0,150.0,0.8)" "--mesh-file /data/meshes/domain_h08.msh --h 0.8")
-declare -a mesh_p3=("--mesh-size (300.0,150.0,1.2)" "--mesh-file /data/meshes/domain_h125.msh --h 1.2")
-declare -a mesh_p4=("--mesh-size (300.0,150.0,1.2)" "--mesh-file /data/meshes/domain_h125.msh --h 1.2")
+declare -a mesh_p1=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.2)")
+declare -a mesh_p2=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.2)")
+declare -a mesh_p3=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.2)")
+declare -a mesh_p4=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.2)")
 
 for poly in 1 2 3 4
 do
@@ -32,7 +43,6 @@ do
     rm -f $output_file
     touch $output_file
     echo "Polynomial order "$poly
-
     mesh_p="mesh_p$poly[@]"
     meshes=( "${!mesh_p}" )
     for mesh in "${meshes[@]}"
@@ -41,16 +51,22 @@ do
         echo "        Untiled ..."
         mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
         mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
+        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
         part_p="part_p$poly[*]"
         for p in ${!part_p}
         do
-            for em in 2 3 4 5 6 7
+            for em in ${em_all[@]}
             do
-                ts_p="ts_p$poly[*]"
-                for ts in ${!ts_p}
+                opts="opts_em$em[@]"
+                opts_em=( "${!opts}" )
+                for opt in "${opts_em[@]}"
                 do
-                    echo "        Tiled (pm="$p", ts="$ts", em="$em") ..."
-                    mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --explicit-mode $em $TILE_OPTS 1>> $output_file 2>> $output_file
+                    ts_p="ts_p$poly[*]"
+                    for ts in ${!ts_p}
+                    do
+                        echo "        Tiled (pm="$p", ts="$ts", em="$em") ..."
+                        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --explicit-mode $em $TILE_OPTS $opt 1>> $output_file 2>> $output_file
+                    done
                 done
             done
         done
