7956a1a7cd137eacb926e84c7509d034586e0186
Fabio Luporini <f.luporini12@imperial.ac.uk>
2016-06-19 10:30:50 +0100

    tiling: Add log mode to launcher script

--- demos/tiling/launchers/erebus/launcher_wave_elastic.sh
+++ demos/tiling/launchers/erebus/launcher_wave_elastic.sh
@@ -4,6 +4,8 @@ rm -rf /data/output/
 
 OPTS="--output 5000 --flatten True --nocache True"
 TILE_OPTS="--fusion-mode only_tile --coloring default"
+LOG=""
+EXTRA_OUT=""
 
 export OMP_NUM_THREADS=1
 export SLOPE_BACKEND=SEQUENTIAL
@@ -12,12 +14,12 @@ export SLOPE_BACKEND=SEQUENTIAL
 declare -a em_all=(1 2 3 4 5 6)
 
 # Extra options for each mode
-declare -a opts_em1=("" "--glb-maps True")
-declare -a opts_em2=("" "--glb-maps True")
-declare -a opts_em3=("" "--glb-maps True")
-declare -a opts_em4=("" "--extra-halo 1" "--glb-maps True" "--glb-maps True --extra-halo 1")
-declare -a opts_em5=("" "--extra-halo 1" "--glb-maps True" "--glb-maps True --extra-halo 1")
-declare -a opts_em6=("" "--extra-halo 1" "--glb-maps True" "--glb-maps True --extra-halo 1")
+declare -a opts_em1=("--glb-maps True")
+declare -a opts_em2=("--glb-maps True")
+declare -a opts_em3=("--glb-maps True")
+declare -a opts_em4=("--glb-maps True" "--glb-maps True --extra-halo 1")
+declare -a opts_em5=("--glb-maps True" "--glb-maps True --extra-halo 1")
+declare -a opts_em6=("--glb-maps True" "--glb-maps True --extra-halo 1")
 
 # Tile sizes for each poly order
 declare -a ts_p1=(140 250 320 400)
@@ -37,7 +39,21 @@ declare -a mesh_p2=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.
 declare -a mesh_p3=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.2)")
 declare -a mesh_p4=("--mesh-size (300.0,150.0,1.0)" "--mesh-size (300.0,150.0,1.2)")
 
-for poly in 1 2 3 4
+# The polynomial orders tested
+declare -a polys=(1 2 3 4)
+
+# If only logging tiling stuff, tweak a few things to run only what is strictly necessary
+if [ "$1" == "onlylog" ]; then
+    declare -a polys=(2)
+    declare -a mesh_p2=("--mesh-size (300.0,150.0,1.2)")
+    declare -a part_p2=("chunk")
+    LOG="--log True --time_max 0.05"
+    EXTRA_OUT="(Just logging!)"
+    mkdir -p all-logs
+fi
+
+
+for poly in ${polys[@]}
 do
     output_file="output_p"$poly".txt"
     rm -f $output_file
@@ -48,10 +64,10 @@ do
     for mesh in "${meshes[@]}"
     do
         echo "    Running "$mesh
-        echo "        Untiled ..."
-        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
-        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
-        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 1>> $output_file 2>> $output_file
+        echo "        Untiled ..."$EXTRA_OUT
+        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 $LOG 1>> $output_file 2>> $output_file
+        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 $LOG 1>> $output_file 2>> $output_file
+        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 0 $LOG 1>> $output_file 2>> $output_file
         part_p="part_p$poly[*]"
         for p in ${!part_p}
         do
@@ -64,8 +80,15 @@ do
                     ts_p="ts_p$poly[*]"
                     for ts in ${!ts_p}
                     do
-                        echo "        Tiled (pm="$p", ts="$ts", em="$em") ..."
-                        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --explicit-mode $em $TILE_OPTS $opt 1>> $output_file 2>> $output_file
+                        echo "        Tiled (pm="$p", ts="$ts", em="$em") ..."$EXTRA_OUT
+                        mpirun -np 4 --bind-to-core python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --explicit-mode $em $TILE_OPTS $opt $LOG 1>> $output_file 2>> $output_file
+                        echo "        Tiled (pm="$p", ts="$ts", em="$em", hyperthreads) ..."$EXTRA_OUT
+                        mpirun -np 8 -H localhost -rf rankfile python wave_elastic.py --poly-order $poly $mesh $OPTS --num-unroll 1 --tile-size $ts --part-mode $p --explicit-mode $em $TILE_OPTS $opt $LOG 1>> $output_file 2>> $output_file
+                        if [ "$1" == "onlylog" ]; then
+                            logdir=log_p"$poly"_em"$em"_part"$part"_ts"$ts"
+                            mv log $logdir
+                            mv $logdir all-logs
+                        fi
                     done
                 done
             done
