c365369143f2119538878334368893a4a23924c8
Thomas Gibson <t.gibson15@imperial.ac.uk>
2016-07-15 19:07:23 +0100

    new trace test in tests/regression/
    compute trace error update

---
+++ tests/regression/test_trace_galerkin_projection.py
@@ -0,0 +1,89 @@
+"""
+The purpose of this test is to check that the trace elements
+are solving the Galerkin projection problem. We project a
+a scalar function f onto a finite element space and solve
+the Galerkin projection problem on the trace space:
+
+(gammar, lambdar)*ds + avg(gammar)*avg(lambdar)*dS
+= (f, gammar)*ds + avg(gammar)*avg(f)*dS for all gammar
+
+Two tests are performed:
+1. Checking the error in the trace approximation of f on the trace space
+(in the trace norm)
+2. Performing a convergence test. The expected order of convergence is
+k+(1/2), where k is the degree of the trace space.
+
+The right hand side function used in this test is:
+f = cos(x[0]*pi*2)*cos(x[1]*pi*2)
+"""
+
+import pytest
+import numpy as np
+from firedrake import *
+
+
+def trace_galerkin_projection(degree, conv_test_flag=0, mesh_res=None):
+    # Create mesh if needed
+    if mesh_res is None:
+        mesh = UnitSquareMesh(10, 10)
+    elif isinstance(mesh_res, int):
+        mesh = UnitSquareMesh(2 ** mesh_res, 2 ** mesh_res)
+    else:
+        raise ValueError("Integers or None are only accepted for mesh_res.")
+
+    # Define the Trace Space
+    T = FunctionSpace(mesh, "HDiv Trace", degree)
+
+    # Define trial and test functions
+    lambdar = TrialFunction(T)
+    gammar = TestFunction(T)
+
+    # Define right hand side function
+    if conv_test_flag == 0:
+        V = FunctionSpace(mesh, "CG", degree)
+        f = Function(V)
+    elif conv_test_flag == 1:
+        hdv = FunctionSpace(mesh, "CG", degree + 1)
+        f = Function(hdv)
+    else:
+        raise ValueError("conv_test should be either 0 or 1")
+    f.interpolate(Expression("cos(x[0]*pi*2)*cos(x[1]*pi*2)"))
+
+    # Construct bilinear form
+    a = lambdar*gammar*ds + avg(lambdar)*avg(gammar)*dS
+
+    # Construct linear form
+    l = f*gammar*ds + avg(f)*avg(gammar)*dS
+
+    # Compute the solution
+    t = Function(T)
+    solve(a == l, t, solver_parameters={'ksp_rtol': 1e-14})
+
+    # Compute error in trace norm
+    trace_error = sqrt(assemble(FacetArea(mesh)*(t - f)('+')*(t - f)('+')*dS))
+
+    return trace_error
+
+
+@pytest.mark.parametrize('degree', range(1, 4))
+def test_trace_galerkin_projection(degree):
+    """Tests the accuracy of the trace solution for the Galerkin
+    projection problem."""
+    tr_err = trace_galerkin_projection(degree, 0)
+    assert tr_err < 1e-13
+
+
+@pytest.mark.parametrize(('testdegree', 'convrate'),
+                         [(1, 1.5), (2, 2.5), (3, 3.5)])
+def test_convergence_rates_trace_galerkin_projection(testdegree, convrate):
+    """Tests for degree + (1/2) order convergence of the trace problem."""
+    l2errors = np.array([trace_galerkin_projection(testdegree, 1, r)
+                         for r in range(1, 5)])
+    conv = np.log2(l2errors[:-1] / l2errors[1:])[-1]
+    print "Convergence order: ", conv
+    assert conv > 0.9*convrate
+
+
+if __name__ == '__main__':
+    import os
+    pytest.main(os.path.abspath(__file__))
