5ccce69bb940b1863b0d2d720e96761f54cf5f17
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2016-07-20 11:55:21 +0100

    Plotting: Add one-dimension plotting
    Method 'one-dimension_plot' will calculate points for a function for
    plotting it using matplotlib
    
    Tests:
        Constant functions,
        Linear functions,
        Quadratic functions

---
+++ firedrake/plot.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def one_dimension_plot(function, num_points):
+    """Calculate a set of points for plotting for a one-dimension function as a
+    numpy array
+
+    :arg function: 1D function for plotting
+    :arg num_points: number of points per element
+    """
+    function_space = function.function_space()
+    mesh = function_space.mesh()
+
+    def __calculate_values(function, function_space, points):
+        "Calculate function values at given points"
+        vals = np.array([], dtype=float)
+        for cell_node in function_space.cell_node_list:
+            data = function.dat.data_ro[cell_node]
+            elem = function_space.fiat_element.tabulate(0, points)[(0,)]
+            val = np.dot(data, elem)
+            vals = np.append(vals, [val])
+        return vals
+
+    points = np.linspace(0, 1.0, num=num_points, dtype=float).reshape(-1, 1)
+    y_vals = __calculate_values(function, function_space, points)
+    x_vals = __calculate_values(mesh.coordinates,
+                                mesh.coordinates.function_space(),
+                                points)
+
+    def __sort_points(x_vals, y_vals):
+        "Sort the points according to x values"
+        order = np.argsort(x_vals)
+        return np.array([x_vals[order], y_vals[order]])
+    x_vals, y_vals = __sort_points(x_vals, y_vals)
+
+    return np.array([x_vals, y_vals])
---
+++ tests/output/test_one_dim_output.py
@@ -0,0 +1,33 @@
+from firedrake import *
+import numpy as np
+
+def test_plot_constant():
+    mesh = UnitIntervalMesh(1)
+    V = FunctionSpace(mesh, "CG", 1)
+    f = Function(V)
+    f.interpolate(as_ufl(1.0))
+    x_vals, y_vals = one_dimension_plot(f, 10)
+    for y in y_vals:
+        assert np.allclose(1.0, y)
+
+def test_plot_linear():
+    mesh = IntervalMesh(10, 20)
+    V = FunctionSpace(mesh, "CG", 1)
+    f = Function(V)
+    x = SpatialCoordinate(mesh)
+    f.interpolate(x[0] * 2.0)
+    x_vals, y_vals = one_dimension_plot(f, 10)
+    points = np.array([x_vals, y_vals]).T
+    for point in points:
+        assert np.allclose(2.0 * point[0],point[1])
+
+def test_plot_quadratic():
+    mesh = IntervalMesh(10, 20)
+    V = FunctionSpace(mesh, "CG", 2)
+    f = Function(V)
+    x = SpatialCoordinate(mesh)
+    f.interpolate(2 * x[0] - x[0] + 3)
+    x_vals, y_vals = one_dimension_plot(f, 10)
+    points = np.array([x_vals, y_vals]).T
+    for point in points:
+        assert np.allclose(2.0 * point[0] - point[0] + 3, point[1])
