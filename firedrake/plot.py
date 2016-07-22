f15f57b70817eb445ceb74c505dda2726d547fed
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2016-07-22 15:15:48 +0100

    Plotting: Separate _calculate_values from plotting
    Take this method outside the 1D plotting method because it would also be
    used for higher dimension plotting

--- firedrake/plot.py
+++ firedrake/plot.py
@@ -50,6 +50,13 @@ def plot(function, axes=None, num_points=100, **kwargs):
     return plt.gcf()
 
 
+def _calculate_values(function, function_space, points):
+    "Calculate function values at given points"
+    elem = function_space.fiat_element.tabulate(0, points)[(0, )]
+    data = function.dat.data_ro[function_space.cell_node_list]
+    return np.dot(data, elem).reshape(-1)
+
+
 def one_dimension_plot(function, num_points):
     """Calculate a set of points for plotting for a one-dimension function as a
     numpy array
@@ -59,18 +66,11 @@ def one_dimension_plot(function, num_points):
     """
     function_space = function.function_space()
     mesh = function_space.mesh()
-
-    def __calculate_values(function, function_space, points):
-        "Calculate function values at given points"
-        elem = function_space.fiat_element.tabulate(0, points)[(0, )]
-        data = function.dat.data_ro[function_space.cell_node_list]
-        return np.dot(data, elem).reshape(-1)
-
     points = np.linspace(0, 1.0, num=num_points, dtype=float).reshape(-1, 1)
-    y_vals = __calculate_values(function, function_space, points)
-    x_vals = __calculate_values(mesh.coordinates,
-                                mesh.coordinates.function_space(),
-                                points)
+    y_vals = _calculate_values(function, function_space, points)
+    x_vals = _calculate_values(mesh.coordinates,
+                               mesh.coordinates.function_space(),
+                               points)
 
     def __sort_points(x_vals, y_vals):
         "Sort the points according to x values"
