89c389fd1ffb96f69512a75b91f25ccaebba5b1f
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-07-27 17:18:41 +0100

    almost no integral_type in geometric.py
    Works.

--- tsfc/geometric.py
+++ tsfc/geometric.py
@@ -7,8 +7,6 @@ from numpy import array, nan, vstack
 from singledispatch import singledispatch
 import sympy
 
-from ufl import interval, triangle, quadrilateral, tetrahedron
-from ufl import TensorProductCell
 from ufl.classes import (CellCoordinate, CellEdgeVectors,
                          CellFacetJacobian, CellOrientation,
                          FacetCoordinate, ReferenceCellVolume,
@@ -17,70 +15,6 @@ from ufl.classes import (CellCoordinate, CellEdgeVectors,
 import gem
 
 from tsfc.constants import NUMPY_TYPE
-from tsfc.fiatinterface import as_fiat_cell
-
-
-interval_x_interval = TensorProductCell(interval, interval)
-triangle_x_interval = TensorProductCell(triangle, interval)
-quadrilateral_x_interval = TensorProductCell(quadrilateral, interval)
-
-
-# Facet normals of the reference cells
-reference_normal = {
-    interval: array([[-1.0],
-                     [1.0]], dtype=NUMPY_TYPE),
-    triangle: array([[1.0, 1.0],
-                     [-1.0, 0.0],
-                     [0.0, -1.0]], dtype=NUMPY_TYPE),
-    quadrilateral: array([[-1.0, 0.0],
-                          [1.0, 0.0],
-                          [0.0, -1.0],
-                          [0.0, 1.0]], dtype=NUMPY_TYPE),
-    tetrahedron: array([[1.0, 1.0, 1.0],
-                        [-1.0, 0.0, 0.0],
-                        [0.0, -1.0, 0.0],
-                        [0.0, 0.0, -1.0]], dtype=NUMPY_TYPE),
-    # Product cells. Convention:
-    # Bottom, top, then vertical facets in the order of the base cell
-    interval_x_interval: array([[0.0, -1.0],
-                                [0.0, 1.0],
-                                [-1.0, 0.0],
-                                [1.0, 0.0]], dtype=NUMPY_TYPE),
-    triangle_x_interval: array([[0.0, 0.0, -1.0],
-                                [0.0, 0.0, 1.0],
-                                [1.0, 1.0, 0.0],
-                                [-1.0, 0.0, 0.0],
-                                [0.0, -1.0, 0.0]], dtype=NUMPY_TYPE),
-    quadrilateral_x_interval: array([[0.0, 0.0, -1.0],
-                                     [0.0, 0.0, 1.0],
-                                     [-1.0, 0.0, 0.0],
-                                     [1.0, 0.0, 0.0],
-                                     [0.0, -1.0, 0.0],
-                                     [0.0, 1.0, 0.0]], dtype=NUMPY_TYPE),
-}
-
-
-def reference_cell(terminal):
-    """Reference cell of terminal"""
-    cell = terminal.ufl_domain().ufl_cell()
-    return cell.reconstruct(geometric_dimension=cell.topological_dimension())
-
-
-def strip_table(table, integral_type):
-    """Select horizontal or vertical parts for facet integrals on
-    extruded cells. No-op in all other cases."""
-    if integral_type == "exterior_facet_bottom":
-        return table[0:1]
-    elif integral_type == "exterior_facet_top":
-        return table[1:2]
-    elif integral_type == "interior_facet_horiz":
-        # Bottom and top
-        return table[:2]
-    elif integral_type in ["exterior_facet_vert", "interior_facet_vert"]:
-        # Vertical facets in the order of the base cell
-        return table[2:]
-    else:
-        return table
 
 
 @singledispatch
@@ -139,16 +73,17 @@ def translate_cell_facet_jacobian(terminal, mt, params):
 
 @translate.register(ReferenceNormal)
 def translate_reference_normal(terminal, mt, params):
-    table = reference_normal[reference_cell(terminal)]
-    table = strip_table(table, params.integral_type)
-    table = table.reshape(table.shape[:1] + terminal.ufl_shape)
-    return params.select_facet(gem.Literal(table), mt.restriction)
+    result = []
+    for facet_i in params.entity_ids:
+        n = params.fiat_cell.compute_scaled_outward_normal(params.integration_dim, facet_i)
+        result.append(n)
+    return params.select_facet(gem.Literal(result), mt.restriction)
 
 
 @translate.register(CellEdgeVectors)
 def translate_cell_edge_vectors(terminal, mt, params):
     from FIAT.reference_element import TensorProductCell as fiat_TensorProductCell
-    fiat_cell = as_fiat_cell(terminal.ufl_domain().ufl_cell())
+    fiat_cell = params.fiat_cell
     if isinstance(fiat_cell, fiat_TensorProductCell):
         raise NotImplementedError("CellEdgeVectors not implemented on TensorProductElements yet")
 
