783d924e6d8530527d4e46932abc7ceb6b382ce2
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-08-08 22:24:29 +0100

    adopt n-ary ufl.TensorProductElement

--- tsfc/fiatinterface.py
+++ tsfc/fiatinterface.py
@@ -192,14 +192,13 @@ def _(element, vector_is_mixed):
                                                                vector_is_mixed))
 
 
-# Now for the OPE-specific stuff
+# Now for the TPE-specific stuff
 @convert.register(ufl.TensorProductElement)  # noqa
 def _(element, vector_is_mixed):
     cell = element.cell()
     if type(cell) is not ufl.TensorProductCell:
-        raise ValueError("OPE not on OPC?")
-    A = element._A
-    B = element._B
+        raise ValueError("TPE not on TPC?")
+    A, B = element.sub_elements()
     return FIAT.TensorProductElement(create_element(A, vector_is_mixed),
                                      create_element(B, vector_is_mixed))
 
@@ -227,7 +226,7 @@ def _(element, vector_is_mixed):
 
     def rec(eles):
         for ele in eles:
-            if ele.num_sub_elements() > 0:
+            if isinstance(ele, ufl.MixedElement):
                 rec(ele.sub_elements())
             else:
                 elements.append(ele)
