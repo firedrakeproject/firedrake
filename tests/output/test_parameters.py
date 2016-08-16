691237e0be2ccaef62e826bf871c2dad5753589a
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2016-08-16 16:30:54 +0100

    Config: add tests for FloatType

--- tests/output/test_parameters.py
+++ tests/output/test_parameters.py
@@ -104,3 +104,96 @@ def test_int_parse_bounded_invalid():
     int_type = IntType(lower_bound=1, upper_bound=3)
     assert int_type.parse("4") is None
     assert int_type.parse("0") is None
+
+
+def test_float_validate_ints():
+    # Float type accepts ints
+    from firedrake.parameters import FloatType
+    float_type = FloatType()
+    assert float_type.validate(1)
+    assert float_type.validate(0)
+    assert float_type.validate(-1)
+
+
+def test_float_validate_bounded_both_sides():
+    from firedrake.parameters import FloatType
+    float_type = FloatType(lower_bound=1, upper_bound=3)
+    assert not float_type.validate(0.99)
+    assert float_type.validate(1.0)
+    assert float_type.validate(2.0)
+    assert float_type.validate(3.0)
+    assert not float_type.validate(3.01)
+    assert not float_type.validate("NaN")
+    assert not float_type.validate("inf")
+    assert not float_type.validate("-inf")
+
+
+def test_float_validate_bounded_upper():
+    from firedrake.parameters import FloatType
+    float_type = FloatType(upper_bound=3)
+    assert float_type.validate(0.0)
+    assert float_type.validate(1.0)
+    assert float_type.validate(2.0)
+    assert float_type.validate(3.0)
+    assert not float_type.validate(3.01)
+    assert not float_type.validate("NaN")
+    assert not float_type.validate("inf")
+    assert float_type.validate("-inf")
+
+
+def test_float_validate_bounded_lower():
+    from firedrake.parameters import FloatType
+    float_type = FloatType(lower_bound=1)
+    assert not float_type.validate(0.99)
+    assert float_type.validate(1.0)
+    assert float_type.validate(2.0)
+    assert float_type.validate(3.0)
+    assert float_type.validate(4.0)
+    assert not float_type.validate("NaN")
+    assert float_type.validate("inf")
+    assert not float_type.validate("-inf")
+
+
+def test_float_validate_floats():
+    from firedrake.parameters import FloatType
+    float_type = FloatType()
+    assert float_type.validate(1.0)
+    assert float_type.validate(1.5)
+    assert float_type.validate(float("NaN"))
+    assert float_type.validate(float("inf"))
+    assert float_type.validate(float("-inf"))
+
+
+def test_float_validate_strs():
+    from firedrake.parameters import FloatType
+    float_type = FloatType()
+    assert float_type.validate("1")
+    assert float_type.validate("0")
+    assert float_type.validate("-1")
+    assert float_type.validate("1.0")
+    assert float_type.validate("1.5")
+    assert float_type.validate("NaN")
+    assert float_type.validate("inf")
+    assert float_type.validate("-inf")
+
+
+def test_float_parse_valid():
+    from firedrake.parameters import FloatType
+    from math import isnan
+    float_type = FloatType()
+    assert float_type.parse("1.0") == 1.0
+    assert float_type.parse("0.0") == 0.0
+    assert float_type.parse("-1.0") == -1.0
+    assert isnan(float_type.parse("NaN"))
+    assert float_type.parse("inf") == float("inf")
+    assert float_type.parse("-inf") == float("-inf")
+
+
+def test_float_parse_bounded_invalid():
+    from firedrake.parameters import FloatType
+    float_type = FloatType(lower_bound=1, upper_bound=3)
+    assert float_type.parse("4.0") is None
+    assert float_type.parse("0.0") is None
+    assert float_type.parse("inf") is None
+    assert float_type.parse("-inf") is None
+    assert float_type.parse("NaN") is None
