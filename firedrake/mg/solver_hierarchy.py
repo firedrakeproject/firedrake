cf2015577714a980d3cf8a40cf4ff9a9a6364873
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2016-08-26 15:44:33 +0100

    mg: Fix coarsening of problems
    When coarsening the coefficients inside a problem, we need to
    have a unique mapping from original coefficient to coarse coefficient.
    Do this by coarsening all coefficients up front and providing a mapping
    to CoarsenIntegrand.

--- firedrake/mg/solver_hierarchy.py
+++ firedrake/mg/solver_hierarchy.py
@@ -23,11 +23,25 @@ def coarsen_problem(problem):
         raise RuntimeError("No hierarchy to coarsen")
     if lvl == 0:
         return None
-    new_u = ufl_utils.coarsen_thing(problem.u)
+
+    # Build set of coefficients we need to coarsen
+    coefficients = set()
+    coefficients.update(problem.F.coefficients())
+    coefficients.update(problem.J.coefficients())
+    if problem.Jp is not None:
+        coefficients.update(problem.Jp.coefficients())
+
+    # Coarsen them, and remember where from.
+    mapping = {}
+    for c in coefficients:
+        mapping[c] = ufl_utils.coarsen_thing(c)
+
+    new_u = mapping[problem.u]
+
     new_bcs = [ufl_utils.coarsen_thing(bc) for bc in problem.bcs]
-    new_J = ufl_utils.coarsen_form(problem.J)
-    new_Jp = ufl_utils.coarsen_form(problem.Jp)
-    new_F = ufl_utils.coarsen_form(problem.F)
+    new_J = ufl_utils.coarsen_form(problem.J, coefficient_mapping=mapping)
+    new_Jp = ufl_utils.coarsen_form(problem.Jp, coefficient_mapping=mapping)
+    new_F = ufl_utils.coarsen_form(problem.F, coefficient_mapping=mapping)
 
     new_problem = firedrake.NonlinearVariationalProblem(new_F,
                                                         new_u,
