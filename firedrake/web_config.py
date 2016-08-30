7d0149d9e520327daefe9ad5067cc68595debf8b
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2016-08-30 14:27:23 +0100

    Config: Expand UI Recursively

--- firedrake/web_config.py
+++ firedrake/web_config.py
@@ -5,11 +5,22 @@ app = Flask(__name__)
 
 def web_config(parameters):
 
+    def format_dict(parameters):
+        ret = []
+        for k, v in parameters.iteritems():
+            if not isinstance(v, dict):
+                ret.append({"key": k,
+                            "type": str(parameters.get_key(k).type),
+                            "value": v})
+            else:
+                ret.append({"key": k,
+                            "type": "dict",
+                            "value": format_dict(v)})
+        return ret
+
     @app.route('/')
     def index():
-        params = [{"key": k,
-                   "type": str(parameters.get_key(k).type),
-                   "value": v} for k, v in parameters.iteritems()]
+        params = format_dict(parameters)
         return render_template('index.html', parameters=params)
 
     @app.route('/validate')
