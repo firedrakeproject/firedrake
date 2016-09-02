cfa682b9b2a3b058399c591e877454fd82543fe2
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2016-09-02 14:54:22 +0100

    Install: Fix Typo

--- firedrake_configuration/__init__.py
+++ firedrake_configuration/__init__.py
@@ -10,8 +10,8 @@ import os
 
 # Attempt to read configuration from file.
 try:
-    with file(os.path.join(__file__,  "configuration.json"), "r") as f:
-        config = json.load(f)
+    with file(os.path.join(__file__, "configuration.json"), "r") as f:
+        _config = json.load(f)
 except IOError:
     _config = None
 
@@ -22,8 +22,8 @@ def write_config(config):
     `firedrake-update`."""
 
     # Create the json as a separate step
-    json_output = json.dumps(config)
-    with file(os.path.join(__file__,  "configuration.json"), "w") as f:
+    json_output = json.dumps(_config)
+    with file(os.path.join(__file__, "configuration.json"), "w") as f:
         f.write(json_output)
 
 
