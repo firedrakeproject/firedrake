12a4652e1c6d560ad60cb026150b2873f01111d2
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-10-10 18:07:58 +0100

    merge interpolate.py into driver.py

--- tsfc/__init__.py
+++ tsfc/__init__.py
@@ -1,4 +1,3 @@
 from __future__ import absolute_import, print_function, division
 
-from tsfc.driver import compile_form  # noqa: F401
-from tsfc.interpolate import compile_expression_at_points  # noqa: F401
+from tsfc.driver import compile_form, compile_expression_at_points  # noqa: F401
