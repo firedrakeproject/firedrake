c220c345a2705691c4e63d8e31a4a5388e1c7234
Miklos Homolya <m.homolya14@imperial.ac.uk>
2016-10-11 14:04:33 +0100

    create gem.utils and add OrderedSet

--- tsfc/fem.py
+++ tsfc/fem.py
@@ -16,6 +16,7 @@ from ufl.classes import (Argument, Coefficient, CellVolume,
                          GeometricQuantity, QuadratureWeight)
 
 import gem
+from gem.utils import cached_property
 
 from tsfc.fiatinterface import create_element, create_quadrature, as_fiat_cell
 from tsfc.modified_terminals import analyse_modified_terminal
@@ -115,24 +116,6 @@ class TabulationManager(object):
 
     def __getitem__(self, key):
         return self.tables[key]
-
-
-# FIXME: copy-paste from PyOP2
-class cached_property(object):
-    """A read-only @property that is only evaluated once. The value is cached
-    on the object itself rather than the function or class; this should prevent
-    memory leakage."""
-    def __init__(self, fget, doc=None):
-        self.fget = fget
-        self.__doc__ = doc or fget.__doc__
-        self.__name__ = fget.__name__
-        self.__module__ = fget.__module__
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        obj.__dict__[self.__name__] = result = self.fget(obj)
-        return result
 
 
 class Parameters(object):
