73cd12670a192f6576ea77eb483f53ff607de636
Alberto Paganini <paganini@maths.ox.ac.uk>
2016-12-07 16:35:27 +0000

    linted

--- tests/regression/test_to_expression.py
+++ tests/regression/test_to_expression.py
@@ -3,6 +3,7 @@ import firedrake.expression as expression
 import pytest
 import numpy as np
 
+
 def test_to_expression_1D():
     mesh = UnitIntervalMesh(5)
     V = FunctionSpace(mesh, "CG", 1)
@@ -17,13 +18,13 @@ def test_to_expression_1D():
     f3 = interpolate(v3, V)
     f4 = interpolate(v4, V)
 
-    assert (np.allclose(f1.dat.data_ro,f2.dat.data_ro) and
-            np.allclose(f3.dat.data_ro,f4.dat.data_ro) and
-            np.allclose(f1.dat.data_ro,f4.dat.data_ro))
+    assert (np.allclose(f1.dat.data_ro, f2.dat.data_ro) and
+            np.allclose(f3.dat.data_ro, f4.dat.data_ro) and
+            np.allclose(f1.dat.data_ro, f4.dat.data_ro))
 
 
 def test_to_texpression_2D():
-    mesh = UnitSquareMesh(3,3)
+    mesh = UnitSquareMesh(3, 3)
     V = VectorFunctionSpace(mesh, "CG", 1)
 
     v1 = expression.to_expression([1., 2.])
@@ -34,8 +35,8 @@ def test_to_texpression_2D():
     f2 = interpolate(v2, V)
     f3 = interpolate(v3, V)
 
-    assert (np.allclose(f1.dat.data_ro,f2.dat.data_ro) and
-            np.allclose(f2.dat.data_ro,f3.dat.data_ro))
+    assert (np.allclose(f1.dat.data_ro, f2.dat.data_ro) and
+            np.allclose(f2.dat.data_ro, f3.dat.data_ro))
 
 
 if __name__ == '__main__':
