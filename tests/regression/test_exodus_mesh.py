255013f66aab5b1df158559fc41a00619525aafa
Julian Andrej <juan@tf.uni-kiel.de>
2017-02-01 15:13:26 +0100

    Expect an exception with PETSC_ERR_SUP to happen if PETSc was not configured with exodus.

--- tests/regression/test_exodus_mesh.py
+++ tests/regression/test_exodus_mesh.py
@@ -8,7 +8,10 @@ cwd = abspath(dirname(__file__))
 
 @pytest.fixture
 def exodus_mesh():
-    return Mesh(join(cwd, "..", "meshes", "brick.e"))
+    try:
+        return Mesh(join(cwd, "..", "meshes", "brick.e"))
+    except PETSc.Error as e:
+        assert e.ierr == 56
 
 
 @pytest.mark.parallel(nprocs=2)
