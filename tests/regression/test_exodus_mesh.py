b27fd8c66e70100ec4d69914d661788342c54909
Julian Andrej <juan@tf.uni-kiel.de>
2017-02-01 19:07:25 +0100

    Skip tests if PETSc is not configured with exodus.

--- tests/regression/test_exodus_mesh.py
+++ tests/regression/test_exodus_mesh.py
@@ -17,13 +17,19 @@ def exodus_mesh():
 
 @pytest.mark.parallel(nprocs=2)
 def test_create_from_file(exodus_mesh):
-    assert exodus_mesh
+    if exodus_mesh is None:
+        pytest.skip("PETSc not configured with exodusII")
+    else:
+        assert exodus_mesh
 
 
 @pytest.mark.parallel(nprocs=2)
 def test_sidesets(exodus_mesh):
-    exodus_mesh.init()
-    assert (exodus_mesh.exterior_facets.unique_markers == [200, 201]).all()
+    if exodus_mesh is None:
+        pytest.skip("PETSc not configured with exodusII")
+    else:
+        exodus_mesh.init()
+        assert (exodus_mesh.exterior_facets.unique_markers == [200, 201]).all()
 
 
 if __name__ == '__main__':
