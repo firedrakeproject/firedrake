6182c9a333006aa71f821f14e7f963f0c92b199f
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-02-19 19:17:42 +0000

    Use callable assemble for constructing data

--- firedrake/slate/preconditioners.py
+++ firedrake/slate/preconditioners.py
@@ -33,7 +33,9 @@ class HybridizationPC(PCBase):
                                TrialFunctions, TestFunction, Function,
                                BrokenElement, MixedElement,
                                FacetNormal, Constant, DirichletBC,
-                               assemble, Projector)
+                               Projector)
+        from firedrake.assemble import (allocate_matrix,
+                                        create_assembly_callable)
         from firedrake.formmanipulation import ArgumentReplacer, split_form
 
         # Extract the problem context
@@ -58,21 +60,20 @@ class HybridizationPC(PCBase):
         replacer = ArgumentReplacer(arg_map)
         new_form = map_integrand_dags(replacer, context.a)
 
-        split_forms = split_form(new_form)
-        # Store these matrices for reconstruction
-        self.A = Tensor([sf.form for sf in split_forms
-                         if sf.indices == (0, 0)][0])
-        self.B = Tensor([sf.form for sf in split_forms
-                         if sf.indices == (1, 0)][0])
-        self.C = Tensor([sf.form for sf in split_forms
-                         if sf.indices == (1, 1)][0])
-
         # Create the space of approximate traces:
         # the space of Lagrange multipliers
-        TraceSpace = FunctionSpace(V.mesh(), "HDiv Trace",
-                                   V_d.ufl_element().degree() - 1)
-        trace_condition = DirichletBC(TraceSpace, Constant(0.0),
-                                      "on_boundary")
+        # NOTE: Once extruded is ready, we will need to be aware of this
+        # and construct the appropriate trace space for the HDiv element
+        if V[0].ufl_element().family() == "Brezzi-Douglas-Marini":
+            tdegree = V_d.ufl_element().degree()
+        else:
+            tdegree = V_d.ufl_element().degree() - 1
+
+        TraceSpace = FunctionSpace(V.mesh(), "HDiv Trace", tdegree)
+
+        # For extruded, we will need to add the flags "on_top" and "on_bottom"
+        trace_conditions = [DirichletBC(TraceSpace, Constant(0.0),
+                                        "on_boundary")]
 
         # Broken flux and scalar terms (solution via reconstruction)
         self.broken_solution = Function(V_d)
@@ -82,7 +83,6 @@ class HybridizationPC(PCBase):
 
         # Solution of the system for the Lagrange multipliers
         self.trace_solution = Function(TraceSpace)
-        self.schur_rhs = Function(TraceSpace)
 
         # unbroken solutions and rhs
         self.unbroken_solution = Function(V)
@@ -91,25 +91,91 @@ class HybridizationPC(PCBase):
         # Create the symbolic Schur-reduction of the discontinuous
         # problem in Slate. Weakly enforce continuity via the Lagrange
         # multipliers
-        self.Atilde = Tensor(new_form)
+        Atilde = Tensor(new_form)
         gammar = TestFunction(TraceSpace)
         n = FacetNormal(V.mesh())
         sigma, _ = TrialFunctions(V_d)
-        self.K = Tensor(gammar('+') * ufl.dot(sigma, n) * ufl.dS)
-        trial = TrialFunction(V_d[0])
-        self.K_local = Tensor(gammar('+') * ufl.dot(trial, n) * ufl.dS)
-        self.schur_comp = assemble(self.K * self.Atilde.inv * self.K.T,
-                                   bcs=trace_condition)
-        self.schur_comp.force_evaluation()
+
+        # NOTE: Once extruded is ready, this will change slightly
+        # to include both horizontal and vertical interior facets
+        K = Tensor(gammar('+') * ufl.dot(sigma, n) * ufl.dS)
+
+        # Assemble the Schur complement operator and right-hand side
+        # in a cell-local way
+        self.schur_rhs = Function(TraceSpace)
+        self._assemble_Srhs = create_assembly_callable(
+            K * Atilde.inv * self.broken_rhs,
+            tensor=self.schur_rhs,
+            form_compiler_parameters=context.fc_params)
+
+        schur_comp = K * Atilde.inv * K.T
+        self.S = allocate_matrix(schur_comp,
+                                 bcs=trace_conditions,
+                                 form_compiler_parameters=context.fc_params)
+        self._assemble_S = create_assembly_callable(
+            schur_comp,
+            tensor=self.S,
+            bcs=trace_conditions,
+            form_compiler_parameters=context.fc_params)
+
+        self._assemble_S()
+        self.S.force_evaluation()
+        Smat = self.S.petscmat
+
+        # Transfer nullspace (if any)
+        # TODO: Have a think about this...Is this right?
+        Smat.setNullSpace(P.getNullSpace())
 
         # Set up the KSP for the system of Lagrange multipliers
         ksp = PETSc.KSP().create(comm=pc.comm)
-        ksp.setOperators(self.schur_comp.petscmat)
         ksp.setOptionsPrefix(prefix + "trace_")
-        ksp.setTolerances(rtol=1e-8)
+        ksp.setTolerances(rtol=1e-13)
+        ksp.setOperators(Smat)
         ksp.setUp()
         ksp.setFromOptions()
         self.ksp = ksp
+
+        # Now we construct the local tensors for the reconstruction stage
+        # TODO: Get Slate to support mixed tensors and these variables
+        # become unnecessary
+        split_forms = split_form(new_form)
+        A = Tensor(next(sf.form for sf in split_forms
+                        if sf.indices == (0, 0)))
+        B = Tensor(next(sf.form for sf in split_forms
+                        if sf.indices == (1, 0)))
+        C = Tensor(next(sf.form for sf in split_forms
+                        if sf.indices == (1, 1)))
+        trial = TrialFunction(V_d[0])
+        K_local = Tensor(gammar('+') * ufl.dot(trial, n) * ufl.dS)
+
+        # Split the solution function and reconstruct
+        # each bit separately
+        sigma_h, u_h = self.broken_solution.split()
+
+        # TODO: Write out full expression for general RHS of the form
+        # RHS = [g; f]
+        _, f = self.broken_rhs.split()
+
+        # Pressure reconstruction
+        M = B * A.inv * B.T + C
+        u_sol = M.inv * f + M.inv * (B * A.inv *
+                                     K_local.T * self.trace_solution)
+        self._assemble_pressure = create_assembly_callable(
+            u_sol,
+            tensor=u_h,
+            form_compiler_parameters=context.fc_params)
+
+        # Velocity reconstructions
+        sigma_sol = A.inv * (B.T * u_h -
+                             K_local.T * self.trace_solution)
+        self._assemble_velocity = create_assembly_callable(
+            sigma_sol,
+            tensor=sigma_h,
+            form_compiler_parameters=context.fc_params)
+
+        # Set up the projector for projecting the broken solution
+        # into the unbroken finite element spaces
+        # NOTE: Tolerance here matters!
         sigma_b, _ = self.broken_solution.split()
         sigma_u, _ = self.unbroken_solution.split()
         self.projector = Projector(sigma_b,
@@ -121,11 +187,10 @@ class HybridizationPC(PCBase):
         """Update by assembling into the operator. No need to
         reconstruct symbolic objects.
         """
-        from firedrake import assemble
-
-        assemble(self.schur_comp, tensor=self.schur_comp,
-                 bcs=self.trace_condition)
-        assemble(self.schur_rhs, tensor=self.schur_rhs)
+        # TODO: Is this correct? Does this PC even have an "update"?
+        self._assemble_S()
+        self.S.force_evaluation()
+        self._assemble_Srhs()
 
     def apply(self, pc, x, y):
         """We solve the forward eliminated problem for the
@@ -135,7 +200,7 @@ class HybridizationPC(PCBase):
         Lastly, we project the broken solutions into the mimetic
         non-broken finite element space.
         """
-        from firedrake import project, assemble
+        from firedrake import project
 
         # Transfer non-broken x into a firedrake function
         with self.unbroken_rhs.dat.vec as v:
@@ -150,34 +215,20 @@ class HybridizationPC(PCBase):
         field1.dat.copy(bfield1.dat)
 
         # Compute the rhs for the multiplier system
-        assemble(self.K * self.Atilde.inv * self.broken_rhs,
-                 tensor=self.schur_rhs)
+        self._assemble_Srhs()
 
         # Solve the system for the Lagrange multipliers
         with self.schur_rhs.dat.vec_ro as b:
             with self.trace_solution.dat.vec as x:
                 self.ksp.solve(b, x)
 
-        # Split the solution function and reconstruct
-        # each bit separately
-        sigma_h, u_h = self.broken_solution.split()
-
-        # TODO: Write out full expression for general RHS of the form
-        # RHS = [g; f]
-        _, f = self.broken_rhs.split()
-
-        # Pressure reconstruction
-        M = self.B * self.A.inv * self.B.T + self.C
-        u_sol = M.inv * f + M.inv * (self.B * self.A.inv *
-                                     self.K_local.T * self.trace_solution)
-        assemble(u_sol, tensor=u_h)
-
-        # Velocity reconstructions
-        sigma_sol = self.A.inv * (self.B.T * u_h -
-                                  self.K_local.T * self.trace_solution)
-        assemble(sigma_sol, tensor=sigma_h)
+        # Assemble the pressure and velocity (in that order)
+        # using the Lagrange multipliers
+        self._assemble_pressure()
+        self._assemble_velocity()
 
         # Project the broken solution into non-broken spaces
+        sigma_h, u_h = self.broken_solution.split()
         sigma_u, u_u = self.unbroken_solution.split()
         u_h.dat.copy(u_u.dat)
         self.projector.project()
