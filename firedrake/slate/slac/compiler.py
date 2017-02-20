d61a0ba5680d2a62916de95f4f9328f007e577a9
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-02-20 18:10:10 +0000

    Correct cell orientations

--- firedrake/slate/slac/compiler.py
+++ firedrake/slate/slac/compiler.py
@@ -149,7 +149,7 @@ def compile_expression(slate_expr, tsfc_parameters=None):
                          for c in builder.coefficient(exp.coefficients()[ci])]
 
                 if kinfo.oriented:
-                    clist.append(cell_orientations)
+                    clist.insert(0, cell_orientations)
 
                 incl.extend(kinfo.kernel._include_dirs)
                 tensor = eigen_tensor(exp, t, index)
@@ -349,7 +349,7 @@ def extruded_int_horiz_facet(exp, builder, top_sks, bottom_sks,
 
         # TODO: Is this safe?
         if top.kinfo.oriented and btm.kinfo.oriented:
-            clist.append(cell_orientations)
+            clist.insert(0, cell_orientations)
 
         dirs = top.kinfo.kernel._include_dirs + btm.kinfo.kernel._include_dirs
         incl.extend(tuple(OrderedDict.fromkeys(dirs)))
@@ -403,7 +403,7 @@ def extruded_top_bottom_facet(cxt_kernel, builder, coordsym, mesh_layer_sym,
                  for c in builder.coefficient(exp.coefficients()[ci])]
 
         if kinfo.oriented:
-            clist.append(cell_orientations)
+            clist.insert(0, cell_orientations)
 
         incl.extend(kinfo.kernel._include_dirs)
         tensor = eigen_tensor(exp, t, index)
@@ -479,7 +479,7 @@ def facet_integral_loop(cxt_kernel, builder, coordsym, cellfacetsym,
         tensor = eigen_tensor(exp, t, index)
 
         if kinfo.oriented:
-            clist.append(cell_orientations)
+            clist.insert(0, cell_orientations)
 
         clist.append(ast.FlatBlock("&%s" % itsym))
         funcalls.append(ast.FunCall(kinfo.kernel.name,
