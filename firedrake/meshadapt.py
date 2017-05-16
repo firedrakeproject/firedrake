8051a9b50b67772a229a956fbff2951beb646933
Nicolas Barral <n.barral@imperial.ac.uk>
2017-05-16 11:45:17 +0100

    clean adaptation object

--- firedrake/meshadapt.py
+++ firedrake/meshadapt.py
@@ -9,10 +9,7 @@ import firedrake.dmplex as dmplex
 __all__ = ['adapt']
 
 
-
-
 class AAdaptation(object):
-
     """
     Object that performs anisotropic mesh adaptation
     """
@@ -24,46 +21,52 @@ class AAdaptation(object):
         self.metric = metric
         self.meshnew = None
 
-
     def adapt(self):
         """
+        Adapt the current mesh to the metric field.
+
+        :return: the new mesh adapted to the metric
         """
-        self.meshnew = adapt(self.mesh, self.metric)
+        plex = self.mesh.topology._plex
+        dim = self.mesh._topological_dimension
+        entity_dofs = np.zeros(dim+1, dtype=np.int32)
+        entity_dofs[0] = self.mesh.geometric_dimension()
+        coordSection = plex.createSection([1], entity_dofs, perm=self.mesh.topology._plex_renumbering)
+        dmCoords = plex.getCoordinateDM()
+        dmCoords.setDefaultSection(coordSection)
 
+        with self.mesh.coordinates.dat.vec_ro as coords:
+            plex.setCoordinatesLocal(coords)
+        with self.metric.dat.vec as vec:
+            dmplex.reorder_metric(plex, vec, coordSection)
+        with self.metric.dat.vec_ro as vec:
+            newplex = plex.adapt(vec, "boundary_ids")
 
+        self.newmesh = Mesh(newplex)
+        return self.newmesh
 
     def transfer_solution(self, f, fnew):
         """
-        """
-        if self.newmesh == None:
-            raise("Cannot transfer solution before generating adapted mesh")
-        #TODO many checks
-        fnew.dat.data[:] = f.at(meshnew.coordinates.dat.data)
+        Transfers a solution field from the old mesh to the new mesh
 
+        :arg f: function defined on the old mesh that one wants to transfer
+        :arg fnew: tranfered function defined on the new mesh
+        """
+        if self.newmesh is None:
+            raise("Cannot transfer solution before generating adapted mesh")
+        # TODO many checks
+        fnew.dat.data[:] = f.at(self.meshnew.coordinates.dat.data)
 
 
 def adapt(mesh, metric):
-    """ Adapt the mesh to a prescribed metric field.
+    """
+    Adapt the mesh to a prescribed metric field.
 
     :arg mesh: the base mesh to adapt
     :arg metric: a metric tensor field (a Function of a TensorFunctionSpace)
 
     :return: a new mesh adapted to the metric
     """
-    dim = mesh._topological_dimension
-    entity_dofs = np.zeros(dim+1, dtype=np.int32)
-    entity_dofs[0] = mesh.geometric_dimension()
-    coordSection = mesh._plex.createSection([1], entity_dofs, perm=mesh.topology._plex_renumbering)
-    dmCoords = mesh.topology._plex.getCoordinateDM()
-    dmCoords.setDefaultSection(coordSection)
-
-    with mesh.coordinates.dat.vec_ro as coords:
-        mesh.topology._plex.setCoordinatesLocal(coords)
-    with metric.dat.vec as vec:
-        dmplex.reorder_metric(mesh.topology._plex, vec, coordSection)
-    with metric.dat.vec_ro as vec:
-        newplex = mesh.topology._plex.adapt(vec, "boundary_ids")
-
-    newmesh = Mesh(newplex)
-
+    adaptor = AAdaptation(mesh, metric)
+    newmesh = adaptor.adapt()
     return newmesh
