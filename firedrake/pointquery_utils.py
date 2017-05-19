b47301f31a9f6bb5ff995c6d41de7c7c6dc8349a
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-05-19 17:09:35 +0100

    add new TSFC-based implementation for Newton update

--- firedrake/pointquery_utils.py
+++ firedrake/pointquery_utils.py
@@ -10,7 +10,15 @@ from pyop2.datatypes import IntType, as_cstr
 from pyop2.base import build_itspace
 from pyop2.sequential import generate_cell_wrapper
 
-from ufl import TensorProductCell
+import ufl
+from ufl.corealg.map_dag import map_expr_dag
+
+import gem
+import gem.impero_utils as impero_utils
+
+import tsfc
+import tsfc.kernel_interface.firedrake as firedrake_interface
+import tsfc.ufl_utils as ufl_utils
 
 
 def make_args(function):
@@ -82,17 +90,21 @@ def set_float_formatting(precision):
     format["epsilon"] = 10.0*eval("1e-%s" % precision)
 
 
-def compile_coordinate_element(ufl_coordinate_element, contains_eps):
+def compile_coordinate_element(ufl_coordinate_element, contains_eps, parameters=None):
     """Generates C code for changing to reference coordinates.
 
     :arg ufl_coordinate_element: UFL element of the coordinates
     :returns: C code as string
     """
-    from tsfc import default_parameters
-    from tsfc.finatinterface import create_element
+    if parameters is None:
+        parameters = tsfc.default_parameters()
+    else:
+        _ = tsfc.default_parameters()
+        _.update(parameters)
+        parameters = _
 
     # Set code generation parameters
-    set_float_formatting(default_parameters()["precision"])
+    set_float_formatting(parameters["precision"])
 
     def dX_norm_square(topological_dimension):
         return " + ".join("dX[{0}]*dX[{0}]".format(i)
@@ -119,20 +131,70 @@ def compile_coordinate_element(ufl_coordinate_element, contains_eps):
         X = numpy.average(vertices, axis=0)
         return "\n".join(f_assign("X[%d]" % i, f_float(v)) for i, v in enumerate(X))
 
-    def to_reference_coordinates(ufl_cell, finat_element):
-        pass
+    def to_reference_coordinates(ufl_coordinate_element):
+        # Set up UFL form
+        cell = ufl_coordinate_element.cell()
+        domain = ufl.Mesh(ufl_coordinate_element)
+        K = ufl.JacobianInverse(domain)
+        x = ufl.SpatialCoordinate(domain)
+        x0_element = ufl.VectorElement("Real", cell, 0)
+        x0 = ufl.Coefficient(ufl.FunctionSpace(domain, x0_element))
+        expr = ufl.dot(K, x - x0)
+
+        # Translation to GEM
+        C = ufl_utils.coordinate_coefficient(domain)
+        expr = ufl_utils.preprocess_expression(expr)
+        expr = ufl_utils.replace_coordinates(expr, C)
+        expr = ufl_utils.simplify_abs(expr)
+
+        builder = firedrake_interface.KernelBuilderBase()
+        builder._coefficient(C, "C")
+        builder._coefficient(x0, "x0")
+
+        dim = cell.topological_dimension()
+        point = gem.Variable('X', (dim,))
+        context = tsfc.fem.GemPointContext(
+            interface=builder,
+            ufl_cell=cell,
+            precision=parameters["precision"],
+            point_indices=(),
+            point_expr=point,
+        )
+        translator = tsfc.fem.Translator(context)
+        ir = map_expr_dag(translator, expr)
+
+        # Unroll result
+        ir = [gem.Indexed(ir, alpha) for alpha in numpy.ndindex(ir.shape)]
+
+        # Unroll IndexSums
+        max_extent = parameters["unroll_indexsum"]
+        if max_extent:
+            def predicate(index):
+                return index.extent <= max_extent
+        ir = gem.optimise.unroll_indexsum(ir, predicate=predicate)
+
+        # Translate to COFFEE
+        ir = impero_utils.preprocess_gem(ir)
+        return_variable = gem.Variable('dX', (dim,))
+        assignments = [(gem.Indexed(return_variable, (i,)), e)
+                       for i, e in enumerate(ir)]
+        impero_c = impero_utils.compile_gem(assignments, ())
+        body = tsfc.coffee.generate(impero_c, {}, parameters["precision"])
+        body.open_scope = False
+
+        return body
 
     # Create FInAT element
-    element = create_element(ufl_coordinate_element)
+    element = tsfc.finatinterface.create_element(ufl_coordinate_element)
 
     cell = ufl_coordinate_element.cell()
-    extruded = isinstance(cell, TensorProductCell)
+    extruded = isinstance(cell, ufl.TensorProductCell)
 
     code = {
         "geometric_dimension": cell.geometric_dimension(),
         "topological_dimension": cell.topological_dimension(),
         "inside_predicate": inside_check(element.cell),
-        "to_reference_coords": to_reference_coordinates(cell, element),
+        "to_reference_coords": to_reference_coordinates(ufl_coordinate_element),
         "init_X": init_X(element.cell),
         "max_iteration_count": 1 if is_affine(ufl_coordinate_element) else 16,
         "convergence_epsilon": 1e-12,
@@ -168,15 +230,14 @@ static inline void to_reference_coords_kernel(void *result_, double *x0, int *re
     double *K = result->K;
     double detJ;
 
-    double dX[%(topological_dimension)d];
     int converged = 0;
-
     for (int it = 0; !converged && it < %(max_iteration_count)d; it++) {
+        double dX[%(topological_dimension)d] = { 0.0 };
 %(to_reference_coords)s
 
-         if (%(dX_norm_square)s < %(convergence_epsilon)g * %(convergence_epsilon)g) {
-             converged = 1;
-         }
+        if (%(dX_norm_square)s < %(convergence_epsilon)g * %(convergence_epsilon)g) {
+            converged = 1;
+        }
 
 %(X_isub_dX)s
     }
