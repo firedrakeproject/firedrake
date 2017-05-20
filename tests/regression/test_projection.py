63b5f2521f73ebd1ed1563d6fd9e33ac161c6d08
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-20 16:58:30 +0100

    Update projector bc test

--- tests/regression/test_projection.py
+++ tests/regression/test_projection.py
@@ -244,7 +244,8 @@ def test_projector_expression():
 
 
 @pytest.mark.parametrize('tensor', ['scalar', 'vector', 'tensor'])
-def test_projector_bcs(tensor):
+@pytest.mark.parametrize('same_fspace', [False, True])
+def test_projector_bcs(tensor, same_fspace):
     mesh = UnitSquareMesh(2, 2)
     x = SpatialCoordinate(mesh)
     if tensor == 'scalar':
@@ -253,7 +254,7 @@ def test_projector_bcs(tensor):
         bcs = [DirichletBC(V_ho, Constant(0.5), (1, 3)),
                DirichletBC(V_ho, Constant(-0.5), (2, 4))]
         fct = cos(x[0]*pi*2)*sin(x[1]*pi*2)
-        v = Function(V).interpolate(fct)
+
     elif tensor == 'vector':
         V = VectorFunctionSpace(mesh, "CG", 1)
         V_ho = VectorFunctionSpace(mesh, "CG", 5)
@@ -261,7 +262,7 @@ def test_projector_bcs(tensor):
                DirichletBC(V_ho, Constant((-0.5, -0.5)), (2, 4))]
         fct = as_vector([cos(x[0]*pi*2)*sin(x[1]*pi*2),
                          cos(x[0]*pi*2)*sin(x[1]*pi*2)])
-        v = Function(V).project(fct)
+
     elif tensor == 'tensor':
         V = TensorFunctionSpace(mesh, "CG", 1)
         V_ho = TensorFunctionSpace(mesh, "CG", 5)
@@ -273,6 +274,10 @@ def test_projector_bcs(tensor):
                           cos(x[0]*pi*2)*sin(x[1]*pi*2)],
                          [cos(x[0]*pi*2)*sin(x[1]*pi*2),
                           cos(x[0]*pi*2)*sin(x[1]*pi*2)]])
+
+    if same_fspace:
+        v = Function(V_ho).project(fct)
+    else:
         v = Function(V).project(fct)
 
     ret = Function(V_ho)
@@ -280,12 +285,15 @@ def test_projector_bcs(tensor):
                                                               "pc_type": "lu"})
     projector.project()
 
+    # Manually solve a Galerkin projection problem to get a reference
     ref = Function(V_ho)
-    # The method "project" already handles bcs.
-    # So we use this as a reference to make sure the
-    # projector does the same thing (up to machine precision).
-    project(v, ref, bcs=bcs, solver_parameters={"ksp_type": "preonly",
-                                                "pc_type": "lu"})
+    p = TrialFunction(V_ho)
+    q = TestFunction(V_ho)
+    a = inner(p, q)*dx
+    L = inner(v, q)*dx
+    solve(a == L, ref, bcs=bcs, solver_parameters={"ksp_type": "preonly",
+                                                   "pc_type": "lu"})
+
     assert errornorm(ret, ref) < 1.0e-10
 
 
