b7d38bc2d2ea1c6a0a40e5f7c438723ab1d32b7f
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2017-05-23 12:32:36 +0100

    Second COFFEE paper appeared, TSFC paper on arxiv

--- firedrake/citations.py
+++ firedrake/citations.py
@@ -154,11 +154,15 @@ Citations().add("Luporini2016", """
   author =       {Fabio Luporini and David A. Ham and Paul H. J. Kelly},
   title =        {An algorithm for the optimization of finite element
                   integration loops},
-  journal =      {Submitted to ACM TOMS},
-  year =         2016,
+  journal =      {ACM Transactions on Mathematical Software},
+  year =         2017,
+  volume =       44,
+  issue =        1,
+  pages =        {3:1--3:26},
   archiveprefix ={arXiv},
   eprint =       {1604.05872},
-  url =          {http://arxiv.org/abs/1604.05872}
+  url =          {http://arxiv.org/abs/1604.05872},
+  doi =          {10.1145/3054944},
 }
 """)
 
@@ -199,5 +203,18 @@ Citations().add("Mitchell2016", """
   eprint =       {1605.00492},
   primaryclass = {cs.MS},
   url =          {http://arxiv.org/abs/1605.00492}
+}
+""")
+
+Citations().add("Homolya2017", """
+@Misc{Homolya2017,
+  author =        {Mikl\'os Homolya and Lawrence Mitchell and Fabio Luporini and
+                   David A. Ham},
+  title =         {{TSFC: a structure-preserving form compiler}},
+  year =          2017,
+  archiveprefix = {arXiv},
+  eprint =        {1705.03667},
+  primaryclass =  {cs.MS},
+  url =           {http://arxiv.org/abs/1705.003667}
 }
 """)
