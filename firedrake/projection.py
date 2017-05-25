e4a2e338dface9d8fb29501d3b3db9475101c474
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-05-25 15:32:29 +0100

    Add reconstruction method

--- firedrake/projection.py
+++ firedrake/projection.py
@@ -6,10 +6,11 @@ from firedrake import functionspaceimpl
 from firedrake import solving
 from firedrake import ufl_expr
 from firedrake import function
+from firedrake.parloops import par_loop, READ, INC
 import firedrake.variational_solver as vs
 
 
-__all__ = ['project', 'Projector']
+__all__ = ['project', 'reconstruct', 'Projector']
 
 # Store the solve function to use in a variable so external packages
 # (dolfin-adjoint) can override it.
@@ -98,6 +99,54 @@ def project(v, V, bcs=None, mesh=None,
            solver_parameters=solver_parameters,
            form_compiler_parameters=form_compiler_parameters)
     return ret
+
+
+def reconstruct(v_b, V):
+    """Reconstruct a :class:`.Function`, defined on a broken function
+    space and transfer its data into a function defined on an unbroken
+    finite element space.
+
+    In other words: suppose we have a function v defined on a space constructed
+    from a :class:`ufl.BrokenElement`. This methods allows one to "project"
+    the data into an unbroken function space.
+
+    This method avoids assembling a mass matrix system to solve a Galerkin
+    projection problem; instead kernels are generated which computes weighted
+    averages between facet degrees of freedom.
+
+    :arg v_b: the :class:`.Function` to reconstruct.
+    :arg V: the target function space.
+    """
+
+    if not isinstance(v_b, function.Function):
+        raise RuntimeError("Argument must be a function. Not %s" % type(v_b))
+
+    if not isinstance(v_b.function_space().ufl_element(), ufl.BrokenElement):
+        raise ValueError("Function space must be defined on a broken element.")
+
+    if not v_b.function_space.ufl_element()._element == V.ufl_element():
+        raise ValueError(
+            "The ufl element of the target function space must "
+            "coincide with the element broken by ufl.BrokenElement."
+        )
+
+    weight_kernel = """
+    for (int i=0; i<weight.dofs; ++i) {
+    weight[i][0] += 1.0;
+    }"""
+
+    average_kernel = """
+    for (int i=0; i<vrec.dofs; ++i) {
+    vrec[i][0] += v_b[i][0]/weight[i][0];
+    }"""
+
+    w = function.Function(V)
+    result = function.Function(V)
+    par_loop(weight_kernel, ufl.dx, {"weight": (w, INC)})
+    par_loop(average_kernel, ufl.dx, {"vrec": (result, INC),
+                                      "v_b": (v_b, READ),
+                                      "weight": (w, READ)})
+    return result
 
 
 class Projector(object):
