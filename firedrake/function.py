3c5190dfaee6753cd04ad4b22e86c87681e7b647
Lawrence Mitchell <lawrence.mitchell@imperial.ac.uk>
2017-06-08 17:41:14 +0100

    Fix parallel point evaluation
    We look in the halo, so need up to date data there.

--- firedrake/function.py
+++ firedrake/function.py
@@ -489,6 +489,8 @@ class Function(ufl.Coefficient):
         """
         # Need to ensure data is up-to-date for reading
         self.dat._force_evaluation(read=True, write=False)
+        self.dat.global_to_local_begin(op2.READ)
+        self.dat.global_to_local_end(op2.READ)
         from mpi4py import MPI
 
         if args:
