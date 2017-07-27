9dc0d4e634b27d23339cbab5761059ca36e6badc
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-07-27 12:31:48 +0100

    test H(div) sum factorisation with split mixed Poisson

--- tests/test_sum_factorisation.py
+++ tests/test_sum_factorisation.py
@@ -7,8 +7,10 @@ import pytest
 from coffee.visitors import EstimateFlops
 
 from ufl import (Mesh, FunctionSpace, FiniteElement, VectorElement,
-                 TestFunction, TrialFunction, TensorProductCell, dx,
-                 action, interval, triangle, quadrilateral, dot, grad)
+                 TestFunction, TrialFunction, TensorProductCell,
+                 EnrichedElement, HDivElement, TensorProductElement,
+                 dx, action, interval, triangle, quadrilateral, dot,
+                 div, grad)
 
 from tsfc import compile_form
 
@@ -19,6 +21,29 @@ def helmholtz(cell, degree):
     u = TrialFunction(V)
     v = TestFunction(V)
     return (u*v + dot(grad(u), grad(v)))*dx
+
+
+def split_mixed_poisson(cell, degree):
+    m = Mesh(VectorElement('CG', cell, 1))
+    if cell.cellname() in ['interval * interval', 'quadrilateral']:
+        hdiv_element = FiniteElement('RTCF', cell, degree)
+    elif cell.cellname() == 'triangle * interval':
+        U0 = FiniteElement('RT', triangle, degree)
+        U1 = FiniteElement('DG', triangle, degree - 1)
+        V0 = FiniteElement('CG', interval, degree)
+        V1 = FiniteElement('DG', interval, degree - 1)
+        Wa = HDivElement(TensorProductElement(U0, V1))
+        Wb = HDivElement(TensorProductElement(U1, V0))
+        hdiv_element = EnrichedElement(Wa, Wb)
+    elif cell.cellname() == 'quadrilateral * interval':
+        hdiv_element = FiniteElement('NCF', cell, degree)
+    RT = FunctionSpace(m, hdiv_element)
+    DG = FunctionSpace(m, FiniteElement('DQ', cell, degree - 1))
+    sigma = TrialFunction(RT)
+    u = TrialFunction(DG)
+    tau = TestFunction(RT)
+    v = TestFunction(DG)
+    return [dot(sigma, tau) * dx, div(tau) * u * dx, div(sigma) * v * dx]
 
 
 def count_flops(form):
@@ -53,6 +78,39 @@ def test_rhs(cell, order):
     flops = [count_flops(action(helmholtz(cell, degree)))
              for degree in degrees]
     rates = numpy.diff(numpy.log(flops)) / numpy.diff(numpy.log(degrees))
+    assert (rates < order).all()
+
+
+@pytest.mark.parametrize(('cell', 'order'),
+                         [(quadrilateral, 5),
+                          (TensorProductCell(interval, interval), 5),
+                          (TensorProductCell(triangle, interval), 7),
+                          (TensorProductCell(quadrilateral, interval), 7)
+                          ])
+def test_mixed_poisson(cell, order):
+    degrees = numpy.arange(3, 8)
+    if cell == TensorProductCell(triangle, interval):
+        degrees = numpy.arange(3, 6)
+    flops = [list(map(count_flops, split_mixed_poisson(cell, degree)))
+             for degree in degrees]
+    rates = numpy.diff(numpy.log(flops).T) / numpy.diff(numpy.log(degrees))
+    assert (rates < order).all()
+
+
+@pytest.mark.parametrize(('cell', 'order'),
+                         [(quadrilateral, 3),
+                          (TensorProductCell(interval, interval), 3),
+                          (TensorProductCell(triangle, interval), 5),
+                          (TensorProductCell(quadrilateral, interval), 4)
+                          ])
+def test_mixed_poisson_action(cell, order):
+    degrees = numpy.arange(3, 8)
+    if cell == TensorProductCell(triangle, interval):
+        degrees = numpy.arange(3, 6)
+    flops = [[count_flops(action(form))
+              for form in split_mixed_poisson(cell, degree)]
+             for degree in degrees]
+    rates = numpy.diff(numpy.log(flops).T) / numpy.diff(numpy.log(degrees))
     assert (rates < order).all()
 
 
