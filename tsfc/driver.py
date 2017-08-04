33b456b8076368f589630e4e3775ac9cb373bf7f
NicholasBermuda <snicholasbarton@gmail.com>
2017-08-04 20:02:37 +0200

    better type checking

--- tsfc/driver.py
+++ tsfc/driver.py
@@ -274,7 +274,7 @@ def compile_expression_at_points(expression, points, coordinates, parameters=Non
     set_scalar_type(parameters["scalar_type"])
 
     # determine if we're in complex mode
-    complx = parameters and parameters["scalar_type"] is 'double complex'
+    complx = parameters and parameters["scalar_type"] == 'double complex'
 
     # Apply UFL preprocessing
     expression = ufl_utils.preprocess_expression(expression, complex_mode=complx)
