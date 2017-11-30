9962e4aab288b27274a3f70a08fe0e18b8ae029f
Thomas Gibson <t.gibson15@imperial.ac.uk>
2017-11-30 19:57:19 +0000

    Zero out negative restrictions

--- firedrake/slate/slate.py
+++ firedrake/slate/slate.py
@@ -25,6 +25,7 @@ from itertools import chain
 
 from ufl.algorithms.map_integrands import map_integrand_dags
 from ufl.algorithms.multifunction import MultiFunction
+from ufl.classes import Zero
 from ufl.domain import join_domains
 from ufl.form import Form
 
@@ -34,14 +35,14 @@ __all__ = ['AssembledVector', 'IndexedTensor', 'Tensor',
            'Add', 'Mul']
 
 
-class CheckRestrictions(MultiFunction):
-    """UFL MultiFunction for enforcing cell-wise integrals to contain
-    only positive (outward) restrictions.
+class RemoveNegativeRestrictions(MultiFunction):
+    """UFL MultiFunction which removes any negative restrictions
+    in a form.
     """
     expr = MultiFunction.reuse_if_untouched
 
-    def negative_restrictions(self, o):
-        raise ValueError("Must contain only positive restrictions!")
+    def negative_restricted(self, o):
+        return Zero(o.ufl_shape, o.ufl_free_indices, o.ufl_index_dimensions)
 
 
 class TensorBase(object, metaclass=ABCMeta):
@@ -441,15 +442,12 @@ class Tensor(TensorBase):
         if r not in (0, 1, 2):
             raise NotImplementedError("No support for tensors of rank %d." % r)
 
-        # Checks for positive restrictions on integrals
-        integrals = form.integrals()
-        mapper = CheckRestrictions()
-        for it in integrals:
-            map_integrand_dags(mapper, it)
+        # Remove any negative restrictions and replace with zero
+        _form = map_integrand_dags(RemoveNegativeRestrictions(), form)
 
         super(Tensor, self).__init__()
 
-        self.form = form
+        self.form = _form
 
     def arg_function_spaces(self):
         """Returns a tuple of function spaces that the tensor
