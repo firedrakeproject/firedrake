NicholasBermuda/tsfc

use correct parameters type, fix type accessing, add complex constant

tsfc/driver.py
@@ -211,7 +211,8 @@ def compile_integral(integral_data, form_data, prefix, parameters,
             index_names.append((quadrature_index, 'ip_%d' % i))
 
     # Construct kernel
-    body = generate_coffee(impero_c, index_names, parameters["precision"], expressions, argument_indices)
+    body = generate_coffee(impero_c, index_names, parameters["precision"], 
+                            parameters["scalar_type"], expressions, argument_indices)
 
     return builder.construct_kernel(kernel_name, body)
 

firedrakeproject/firedrake

tsfc/driver.py
@@ -237,7 +237,8 @@ def compile_integral(integral_data, form_data, prefix, parameters,
         name_multiindex(multiindex, name)
 
     # Construct kernel
-    body = generate_coffee(impero_c, index_names, parameters["precision"], expressions, split_argument_indices)
+    body = generate_coffee(impero_c, index_names, parameters["precision"], 
+                            parameters["scalar_type"], expressions, split_argument_indices)
 
     return builder.construct_kernel(kernel_name, body)
 