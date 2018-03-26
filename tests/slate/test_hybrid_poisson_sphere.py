abaeaa7130fc66af5bfc1e4c061b5baf8db43940
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-03-26 19:17:03 +0100

    Provide nullspace callback

--- tests/slate/test_hybrid_poisson_sphere.py
+++ tests/slate/test_hybrid_poisson_sphere.py
@@ -25,7 +25,6 @@ def run_hybrid_poisson_sphere(MeshClass, refinement, hdiv_space):
     L = f*v*dx
     w = Function(W)
 
-    basis = VectorSpaceBasis(constant=True)
     params = {'mat_type': 'matfree',
               'ksp_type': 'preonly',
               'pc_type': 'python',
@@ -34,7 +33,11 @@ def run_hybrid_poisson_sphere(MeshClass, refinement, hdiv_space):
                                 'pc_type': 'lu',
                                 'pc_factor_mat_solver_type': 'mumps'}}
 
-    appctx = {'hybridization_trace_nullspace': basis}
+    # Provide a callback to construct the trace nullspace
+    def nullspace_basis(T):
+        return VectorSpaceBasis(constant=True)
+
+    appctx = {'trace_nullspace': nullspace_basis}
     solve(a == L, w, solver_parameters=params, appctx=appctx)
     u_h, _ = w.split()
     error = errornorm(u_exact, u_h)
