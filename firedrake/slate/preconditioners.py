14769f3a753d88eba7107072fe7bb28a6861d217
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-10-11 10:10:07 +0100

    Use Firedrake complex convention for surface forms

--- firedrake/slate/preconditioners.py
+++ firedrake/slate/preconditioners.py
@@ -123,10 +123,10 @@ class HybridizationPC(PCBase):
         sigma = TrialFunctions(V_d)[self.vidx]
 
         if mesh.cell_set._extruded:
-            Kform = (gammar('+') * ufl.jump(sigma, n=n) * ufl.dS_h +
-                     gammar('+') * ufl.jump(sigma, n=n) * ufl.dS_v)
+            Kform = (ufl.inner(ufl.jump(sigma, n=n), gammar('+')) * ufl.dS_h +
+                     ufl.inner(ufl.jump(sigma, n=n), gammar('+')) * ufl.dS_v)
         else:
-            Kform = (gammar('+') * ufl.jump(sigma, n=n) * ufl.dS)
+            Kform = (ufl.inner(ufl.jump(sigma, n=n), gammar('+')) * ufl.dS)
 
         # Here we deal with boundaries. If there are Neumann
         # conditions (which should be enforced strongly for
@@ -157,7 +157,7 @@ class HybridizationPC(PCBase):
             extruded_neumann_subdomains = neumann_subdomains & {"top", "bottom"}
             neumann_subdomains = neumann_subdomains - extruded_neumann_subdomains
 
-            integrand = gammar * ufl.dot(sigma, n)
+            integrand = ufl.inner(ufl.dot(sigma, n), gammar)
             measures = []
             trace_subdomains = []
             if mesh.cell_set._extruded:
