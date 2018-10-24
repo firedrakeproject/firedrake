6d247d20a7e0373b6e8fad80e900c8558f77ddec
Lawrence Mitchell <lawrence@wence.uk>
2018-10-24 20:01:19 +0100

    mg: Add test of multiple custom transfer operators
    Not yet working correctly.

--- tests/multigrid/test_custom_transfer.py
+++ tests/multigrid/test_custom_transfer.py
@@ -36,6 +36,50 @@ def test_repeated_custom_transfer():
     assert count == 1
 
 
+@pytest.mark.xfail(reason="Transfer operators on subspaces not carried through for monolithic multigrid")
+def test_multiple_custom_transfer():
+    mesh = UnitIntervalMesh(2)
+    mh = MeshHierarchy(mesh, 1)
+    mesh = mh[-1]
+    count_V = 0
+    count_Q = 0
+
+    def prolong_V(coarse, fine):
+        nonlocal count_V
+        prolong(coarse, fine)
+        count_V += 1
+
+    def prolong_Q(coarse, fine):
+        nonlocal count_Q
+        prolong(coarse, fine)
+        count_Q -= 1
+
+    V = FunctionSpace(mesh, "CG", 1)
+    Q = FunctionSpace(mesh, "DG", 0)
+
+    W = V*Q
+    u, p = TrialFunctions(W)
+    v, q = TestFunctions(W)
+
+    a = u*v*dx + p*q*dx
+    L = v*dx
+
+    options = {"ksp_type": "preonly",
+               "pc_type": "mg",
+               "mat_type": "aij"}
+
+    wh = Function(W)
+    problem = LinearVariationalProblem(a, L, wh)
+    solver = LinearVariationalSolver(problem, solver_parameters=options)
+    solver.set_transfer_operators(dmhooks.transfer_operators(V, prolong=prolong_V),
+                                  dmhooks.transfer_operators(Q, prolong=prolong_Q))
+
+    solver.solve()
+
+    assert count_V == 1
+    assert count_Q == -1
+
+
 def test_custom_transfer_setting():
     mesh = UnitIntervalMesh(2)
     mh = MeshHierarchy(mesh, 1)
