a3abda31ebffdc09b5de749b0b423a98f4aa86bf
Lawrence Mitchell <lawrence@wence.uk>
2018-10-25 10:02:11 +0100

    mg: Tests of multiple transfers on mixed spaces

--- tests/multigrid/test_custom_transfer.py
+++ tests/multigrid/test_custom_transfer.py
@@ -36,10 +36,9 @@ def test_repeated_custom_transfer():
     assert count == 1
 
 
-@pytest.mark.xfail(reason="Transfer operators on subspaces not carried through for monolithic multigrid")
-def test_multiple_custom_transfer():
+def test_multiple_custom_transfer_split():
     mesh = UnitIntervalMesh(2)
-    mh = MeshHierarchy(mesh, 1)
+    mh = MeshHierarchy(mesh, 2)
     mesh = mh[-1]
     count_V = 0
     count_Q = 0
@@ -49,9 +48,55 @@ def test_multiple_custom_transfer():
         prolong(coarse, fine)
         count_V += 1
 
-    def prolong_Q(coarse, fine):
+    def prolong_Q(fine, coarse):
         nonlocal count_Q
+        prolong(fine, coarse)
+        count_Q -= 1
+
+    V = FunctionSpace(mesh, "CG", 1)
+    Q = FunctionSpace(mesh, "DG", 0)
+
+    W = V*Q
+    u, p = TrialFunctions(W)
+    v, q = TestFunctions(W)
+
+    a = u*v*dx + p*q*dx
+    L = v*dx
+
+    options = {"ksp_type": "preonly",
+               "pc_type": "fieldsplit",
+               "fieldsplit_pc_type": "mg",
+               "pc_fieldsplit_type": "additive",
+               "fieldsplit_ksp_type": "preonly",
+               "mat_type": "aij"}
+
+    wh = Function(W)
+    problem = LinearVariationalProblem(a, L, wh)
+    solver = LinearVariationalSolver(problem, solver_parameters=options)
+    solver.set_transfer_operators(dmhooks.transfer_operators(V, prolong=prolong_V),
+                                  dmhooks.transfer_operators(Q, prolong=prolong_Q))
+
+    solver.solve()
+
+    assert count_V == 2
+    assert count_Q == -2
+
+
+def test_multiple_custom_transfer_monolithc():
+    mesh = UnitIntervalMesh(2)
+    mh = MeshHierarchy(mesh, 2)
+    mesh = mh[-1]
+    count_V = 0
+    count_Q = 0
+
+    def prolong_V(coarse, fine):
+        nonlocal count_V
         prolong(coarse, fine)
+        count_V += 1
+
+    def prolong_Q(fine, coarse):
+        nonlocal count_Q
+        prolong(fine, coarse)
         count_Q -= 1
 
     V = FunctionSpace(mesh, "CG", 1)
@@ -76,8 +121,8 @@ def test_multiple_custom_transfer():
 
     solver.solve()
 
-    assert count_V == 1
-    assert count_Q == -1
+    assert count_V == 2
+    assert count_Q == -2
 
 
 def test_custom_transfer_setting():
