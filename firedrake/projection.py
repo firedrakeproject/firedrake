f4ca677780b94868beb6748d5de9cdceee486c15
Lawrence Mitchell <lawrence@wence.uk>
2018-11-12 13:58:11 +0000

    projection: Refactor to make use of Projector

--- firedrake/projection.py
+++ firedrake/projection.py
@@ -1,19 +1,63 @@
 import ufl
 
+import firedrake
 from firedrake import expression
 from firedrake import functionspace
 from firedrake import functionspaceimpl
-from firedrake import solving
-from firedrake import ufl_expr
 from firedrake import function
 import firedrake.variational_solver as vs
+from pyop2.utils import as_tuple
 
 
 __all__ = ['project', 'Projector']
 
-# Store the solve function to use in a variable so external packages
-# (dolfin-adjoint) can override it.
-_solve = solving.solve
+
+def sanitise_input(v, V):
+    if isinstance(v, expression.Expression):
+        shape = v.value_shape()
+        # Build a function space that supports PointEvaluation so that
+        # we can interpolate into it.
+        deg = max(as_tuple(V.ufl_element().degree()))
+
+        if v.rank() == 0:
+            fs = functionspace.FunctionSpace(V.mesh(), 'DG', deg+1)
+        elif v.rank() == 1:
+            fs = functionspace.VectorFunctionSpace(V.mesh(), 'DG',
+                                                   deg+1,
+                                                   dim=shape[0])
+        else:
+            fs = functionspace.TensorFunctionSpace(V.mesh(), 'DG',
+                                                   deg+1,
+                                                   shape=shape)
+        f = function.Function(fs)
+        f.interpolate(v)
+        return f
+    elif isinstance(v, function.Function):
+        return v
+    elif isinstance(v, ufl.classes.Expr):
+        return v
+    else:
+        raise ValueError("Can't project from source object %r" % v)
+
+
+def create_output(V, name=None):
+    if isinstance(V, functionspaceimpl.WithGeometry):
+        return function.Function(V, name=name)
+    elif isinstance(V, function.Function):
+        return V
+    else:
+        raise ValueError("Can't project into target object %r" % V)
+
+
+def check_meshes(source, target):
+    source_mesh = source.ufl_domain()
+    target_mesh = target.ufl_domain()
+    if source_mesh.ufl_cell() != target_mesh.ufl_cell():
+        raise ValueError("Mismatching cells in source (%r) and target (%r) meshes" %
+                         (source_mesh.ufl_cell(), target_mesh.ufl_cell()))
+    if source_mesh != target_mesh:
+        raise ValueError("Can't projecting between non-matching meshes yet")
+    return source_mesh, target_mesh
 
 
 def project(v, V, bcs=None,
@@ -34,69 +78,12 @@ def project(v, V, bcs=None,
     If ``V`` is a :class:`.Function` then ``v`` is projected into
     ``V`` and ``V`` is returned. If `V` is a :class:`.FunctionSpace`
     then ``v`` is projected into a new :class:`.Function` and that
-    :class:`.Function` is returned.
+    :class:`.Function` is returned."""
 
-    The ``form_compiler_parameters`` are currently ignored."""
-    from firedrake import function
-
-    if isinstance(V, functionspaceimpl.WithGeometry):
-        ret = function.Function(V, name=name)
-    elif isinstance(V, function.Function):
-        ret = V
-        V = V.function_space()
-    else:
-        raise RuntimeError(
-            'Can only project into functions and function spaces, not %r'
-            % type(V))
-
-    if isinstance(v, expression.Expression):
-        shape = v.value_shape()
-        # Build a function space that supports PointEvaluation so that
-        # we can interpolate into it.
-        if isinstance(V.ufl_element().degree(), tuple):
-            deg = max(V.ufl_element().degree())
-        else:
-            deg = V.ufl_element().degree()
-
-        if v.rank() == 0:
-            fs = functionspace.FunctionSpace(V.mesh(), 'DG', deg+1)
-        elif v.rank() == 1:
-            fs = functionspace.VectorFunctionSpace(V.mesh(), 'DG',
-                                                   deg+1,
-                                                   dim=shape[0])
-        else:
-            fs = functionspace.TensorFunctionSpace(V.mesh(), 'DG',
-                                                   deg+1,
-                                                   shape=shape)
-        f = function.Function(fs)
-        f.interpolate(v)
-        v = f
-    elif isinstance(v, function.Function):
-        if v.function_space().mesh() != ret.function_space().mesh():
-            raise RuntimeError("Can't project between mismatching meshes")
-    elif not isinstance(v, ufl.core.expr.Expr):
-        raise RuntimeError("Can only project from expressions and functions, not %r" % type(v))
-
-    if v.ufl_shape != ret.ufl_shape:
-        raise RuntimeError('Shape mismatch between source %s and target function spaces %s in project' %
-                           (v.ufl_shape, ret.ufl_shape))
-
-    p = ufl_expr.TestFunction(V)
-    q = ufl_expr.TrialFunction(V)
-    a = ufl.inner(p, q) * ufl.dx(domain=V.mesh())
-    L = ufl.inner(p, v) * ufl.dx(domain=V.mesh())
-
-    # Default to 1e-8 relative tolerance
-    if solver_parameters is None:
-        solver_parameters = {'ksp_type': 'cg', 'ksp_rtol': 1e-8}
-    else:
-        solver_parameters.setdefault('ksp_type', 'cg')
-        solver_parameters.setdefault('ksp_rtol', 1e-8)
-
-    _solve(a == L, ret, bcs=bcs,
-           solver_parameters=solver_parameters,
-           form_compiler_parameters=form_compiler_parameters)
-    return ret
+    val = Projector(v, V, bcs=bcs, solver_parameters=solver_parameters,
+                    form_compiler_parameters=form_compiler_parameters).project()
+    val.rename(name)
+    return val
 
 
 class Projector(object):
@@ -109,40 +96,46 @@ class Projector(object):
 
     :arg v: the :class:`ufl.Expr` or
          :class:`.Function` to project
-    :arg v_out: :class:`.Function` to put the result in
+    :arg V: :class:`.Function` (or :class:`~.FunctionSpace`) to put the result in.
     :arg bcs: an optional set of :class:`.DirichletBC` objects to apply
               on the target function space.
     :arg solver_parameters: parameters to pass to the solver used when
          projecting.
     """
 
-    def __init__(self, v, v_out, bcs=None, solver_parameters=None, constant_jacobian=True):
+    def __init__(self, v, v_out, bcs=None, solver_parameters=None,
+                 form_compiler_parameters=None, constant_jacobian=True):
+        target = create_output(v_out)
+        source = sanitise_input(v, target.function_space())
+        source_mesh, target_mesh = check_meshes(source, target)
+        if source.ufl_shape != target.ufl_shape:
+            raise ValueError("Shape mismatch between source %s and target %s in project" %
+                             (source.ufl_shape, target.ufl_shape))
 
-        if isinstance(v, expression.Expression) or \
-           not isinstance(v, (ufl.core.expr.Expr, function.Function)):
-            raise ValueError("Can only project UFL expression or Functions not '%s'" % type(v))
-
-        self._same_fspace = (isinstance(v, function.Function) and v.function_space()
-                             == v_out.function_space())
-        self.v = v
-        self.v_out = v_out
+        self._use_assign = (isinstance(v, function.Function)
+                            and not self.bcs
+                            and v.function_space() == v_out.function_space())
+        self.source = source
+        self.target = target
         self.bcs = bcs
 
-        if not self._same_fspace or self.bcs:
-            V = v_out.function_space()
+        if not self._use_assign:
+            V = target.function_space()
 
-            p = ufl_expr.TestFunction(V)
-            q = ufl_expr.TrialFunction(V)
+            p = firedrake.TestFunction(V)
+            q = firedrake.TrialFunction(V)
 
             a = ufl.inner(p, q)*ufl.dx
-            L = ufl.inner(p, v)*ufl.dx
+            L = ufl.inner(p, source)*ufl.dx
 
-            problem = vs.LinearVariationalProblem(a, L, v_out, bcs=self.bcs,
+            problem = vs.LinearVariationalProblem(a, L, target, bcs=self.bcs,
+                                                  form_compiler_parameters=form_compiler_parameters,
                                                   constant_jacobian=constant_jacobian)
 
             if solver_parameters is None:
                 solver_parameters = {}
-
+            else:
+                solver_parameters = solver_parameters.copy()
             solver_parameters.setdefault("ksp_type", "cg")
 
             self.solver = vs.LinearVariationalSolver(problem,
@@ -152,8 +145,8 @@ class Projector(object):
         """
         Apply the projection.
         """
-        if self._same_fspace and not self.bcs:
-            self.v_out.assign(self.v)
+        if self._use_assign:
+            self.target.assign(self.source)
         else:
             self.solver.solve()
-        return self.v_out
+        return self.target
--- tests/regression/test_projection.py
+++ tests/regression/test_projection.py
@@ -128,13 +128,13 @@ def test_project_mismatched_rank():
     u = Function(U)
     ev = Expression('x[0]')
     eu = Expression(('x[0]', 'x[1]'))
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(v, U)
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(u, V)
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(ev, U)
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(eu, V)
 
 
@@ -148,10 +148,10 @@ def test_project_mismatched_mesh():
     u = Function(U)
     v = Function(V)
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(u, V)
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(v, U)
 
 
@@ -164,10 +164,10 @@ def test_project_mismatched_shape():
     u = Function(U)
     v = Function(V)
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(u, V)
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(ValueError):
         project(v, U)
 
 
@@ -238,8 +238,8 @@ def test_projector_expression():
     V = FunctionSpace(mesh, "CG", 1)
     vo = Function(V)
     expr = Expression("1")
-    with pytest.raises(ValueError):
-        Projector(expr, vo)
+    Projector(expr, vo)
+    assert np.allclose(vo.dat.data_ro, 1.0)
 
 
 @pytest.mark.parametrize('tensor', ['scalar', 'vector', 'tensor'])
