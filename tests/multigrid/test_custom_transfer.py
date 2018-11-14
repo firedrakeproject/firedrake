185df225f2978d53512200d33001ad318459d458
Lawrence Mitchell <lawrence@wence.uk>
2018-11-14 09:21:09 +0000

    mg: Add standalone test of multiple transfer operators

--- tests/multigrid/test_custom_transfer.py
+++ tests/multigrid/test_custom_transfer.py
@@ -1,5 +1,9 @@
 import pytest
+import numpy
+from functools import partial
+from itertools import count
 from firedrake import *
+from firedrake.mg.ufl_utils import coarsen
 
 
 def test_repeated_custom_transfer():
@@ -157,3 +161,45 @@ def test_custom_transfer_setting():
         solver.set_transfer_operators(dmhooks.transfer_operators(V, prolong=myprolong))
 
     assert count == 1
+
+
+def test_transfers_transferring():
+    mesh = UnitIntervalMesh(2)
+    mh = MeshHierarchy(mesh, 1)
+
+    mesh = mh[-1]
+
+    Vf = FunctionSpace(mesh, "P", 1)
+    Qf = FunctionSpace(mesh, "DP", 0)
+    Wf = Vf*Qf
+
+    Vcount = count()
+    Qcount = count()
+    Wcount = count()
+
+    def myinject(f, c, counter=None):
+        next(counter)
+        inject(f, c)
+
+    injectQ = partial(myinject, counter=Qcount)
+    injectV = partial(myinject, counter=Vcount)
+    injectW = partial(myinject, counter=Wcount)
+
+    f = Function(Wf)
+    f.assign(1)
+
+    with dmhooks.transfer_operators(Wf, inject=injectW):
+        c = coarsen(f, coarsen)
+        assert next(Wcount) == 1
+        for d in c.split():
+            assert numpy.allclose(d.dat.data_ro, 1)
+
+    f.sub(0).assign(2)
+    f.sub(1).assign(3)
+    with dmhooks.transfer_operators(Wf.sub(0), inject=injectV), dmhooks.transfer_operators(Wf.sub(1), inject=injectQ):
+        c = coarsen(f, coarsen)
+        assert next(Vcount) == 1
+        assert next(Qcount) == 1
+
+        assert numpy.allclose(c.sub(0).dat.data_ro, 2)
+        assert numpy.allclose(c.sub(1).dat.data_ro, 3)
