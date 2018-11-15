dc53d2cb08d0826cab372c843c74d4f4709da70b
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-11-15 13:23:35 +0000

    Specify which fields to eliminate in HDG test

--- tests/slate/test_hdg_poisson.py
+++ tests/slate/test_hdg_poisson.py
@@ -72,6 +72,7 @@ def run_LDG_H_problem(r, degree, quads=False):
               'ksp_type': 'preonly',
               'pc_type': 'python',
               'pc_python_type': 'firedrake.SCPC',
+              'pc_sc_eliminate_fields': '0, 1',
               'condensed_field': {'ksp_type': 'preonly',
                                   'pc_type': 'lu',
                                   'pc_factor_mat_solver_type': 'mumps'}}
