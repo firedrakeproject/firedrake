f9fa708c5d40555ec25cf7dcb1c2fc512f1d9ce5
Florian Wechsung <f.wechsung@gmail.com>
2018-11-25 19:26:39 +0000

    some fixes for bcs on components of vector spaces

--- firedrake/solving_utils.py
+++ firedrake/solving_utils.py
@@ -6,6 +6,7 @@ from firedrake.exceptions import ConvergenceError
 from firedrake.petsc import PETSc
 from firedrake.formmanipulation import ExtractSubBlock
 from firedrake.utils import cached_property
+from ufl import VectorElement
 
 
 def _make_reasons(reasons):
@@ -210,8 +211,12 @@ class _SNESContext(object):
                 Jp = None
             bcs = []
             for bc in problem.bcs:
-                index = bc.function_space().index
-                cmpt = bc.function_space().component
+                Vbc = bc.function_space()
+                if Vbc.parent is not None and isinstance(Vbc.parent.ufl_element(), VectorElement):
+                    index = Vbc.parent.index
+                else:
+                    index = Vbc.index
+                cmpt = Vbc.component
                 # TODO: need to test this logic
                 if index in field:
                     if len(field) == 1:
