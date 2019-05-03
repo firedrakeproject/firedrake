93834fa11628c946954c05501d37c69714dda16f
nbouziani <nacime.bouziani@gmail.com>
2019-05-03 14:44:23 +0100

    PointwiseOperator update

--- firedrake/pointwise_operators.py
+++ firedrake/pointwise_operators.py
@@ -18,6 +18,10 @@ class AbstractPointwiseOperator(Function, ExternalOperator, metaclass=ABCMeta):
         self.operator_data = operator_data
 
     @abstractmethod
+    def compute_derivatives(self):
+        """apply the derivatives on operator_data"""
+
+    @abstractmethod
     def evaluate(self):
         """define the evaluation method for the ExternalOperator object"""
 
@@ -37,6 +41,8 @@ class AbstractPointwiseOperator(Function, ExternalOperator, metaclass=ABCMeta):
 
 
 class PointexprOperator(AbstractPointwiseOperator):
+    r"""A :class:`PointexprOperator` ... TODO :
+     """
 
     def __init__(self, *operands, eval_space, derivatives=None, shape=None, count=None, val=None, name=None, dtype=ScalarType, point_expr):
         AbstractPointwiseOperator.__init__(self, *operands, eval_space=eval_space, derivatives=derivatives, shape=shape, count=count, val=val, name=name, dtype=dtype, operator_data=point_expr)
@@ -45,24 +51,99 @@ class PointexprOperator(AbstractPointwiseOperator):
         if not isinstance(point_expr, types.FunctionType):
             error("Expecting a FunctionType pointwise expression")
 
+    def compute_derivatives(self):
+        try:
+            import sympy as sp  # Reference used to avoid conflict between the sympy diff object and the UFL one
+        except ImportError:
+            raise ImportError("Error when trying to import Sympy")
+
+        deriv_index = self.derivatives
+        if deriv_index == (0,)*len(deriv_index):
+            return self.operator_data
+
+        symb = sp.symbols(' '.join(chr(i) for i in range(97, 97+len(self.ufl_operands))))
+        der = []
+        for i, di in enumerate(deriv_index):
+            if di != 0:
+                der.append(symb[i] * di)
+        r = sp.diff(self.operator_data(*symb), *der)
+        return sp.lambdify(symb, r)
+
     def evaluate(self):
         operands = self.ufl_operands
-        expr = self.operator_data(*operands)
+        operator = self.compute_derivatives()
+        expr = operator(*operands)
         return self.interpolate(expr)
 
 
 class PointsolveOperator(AbstractPointwiseOperator):
+    r"""A :class:`PointsolveOperator` ... TODO :
 
-    def __init__(self, *operands, eval_space, derivatives=None, shape=None, count=None, val=None, name=None, dtype=ScalarType, point_expr):
-        AbstractPointwiseOperator.__init__(self, *operands, eval_space=eval_space, derivatives=derivatives, shape=shape, count=count, val=val, name=name, dtype=dtype, operator_data=point_expr)
+        scipy syntax : newton_params = {'fprime':None, 'args':(), 'tol':1.48e-08, 'maxiter':50, 'fprime2':None, 'x1':None,
+                             'rtol':0.0, 'full_output':False, 'disp':True}
+        https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.newton.html
+     """
+
+    def __init__(self, *operands, eval_space, derivatives=None, shape=None, count=None, val=None, name=None, dtype=ScalarType, point_solve, params, solver):
+        AbstractPointwiseOperator.__init__(self, *operands, eval_space=eval_space, derivatives=derivatives, shape=shape, count=count, val=val, name=name, dtype=dtype, operator_data=point_solve)
 
         # Check
-        if not isinstance(point_expr, types.FunctionType):
+        if not isinstance(point_solve, types.FunctionType):
             error("Expecting a FunctionType pointwise expression")
 
+        if not (isinstance(params, tuple) or params <= point_solve.__code__.co_varnames):
+            error("Expecting a tuple subset of %s instead of : %s" % (point_solve.__code__.co_varnames, params))
+        if len(params) != len(operands):
+            error("Expecting %s operands" % len(params))
+        if point_solve.__code__.co_argcount - len(operands) > 1:
+            error("Multidimensional Newton : Not yet impelemented")  # Needed feature ?
+
+        self.params = params
+
+        if not isinstance(solver, dict):
+            error("Expecting a dict with the solver arguments instead of %s" % solver)
+        elif 'x0' not in solver.keys():
+            error("Expecting an initial condition x0")
+
+        self.solver = solver
+
+    def compute_derivatives(self, f):
+        from ufl.operators import diff
+        deriv_index = self.derivatives
+        df = f
+        if deriv_index == (0,)*len(deriv_index):
+            return df
+
+        for i, di in enumerate(deriv_index):
+            op = self.ufl_operands[i]
+            for j in range(di):
+                df = diff(df, op)
+        return df
+
     def evaluate(self):
-        # TODO: Use Newton-type solver
-        raise NotImplementedError("TODO !")
+        try:
+            from scipy import optimize
+        except ImportError:
+            raise ImportError("Error when trying to import Scipy.optimize")
+
+        space = self.ufl_function_space()
+        solver = self.solver
+
+        # Pre-processing to get the values of the initial guesses
+        solver['x0'] = Function(space).interpolate(solver['x0']).dat.data
+        if 'x1' in solver.keys():
+            solver['x1'] = Function(space).interpolate(solver['x1']).dat.data
+
+        # Vectorized Newton
+        args = (Function(space).interpolate(pi) for pi in self.ufl_operands)
+        vals = tuple(coeff.dat.data for coeff in args)
+        g = partial(self.operator_data, **dict(zip(self.params, vals)))
+
+        result = Function(space)
+        result.dat.data[:] = optimize.newton(g, **self.solver)
+        result = self.compute_derivatives(result)
+
+        return self.assign(result)
 
 
 def PointExprOp(*operands, eval_space, derivatives=None, count=None, val=None, name=None, dtype=ScalarType, point_expr):
@@ -72,3 +153,11 @@ def PointExprOp(*operands, eval_space, derivatives=None, count=None, val=None, n
 
 def point_expr(point_expr):
     return partial(PointExprOp, point_expr=point_expr)
+
+
+def PointSolveOp(*operands, eval_space, derivatives=None, count=None, shape=(), val=None, name=None, dtype=ScalarType, point_solve, params, solver):
+    return PointsolveOperator(*operands, eval_space=eval_space, derivatives=derivatives, shape=shape, count=count, val=val, name=name, dtype=dtype, point_solve=point_solve, params=params, solver=solver)
+
+
+def point_solve(point_solve, solver, params=()):
+    return partial(PointSolveOp, point_solve=point_solve, params=params, solver=solver)
--- tests/regression/test_external_operator.py
+++ tests/regression/test_external_operator.py
@@ -1,5 +1,6 @@
 import pytest
 from firedrake import *
+import numpy as np
 
 
 @pytest.fixture(scope='module')
@@ -10,12 +11,11 @@ def mesh():
 def test_pointwise_operator(mesh):
     V = FunctionSpace(mesh, "CG", 1)
     P = FunctionSpace(mesh, "DG", 0)
-    v = Function(V)
-    u = Function(V)
 
     x, y = SpatialCoordinate(mesh)
-    u.interpolate(cos(x))
-    v.interpolate(sin(x))
+
+    v = Function(V).interpolate(sin(x))
+    u = Function(V).interpolate(cos(x))
 
     m = u*v
     a1 = m*dx
@@ -30,6 +30,72 @@ def test_pointwise_operator(mesh):
     assert p2.derivatives == (0, 0)
     assert p2.ufl_shape == ()
     assert p2.operator_data(u, v) == u*v
+
+    assemble_a1 = assemble(a1)
+    assemble_a2 = assemble(a2)
+
+    assert abs(assemble_a1 - assemble_a2) < 1.0e-3  # Not evaluate on the same space whence the lack of precision
+
+
+def test_pointwise_solver(mesh):
+    V = FunctionSpace(mesh, "CG", 1)
+    P = FunctionSpace(mesh, "DG", 0)
+
+    x, y = SpatialCoordinate(mesh)
+
+    uexact = Function(P).assign(1)
+
+    m = uexact
+    a1 = m*dx
+
+    a = Function(V).assign(0)
+    b = Function(V).assign(1)
+    c = Function(V).assign(1)
+
+    x0 = Function(V).assign(1.3)
+    fprime = lambda x: 2*(x-1) + 400*x*(x**2 - 1)
+    newton_params = {'x0': x0, 'fprime': fprime, 'maxiter': 30}
+
+    # Conflict with ufl if we use directly cos()
+    p = point_solve(lambda x, y, m1, m2: np.cos(m1)*(1-x)**2 + 100*m2*(y-x**2)**2, solver=newton_params, params=('y', 'm2', 'm1'))
+    p2 = p(c, b, a, eval_space=P)  # Rosenbrock function for (m1,m2) = (0,1), the global minimum is reached in (1,1)
+    a2 = p2*dx
+
+    assert p2.ufl_operands == (c, b, a)
+    assert p2._ufl_function_space == P
+    assert p2.derivatives == (0, 0, 0)
+    assert p2.ufl_shape == ()
+    assert p2.params == ('y', 'm2', 'm1')
+    assert p2.solver == newton_params
+
+    assemble_a1 = assemble(a1)
+    assemble_a2 = assemble(a2)
+
+    assert abs(assemble_a1 - assemble_a2) < 1.0e-7
+
+
+def test_compute_derivatives(mesh):
+    V = FunctionSpace(mesh, "CG", 1)
+    P = FunctionSpace(mesh, "DG", 0)
+
+    x, y = SpatialCoordinate(mesh)
+
+    v = Function(V).interpolate(sin(x))
+    u = Function(V).interpolate(cos(x))
+
+    m = u*v
+    a1 = m*dx
+
+    p = point_expr(lambda x, y: 0.5*x**2*y)
+    p2 = p(u, v, eval_space=P, derivatives=(1, 0))
+    a2 = p2*dx
+
+    assert p2.ufl_operands[0] == u
+    assert p2.ufl_operands[1] == v
+    assert p2._ufl_function_space == P
+    assert p2.derivatives == (1, 0)
+    assert p2.ufl_shape == ()
+    assert p2.operator_data(u, v) == 0.5*u**2*v
 
     assemble_a1 = assemble(a1)
     assemble_a2 = assemble(a2)
