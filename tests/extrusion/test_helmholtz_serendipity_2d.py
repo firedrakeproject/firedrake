14513aec0d0a3633031438a4ca85902fac6cdb22
cyruscycheng21 <cyruscycheng21@gmail.com>
2019-05-30 16:12:02 +0100

    new serendipity tests

--- tests/extrusion/test_helmholtz_serendipity_2d.py
+++ tests/extrusion/test_helmholtz_serendipity_2d.py
@@ -6,30 +6,30 @@ from firedrake import *
 
 
 @pytest.mark.parametrize(('testcase', 'convrate'),
-                         [#(("S", 1, (4, 6)), 0.9),
-                          #(("S", 2, (3, 5)), 1.9),
-                          #(("S", 3, (2, 4)), 2.9),
-                          #(("S", 4, (2, 4)), 3.8),
-                          (("S", 5, (2, 4)), 4.7),
-                          (("S", 6, (2, 4)), 5.8)])
+                         [(("S", 1, (4, 6)), 1.9),
+                          (("S", 2, (3, 5)), 2.9),
+                          (("S", 3, (2, 4)), 3.9),
+                          (("S", 4, (2, 4)), 4.8),
+                          (("S", 5, (2, 4)), 5.7)])
 def test_scalar_convergence(extmesh, testcase, convrate):
     family, degree, (start, end) = testcase
     l2err = np.zeros(end - start)
     for ii in [i + start for i in range(len(l2err))]:
-        mesh = extmesh(2**ii, 2**ii, 2**ii, quadrilateral=True)
+        mesh = UnitIntervalMesh(2**ii)
+        mesh = ExtrudedMesh(mesh, 2**ii)
 
         fspace = FunctionSpace(mesh, family, degree)
 
         u = TrialFunction(fspace)
         v = TestFunction(fspace)
 
-        x, y, z = SpatialCoordinate(mesh)
+        x, y = SpatialCoordinate(mesh)
 
-        uex = cos(2*np.pi*x)*cos(2*np.pi*y)*cos(2*np.pi*z)
+        uex = cos(2*np.pi*x)*cos(2*np.pi*y)
         f = -div(grad(uex)) + uex
 
-        a = (inner(grad(u), grad(v)) + inner(u, v))*dx(degree=degree+7)
-        L = inner(f, v)*dx(degree=degree+7)
+        a = (inner(grad(u), grad(v)) + inner(u, v))*dx(degree=2*degree)
+        L = inner(f, v)*dx(degree=2*degree)
 
         params = {"snes_type": "ksponly",
                   "ksp_type": "preonly",
@@ -38,6 +38,5 @@ def test_scalar_convergence(extmesh, testcase, convrate):
         sol = Function(fspace)
         solve(a == L, sol, solver_parameters=params)
 
-        l2err[ii - start] = errornorm(sol, uex, norm_type="H1")
-        print(l2err)
+        l2err[ii - start] = sqrt(assemble((sol-uex)*(sol-uex)*dx))
     assert (np.array([np.log2(l2err[i]/l2err[i+1]) for i in range(len(l2err)-1)]) > convrate).all()
