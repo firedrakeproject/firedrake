45307db0b142bb98c266077a1f80b83863f6d75b
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-07-05 16:42:49 +0100

    Push context and DM for coarse solver

--- firedrake/preconditioners/gtmg.py
+++ firedrake/preconditioners/gtmg.py
@@ -1,6 +1,6 @@
 from firedrake.petsc import PETSc
 from firedrake.preconditioners.base import PCBase
-from firedrake.dmhooks import get_function_space, get_appctx, push_appctx, pop_appctx
+from firedrake.dmhooks import get_appctx, push_appctx, pop_appctx
 
 
 __all__ = ['GTMGPC']
@@ -17,15 +17,18 @@ class GTMGPC(PCBase):
         from firedrake.assemble import allocate_matrix, create_assembly_callable
         from firedrake.interpolation import Interpolator
 
+        _, P = pc.getOperators()
+        context = P.getPythonContext()
+
+        if pc.getType() != "python":
+            raise ValueError("Expecting PC type python")
+        opc = pc
+        appctx = self.get_appctx(pc)
+        fcp = appctx.get("form_compiler_parameters")
+
         prefix = pc.getOptionsPrefix()
         options_prefix = prefix + self._prefix
         opts = PETSc.Options()
-
-        _, P = pc.getOperators()
-        opc = pc
-        context = P.getPythonContext()
-        appctx = context.appctx
-        fcp = appctx.get("form_compiler_parameters")
 
         # Handle the fine operator
         fine_mat_type = opts.getString(options_prefix + "mat_type",
@@ -89,13 +92,13 @@ class GTMGPC(PCBase):
         # Set nullspace if provided
         if get_coarse_nullspace:
             coarse_opmat.setNullSpace(get_coarse_nullspace())
+
         if get_coarse_transpose_nullspace:
             coarse_opmat.setTransposeNullSpace(get_coarse_transpose_nullspace())
+
         interp_petscmat = appctx.get("interpolation_matrix", None)
         if interp_petscmat is None:
             # Create interpolation matrix from coarse space to fine space
-            # fine_space = get_function_space(opc.getDM())
-            # import ipdb; ipdb.set_trace()
             fine_space = context.a.arguments()[0].function_space()
             interpolator = Interpolator(TestFunction(coarse_space), fine_space)
             interpolation_matrix = interpolator.callable()
@@ -105,46 +108,47 @@ class GTMGPC(PCBase):
         # We set up a PCMG object that uses the constructed interpolation
         # matrix to generate the restriction/prolongation operators.
         # This is a two-level multigrid preconditioner.
-        pcmg = PETSc.PC().create(comm=pc.comm)
-        pcmg.incrementTabLevel(1, parent=opc)
-        pcmg.setType(pcmg.Type.MG)
-        pcmg.setOptionsPrefix(options_prefix)
-        pcmg.setMGLevels(2)
-        pcmg.setMGInterpolation(1, interp_petscmat)
-        pcmg.setOperators(fine_petscmat)
+        pc = PETSc.PC().create(comm=opc.comm)
+        pc.incrementTabLevel(1, parent=opc)
 
-        # # Now we configure the coarse solver. We need to set a DM
-        # # and an appropriate SNESContext on the pc so we can do, for example,
-        # # geometric multigrid on the coarse solve.
-        # from firedrake.variational_solver import NonlinearVariationalProblem
-        # from firedrake.solving_utils import _SNESContext
-        # from firedrake.function import Function
-        # from firedrake.ufl_expr import action
-        # import ipdb
+        pc.setType(pc.Type.MG)
+        pc.setOptionsPrefix(options_prefix)
+        pc.setMGLevels(2)
+        pc.setMGInterpolation(1, interp_petscmat)
+        pc.setOperators(fine_petscmat, fine_petscmat)
 
-        # ipdb.set_trace()
-        # dm = opc.getDM()
-        # oappctx = get_appctx(dm)
-        # ipdb.set_trace()
-        # cu = Function(coarse_space)
-        # cF = action(coarse_operator, cu)
-        # nproblem = NonlinearVariationalProblem(cF, cu, coarse_space_bcs,
-        #                                        J=coarse_operator,
-        #                                        form_compiler_parameters=fcp)
-        # ncontext = _SNESContext(nproblem, coarse_mat_type, coarse_mat_type,
-        #                         oappctx.appctx)
-        # push_appctx(dm, ncontext)
-        # self._ctx_ref = ncontext
+        coarse_solver = pc.getMGCoarseSolve()
+        coarse_solver.setOperators(coarse_opmat, coarse_opmat)
 
-        # ipdb.set_trace()
+        # We set a DM and an appropriate SNESContext for the coarse solver
+        # so we can do multigrid or patch solves.
+        from firedrake.variational_solver import NonlinearVariationalProblem
+        from firedrake.solving_utils import _SNESContext
+        from firedrake.function import Function
+        from firedrake.ufl_expr import action
 
-        # pcmg.setDM(dm)
-        coarse_solver = pcmg.getMGCoarseSolve()
-        coarse_solver.setOperators(coarse_opmat)
-        pcmg.setFromOptions()
-        pcmg.setUp()
-        self._pcmg = pcmg
-        # pop_appctx(dm)
+        dm = opc.getDM()
+        octx = get_appctx(dm)
+        coarse_tmp = Function(coarse_space)
+        F = action(coarse_operator, coarse_tmp)
+        nprob = NonlinearVariationalProblem(F, coarse_tmp,
+                                            bcs=coarse_space_bcs,
+                                            J=coarse_operator,
+                                            form_compiler_parameters=fcp)
+        nctx = _SNESContext(nprob, coarse_mat_type, coarse_mat_type, octx.appctx)
+        self._ctx_ref = nctx
+
+        # Push new context onto the coarse space dm
+        coarse_dm = coarse_space.dm
+        push_appctx(coarse_dm, nctx)
+
+        coarse_solver.setDM(coarse_dm)
+        coarse_solver.setDMActive(False)
+        pc.setFromOptions()
+        pc.setUp()
+        self.pc = pc
+        self.coarse_solver = coarse_solver
+        pop_appctx(coarse_dm)
 
     def update(self, pc):
         self._assemble_fine_op()
@@ -153,11 +157,17 @@ class GTMGPC(PCBase):
         self.coarse_op.force_evaluation()
 
     def apply(self, pc, X, Y):
-        self._pcmg.apply(X, Y)
+        dm = self.coarse_solver.getDM()
+        push_appctx(dm, self._ctx_ref)
+        self.pc.apply(X, Y)
+        pop_appctx(dm)
 
     def applyTranspose(self, pc, X, Y):
-        self._pcmg.applyTranspose(X, Y)
+        dm = self.coarse_solver.getDM()
+        push_appctx(dm, self._ctx_ref)
+        self.pc.applyTranspose(X, Y)
+        pop_appctx(dm)
 
     def view(self, pc, viewer=None):
         super(GTMGPC, self).view(pc, viewer)
-        self._pcmg.view(viewer)
+        self.pc.view(viewer)
