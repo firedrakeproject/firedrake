aa916d5a62dc61ad521e8c4627412f1238fafd19
David Ham <david.ham@imperial.ac.uk>
2019-08-07 15:36:32 +0100

    Skip test not expected to pass

--- tests/regression/test_auxiliary_dm.py
+++ tests/regression/test_auxiliary_dm.py
@@ -1,5 +1,6 @@
 from firedrake.petsc import *
 from firedrake import *
+import pytest
 distribution_parameters = {"partition": True, "overlap_type": (DistributedMeshOverlapType.VERTEX, 2)}
 
 
@@ -43,7 +44,7 @@ class BiharmonicProblem(object):
         f = div(grad(div(grad(u_exact)))) + u_exact
         return f
 
-
+@pytest.mark.skipif(utils.complex_mode, reason="Differentiation of energy not defined in Complex.")
 def test_auxiliary_dm():
     problem = BiharmonicProblem(5, 1)
     mesh = problem.mesh()
