ad0f2fb41a4d49765b27d28898ab76537dab5c4f
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-08-21 11:20:48 +0100

    Remove hypercube reconstruction

--- tsfc/fiatinterface.py
+++ tsfc/fiatinterface.py
@@ -144,17 +144,6 @@ def convert_finiteelement(element, vector_is_mixed):
             lmbda = FIAT.GaussLegendre
         else:
             raise ValueError("Variant %r not supported on %s" % (kind, element.cell()))
-    elif element.family() in ["DPC", "DPC L2"]:
-        if element.cell().geometric_dimension() == 2:
-            element = element.reconstruct(cell=ufl.hypercube(2))
-        elif element.cell.geometric_dimension() == 3:
-            element = element.reconstruct(cell=ufl.hypercube(3))
-    elif element.family() == "S":
-        if element.cell().geometric_dimension() == 2:
-            element = element.reconstruct(cell=ufl.hypercube(2))
-        elif element.cell().geometric_dimension() == 3:
-            element = element.reconstruct(cell=ufl.hypercube(3))
-
     return lmbda(cell, element.degree())
 
 
