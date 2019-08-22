abe430c60d6c65e23f11ad8324af0cfde5a53d45
Lawrence Mitchell <lawrence@wence.uk>
2019-08-22 15:02:46 +0100

    mesh: Remove creation of UID, and let UFL create it

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -990,9 +990,8 @@ class MeshGeometry(ufl.Mesh):
         """Create mesh geometry object."""
         utils._init()
         mesh = super(MeshGeometry, cls).__new__(cls)
-        mesh.uid = utils._new_uid()
         assert isinstance(element, ufl.FiniteElementBase)
-        ufl.Mesh.__init__(mesh, element, ufl_id=mesh.uid)
+        ufl.Mesh.__init__(mesh, element)
         return mesh
 
     def __init__(self, coordinates):
