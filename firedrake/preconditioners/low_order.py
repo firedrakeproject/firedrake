6d03cdaba4f41a03b84b6b8fee07eedc9045e5f1
Miklós Homolya <miklos.homolya@tum.de>
2019-08-26 20:59:15 +0200

    Eliminate a recently added reference to SCALAR_TYPE

--- firedrake/preconditioners/low_order.py
+++ firedrake/preconditioners/low_order.py
@@ -34,9 +34,10 @@ def transfer_kernel(Pk, P1):
     """
     # Mapping of a residual in Pk into a residual in P1
     from coffee import base as coffee
-    from tsfc.coffee import generate as generate_coffee, SCALAR_TYPE
+    from tsfc.coffee import generate as generate_coffee
     from tsfc.parameters import default_parameters
     from gem import gem, impero_utils as imp
+    from firedrake.utils import ScalarType_c
 
     # Pk should be at least the same size as P1
     assert Pk.finat_element.space_dimension() >= P1.finat_element.space_dimension()
@@ -58,7 +59,7 @@ def transfer_kernel(Pk, P1):
 
     shape = (P1e.space_dimension() * Vout.value_size,
              Pke.space_dimension() * Vin.value_size)
-    outarg = coffee.Decl(SCALAR_TYPE, coffee.Symbol("A", rank=shape))
+    outarg = coffee.Decl(ScalarType_c, coffee.Symbol("A", rank=shape))
     i = gem.Index()
     j = gem.Index()
     k = gem.Index()
