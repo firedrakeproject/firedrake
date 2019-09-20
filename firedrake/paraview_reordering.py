aeffcb4facce7c4bad55dcabfb15ae0aa31324b9
Teodoro Fields Collin <teocollin1995@gmail.com>
2019-09-20 11:50:53 -0500

    first pass at adding vtk

--- firedrake/paraview_reordering.py
+++ firedrake/paraview_reordering.py
@@ -1,6 +1,7 @@
 from tsfc.fiatinterface import create_element
 import numpy as np
 from pyop2.utils import as_tuple
+import vtk
 
 
 def firedrake_local_to_cart(element):
@@ -33,159 +34,14 @@ def invert(list1, list2):
 
 
 """
-The following functions are translations of funtions in the VTK source;we use them find the order of nodes in the lagrange bases that Paraview uses. We don't document them fully, but link back to the vtk source.
-These come from VTK version 8.2.0.
-We could simplify all of this by depending on VTK's python bindings.
+The following functions wrap around functions provided by vtk (pip install vtk). The vtk functions do one of two things:
+
+1. They convert (i,j,k) indecies, locations on a rect or hex, to index into the dofs.
+
+2. They convert an index into a dofs into an index into some sort of space (barycentric indecies).
+
+The follow functions call the two types of VTK functions and use them to produce maps from dof indicies to node locations. These functions are then wrapped to be applied to ufl_elements after the next block commment.
 """
-
-
-def triangle_barycentric_index(index, order):
-    r"""
-    See vtkLagrangeTriangle::BarycentricIndex
-    """
-    idxmax = order
-    idxmin = 0
-
-    bindex = [0, 0, 0]
-
-    while (index != 0 and index >= 3 * order):
-        index -= 3 * order
-        idxmax -= 2
-        idxmin += 1
-        order -= 3
-
-    if (index < 3):
-        bindex[index] = idxmin
-        bindex[(index + 1) % 3] = idxmin
-        bindex[(index + 2) % 3] = idxmax
-    else:
-        index -= 3
-        dim = index // (order - 1)
-        offset = (index - dim * (order - 1))
-        bindex[(dim + 1) % 3] = idxmin
-        bindex[(dim + 2) % 3] = (idxmax - 1) - offset
-        bindex[dim] = (idxmin + 1) + offset
-    return bindex
-
-
-def tet_barycentric_index(index, order):
-    r"""
-    See vtkLagrangeTetra::BarycentricIndex
-    """
-    idxmax = order
-    idxmin = 0
-    VertexMaxCoords = [3, 0, 1, 2]
-    LinearVertices = [[0, 0, 0, 1], [1, 0, 0, 0],
-                      [0, 1, 0, 0], [0, 0, 1, 0]]
-    EdgeVertices = [[0, 1], [1, 2], [2, 0], [0, 3],
-                    [1, 3], [2, 3]]
-    FaceBCoords = [[0, 2, 3], [2, 0, 1],
-                   [2, 1, 3], [1, 0, 3]]
-    FaceMinCoord = [1, 3, 0, 2]
-    bindex = [0, 0, 0, 0]
-    # Can this condition ever fire?
-    while (index >= 2 * (order * order + 1) and index != 0 and order > 3):
-        index -= 2 * (order * order + 1)
-        idxmax -= 3
-        idxmin += 1
-        order -= 4
-    if (index < 4):
-        # we are on a vertex
-        for i in range(4):
-            bindex[i] = (idxmax if VertexMaxCoords[index] == i else idxmin)
-        return bindex
-    elif ((index - 4) < 6 * (order - 1)):
-        # we are on an edge
-        edgeId = (index - 4) // (order - 1)
-        vertexId = (index - 4) % (order - 1)
-        for coord in range(4):
-            temp1 = LinearVertices[EdgeVertices[edgeId][0]][coord]
-            temp2 = (idxmax - idxmin - 1 - vertexId)
-            temp3 = LinearVertices[EdgeVertices[edgeId][1]][coord]
-            temp4 = (1 + vertexId)
-            bindex[coord] = idxmin + (temp1 * temp2 + temp3 * temp4)
-
-        return bindex
-    else:
-        # we are on a face
-        faceId = (index - 4 - 6 * (order - 1)) // ((order - 2) * (order - 1) // 2)
-        vertexId = (index - 4 - 6 * (order - 1)) % ((order - 2) * (order - 1) // 2)
-        pbindex = [0, 0, 0]
-        if (order != 3):
-            pbindex = triangle_barycentric_index(vertexId, order - 3)
-        for i in range(3):
-            bindex[FaceBCoords[faceId][i]] = (idxmin + 1 + pbindex[i])
-        bindex[FaceMinCoord[faceId]] = idxmin
-        return bindex
-
-
-def qsynatx(test, t, f):
-    if test:
-        return t
-    else:
-        return f
-
-
-def vtk_hex_point_index_from_ijk(i, j, k, order=None):
-    r"""
-    See vtkLagrangeHexahedron::PointIndexFromIJK
-    """
-    ibdy = (i == 0 or i == order[0])
-    jbdy = (j == 0 or j == order[1])
-    kbdy = (k == 0 or k == order[2])
-    nbdy = int(jbdy) + int(kbdy) + int(ibdy)
-
-    if nbdy == 3:
-        # return vertex
-        # interprets:  (i ? (j ? 2 : 1) : (j ? 3 : 0)) + (k ? 4 : 0);
-        ret = 4 if k != 0 else 0
-        if i != 0:
-            ret += 2 if j != 0 else 1
-        else:
-            ret += 3 if j != 0 else 0
-        return ret
-    offset = 8
-    if nbdy == 2:  # edge
-        if not ibdy:  # on the i axis
-            offset += i - 1
-            offset += (order[0] - 1 + order[1] - 1 if j != 0 else 0)
-            offset += 2 * (order[0] - 1 + order[1] - 1) if k != 0 else 0
-            return offset
-        elif not jbdy:  # on the j axis
-            offset += j - 1
-            offset += order[0] - 1 if i != 0 else 2 * (order[0] - 1) + order[1] - 1
-            offset += 2 * (order[0] - 1 + order[1] - 1) if k != 0 else 0
-            return offset
-        else:  # on the k axis
-            offset += 4 * (order[0] - 1) + 4 * (order[1] - 1)
-            offset += k - 1
-            offset += (order[2] - 1) * ((3 if j != 0 else 1) if i != 0 else (2 if j != 0 else 0))
-            return offset
-    offset += 4 * (order[0] - 1 + order[1] - 1 + order[2] - 1)
-    if nbdy == 1:  # face
-        if ibdy:
-            offset += j - 1
-            offset += (order[1] - 1) * (k-1)
-            offset += qsynatx(i != 0, (order[1] - 1) * (order[2] - 1), 0)
-            return offset
-        offset += 2 * (order[1] - 1) * (order[2] - 1)
-        if jbdy:
-            offset += i - 1
-            offset += (order[0] - 1) * (k - 1)
-            offset += qsynatx(j != 0, (order[2] - 1) * (order[0] - 1), 0)
-            return offset
-        else:
-            offset += 2 * (order[2] - 1) * (order[0] - 1)
-            offset += i - 1
-            offset += ((order[0] - 1) * (j - 1))
-            offset += qsynatx(k != 0, (order[0] - 1) * (order[1] - 1), 0)
-            return offset
-    offset += 2 * ((order[1] - 1) * (order[2] - 1)
-                   + (order[2] - 1) * (order[0] - 1)
-                   + (order[0] - 1) * (order[1] - 1))
-    offset += (i - 1)
-    offset += (order[0] - 1) * ((j - 1) + (order[1] - 1) * (k - 1))
-    return offset
 
 
 def vtk_interval_local_coord(i, order):
@@ -200,99 +56,6 @@ def vtk_interval_local_coord(i, order):
         return i / order
 
 
-def vtk_quad_index_from_ij(i, j, order):
-    r"""
-    See vtkLagrangeQuadrilateral::PointIndexFromIJK
-    """
-    ibdy = (i == 0 or i == order[0])
-    jbdy = (j == 0 or j == order[1])
-
-    nbdy = int(ibdy) + int(jbdy)
-
-    if (nbdy == 2):
-        return qsynatx(i != 0, qsynatx(j != 0, 2, 1), qsynatx(j != 0, 3, 0))
-
-    offset = 4
-    if nbdy == 1:
-        if not ibdy:
-            offset += i - 1
-            offset += qsynatx(j != 0, order[0] - 1 + order[1] - 1, 0)
-            return offset
-        if not jbdy:
-            offset += j - 1
-            offset += qsynatx(i != 0, order[0] - 1, 2 * (order[0] - 1) + order[1] - 1)
-            return offset
-
-    offset += 2 * (order[0] - 1 + order[1] - 1)
-    return offset + (i - 1) + (order[0] - 1) * (j - 1)
-
-
-def triangle_dof_offset(order, i, j):
-    r"""
-    See vtkLagrangeWedge::PointIndexFromIJK
-    """
-    return i + order * (j - 1) - (j * (j + 1)) // 2
-
-
-def wedge_point_index_from_ijk(i, j, k, order):
-    r"""
-    See vtkLagrangeWedge::PointIndexFromIJK
-    """
-    rsOrder = order[0]
-    rm1 = rsOrder - 1
-    tOrder = order[2]
-    tm1 = tOrder - 1
-    ibdy = i == 0
-    jbdy = j == 0
-    ijbdy = (i + j) == rsOrder
-    kbdy = (k == 0 or k == tOrder)
-    nbdy = int(ibdy) + int(jbdy) + int(ijbdy) + int(kbdy)
-    if (i < 0 or i > rsOrder or j < 0 or j > rsOrder or i + j > rsOrder or k < 0 or k > tOrder):
-        return -1
-    if nbdy == 3:
-        return qsynatx(ibdy and jbdy, 0, qsynatx(jbdy and ijbdy, 1, 2)) +\
-            qsynatx(k != 0, 3, 0)
-
-    offset = 6
-    if nbdy == 2:
-        if not kbdy:
-            offset += rm1 * 6
-            offset += k - 1
-            offset += qsynatx(ibdy and jbdy, 0, qsynatx(jbdy and ijbdy, 1, 2)) * tm1
-            return offset
-        else:
-            offset += qsynatx(k == tOrder, 3 * rm1, 0)
-            if jbdy:
-                return offset + i - 1
-            offset += rm1
-            if ijbdy:
-                return offset + j - 1
-            offset += rm1
-            return offset + (rsOrder - j - 1)
-    offset += 6 * rm1 + 3 * tm1
-
-    ntfdof = (rm1 - 1) * rm1 // 2
-    nqfdof = rm1 * tm1
-    if nbdy == 1:
-        if kbdy:
-            if k > 0:
-                offset += ntfdof
-            return offset + triangle_dof_offset(rsOrder, i, j)
-        offset += 2 * ntfdof
-
-        if jbdy:
-            return offset + (i - 1) + rm1 * (k - 1)
-        offset += nqfdof
-        if ijbdy:
-            return offset + (rsOrder - i - 1) + rm1 * (k - 1)
-        offset += nqfdof
-        return offset + j - 1 + rm1 * (k - 1)
-
-    offset += 2 * ntfdof + 3 * nqfdof
-
-    return offset + triangle_dof_offset(rsOrder, i, j) + ntfdof * (k - 1)
-
-
 def bar_to_cart_3d(bar):
     v0 = np.array([0, 0, 0])
     v1 = np.array([1, 0, 0])
@@ -302,14 +65,23 @@ def bar_to_cart_3d(bar):
     return np.dot(bar, mat)
 
 
+def tet_barycentric_index(tet, index, order):
+    """
+    Wrapper for vtkLagrangeTetra::BarycentricIndex.
+    """
+    bindex = [-1, -1, -1, -1]
+    tet.BarycentricIndex(index, bindex, order)
+    return bar_to_cart_3d(np.array(bindex) / order)
+
+
 def vtk_tet_local_to_cart(order):
     r"""Produces a list of nodes for VTK's lagrange tet basis.
     :arg order: the order of the tet
     :return a list of arrays of floats
     """
     count = int((order + 1) * (order + 2) * (order + 3) // 6)
-    bars = [np.array(tet_barycentric_index(i, order))/order for i in range(count)]
-    carts = [bar_to_cart_3d(b) for b in bars]
+    tet = vtk.vtkLagrangeTetra()
+    carts = [tet_barycentric_index(tet, i, order) for i in range(count)]
     return carts
 
 
@@ -322,8 +94,9 @@ def vtk_hex_local_to_cart(orders):
     sizes = tuple([o + 1 for o in orders])
     size = np.product(sizes)
     loc_to_cart = np.empty(size, dtype="object")
+    hexa = vtk.vtkLagrangeHexahedron()
     for loc in np.ndindex(sizes):
-        idx = vtk_hex_point_index_from_ijk(*loc, order=orders)
+        idx = hexa.PointIndexFromIJK(loc[0], loc[1], loc[2], orders)
         cart = np.array([c / o for (c, o) in zip(loc, orders)])
         loc_to_cart[idx] = cart
     return(loc_to_cart)
@@ -337,15 +110,21 @@ def bar_to_cart_2d(bar):
     return(np.dot(bar, mat))
 
 
-def vtk_triangle_index_cart(index, order):
-    bindex = triangle_barycentric_index(index, order)
+def vtk_triangle_index_cart(tri, index, order):
+    """
+    Wrapper for vtkLagrangeTriangle::BarycentricIndex
+    """
+    bindex = [-1, -1, -1]
+    tri.BarycentricIndex(index, bindex, order)
     cart = bar_to_cart_2d(bindex)
     return cart
 
 
-def all_bar_index2d(order):
+def vtk_triangle_local_to_cart(order):
     count = (order + 1) * (order + 2) // 2
-    return [vtk_triangle_index_cart(idx, order) / order for idx in range(count)]
+    tri = vtk.vtkLagrangeTriangle()
+    return [vtk_triangle_index_cart(tri, idx, order) / order
+            for idx in range(count)]
 
 
 def vtk_quad_local_to_cart(orders):
@@ -356,8 +135,9 @@ def vtk_quad_local_to_cart(orders):
     sizes = tuple([o + 1 for o in orders])
     size = np.product(sizes)
     loc_to_cart = np.empty(size, dtype="object")
+    quad = vtk.vtkLagrangeQuadrilateral()
     for loc in np.ndindex(sizes):
-        idx = vtk_quad_index_from_ij(*loc, order=orders)
+        idx = quad.PointIndexFromIJK(loc[0], loc[1], orders)
         cart = np.array([c / o for (c, o) in zip(loc, orders)])
         loc_to_cart[idx] = cart
     return(loc_to_cart)
@@ -373,10 +153,11 @@ def vtk_wedge_local_to_cart(ordersp):
     triSize = (orders[0] + 1) * (orders[0] + 2) // 2
     totalSize = triSize * (orders[2] + 1)
     loc_to_cart = np.empty(totalSize, dtype="object")
+    wedge = vtk.vtkLagrangeWedge()
     for loc in np.ndindex(sizes):
         if loc[0] + loc[1] > orders[0]:
             continue
-        idx = wedge_point_index_from_ijk(loc[0], loc[1], loc[2], orders)
+        idx = wedge.PointIndexFromIJK(loc[0], loc[1], loc[2], orders)
         cart = np.array([c / o for (c, o) in zip(loc, orders)])
         loc_to_cart[idx] = cart
     return(loc_to_cart)
@@ -399,7 +180,7 @@ def vtk_lagrange_interval_reorder(ufl_element):
 
 def vtk_lagrange_triangle_reorder(ufl_element):
     degree = ufl_element.degree()
-    vtk_local = all_bar_index2d(degree)
+    vtk_local = vtk_triangle_local_to_cart(degree)
     firedrake_local = firedrake_local_to_cart(ufl_element)
     return invert(vtk_local, firedrake_local)
 
