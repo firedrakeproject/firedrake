7c46ec1531c4df8236fda8079918bf2195d9a267
nbouziani <nacime.bouziani@gmail.com>
2019-10-24 00:30:39 +0100

    Slate external operator KernelInfo

--- firedrake/slate/slac/compiler.py
+++ firedrake/slate/slac/compiler.py
@@ -258,7 +258,7 @@ def generate_kernel_ast(builder, statements, declared_temps):
                        subdomain_id="otherwise",
                        domain_number=0,
                        coefficient_map=tuple(range(len(expr_coeffs))),
-                       external_operators=kernel.external_operators,
+                       external_operators={},
                        needs_cell_facets=builder.needs_cell_facets,
                        pass_layer_arg=builder.needs_mesh_layers,
                        needs_cell_sizes=builder.needs_cell_sizes)
