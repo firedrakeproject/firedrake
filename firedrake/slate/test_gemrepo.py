e04ab6b6b9f83e22920f584b385bcd8a4d2d4566
Sophia Vorderwuelbecke <sv2518@dyn193-163.dyn.ic.ac.uk>
2019-11-01 10:41:50 +0000

    more tests(neg,trans,mul)

--- firedrake/slate/test_gemrepo.py
+++ firedrake/slate/test_gemrepo.py
@@ -3,7 +3,7 @@ from firedrake import *
 
 #FEM problem (Helmholtz equation)
 mesh = UnitSquareMesh(10, 10)
-V = FunctionSpace(mesh, "CG", 1)
+V = FunctionSpace(mesh, "CG", 2)
 u = TrialFunction(V)
 v = TestFunction(V)
 f = Function(V)
@@ -12,8 +12,12 @@ f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
 a = (dot(grad(v), grad(u)) + v * u) * dx
 L = f * v * dx
 
-#Element tensors defining the local 3-by-3 block system
 _A = Tensor(a)
 _F = Tensor(L)
 #assemble(_A)
-assemble(_A*_A)
+#assemble(_A+_A)
+#test=assemble(_A*_F)
+#test=assemble(_A*_A)
+#test=assemble(-_A)
+#test=assemble(Transpose(_A))
+print(test)
