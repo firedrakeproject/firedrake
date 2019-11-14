f7dff97a09ef3b04e36992adca3a44bb2fd1d349
Lawrence Mitchell <lawrence@wence.uk>
2019-11-14 16:16:24 +0000

    compilation: -mnoavx512f with gcc 7.4
    Triggers some bugs in the vectorisers (fixed in 8.x and 9.x).

--- pyop2/compilation.py
+++ pyop2/compilation.py
@@ -215,7 +215,10 @@ class Compiler(object):
             if version.StrictVersion("7.3") <= ver < version.StrictVersion("7.5"):
                 # GCC bug https://gcc.gnu.org/bugzilla/show_bug.cgi?id=90055
                 # See also https://github.com/firedrakeproject/firedrake/issues/1442
-                return ["-fno-tree-loop-vectorize"]
+                # Bug also on skylake with the vectoriser in this
+                # combination (disappears without
+                # -fno-tree-loop-vectorize!)
+                return ["-fno-tree-loop-vectorize" "-mnoavx512f"]
         return []
 
     @collective
