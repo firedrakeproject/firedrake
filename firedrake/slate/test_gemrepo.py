5f42e9be49e33dd3fd16d25e4692bb8339dfb053
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2019-12-06 11:46:49 +0000

    polish over the tests

--- firedrake/slate/test_gemrepo.py
+++ firedrake/slate/test_gemrepo.py
@@ -12,30 +12,59 @@ f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
 a = (dot(grad(v), grad(u)) + v * u) * dx
 L = f * v * dx
 
-_A = Tensor(a)
-_A2 = Tensor(a)
-_F = AssembledVector(assemble(L))
+def test_assembletensorandsolve(a,L,V):
+    _A = Tensor(a)
+    _F = Tensor(L)
+    A=assemble(_A)
+    F=assemble(_F)
+    A_comp=assemble(a)
+    F_comp=assemble(F)
 
-#TEST: assemble tensor
-#test=assemble(_A)
-#test1=assemble(_A2)
-#comp=assemble(a)
+    #TEST1: compare slate and non-slate assembled tensors
+    print((A.M.handle-A_comp.M.handle).norm())
+    print((F.M.handle-F_comp.M.handle).norm())
 
-#TODO: TEST: assemble coefficients
-test=assemble(_F)
-comp=assemble(L)
+    #TEST2: solve
+    u=Function(V)
+    u_comp=Function(V)
+    solve(test1, u, test2, solver_parameters={'ksp_type': 'cg'})
+    solve(a==L, u_comp, solver_parameters={'ksp_type': 'cg'})
+    print((u.dat.data-u_comp.dat.data).norm())
 
-#TEST: assemble addition
-#test=assemble(_A+_A2)
-#comp=assemble(a+a)
+def test_assembledvector(L):
+    _coeff_F = AssembledVector(assemble(L))
+    coeff_F=assemble(_coeff_F)
+    coeff_F_comp = assemble(L)
+    print((coeff_F.dat.data-coeff_F_comp.dat.data).norm())
 
-#TEST: assemble negative
-#test=assemble(-_A)
-#comp=assemble(-a)
+def test_add(a):
+    _A = Tensor(a)
+    add_A=assemble(_A+_A)
+    add_A_comp=assemble(a+a)
+    print((add_A.M.handle-add_A_comp.M.handle).norm())
 
-#TODO: TEST: assemble transpose
-#test=assemble(Transpose(_A))
 
+def test_negative(a):
+    _A = Tensor(a)
+    neg_A=assemble(-_A)
+    neg_A_comp=assemble(-a)
+    print((neg_A.M.handle-neg_A_comp.M.handle).norm())
+
+#TODO we need advection problem rather than hemlholtz to have non symmetric tensors
+def test_transpose(a):
+    _A = Tensor(a)
+    A = assemble(_A)
+    trans_A=assemble(Transpose(_A))
+    print((trans_A.M.handle-A_comp.M.handle).norm())#should be 0 here because of symmetry
+
+
+#run test
+test_assembletensorandsolve(a,L,V)
+
+
+
+
+###############################################
 #TODO: TEST: assemble contraction
 #test=assemble(_A*_A)
 
@@ -45,15 +74,3 @@ comp=assemble(L)
 #TODO: TEST: assemble blocks
 #this is getting more interesting if mixed
 #b=assemble(_A.blocks[0,0])
-
-#Test the output
-print(test.dat.data)
-
-print(test.dat.data)
-print(comp.dat.data)
-print(comp.M.handle.view())
-
-print((test.M.handle-comp.M.handle).view())
-#print((test.M.handle-test3.M.handle).norm())
-#print((test.M.handle-test2.M.handle).norm())
-print((test.M.handle-comp.M.handle).norm())
