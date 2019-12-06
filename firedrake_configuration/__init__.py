ff85dcb03d3413d361562b08e39d76c0852307e7
David Ham <david.ham@imperial.ac.uk>
2019-12-06 15:47:53 +0000

    fix upgrade path problem

--- firedrake_configuration/__init__.py
+++ firedrake_configuration/__init__.py
@@ -9,12 +9,19 @@ import os
 
 # Attempt to read configuration from file.
 try:
-        with open(os.path.join(os.environ["VIRTUAL_ENV"],
-                               ".configuration.json"), "r") as f:
-            _config = json.load(f)
+     with open(os.path.join(os.environ["VIRTUAL_ENV"],
+                            ".configuration.json"), "r") as f:
+     _config = json.load(f)
 
 except IOError:
-    _config = None
+    # Fall back to old location.
+    try:
+        with open(os.path.join(os.path.dirname(__file__),
+                               ".configuration.json"), "r") as f:
+        _config = json.load(f)
+
+    except IOError:
+            _config = None
 
 
 def get_config():
