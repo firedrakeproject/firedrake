92ab9f56748c08a58dcf29b7c2d31e48c9b1f086
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2019-12-11 14:45:33 +0000

    neatify tests

--- firedrake/slate/test_gemrepo.py
+++ firedrake/slate/test_gemrepo.py
@@ -1,88 +1,76 @@
 
 from firedrake import *
 
+
 def test_assemble2form(a):
     _A = Tensor(a)
-    A=assemble(_A)
-    A_comp=assemble(a)
-    print((A.M.handle-A_comp.M.handle).norm())
+    A = assemble(_A)
+    A_comp = assemble(a)
+    assert F.M.handle.norm() == F_comp.M.handle.norm(), "Test for assembly of 2-form failed"
 
-#def test_assemble1form(L):
-#    _F = Tensor(L)
-#    F=assemble(_F)
-#    F_comp=assemble(F)
-#    print((F.M.handle-F_comp.M.handle).norm())
+def test_assemble1form(L):
+    _F = Tensor(L)
+    F = assemble(_F)
+    F_comp = assemble(F)
+    assert F.M.handle.norm() == F_comp.M.handle.norm(), "Test for assembly of 1-form failed"
 
 #in order to be able to do solve I need to do mul first
 def test_solve(a,L,V):
     #assemble
     _A = Tensor(a)
     _F = AssembledVector(assemble(L))
-    A=assemble(_A)
-    F=assemble(_F)
 
     #solve
-    u=Function(V)
-    u_comp=Function(V)
+    u = Function(V)
+    u_comp = Function(V)
     solve(assemble(_A), u, assemble(_F))
-    solve(a==L, u_comp, solver_parameters={'ksp_type': 'cg'})
-    print((u.dat.data-u_comp.dat.data).norm())
+    solve(a == L, u_comp, solver_parameters={'ksp_type': 'cg'})
+    assert u.dat.data == u_comp.dat.data, "Test for solved on assembled forms failed"
 
-#test only works for discontinuous function spaces
+#Note: this test only works for discontinuous function spaces
 def test_assembledvector(L):
-    b=Function(assemble(L))
-    _coeff_F = AssembledVector(b)
-
-    print(_coeff_F._function.dat.data)
-    coeff_F=assemble(_coeff_F)
+    _coeff_F = AssembledVector(Function(assemble(L)))
+    coeff_F = assemble(_coeff_F)
     coeff_F_comp = assemble(L)
-    print(coeff_F.dat.data)
-    print(coeff_F_comp.dat.data)
+    assert coeff_F.dat.data == coeff_F_comp.dat.data, "Test for assembled vectors failed"
 
 def test_add(a):
     _A = Tensor(a)
-    add_A=assemble(_A+_A)
-    add_A_comp=assemble(a+a)
-    print((add_A.M.handle-add_A_comp.M.handle).norm())
-
+    add_A = assemble(_A+_A)
+    add_A_comp = assemble(a+a)
+    assert add_A.M.handle == add_A_comp.M.handle,  "Test for adding of two 2-forms failed"
 
 def test_negative(a):
     _A = Tensor(a)
     neg_A=assemble(-_A)
     neg_A_comp=assemble(-a)
-    print((neg_A.M.handle-neg_A_comp.M.handle).norm())
+    assert neg_A.M.handle == neg_A_comp.M.handle,  "Test for negative of a two 2-form failed"
 
-#TODO we need advection problem rather than hemlholtz to have non symmetric tensors
+#Note: this only really a test for a problem containing an unsymmetric operator 
 def test_transpose(a):
     _A = Tensor(a)
     A = assemble(_A)
     trans_A=assemble(Transpose(_A))
-    print((trans_A.M.handle-A_comp.M.handle).norm())#should be 0 here because of symmetry
+    assert trans_A.M.handle == trans_A_comp.M.handle,  "Test for transpose of a two 2-form failed"
 
-#TODO 
-def test_mul(A,L):
+
+#Note: this tests only a mat vec contraction atm
+def test_mul(A,L,V):
     _A = Tensor(a)
-    #mul_matmat=assemble(_A*_A)
-    #print(assemble(a).M.handle.view())
-    #print(mul_matmat.M.handle.view())
-
-    print("TEST MATVEC\n")
-    mat_comp=assemble(a)
-    b=Function(assemble(L))
+    mat_comp = assemble(a)
+    b = Function(assemble(L))
     _coeff_F = AssembledVector(b)
-    test=assemble(_coeff_F)
-    mul_matvec=assemble(_A*_coeff_F)
-    print("assembled UFL right hand side\n",b.dat.data,"\n")
-    print("assembled Slate right hand side\n",test.dat.data,"\n")
-    print("assembled UFL mat\n",mat_comp.M.handle.view(),"\n")
-    print("assembled slate mat mult vec\n",mul_matvec.dat.data,"\n")
+    mul_matvec = assemble(_A*_coeff_F)
+    mul_matvec_comp = assemble(action(a,b))
+    assert mul_matvec.dat.data == mul_matvec_comp.dat.data, "Test for contraction (mat-vec-mul) failed"
+
 
 
 ###########
 #run tests
 ###########
 
-#discontinuous FEM problem (Helmholtz equation)
+#discontinuous Helmholtz equation
 mesh = UnitSquareMesh(5,5)
 V = FunctionSpace(mesh, "DG", 1)
 u = TrialFunction(V)
@@ -94,23 +82,33 @@ a = (dot(grad(v), grad(u)) + v * u) * dx
 L = f * v * dx
 
 test_assembledvector(L)
-
-#TODO
-test_mul(a,L)
-
-
-
+#test_mul(a,L,V)
 #test_solve(a,L,V)
 
+#continuous Helmholtz equation
+mesh = UnitSquareMesh(5,5)
+V = FunctionSpace(mesh, "CG", 1)
+u = TrialFunction(V)
+v = TestFunction(V)
+f = Function(V)
+x, y = SpatialCoordinate(mesh)
+f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
+a = (dot(grad(v), grad(u)) + v * u) * dx
+L = f * v * dx
 
 #test_assemble2form(a)
+#test_assemble1form(a)
+#test_negative(a)
+#test_add(a)
+
+#continuous advection problem
+#TODO
+
+#test_transpose(a)
 
 
 
 ###############################################
-#TODO: TEST: assemble contraction
+#TODO: TEST: assemble mul of two 2-froms
 #test=assemble(_A*_A)
-
 #TODO: TEST: assemble blocks
-#this is getting more interesting if mixed
-#b=assemble(_A.blocks[0,0])
