4d82203d16ba9112b3864f6fdaf9baa8001b4615
nbouziani <nacime.bouziani@gmail.com>
2019-12-17 15:17:34 +0000

    Add MeshInputBlock and MeshOutputBlock

--- firedrake/adjoint/blocks.py
+++ firedrake/adjoint/blocks.py
@@ -1,5 +1,6 @@
 from dolfin_adjoint_common.compat import compat
 from dolfin_adjoint_common import blocks
+from pyadjoint.block import Block
 
 class Backend:
     @property
@@ -42,3 +43,51 @@ class SolveBlock(blocks.SolveBlock, Backend):
 class ProjectBlock(blocks.ProjectBlock, Backend):
     pass
 
+
+class MeshInputBlock(Block):
+    """
+    Block which links a MeshGeometry to its coordinates, which is a firedrake
+    function.
+    """
+    def __init__(self, mesh):
+        super(MeshInputBlock, self).__init__()
+        self.add_dependency(mesh)
+
+    def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
+        return adj_inputs[0]
+
+    def evaluate_tlm_component(self, inputs, tlm_inputs, block_variable, idx, prepared=None):
+        return tlm_inputs[0]
+
+    def evaluate_hessian_component(self, inputs, hessian_inputs, adj_inputs, idx, block_variable,
+                                   relevant_dependencies, prepared=None):
+        return hessian_inputs[0]
+
+    def recompute_component(self, inputs, block_variable, idx, prepared):
+        mesh = self.get_dependencies()[0].saved_output
+        return mesh.coordinates
+
+
+class MeshOutputBlock(Block):
+    """
+    Block which is called when the coordinates of a mesh is changed.
+    """
+    def __init__(self, func, mesh):
+        super(MeshOutputBlock, self).__init__()
+        self.add_dependency(func)
+
+    def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
+        return adj_inputs[0]
+
+    def evaluate_tlm_component(self, inputs, tlm_inputs, block_variable, idx, prepared=None):
+        return tlm_inputs[0]
+
+    def evaluate_hessian_component(self, inputs, hessian_inputs, adj_inputs, idx, block_variable,
+                                   relevant_dependencies, prepared=None):
+        return hessian_inputs[0]
+
+    def recompute_component(self, inputs, block_variable, idx, prepared):
+        vector = self.get_dependencies()[0].saved_output
+        mesh = vector.function_space().mesh()
+        mesh.coordinates.assign(vector, annotate=False)
+        return mesh._ad_create_checkpoint()
