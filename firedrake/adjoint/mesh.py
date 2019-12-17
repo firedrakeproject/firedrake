3ff6c4a05c35d1fb90c9f8c5607d88f6edbe571a
sebastkm <s.mitusch@hotmail.com>
2019-12-17 15:25:53 +0100

    Add my mesh adjoint implementation WIP

---
+++ firedrake/adjoint/mesh.py
@@ -0,0 +1,34 @@
+from pyadjoint.overloaded_type import OverloadedType
+
+
+class MeshGeometryMixin(OverloadedType):
+    @staticmethod
+    def _ad_annotate_init(init):
+        def wrapper(self, *args, **kwargs):
+            backend.mesh.MeshGeometry.__init__(self, *args, **kwargs)
+            init(self, *args, **kwargs)
+        return wrapper
+
+    @no_annotations
+    def _ad_create_checkpoint(self):
+        return self.coordinates.copy(deepcopy=True)
+
+    @no_annotations
+    def _ad_restore_at_checkpoint(self, checkpoint):
+        self.coordinates.assign(checkpoint)
+        return self
+
+    @staticmethod
+    def _ad_annotate_coordinates_function(coordinates_function):
+        def wrapper(self, *args, **kwargs):
+            from firedrake_adjoint.types.mesh import MeshInputBlock, MeshOutputBlock
+            f = coordinates_function()
+            f.block_class = MeshInputBlock
+            f._ad_floating_active = True
+            f._ad_args = [self]
+
+            f._ad_output_args = [self]
+            f.output_block_class = MeshOutputBlock
+            f._ad_outputs = [self]
+            return f
+        return wrapper
