90509d74b9b0b76986d7c552d57b6337197f31bc
Scott MacLachlan <smaclachlan@mun.ca>
2020-01-28 10:27:16 -0330

    First pass at python-side changes for PCPatch with exterior facet integrals

--- firedrake/cython/patchimpl.pyx
+++ firedrake/cython/patchimpl.pyx
@@ -1,17 +1,29 @@
 from libc.stdint cimport uintptr_t
+from enum import IntEnum
 
 include "petschdr.pxi"
 
-def set_patch_residual(patch, function, ctx, is_snes=False, interior_facets=False):
+class JacType(IntEnum):
+      CELL = 1
+      INTERIOR_FACET = 2
+      EXTERIOR_FACET = 3
+
+def set_patch_residual(patch, function, ctx, is_snes=False, jac_type = JacType.CELL):
     if is_snes:
-        if interior_facets:
+        if jac_type == JacType.INTERIOR_FACET:
+            raise NotImplementedError
+        if jac_type == JacType.EXTERIOR_FACET:
             raise NotImplementedError
         CHKERR(SNESPatchSetComputeFunction((<PETSc.SNES?>patch).snes,
                                            <PetscPCPatchComputeFunction><uintptr_t>function,
                                            <void *><uintptr_t>ctx))
     else:
-        if interior_facets:
+        if jac_type == JacType.INTERIOR_FACET:
             CHKERR(PCPatchSetComputeFunctionInteriorFacets((<PETSc.PC?>patch).pc,
+                                                           <PetscPCPatchComputeFunction><uintptr_t>function,
+                                                           <void *><uintptr_t>ctx))
+        elif jac_type == JacType.EXTERIOR_FACET:
+            CHKERR(PCPatchSetComputeFunctionExteriorFacets((<PETSc.PC?>patch).pc,
                                                            <PetscPCPatchComputeFunction><uintptr_t>function,
                                                            <void *><uintptr_t>ctx))
         else:
@@ -20,16 +32,22 @@ def set_patch_residual(patch, function, ctx, is_snes=False, interior_facets=Fals
                                              <void *><uintptr_t>ctx))
 
 
-def set_patch_jacobian(patch, function, ctx, is_snes=False, interior_facets=False):
+def set_patch_jacobian(patch, function, ctx, is_snes=False, jac_type = JacType.CELL):
     if is_snes:
-        if interior_facets:
+        if jac_type == JacType.INTERIOR_FACET:
+            raise NotImplementedError
+        if jac_type == JacType.EXTERIOR_FACET:
             raise NotImplementedError
         CHKERR(SNESPatchSetComputeOperator((<PETSc.SNES?>patch).snes,
                                                <PetscPCPatchComputeOperator><uintptr_t>function,
                                                <void *><uintptr_t>ctx))
     else:
-        if interior_facets:
+        if jac_type == JacType.INTERIOR_FACET:
             CHKERR(PCPatchSetComputeOperatorInteriorFacets((<PETSc.PC?>patch).pc,
+                                                           <PetscPCPatchComputeOperator><uintptr_t>function,
+                                                           <void *><uintptr_t>ctx))
+        elif jac_type == JacType.EXTERIOR_FACET:
+            CHKERR(PCPatchSetComputeOperatorExteriorFacets((<PETSc.PC?>patch).pc,
                                                            <PetscPCPatchComputeOperator><uintptr_t>function,
                                                            <void *><uintptr_t>ctx))
         else:
