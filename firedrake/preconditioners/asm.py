c8de9adaac9a3fd6e0e69b1a5a5d21da26b3fa74
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2020-01-29 11:59:56 +0000

    Linted code

--- firedrake/preconditioners/asm.py
+++ firedrake/preconditioners/asm.py
@@ -1,5 +1,4 @@
 import abc
-import itertools
 
 from pyop2.mpi import COMM_SELF
 from firedrake.preconditioners.base import PCBase
@@ -8,13 +7,14 @@ from firedrake.dmhooks import get_appctx
 
 __all__ = ("ASMPatchPC", "ASMLinesmooth")
 
+
 class ASMPatchPC(PCBase):
     ''' PC for PETSc PCASM
-    
+
     should implement:
     - :meth:`get_patches`
     '''
-    
+
     def initialize(self, pc):
         # Get context from pc
         _, P = pc.getOperators()
@@ -22,15 +22,15 @@ class ASMPatchPC(PCBase):
             ctx = P.getPythonContext()
         else:
             ctx = get_appctx(pc.getDM()).appctx
-        
+
         self.prefix = pc.getOptionsPrefix() + "pc_asm_"
-        
+
         # Extract function space and mesh to obtain plex and indexing functions
         V = ctx['TraceSpace']
-        
+
         # Obtain patches from user defined funtion
         ises = self.get_patches(V)
-        
+
         # Create new PC object as ASM type and set index sets for patches
         asmpc = PETSc.PC().create(comm=pc.comm)
         asmpc.incrementTabLevel(1, parent=pc)
@@ -40,13 +40,13 @@ class ASMPatchPC(PCBase):
         asmpc.setASMLocalSubdomains(len(ises), ises)
         asmpc.setFromOptions()
         self.asmpc = asmpc
-    
+
     @abc.abstractmethod
     def get_patches(self, V):
         ''' Get the patches used for PETSc PSASM
         '''
         pass
-    
+
     def update(self, pc):
         pass
 
@@ -62,21 +62,19 @@ class ASMLinesmooth(ASMPatchPC):
     These patches are made by looping over base mesh interior DOFs
     and collecting all the DOFs on the "horizontal" faces.
     Then looping over all facet DOFs and collecting the DOFs on the
-    "vertical" faces. 
+    "vertical" faces.
     '''
-    
+
     def get_patches(self, V):
         mesh = V._mesh
         dm = mesh._plex
         section = V.dm.getDefaultSection()
         lgmap = V.dof_dset.lgmap
-        
+
         # Obtain the codimensions to loop over from options, if present
-        codim_list = PETSc.Options().getString(self.prefix
-                                                + "codims",
-                                                "0, 1")
+        codim_list = PETSc.Options().getString(self.prefix + "codims", "0, 1")
         codim_list = [int(ii) for ii in codim_list.split(',')]
-        
+
         # Build index sets for the patches
         ises = []
         # Loop over faces of base mesh, then edges
@@ -95,5 +93,5 @@ class ASMLinesmooth(ASMPatchPC):
                 global_index = lgmap.apply(indices)
                 iset = PETSc.IS().createGeneral(global_index, comm=COMM_SELF)
                 ises.append(iset)
-        
+
         return ises
