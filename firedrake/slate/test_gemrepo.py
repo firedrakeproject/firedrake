1a7c699864487886d5dbcb0239f151c16b3dc1f2
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2020-01-30 10:39:41 +0000

    Fixed test of mat-mat multiplication for ds integrals

--- firedrake/slate/test_gemrepo.py
+++ firedrake/slate/test_gemrepo.py
@@ -13,9 +13,10 @@ def test_assemble_matrix(a):
     A_comp = assemble(a)
     for i in range(A.M.handle.getSize()[0]):
         for j in range(A.M.handle.getSize()[1]):
-            assert math.isclose(A.M.handle.getValues(i,j),A_comp.M.handle.getValues(i,j)),  "Test for assembly of 2-form failed"
+            assert math.isclose(A.M.handle.getValues(i,j),A_comp.M.handle.getValues(i,j)),  "Test for assembly of tensor failed"
 
-#in order to be able to do solve I need to do mul first
+#Note: this test only works for DG problems because assembled vector does not do the right thing
+#the bug is also in the earlier version of slate compiler
 def test_solve(a,L,V):
     #assemble
     _A = Tensor(a)
@@ -26,7 +27,7 @@ def test_solve(a,L,V):
     u_comp = Function(V)
     solve(assemble(_A), u, assemble(_F),solver_parameters={'ksp_type': 'cg'})
     solve(a == L, u_comp, solver_parameters={'ksp_type': 'cg'})
-    assert u.dat.data.all()  == u_comp.dat.data.all() , "Test for solved on assembled forms failed"
+    assert u.dat.data.all()  == u_comp.dat.data.all() , "Test for solve on assembled forms failed"
 
 #Note: this test only works for discontinuous function spaces
 def test_assembled_vector(L):
@@ -45,8 +46,7 @@ def test_add(a):
     add_A_comp = assemble(a+a)
     for i in range(add_A.M.handle.getSize()[0]):
         for j in range(add_A.M.handle.getSize()[1]):
-            assert math.isclose(add_A.M.handle.getValues(i,j),add_A_comp.M.handle.getValues(i,j)),  "Test for adding of a two 2-form failed"
-
+            assert math.isclose(add_A.M.handle.getValues(i,j),add_A_comp.M.handle.getValues(i,j)),  "Test for adding of a two tensor failed"
 
 def test_negative(a):
     print("Test of negative")
@@ -56,18 +56,17 @@ def test_negative(a):
     neg_A_comp = assemble(-a)
     for i in range(neg_A.M.handle.getSize()[0]):
         for j in range(neg_A.M.handle.getSize()[1]):
-            assert math.isclose(neg_A.M.handle.getValues(i,j),neg_A_comp.M.handle.getValues(i,j)),  "Test for negative of a two 2-form failed"
+            assert math.isclose(neg_A.M.handle.getValues(i,j),neg_A_comp.M.handle.getValues(i,j)),  "Test for negative of tensor failed"
 
 #TODO: this only really a test for a problem containing an unsymmetric operator 
 def test_transpose(a):
     print("Test of transpose")
-
     _A = Tensor(a)
-    A = assemble(_A)
     trans_A = assemble(Transpose(_A))
+    A_comp = assemble(_A)
     for i in range(trans_A.M.handle.getSize()[0]):
         for j in range(trans_A.M.handle.getSize()[1]):
-            assert math.isclose(trans_A.M.handle.getValues(i, j), A_comp.M.handle.getValues(j, i)),  "Test for negative of a two 2-form failed"
+            assert math.isclose(trans_A.M.handle.getValues(i, j), A_comp.M.handle.getValues(j, i)),  "Test for inverse failed"
 
 def test_mul_dx(A,L,V,mesh):
     print("Test of mul")
@@ -107,22 +106,22 @@ def test_mul_ds(A,L,V,mesh):
     mul_matvec_comp = assemble(action(a,b))
     assert math.isclose(mul_matvec.dat.data.all(),mul_matvec_comp.dat.data.all()) , "Test for contraction (mat-vec-mul) on facet integrals failed"
 
-
-    #TODO I think this should be postponed to a later stage then in gem since this is kind of a global operation
     #test for mat-mat multiplication
-    # u2 = TrialFunction(V)
-    # v2 = TestFunction(V)
-    # a2 = (u2*v2) * ds
-    # _A2 = Tensor(a2)
-    # comp1=assemble(_A)
-    # comp2=assemble(_A2)
-    # mul_matmat_comp = comp1.M.handle * comp2.M.handle
-    # mul_matmat = assemble(_A*_A2)
+    #only works for facet integrals when there is no coupling between cells involved
+    #so for example a flux going across the joint facet of two cells
+    #otherwise this becomes kind of a global operation (similar e.g. to an inverse)
+    u2 = TrialFunction(V)
+    v2 = TestFunction(V)
+    a2 = (u2*v2) * ds + u("+")*v("+")*dS+ u("-")*v("-")*dS
+    _A2 = Tensor(a2)
+    comp1=assemble(_A)
+    comp2=assemble(_A2)
+    mul_matmat_comp = comp1.M.handle * comp2.M.handle
+    mul_matmat = assemble(_A*_A2)
 
-    # for i in range(mul_matmat.M.handle.getSize()[0]):
-    #     for j in range(mul_matmat.M.handle.getSize()[1]):
-    #         assert math.isclose(mul_matmat_comp.getValues(i,j),mul_matmat.M.handle.getValues(i,j)),  "Test for mat-mat-mul  on facet integrals  failed"
-
+    for i in range(mul_matmat.M.handle.getSize()[0]):
+        for j in range(mul_matmat.M.handle.getSize()[1]):
+            assert math.isclose(mul_matmat_comp.getValues(i,j),mul_matmat.M.handle.getValues(i,j)),  "Test for mat-mat-mul on facet integrals failed"
 
 def test_blocks():
     print("Test of blocks")
@@ -167,6 +166,17 @@ def test_layers():
         block = assemble(_A[x, y])
         assert np.allclose(block.M.values, ref.M.values, rtol=1e-14)
 
+def test_inverse(a):
+    print("Test of inverse")
+
+    _A = Tensor(a)
+    A = assemble(Inverse(_A))
+    A_comp = A
+    for i in range(A.M.handle.getSize()[0]):
+        for j in range(A.M.handle.getSize()[1]):
+            assert math.isclose(A.M.handle.getValues(i,j),abs(A.M.handle.getValues(i,j))),  "Test for assembly failed"
+
+
 ###########
 print("Run test for slate to loopy compilation.\n\n")
 
@@ -181,12 +191,14 @@ f.interpolate((1+8*pi*pi)*cos(x*pi*2)*cos(y*pi*2))
 a = (dot(grad(v), grad(u)) + v * u) * dx
 L = f * v * dx
 
+test_inverse(a)
 test_assemble_matrix(a)
 test_negative(a)
 test_add(a)
 test_assembled_vector(L) 
 test_mul_dx(a,L,V,mesh)
-#test_solve(a,L,V) #fails
+test_transpose(a)
+test_solve(a,L,V)
 
 #discontinuous Helmholtz equation on facet integrals
 mesh = UnitSquareMesh(5,5)
@@ -216,7 +228,6 @@ a= (dot(grad(v), grad(u))  +u*v) * ds
 L = f * v * ds
 
 test_assemble_matrix(a)
-test_mul_ds(a,L,V,mesh)
 test_negative(a)
 test_add(a)
 
@@ -243,5 +254,7 @@ F = (u_*div(v*u))*dx
 ###############################################
 #TODO: assymetric problem test
 #TODO: write test for subdomain integrals as well
+#TODO: make argument generation nicer
+#TODO: fix dependency generation for transpose on facets
 
 print("\n\nAll tests passed.")
