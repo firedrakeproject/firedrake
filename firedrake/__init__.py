47978eba410ad070dd3288dc1e281d9796a5a2fb
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-02-10 17:15:33 -0600

    minor formatting

--- firedrake/__init__.py
+++ firedrake/__init__.py
@@ -10,7 +10,7 @@ elif "PETSC_DIR" not in os.environ and config["options"]["honour_petsc_dir"]:
                       "Please set PETSC_DIR (and PETSC_ARCH) before using Firedrake.")
 del os, sys, config
 
-# Ensure pycuda context is inited
+# Ensure pycuda context is inited before petsc
 try:
     import pycuda.autoinit  # noqa: F401
 except ImportError:
@@ -19,7 +19,6 @@ except ImportError:
 # Ensure petsc is initialised by us before anything else gets in there.
 import firedrake.petsc as petsc
 del petsc
-
 
 # UFL Exprs come with a custom __del__ method, but we hold references
 # to them /everywhere/, some of which are circular (the Mesh object
