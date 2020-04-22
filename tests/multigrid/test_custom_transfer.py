9216b84fa2740de564b5cf20f34ee59f040879be
Lawrence Mitchell <lawrence@wence.uk>
2020-04-22 15:55:37 +0100

    tests/mg: Check for known complex not implemented errors

--- tests/multigrid/test_custom_transfer.py
+++ tests/multigrid/test_custom_transfer.py
@@ -124,10 +124,10 @@ def test_multiple_custom_transfer_monolithc():
     solver = LinearVariationalSolver(problem, solver_parameters=options)
     solver.set_transfer_manager(transfer)
 
-    solver.solve()
-
-    assert count_V == 2
-    assert count_Q == -2
+    with raises_in_complex(NotImplementedError):
+        solver.solve()
+        assert count_V == 2
+        assert count_Q == -2
 
 
 def test_custom_transfer_setting():
