7b38a2a9f0b599e79ddc3d91916730679a519b76
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-05-04 03:34:14 -0500

    leftover DataSet->op2.compute_backend.DataSet

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -12,7 +12,6 @@ import numbers
 from mpi4py import MPI
 from pyop2.datatypes import IntType
 from pyop2 import op2
-from pyop2.base import DataSet
 from pyop2.mpi import COMM_WORLD, dup_comm
 from pyop2.profiling import timed_function, timed_region
 from pyop2.utils import as_tuple, tuplify
@@ -625,9 +624,9 @@ class MeshTopology(object):
                                                  self._cell_numbering,
                                                  self.cell_closure)
         if isinstance(self.cell_set, op2.ExtrudedSet):
-            dataset = DataSet(self.cell_set.parent, dim=cell_facets.shape[1:])
+            dataset = op2.compute_backend.DataSet(self.cell_set.parent, dim=cell_facets.shape[1:])
         else:
-            dataset = DataSet(self.cell_set, dim=cell_facets.shape[1:])
+            dataset = op2.compute_backend.DataSet(self.cell_set, dim=cell_facets.shape[1:])
         return op2.compute_backend.Dat(dataset, cell_facets, dtype=cell_facets.dtype,
                        name="cell-to-local-facet-dat")
 
