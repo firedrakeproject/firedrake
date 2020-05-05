01f056ecb93564b0aedd8a95d3ab92c0498b06be
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-05-04 22:37:29 -0500

    typo correction

--- firedrake/offload.py
+++ firedrake/offload.py
@@ -4,10 +4,10 @@ from contextlib import contextmanager
 @contextmanager
 def offloading(backend):
     """
-    Changes to the compute backend to ``backend`` within the context block.
+    Switches the compute backend to ``backend`` within the context block.
 
-    This would ensure that all the operations (for ex. assemble, interpolation,
-    etc) would be executed on ``backend``. Any backend specific object
+    Operations (for ex. assemble, interpolation, etc) within the offloading
+    region will be executed on ``backend``. Any backend specific object
     instantiated in the context will be allocated on ``backend``.
     """
     from pyop2 import op2
