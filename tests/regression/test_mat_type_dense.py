20317e193dff6cdca374e3153f0aa7d258f0cdb1
Patrick Farrell <patrick@pefarrell.org>
2020-05-08 12:51:50 +0100

    Add test for mixed problems also

--- tests/regression/test_mat_type_dense.py
+++ tests/regression/test_mat_type_dense.py
@@ -1,16 +1,25 @@
 from firedrake import *
 from numpy import allclose
+import pytest
 
 
-def solve_poisson(mat_type):
+def solve_poisson(mat_type, kind):
     mesh = UnitSquareMesh(3, 3, quadrilateral=True)
-    V = VectorFunctionSpace(mesh, "CG", 2)
+    if kind == "vector":
+        V = VectorFunctionSpace(mesh, "CG", 2)
+    elif kind == "mixed":
+        W = FunctionSpace(mesh, "CG", 2)
+        V = MixedFunctionSpace([W, W])
 
     u = Function(V)
     v = TestFunction(V)
 
     F = inner(grad(u), grad(v))*dx - inner(Constant((1, 2)), v)*dx
-    bcs = DirichletBC(V, Constant((0, 0)), "on_boundary")
+
+    if kind == "vector":
+        bcs = DirichletBC(V, Constant((0, 0)), "on_boundary")
+    elif kind == "mixed":
+        bcs = [DirichletBC(V.sub(i), Constant(0), "on_boundary") for i in range(2)]
 
     sp = {"snes_type": "ksponly",
           "ksp_type": "preonly",
@@ -21,8 +30,9 @@ def solve_poisson(mat_type):
     return u
 
 
-def test_mat_type_dense():
-    u_aij = solve_poisson("aij")
-    u_dense = solve_poisson("dense")
+@pytest.mark.parametrize(('kind',), [('vector',), ('mixed',)])
+def test_compare_mat_type_dense(kind):
+    u_aij = solve_poisson("aij", kind)
+    u_dense = solve_poisson("dense", kind)
 
     assert allclose(u_aij.dat.data_ro, u_dense.dat.data_ro)
