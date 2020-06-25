67307b46e2f51fa075259b2610d6fb3bae60ba0f
Lawrence Mitchell <lawrence@wence.uk>
2020-06-25 13:42:15 +0100

    mg: Handle both refinement and coarsening in transfer
    Will be used to support grid sequencing in SNES.

--- firedrake/mg/ufl_utils.py
+++ firedrake/mg/ufl_utils.py
@@ -1,3 +1,4 @@
+from enum import IntEnum
 import numpy
 import ufl
 from ufl.corealg.map_dag import map_expr_dag
@@ -10,69 +11,95 @@ from firedrake.petsc import PETSc
 from . import utils
 
 
-__all__ = ["coarsen"]
+__all__ = ("coarsen", "refine")
 
 
-class CoarseningError(Exception):
+class TransferError(Exception):
     """Exception raised when coarsening symbolic information fails."""
     pass
 
 
-class CoarsenIntegrand(MultiFunction):
+class TransferIntegrand(MultiFunction):
 
-    """'Coarsen' a :class:`ufl.Expr` by replacing coefficients,
-    arguments and domain data with coarse mesh equivalents."""
+    """'Transfer' a :class:`ufl.Expr` by replacing coefficients,
+    arguments and domain data with transferred equivalents."""
 
-    def __init__(self, coarsen, coefficient_mapping):
-        self.coefficient_mapping = coefficient_mapping or {}
-        self.coarsen = coarsen
-        super(CoarsenIntegrand, self).__init__()
+    def __init__(self, transfer, coefficient_mapping):
+        """
+        :arg transfer: function to transfer expressions to a new mesh.
+        :arg coefficient_mapping: mapping from old to new coefficients.
+        """
+        self.coefficient_mapping = coefficient_mapping
+        self.transfer = transfer
+        super().__init__()
 
     expr = MultiFunction.reuse_if_untouched
 
     def argument(self, o):
-        V = self.coarsen(o.function_space(), self.coarsen)
+        V = self.transfer(o.function_space(), self.transfer)
         return o.reconstruct(V)
 
     def coefficient(self, o):
-        return self.coarsen(o, self.coarsen, coefficient_mapping=self.coefficient_mapping)
+        return self.transfer(o, self.transfer, coefficient_mapping=self.coefficient_mapping)
 
     def geometric_quantity(self, o):
-        return type(o)(self.coarsen(o.ufl_domain(), self.coarsen))
+        return type(o)(self.transfer(o.ufl_domain(), self.transfer))
 
     def circumradius(self, o):
-        mesh = self.coarsen(o.ufl_domain(), self.coarsen)
+        mesh = self.transfer(o.ufl_domain(), self.transfer)
         return firedrake.Circumradius(mesh)
 
     def facet_normal(self, o):
-        mesh = self.coarsen(o.ufl_domain(), self.coarsen)
+        mesh = self.transfer(o.ufl_domain(), self.transfer)
         return firedrake.FacetNormal(mesh)
 
 
+class TransferDirection(IntEnum):
+    COARSEN = 0
+    REFINE = 1
+
+
 @singledispatch
-def coarsen(expr, self, coefficient_mapping=None):
+def transfer(expr, self, direction, *, coefficient_mapping=None):
     # Default, just send it back
     return expr
 
 
-@coarsen.register(ufl.Mesh)
-def coarsen_mesh(mesh, self, coefficient_mapping=None):
+def coarsen(expr, self, direction=TransferDirection.COARSEN, *,
+            coefficient_mapping=None):
+    return transfer(expr, self, direction=TransferDirection.COARSEN,
+                    coefficient_mapping=coefficient_mapping)
+
+
+def refine(expr, self, direction=TransferDirection.REFINE, *,
+           coefficient_mapping=None):
+    return transfer(expr, self, direction=TransferDirection.REFINE,
+                    coefficient_mapping=coefficient_mapping)
+
+
+@transfer.register(ufl.Mesh)
+def transfer_mesh(mesh, self, direction, *, coefficient_mapping=None):
     hierarchy, level = utils.get_level(mesh)
     if hierarchy is None:
-        raise CoarseningError("No mesh hierarchy available")
-    return hierarchy[level - 1]
+        raise TransferError("No mesh hierarchy available")
+    if direction == TransferDirection.COARSEN:
+        return hierarchy[level - 1]
+    elif direction == TransferDirection.REFINE:
+        return hierarchy[level + 1]
+    else:
+        raise TransferError(f"Unhandled transfer direction {direction}")
 
 
-@coarsen.register(ufl.classes.Expr)
-def coarse_expr(expr, self, coefficient_mapping=None):
+@transfer.register(ufl.classes.Expr)
+def transfer_expr(expr, self, direction, *, coefficient_mapping=None):
     if expr is None:
         return None
-    mapper = CoarsenIntegrand(self, coefficient_mapping)
+    mapper = TransferIntegrand(self, coefficient_mapping)
     return map_expr_dag(mapper, expr)
 
 
-@coarsen.register(ufl.Form)
-def coarsen_form(form, self, coefficient_mapping=None):
+@transfer.register(ufl.Form)
+def transfer_form(form, self, direction, *, coefficient_mapping=None):
     """Return a coarse mesh version of a form
 
     :arg form: The :class:`~ufl.classes.Form` to coarsen.
@@ -84,17 +111,17 @@ def coarsen_form(form, self, coefficient_mapping=None):
     if form is None:
         return None
 
-    mapper = CoarsenIntegrand(self, coefficient_mapping)
+    mapper = TransferIntegrand(self, coefficient_mapping)
     integrals = []
     for it in form.integrals():
         integrand = map_expr_dag(mapper, it.integrand())
         mesh = it.ufl_domain()
-        hierarchy, level = utils.get_level(mesh)
-        new_mesh = hierarchy[level-1]
+        new_mesh = self(mesh, self, direction,
+                        coefficient_mapping=coefficient_mapping)
         if isinstance(integrand, ufl.classes.Zero):
             continue
         if it.subdomain_data() is not None:
-            raise CoarseningError("Don't know how to coarsen subdomain data")
+            raise TransferError("Don't know how to coarsen subdomain data")
         new_itg = it.reconstruct(integrand=integrand,
                                  domain=new_mesh)
         integrals.append(new_itg)
@@ -103,10 +130,12 @@ def coarsen_form(form, self, coefficient_mapping=None):
     return form
 
 
-@coarsen.register(firedrake.DirichletBC)
-def coarsen_bc(bc, self, coefficient_mapping=None):
-    V = self(bc.function_space(), self, coefficient_mapping=coefficient_mapping)
-    val = self(bc._original_val, self, coefficient_mapping=coefficient_mapping)
+@transfer.register(firedrake.DirichletBC)
+def transfer_bc(bc, self, direction, *, coefficient_mapping=None):
+    V = self(bc.function_space(), self, direction,
+             coefficient_mapping=coefficient_mapping)
+    val = self(bc._original_val, self, direction,
+               coefficient_mapping=coefficient_mapping)
     zeroed = bc._currently_zeroed
     subdomain = bc.sub_domain
     method = bc.method
@@ -119,13 +148,19 @@ def coarsen_bc(bc, self, coefficient_mapping=None):
     return bc
 
 
-@coarsen.register(firedrake.functionspaceimpl.FunctionSpace)
-@coarsen.register(firedrake.functionspaceimpl.WithGeometry)
-def coarsen_function_space(V, self, coefficient_mapping=None):
-    if hasattr(V, "_coarse"):
-        return V._coarse
+@transfer.register(firedrake.functionspaceimpl.FunctionSpace)
+@transfer.register(firedrake.functionspaceimpl.WithGeometry)
+def transfer_function_space(V, self, direction, *, coefficient_mapping=None):
+    if direction == TransferDirection.COARSEN:
+        attr = "_coarse"
+    elif direction == TransferDirection.REFINE:
+        attr = "_fine"
+    else:
+        raise TransferError(f"Unhandled transfer direction {direction}")
+    if hasattr(V, attr):
+        return getattr(V, attr)
     from firedrake.dmhooks import get_parent, push_parent, pop_parent, add_hook
-    fine = V
+    oldV = V
     indices = []
     while True:
         if V.index is not None:
@@ -137,14 +172,18 @@ def coarsen_function_space(V, self, coefficient_mapping=None):
         else:
             break
 
-    mesh = self(V.mesh(), self)
+    mesh = self(V.mesh(), self, direction)
 
-    V = firedrake.FunctionSpace(mesh, V.ufl_element())
+    newV = firedrake.FunctionSpace(mesh, V.ufl_element())
 
     for i in reversed(indices):
-        V = V.sub(i)
-    V._fine = fine
-    fine._coarse = V
+        newV = newV.sub(i)
+    if direction == TransferDirection.COARSEN:
+        newV._fine = oldV
+        oldV._coarse = newV
+    elif direction == TransferDirection.REFINE:
+        newV._coarse = oldV
+        oldV._fine = newV
 
     # FIXME: This replicates some code from dmhooks.coarsen, but we
     # can't do things there because that code calls this code.
@@ -154,39 +193,47 @@ def coarsen_function_space(V, self, coefficient_mapping=None):
     # than which we do the MG, dm.coarsen is never called, so the
     # hooks are not attached. Instead we just call (say) inject which
     # coarsens the functionspace.
-    cdm = V.dm
-    parent = get_parent(fine.dm)
+    newdm = newV.dm
+    parent = get_parent(oldV.dm)
     try:
-        add_hook(parent, setup=partial(push_parent, cdm, parent), teardown=partial(pop_parent, cdm, parent),
+        add_hook(parent, setup=partial(push_parent, newdm, parent), teardown=partial(pop_parent, newdm, parent),
                  call_setup=True)
     except ValueError:
         # Not in an add_hooks context
         pass
 
-    return V
+    return newV
 
 
-@coarsen.register(firedrake.Function)
-def coarsen_function(expr, self, coefficient_mapping=None):
+@transfer.register(firedrake.Function)
+def transfer_function(expr, self, direction, *, coefficient_mapping=None):
     if coefficient_mapping is None:
         coefficient_mapping = {}
     new = coefficient_mapping.get(expr)
     if new is None:
         V = expr.function_space()
         manager = firedrake.dmhooks.get_transfer_manager(expr.function_space().dm)
-        V = self(expr.function_space(), self)
-        new = firedrake.Function(V, name="coarse_%s" % expr.name())
-        manager.inject(expr, new)
+        V = self(expr.function_space(), self, direction)
+        if direction == TransferDirection.COARSEN:
+            name = f"coarse_{expr.name()}"
+            transfer = manager.inject
+        elif direction == TransferDirection.REFINE:
+            name = f"fine_{expr.name()}"
+            transfer = manager.prolong
+        else:
+            raise TransferError(f"Unhandled transfer direction {direction}")
+        new = firedrake.Function(V, name=name)
+        transfer(expr, new)
     return new
 
 
-@coarsen.register(firedrake.Constant)
-def coarsen_constant(expr, self, coefficient_mapping=None):
+@transfer.register(firedrake.Constant)
+def transfer_constant(expr, self, direction, *, coefficient_mapping=None):
     if coefficient_mapping is None:
         coefficient_mapping = {}
     new = coefficient_mapping.get(expr)
     if new is None:
-        mesh = self(expr.ufl_domain(), self)
+        mesh = self(expr.ufl_domain(), self, direction)
         new = firedrake.Constant(numpy.zeros(expr.ufl_shape,
                                              dtype=expr.dat.dtype),
                                  domain=mesh)
@@ -195,8 +242,8 @@ def coarsen_constant(expr, self, coefficient_mapping=None):
     return new
 
 
-@coarsen.register(firedrake.NonlinearVariationalProblem)
-def coarsen_nlvp(problem, self, coefficient_mapping=None):
+@transfer.register(firedrake.NonlinearVariationalProblem)
+def transfer_nlvp(problem, self, direction, *, coefficient_mapping=None):
     # Build set of coefficients we need to coarsen
     seen = set()
     coefficients = problem.F.coefficients() + problem.J.coefficients()
@@ -208,56 +255,70 @@ def coarsen_nlvp(problem, self, coefficient_mapping=None):
         coefficient_mapping = {}
     for c in coefficients:
         if c not in seen:
-            coefficient_mapping[c] = self(c, self, coefficient_mapping=coefficient_mapping)
+            coefficient_mapping[c] = self(c, self, direction, coefficient_mapping=coefficient_mapping)
             seen.add(c)
 
     u = coefficient_mapping[problem.u]
 
-    bcs = [self(bc, self) for bc in problem.bcs]
-    J = self(problem.J, self, coefficient_mapping=coefficient_mapping)
-    Jp = self(problem.Jp, self, coefficient_mapping=coefficient_mapping)
-    F = self(problem.F, self, coefficient_mapping=coefficient_mapping)
+    bcs = [self(bc, self, direction) for bc in problem.bcs]
+    J = self(problem.J, self, direction,
+             coefficient_mapping=coefficient_mapping)
+    Jp = self(problem.Jp, self, direction,
+              coefficient_mapping=coefficient_mapping)
+    F = self(problem.F, self, direction,
+             coefficient_mapping=coefficient_mapping)
 
     problem = firedrake.NonlinearVariationalProblem(F, u, bcs=bcs, J=J, Jp=Jp,
                                                     form_compiler_parameters=problem.form_compiler_parameters)
     return problem
 
 
-@coarsen.register(firedrake.VectorSpaceBasis)
-def coarsen_vectorspacebasis(basis, self, coefficient_mapping=None):
-    coarse_vecs = [self(vec, self, coefficient_mapping=coefficient_mapping) for vec in basis._vecs]
-    vsb = firedrake.VectorSpaceBasis(coarse_vecs, constant=basis._constant)
+@transfer.register(firedrake.VectorSpaceBasis)
+def transfer_vectorspacebasis(basis, self, direction, *, coefficient_mapping=None):
+    new_vecs = [self(vec, self, direction,
+                     coefficient_mapping=coefficient_mapping) for vec in basis._vecs]
+    vsb = firedrake.VectorSpaceBasis(new_vecs, constant=basis._constant)
     vsb.orthonormalize()
     return vsb
 
 
-@coarsen.register(firedrake.MixedVectorSpaceBasis)
-def coarsen_mixedvectorspacebasis(mspbasis, self, coefficient_mapping=None):
-    coarse_V = self(mspbasis._function_space, self, coefficient_mapping=coefficient_mapping)
-    coarse_bases = []
+@transfer.register(firedrake.MixedVectorSpaceBasis)
+def transfer_mixedvectorspacebasis(mspbasis, self, direction, *, coefficient_mapping=None):
+    new_V = self(mspbasis._function_space, self, direction,
+                 coefficient_mapping=coefficient_mapping)
+    new_bases = []
 
     for basis in mspbasis._bases:
         if isinstance(basis, firedrake.VectorSpaceBasis):
-            coarse_bases.append(self(basis, self, coefficient_mapping=coefficient_mapping))
+            new_bases.append(self(basis, self, direction,
+                                  coefficient_mapping=coefficient_mapping))
         elif basis.index is not None:
-            coarse_bases.append(coarse_V.sub(basis.index))
+            new_bases.append(new_V.sub(basis.index))
         else:
             raise RuntimeError("MixedVectorSpaceBasis can only contain vector space bases or indexed function spaces")
 
-    return firedrake.MixedVectorSpaceBasis(coarse_V, coarse_bases)
+    return firedrake.MixedVectorSpaceBasis(new_V, new_bases)
 
 
-@coarsen.register(firedrake.solving_utils._SNESContext)
-def coarsen_snescontext(context, self, coefficient_mapping=None):
+@transfer.register(firedrake.solving_utils._SNESContext)
+def transfer_snescontext(context, self, direction, *, coefficient_mapping=None):
     if coefficient_mapping is None:
         coefficient_mapping = {}
 
-    # Have we already done this?
-    coarse = context._coarse
-    if coarse is not None:
-        return coarse
+    if direction == TransferDirection.COARSEN:
+        # Have we already done this?
+        coarse = context._coarse
+        if coarse is not None:
+            return coarse
+    elif direction == TransferDirection.REFINE:
+        fine = context._fine
+        if fine is not None:
+            return fine
+    else:
+        raise TransferError(f"Unhandled transfer direction {direction}")
 
-    problem = self(context._problem, self, coefficient_mapping=coefficient_mapping)
+    problem = self(context._problem, self, direction,
+                   coefficient_mapping=coefficient_mapping)
     appctx = context.appctx
     new_appctx = {}
     for k in sorted(appctx.keys()):
@@ -265,45 +326,55 @@ def coarsen_snescontext(context, self, coefficient_mapping=None):
         if k != "state":
             # Constructor makes this one.
             try:
-                new_appctx[k] = self(v, self, coefficient_mapping=coefficient_mapping)
-            except CoarseningError:
+                new_appctx[k] = self(v, self, direction,
+                                     coefficient_mapping=coefficient_mapping)
+            except TransferError:
                 # Assume not something that needs coarsening (e.g. float)
                 new_appctx[k] = v
-    coarse = type(context)(problem,
-                           mat_type=context.mat_type,
-                           pmat_type=context.pmat_type,
-                           appctx=new_appctx,
-                           transfer_manager=context.transfer_manager)
-    coarse._fine = context
-    context._coarse = coarse
+    new_ctx = type(context)(problem,
+                            mat_type=context.mat_type,
+                            pmat_type=context.pmat_type,
+                            appctx=new_appctx,
+                            transfer_manager=context.transfer_manager)
+    if direction == TransferDirection.COARSEN:
+        new_ctx._fine = context
+        context._coarse = new_ctx
+    elif direction == TransferDirection.REFINE:
+        new_ctx._coarse = context
+        context._fine = new_ctx
+    else:
+        raise TransferError(f"Unhandled transfer direction {direction}")
 
-    # Now that we have the coarse snescontext, push it to the coarsened DMs
-    # Otherwise they won't have the right transfer manager when they are
-    # coarsened in turn
+    # Now that we have the transferred snescontext, push it to the
+    # transferred DMs. Otherwise they won't have the right transfer
+    # manager when they are transferred in turn
     from firedrake.dmhooks import get_appctx, push_appctx, pop_appctx
     from firedrake.dmhooks import add_hook, get_parent
     from itertools import chain
     for val in chain(coefficient_mapping.values(), (bc._original_val for bc in problem.bcs)):
         if isinstance(val, firedrake.function.Function):
             V = val.function_space()
-            coarseneddm = V.dm
+            newdm = V.dm
             parentdm = get_parent(context._problem.u.function_space().dm)
 
             # Now attach the hook to the parent DM
-            if get_appctx(coarseneddm) is None:
-                push_appctx(coarseneddm, coarse)
-                teardown = partial(pop_appctx, coarseneddm, coarse)
+            if get_appctx(newdm) is None:
+                push_appctx(newdm, new_ctx)
+                teardown = partial(pop_appctx, newdm, new_ctx)
                 add_hook(parentdm, teardown=teardown)
 
     ises = problem.J.arguments()[0].function_space()._ises
-    coarse._nullspace = self(context._nullspace, self, coefficient_mapping=coefficient_mapping)
-    coarse.set_nullspace(coarse._nullspace, ises, transpose=False, near=False)
-    coarse._nullspace_T = self(context._nullspace_T, self, coefficient_mapping=coefficient_mapping)
-    coarse.set_nullspace(coarse._nullspace_T, ises, transpose=True, near=False)
-    coarse._near_nullspace = self(context._near_nullspace, self, coefficient_mapping=coefficient_mapping)
-    coarse.set_nullspace(coarse._near_nullspace, ises, transpose=False, near=True)
+    new_ctx._nullspace = self(context._nullspace, self, direction,
+                              coefficient_mapping=coefficient_mapping)
+    new_ctx.set_nullspace(new_ctx._nullspace, ises, transpose=False, near=False)
+    new_ctx._nullspace_T = self(context._nullspace_T, self, direction,
+                                coefficient_mapping=coefficient_mapping)
+    new_ctx.set_nullspace(new_ctx._nullspace_T, ises, transpose=True, near=False)
+    new_ctx._near_nullspace = self(context._near_nullspace, self, direction,
+                                   coefficient_mapping=coefficient_mapping)
+    new_ctx.set_nullspace(new_ctx._near_nullspace, ises, transpose=False, near=True)
 
-    return coarse
+    return new_ctx
 
 
 class Interpolation(object):
