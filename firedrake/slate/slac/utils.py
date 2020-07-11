1c370174351cd5efa011649d9fbc7cc415577100
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2020-07-11 12:04:33 +0100

    Docstring.

--- firedrake/slate/slac/utils.py
+++ firedrake/slate/slac/utils.py
@@ -153,7 +153,13 @@ class Transformer(Visitor):
 
 
 def slate_to_gem(expression):
-    """ Method encapsulating stage 1 of tsslac. """
+    """Convert a slate expression to gem.
+
+        :arg expression: A slate expression.
+        :returns: A singleton list of gem expressions and
+        a mapping from gem variables to UFL "terminal" forms.
+    """
+
     mapper, var2terminal = slate2gem(expression)
     return mapper, var2terminal
 
