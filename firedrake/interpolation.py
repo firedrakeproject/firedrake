75747ba0038010f09a5a4ae7c12ad74401bdbe84
Lawrence Mitchell <lawrence@wence.uk>
2020-07-13 17:03:48 +0100

    interpolation: Provide default domain to compile expression
    It might be that the provided expression has no domain attached.

--- firedrake/interpolation.py
+++ firedrake/interpolation.py
@@ -222,7 +222,8 @@ def _interpolator(V, tensor, expr, subset, arguments, access):
     if not isinstance(expr, firedrake.Expression):
         if expr.ufl_domain() and expr.ufl_domain() != V.mesh():
             raise NotImplementedError("Interpolation onto another mesh not supported.")
-        ast, oriented, needs_cell_sizes, coefficients, _ = compile_expression_dual_evaluation(expr, to_element, coords, coffee=False)
+        ast, oriented, needs_cell_sizes, coefficients, _ = compile_expression_dual_evaluation(expr, to_element, coords,
+                                                                                              domain=V.mesh(), coffee=False)
         kernel = op2.Kernel(ast, ast.name, requires_zeroed_output_arguments=True)
     elif hasattr(expr, "eval"):
         to_pts = []
