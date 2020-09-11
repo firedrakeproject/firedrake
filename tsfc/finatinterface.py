ef528e140a0ba29bc1ab63d16ba55afeadcc6bef
Robert Kirby <robert.c.kirby@gmail.com>
2020-09-11 12:23:08 -0500

    Hook up KMV elements (#231)

--- tsfc/finatinterface.py
+++ tsfc/finatinterface.py
@@ -51,6 +51,7 @@ supported_elements = {
     "Nonconforming Arnold-Winther": finat.ArnoldWintherNC,
     "Conforming Arnold-Winther": finat.ArnoldWinther,
     "Hermite": finat.Hermite,
+    "Kong-Mulder-Veldhuizen": finat.KongMulderVeldhuizen,
     "Argyris": finat.Argyris,
     "Mardal-Tai-Winther": finat.MardalTaiWinther,
     "Morley": finat.Morley,
