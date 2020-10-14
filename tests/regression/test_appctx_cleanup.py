3bcbeb9447da9a42fe245f8489cdbf15f1a61bca
Reuben Hill <ReubenHill@users.noreply.github.com>
2020-10-14 14:02:32 +0100

    Don't skip test_appctx_cleanup in complex mode

--- tests/regression/test_appctx_cleanup.py
+++ tests/regression/test_appctx_cleanup.py
@@ -1,5 +1,4 @@
 import numpy
-import pytest
 from firedrake import *
 
 
@@ -24,7 +23,6 @@ class NonePC(PCBase):
         x.copy(y)
 
 
-@pytest.mark.skipif(utils.complex_mode, reason="Broken: FIX BEFORE MERGING")
 def test_appctx_cleanup():
     mesh = UnitSquareMesh(1, 1)
     mh = MeshHierarchy(mesh, 2)
