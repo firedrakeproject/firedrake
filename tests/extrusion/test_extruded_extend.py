dd7447e06aa04b1983c4e528ea9b50ded84a48f5
Ed Bueler <elbueler@alaska.edu>
2020-10-15 17:35:34 -0800

    This test gives 71% coverage of firedrake/extrusion_extend.py.
    I am working on covering the VectorFunctionSpace and mixed cases.

--- tests/extrusion/test_extruded_extend.py
+++ tests/extrusion/test_extruded_extend.py
@@ -1,27 +1,37 @@
 from firedrake import *
 
-def test_extruded_extend():
-    Nx, Nz = 10, 4
+def standard2d():
+    Nx, Nz = 4, 3
     base_mesh = UnitIntervalMesh(Nx)
-    mesh = ExtrudedMesh(base_mesh, layers=Nz, layer_height=1.0/Nx)
-    Q1D = FunctionSpace(base_mesh, 'CG', 1)
-    x = SpatialCoordinate(base_mesh)
-    f1D = Function(Q1D).interpolate(cos(2.0 * pi* x[0]))
-    fextended = ExtrudedExtendFunction(mesh, f1D)
+    mesh = ExtrudedMesh(base_mesh, layers=Nz, layer_height=1.0/Nz)
+    return mesh
+
+def standard3d():
+    Nx, Ny, Nz = 4, 3, 3
+    base_mesh = UnitSquareMesh(Nx, Ny, quadrilateral=True)
+    mesh = ExtrudedMesh(base_mesh, layers=Nz, layer_height=1.0/Nz)
+    return mesh
+
+def test_extruded_extend_scalar():
+    mesh = standard2d()
+    Vbase = FunctionSpace(mesh._base_mesh,'P',1)
+    x = SpatialCoordinate(mesh._base_mesh)
+    fbase = Function(Vbase).interpolate(cos(2.0 * pi* x[0]))
+    fextended = ExtrudedExtendFunction(mesh, fbase)
     x,y = SpatialCoordinate(mesh)
-    Q2D = FunctionSpace(mesh, 'CG', 1)
-    f = Function(Q2D).interpolate(cos(2.0 * pi* x))
+    Vextruded = FunctionSpace(mesh,'Q',1)
+    f = Function(Vextruded).interpolate(cos(2.0 * pi* x))
     assert errornorm(f, fextended) < 1.0e-10
+    fextendedagain = ExtrudedExtendFunction(mesh, fbase, target=fextended)
+    assert errornorm(f, fextendedagain) < 1.0e-10
 
 def test_extruded_extend_topbottom():
-    Nx, Nz = 10, 4
-    base_mesh = UnitIntervalMesh(Nx)
-    mesh = ExtrudedMesh(base_mesh, layers=Nz, layer_height=1.0/Nx)
+    mesh = standard2d()
     x,y = SpatialCoordinate(mesh)
-    V = FunctionSpace(mesh, 'CG', 1)
+    V = FunctionSpace(mesh,'Q',1)
     f = Function(V).interpolate(cos(2.0 * pi* x) * y)
     fbottom = Function(V).interpolate(0.0 * x)
-    ftop = Function(V).interpolate(cos(2.0 * pi* x) * 0.4)
+    ftop = Function(V).interpolate(cos(2.0 * pi* x))
     fbottomextend = ExtrudedExtendFunction(mesh, f, extend='bottom')
     ftopextend = ExtrudedExtendFunction(mesh, f, extend='top')
     assert errornorm(fbottom, fbottomextend) < 1.0e-10
