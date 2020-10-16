b549eda5fca602d8e7e70ca4dd00acc301bbe71d
Ed Bueler <elbueler@alaska.edu>
2020-10-16 13:18:49 -0800

    Implement and test {Vector,Tensor}Element cases.

--- firedrake/extrusion_function.py
+++ firedrake/extrusion_function.py
@@ -13,6 +13,26 @@ def expand_element(element):
     return ufl.TensorProductElement(element,
                                     ufl.FiniteElement("R", ufl.interval, 0))
 
+@expand.register(ufl.VectorElement)
+def expand_vector(element):
+    shapes = set(element.value_shape())
+    if shapes != {element.cell().geometric_dimension()}:
+        raise NotImplementedError("Can't infer shape of expanded element")
+    sub_element = expand(element.sub_elements()[0])
+    return type(element)(sub_element,dim=element.num_sub_elements())
+
+@expand.register(ufl.TensorElement)
+def expand_tensor(element):
+    shapes = set(element.value_shape())
+    if shapes != {element.cell().geometric_dimension()}:
+        raise NotImplementedError("Can't infer shape of expanded element")
+    sub_element = expand(element.sub_elements()[0])
+    return type(element)(sub_element,shape=element.value_shape())
+
+@expand.register(ufl.MixedElement)
+def expand_mixed(element):
+    return ufl.MixedElement(*(expand(e) for e in element.sub_elements()))
+
 @expand.register(ufl.BrokenElement)
 def expand_broken(element):
     return ufl.BrokenElement(expand(element._element))
@@ -21,20 +41,6 @@ def expand_broken(element):
 def expand_restricted(element):
     return ufl.RestrictedElement(expand(element._element),
                                  element.restriction_domain())
-
-@expand.register(ufl.VectorElement)
-@expand.register(ufl.TensorElement)
-def expand_vector(element):
-    shapes = set(element.value_shape())
-    if shapes != {element.cell().geometric_dimension()}:
-        raise NotImplementedError("Can't infer shape of expanded element")
-    sub_element = expand(element.sub_elements()[0])
-    return type(element)(sub_element)
-
-@expand.register(ufl.MixedElement)
-def expand_mixed(element):
-    return ufl.MixedElement(*(expand(e) for e in element.sub_elements()))
-
 
 @expand.register(ufl.EnrichedElement)
 def expand_enriched(element):
@@ -50,6 +56,15 @@ def extract_tpe(element):
     assert extruded.cell() == ufl.interval
     return ufl.TensorProductElement(base,
                                     ufl.FiniteElement("R", ufl.interval, 0))
+
+@extract.register(ufl.VectorElement)
+@extract.register(ufl.TensorElement)
+def extract_vector(element):
+    shapes = set(element.value_shape())
+    if shapes != {element.cell().geometric_dimension()}:
+        raise NotImplementedError("Can't infer shape of extracted element")
+    sub_element = extract(element.sub_elements()[0])
+    return type(element)(sub_element)
 
 @extract.register(ufl.BrokenElement)
 def extract_broken(element):
@@ -73,15 +88,6 @@ def extract_mixed(element):
 def extract_hdivcurl(element):
     return type(element)(extract(element._element))
 
-@extract.register(ufl.VectorElement)
-@extract.register(ufl.TensorElement)
-def extract_vector(element):
-    shapes = set(element.value_shape())
-    if shapes != {element.cell().geometric_dimension()}:
-        raise NotImplementedError("Can't infer shape of extracted element")
-    sub_element = extract(element.sub_elements()[0])
-    return type(element)(sub_element)
-
 def extrude_function(mesh, fcn, extend=None, target=None):
     """On an extruded mesh, extend a function by a constant in the extruded
     direction.
@@ -92,24 +98,29 @@ def extrude_function(mesh, fcn, extend=None, target=None):
 
     ``extend`` has three cases:
 
-    ``None``
-        ``fcn`` is defined on the base mesh and the returned Function has
-        the same values in the extruded direction.  The returned function
-        uses the degree 0 'R' space.
+        ``None``
+            ``fcn`` is defined on the base mesh (mesh._base_mesh).  The returned
+            Function has the same values, and same dimension for VectorElement
+            and TensorElement cases, in the extruded direction.  The returned
+            function uses the degree 0 'R' space.
 
-    ``"top"`` or ``"bottom"``
-        ``fcn`` is defined on the extruded mesh.  The values of the returned
-        Function come from the values of ``fcn`` on the ``"top"``/``"bottom"``
-        boundary of the extruded mesh.  The returned function has the same
-        layout as the ``fcn``.
+        ``"top"``/``"bottom"``
+            ``fcn`` is defined on the extruded mesh.  The values of the returned
+            Function come from the values of ``fcn`` on the ``"top"``/``"bottom"``
+            boundary of the extruded mesh.  The returned function has the same
+            layout as ``fcn``.
 
     ``target``: If not None, this function must have the same layout as
         the returned function, and it will be modified.
     """
 
+    # note: extract() applies when fcn is already defined on the extruded mesh
+    #       expand() applies when fcn is only defined on the base_mesh
     if extend is not None:
         Q = FunctionSpace(mesh, extract(fcn.ufl_element()))
     else:
+        if type(fcn.ufl_element()) not in {ufl.FiniteElement,ufl.VectorElement,ufl.TensorElement}:
+            raise NotImplementedError('not implemented for this element type')
         Q = FunctionSpace(mesh, expand(fcn.ufl_element()))
 
     if target is not None:
--- tests/extrusion/test_extrude_function.py
+++ tests/extrusion/test_extrude_function.py
@@ -1,50 +1,71 @@
 from firedrake import *
 
-def standard2d():
+def standardextruded2d():
     Nx, Nz = 4, 3
     base_mesh = UnitIntervalMesh(Nx)
     mesh = ExtrudedMesh(base_mesh, layers=Nz, layer_height=1.0/Nz)
     return mesh
 
-def standard3d():
+def standardextruded3d():
     Nx, Ny, Nz = 4, 3, 3
     base_mesh = UnitSquareMesh(Nx, Ny, quadrilateral=True)
     mesh = ExtrudedMesh(base_mesh, layers=Nz, layer_height=1.0/Nz)
     return mesh
 
 def test_extruded_extend_scalar():
-    mesh = standard2d()
+    mesh = standardextruded2d()
+    xbase = SpatialCoordinate(mesh._base_mesh)
     Vbase = FunctionSpace(mesh._base_mesh,'P',1)
-    x = SpatialCoordinate(mesh._base_mesh)
-    fbase = Function(Vbase).interpolate(cos(2.0 * pi* x[0]))
-    fextended = extrude_function(mesh, fbase)
+    fbase = Function(Vbase).interpolate(cos(2.0 * pi* xbase[0]))
+    fextruded = extrude_function(mesh, fbase)
     x,y = SpatialCoordinate(mesh)
-    Vextruded = FunctionSpace(mesh,'Q',1)
-    f = Function(Vextruded).interpolate(cos(2.0 * pi* x))
-    assert errornorm(f, fextended) < 1.0e-10
-    fextendedagain = extrude_function(mesh, fbase, target=fextended)
-    assert errornorm(f, fextendedagain) < 1.0e-10
+    V = FunctionSpace(mesh,'Q',1)
+    f = Function(V).interpolate(cos(2.0 * pi* x))
+    assert errornorm(f, fextruded) < 1.0e-10
+    fextrudedagain = extrude_function(mesh, fbase, target=fextruded)
+    assert errornorm(f, fextrudedagain) < 1.0e-10
 
-def test_extruded_extend_vector():
-    mesh = standard3d()
+def test_extruded_extend_vector_tensor():
+    mesh = standardextruded3d()
+    xbase,ybase = SpatialCoordinate(mesh._base_mesh)
     Vbase = VectorFunctionSpace(mesh._base_mesh,FiniteElement('Q',quadrilateral,1))
-    x,y = SpatialCoordinate(mesh._base_mesh)
-    fbase = Function(Vbase).interpolate(as_vector([cos(2.0 * pi * x),cos(2.0 * pi * y)]))
-    fextended = extrude_function(mesh, fbase)
+    fbase = Function(Vbase).interpolate(as_vector([cos(2.0 * pi * xbase),cos(2.0 * pi * ybase)]))
+    fextruded = extrude_function(mesh, fbase)
     x,y,z = SpatialCoordinate(mesh)
-    Vextruded = VectorFunctionSpace(mesh,'Q',1, dim=2)
-    f = Function(Vextruded).interpolate(as_vector([cos(2.0 * pi * x),cos(2.0 * pi * y)]))
-    assert errornorm(f, fextended) < 1.0e-10
+    V = VectorFunctionSpace(mesh,'Q',1, dim=2)  # note dimension matches base_mesh
+    f = Function(V).interpolate(as_vector([cos(2.0 * pi * x),cos(2.0 * pi * y)]))
+    assert errornorm(f, fextruded) < 1.0e-10
+    Tbase = TensorFunctionSpace(mesh._base_mesh,FiniteElement('Q',quadrilateral,1))
+    fbase = Function(Tbase).interpolate(as_tensor([[cos(2.0 * pi * xbase),cos(2.0 * pi * ybase)],
+                                                   [xbase,ybase]]))
+    fextruded = extrude_function(mesh, fbase)
+    x,y,z = SpatialCoordinate(mesh)
+    T = TensorFunctionSpace(mesh,'Q',1, shape=(2,2))  # note dimension matches base_mesh
+    f = Function(T).interpolate(as_tensor([[cos(2.0 * pi * x),cos(2.0 * pi * y)],
+                                           [x,y]]))
+    assert errornorm(f, fextruded) < 1.0e-10
 
 def test_extruded_extend_topbottom():
-    mesh = standard2d()
+    mesh = standardextruded2d()
     x,y = SpatialCoordinate(mesh)
     V = FunctionSpace(mesh,'Q',1)
     f = Function(V).interpolate(cos(2.0 * pi* x) * y)
     fbottom = Function(V).interpolate(0.0 * x)
     ftop = Function(V).interpolate(cos(2.0 * pi* x))
-    fbottomextend = extrude_function(mesh, f, extend='bottom')
-    ftopextend = extrude_function(mesh, f, extend='top')
-    assert errornorm(fbottom, fbottomextend) < 1.0e-10
-    assert errornorm(ftop, ftopextend) < 1.0e-10
+    fbottomextruded = extrude_function(mesh, f, extend='bottom')
+    ftopextruded = extrude_function(mesh, f, extend='top')
+    assert errornorm(fbottom, fbottomextruded) < 1.0e-10
+    assert errornorm(ftop, ftopextruded) < 1.0e-10
+
+def test_extruded_extend_topbottom_vector():
+    mesh = standardextruded2d()
+    x,y = SpatialCoordinate(mesh)
+    V = VectorFunctionSpace(mesh,'Q',1)
+    f = Function(V).interpolate(as_vector([cos(2.0 * pi* x) * y,x]))
+    fbottom = Function(V).interpolate(as_vector([0.0 * x,x]))
+    ftop = Function(V).interpolate(as_vector([cos(2.0 * pi* x),x]))
+    fbottomextruded = extrude_function(mesh, f, extend='bottom')
+    ftopextruded = extrude_function(mesh, f, extend='top')
+    assert errornorm(fbottom, fbottomextruded) < 1.0e-10
+    assert errornorm(ftop, ftopextruded) < 1.0e-10
 
