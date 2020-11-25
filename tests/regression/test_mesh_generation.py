f778d4cdc826eb3213fb6cf90ddb722409d8ebf2
jingminxia <jingmin.xia@maths.ox.ac.uk>
2020-11-25 16:06:10 +0000

    Remove the test for the cuttings of hexahedra due to adding NotImplementedError in BoxMesh generation

--- tests/regression/test_mesh_generation.py
+++ tests/regression/test_mesh_generation.py
@@ -435,10 +435,3 @@ def test_changing_default_reorder_works(reorder):
     finally:
         parameters["reorder_meshes"] = old_reorder
 
-
-@pytest.mark.parametrize("kind, num_cells",
-                         [("default", 6), ("crossed", 5)])
-def test_boxmesh_kind(kind, num_cells):
-    m = BoxMesh(1, 1, 1, 1, 1, 1, diagonal=kind)
-    m.init()
-    assert m.num_cells() == num_cells
