1be78398286341011a50a40445d4622dd7f3be7d
Jack Betteridge <J.D.Betteridge@bath.ac.uk>
2020-12-11 10:48:53 +0000

    Fixed warning generated by if L is 0

--- firedrake/variational_solver.py
+++ firedrake/variational_solver.py
@@ -298,7 +298,7 @@ class LinearVariationalProblem(NonlinearVariationalProblem):
         # In the linear case, the Jacobian is the equation LHS.
         J = a
         # Jacobian is checked in superclass, but let's check L here.
-        if L is 0:  # noqa: F632
+        if not isinstance(L, (ufl.Form, slate.slate.TensorBase)) and L == 0:
             F = ufl_expr.action(J, u)
         else:
             if not isinstance(L, (ufl.Form, slate.slate.TensorBase)):
