8e23101ea218d155a6329453728bfcfcef970bcd
JDBetteridge <43041811+JDBetteridge@users.noreply.github.com>
2021-01-20 16:47:04 +0000

    Slate BLAS/LAPACK fix (#1928)
    * Adds installed BLAS/LAPACK to compiler options in Slate
    
    * Add compiler flags for OpenBLAS downloaded by PETSc to Slate
    
    * Change logic for determining linker paths for BLAS
    
    * Change None variables to empty lists
    
    * Added get_petsc_variables convenience function (used in slac/compiler.py)
    
    * Changed functools.cache to older functools.lru_cache
    
    * Fixed syntax errors due to dirty caches
    
    * Strip -I from EIGEN_INCLUDE_DIR
    
    * Replaced pathlib Path with os.path.join
    
    * Use maxsplit for splitting key and value

--- firedrake/slate/slac/compiler.py
+++ firedrake/slate/slac/compiler.py
@@ -18,7 +18,6 @@ from coffee import base as ast
 
 import time
 from hashlib import md5
-import os.path
 
 from firedrake_citations import Citations
 from firedrake.tsfc_interface import SplitKernel, KernelInfo, TSFCKernel
@@ -27,6 +26,7 @@ from firedrake.slate.slac.utils import topological_sort, slate_to_gem, merge_loo
 from firedrake import op2
 from firedrake.logging import logger
 from firedrake.parameters import parameters
+from firedrake.petsc import get_petsc_variables
 from firedrake.utils import complex_mode, ScalarType_c, as_cstr
 from ufl.log import GREEN
 from gem.utils import groupby
@@ -55,19 +55,25 @@ except ValueError:
     PETSC_ARCH = None
 
 EIGEN_INCLUDE_DIR = None
+BLASLAPACK_LIB = None
+BLASLAPACK_INCLUDE = None
 if not complex_mode:
     if COMM_WORLD.rank == 0:
-        filepath = os.path.join(PETSC_ARCH or PETSC_DIR, "lib", "petsc", "conf", "petscvariables")
-        with open(filepath) as file:
-            for line in file:
-                if line.find("EIGEN_INCLUDE") == 0:
-                    EIGEN_INCLUDE_DIR = line[18:].rstrip()
-                    break
+        petsc_variables = get_petsc_variables()
+        EIGEN_INCLUDE_DIR = petsc_variables.get("EIGEN_INCLUDE")
         if EIGEN_INCLUDE_DIR is None:
             raise ValueError("""Could not find Eigen configuration in %s. Did you build PETSc with Eigen?""" % PETSC_ARCH or PETSC_DIR)
+        EIGEN_INCLUDE_DIR = EIGEN_INCLUDE_DIR.lstrip('-I')
         EIGEN_INCLUDE_DIR = COMM_WORLD.bcast(EIGEN_INCLUDE_DIR, root=0)
+
+        BLASLAPACK_LIB = petsc_variables.get("BLASLAPACK_LIB", "")
+        BLASLAPACK_LIB = COMM_WORLD.bcast(BLASLAPACK_LIB, root=0)
+        BLASLAPACK_INCLUDE = petsc_variables.get("BLASLAPACK_INCLUDE", "")
+        BLASLAPACK_INCLUDE = COMM_WORLD.bcast(BLASLAPACK_INCLUDE, root=0)
     else:
         EIGEN_INCLUDE_DIR = COMM_WORLD.bcast(None, root=0)
+        BLASLAPACK_LIB = COMM_WORLD.bcast(None, root=0)
+        BLASLAPACK_INCLUDE = COMM_WORLD.bcast(None, root=0)
 
 cell_to_facets_dtype = np.dtype(np.int8)
 
@@ -164,7 +170,10 @@ def generate_loopy_kernel(slate_expr, tsfc_parameters=None):
     # then attach code as a c-string to the op2kernel
     code = loopy.generate_code_v2(loopy_merged).device_code()
     code = code.replace(f'void {loopy_merged.name}', f'static void {loopy_merged.name}')
-    loopykernel = op2.Kernel(code, loopy_merged.name, ldargs=["-llapack"])
+    loopykernel = op2.Kernel(code,
+                             loopy_merged.name,
+                             include_dirs=BLASLAPACK_INCLUDE.split(),
+                             ldargs=BLASLAPACK_LIB.split())
 
     kinfo = KernelInfo(kernel=loopykernel,
                        integral_type="cell",  # slate can only do things as contributions to the cell integrals
