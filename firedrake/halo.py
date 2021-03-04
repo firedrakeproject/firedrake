44fd923563624a55a56f40995a1f1c248cd0426d
Lawrence Mitchell <lawrence@wence.uk>
2021-03-04 10:54:41 +0000

    halo: Adapt to new PetscSFBcast interface
    Need to provide op in this case too. Since they're now wrapped in
    petsc4py, just use those definitions.

--- firedrake/halo.py
+++ firedrake/halo.py
@@ -131,14 +131,14 @@ class Halo(op2.Halo):
         if self.comm.size == 1:
             return
         mtype, _ = _get_mtype(dat)
-        dmcommon.halo_begin(self.sf, dat, mtype, False)
+        self.sf.bcastBegin(mtype, dat._data, dat._data, MPI.REPLACE)
 
     def global_to_local_end(self, dat, insert_mode):
         assert insert_mode is op2.WRITE, "Only WRITE GtoL supported"
         if self.comm.size == 1:
             return
         mtype, _ = _get_mtype(dat)
-        dmcommon.halo_end(self.sf, dat, mtype, False)
+        self.sf.bcastEnd(mtype, dat._data, dat._data, MPI.REPLACE)
 
     def local_to_global_begin(self, dat, insert_mode):
         assert insert_mode in {op2.INC, op2.MIN, op2.MAX}, "%s LtoG not supported" % insert_mode
@@ -151,7 +151,7 @@ class Halo(op2.Halo):
               (True, op2.MIN): MPI.MIN,
               (False, op2.MAX): _contig_max_op,
               (True, op2.MAX): MPI.MAX}[(builtin, insert_mode)]
-        dmcommon.halo_begin(self.sf, dat, mtype, True, op=op)
+        self.sf.reduceBegin(mtype, dat._data, dat._data, op)
 
     def local_to_global_end(self, dat, insert_mode):
         assert insert_mode in {op2.INC, op2.MIN, op2.MAX}, "%s LtoG not supported" % insert_mode
@@ -164,4 +164,4 @@ class Halo(op2.Halo):
               (True, op2.MIN): MPI.MIN,
               (False, op2.MAX): _contig_max_op,
               (True, op2.MAX): MPI.MAX}[(builtin, insert_mode)]
-        dmcommon.halo_end(self.sf, dat, mtype, True, op=op)
+        self.sf.reduceEnd(mtype, dat._data, dat._data, op)
