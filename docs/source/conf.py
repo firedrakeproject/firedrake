7d689e57f546125c5464aeb2d4ce5884f7b56917
Lawrence Mitchell <lawrence@wence.uk>
2021-04-08 10:27:09 +0100

    Remove upper bound in copyright range on webpage

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Firedrake'
-copyright = u'2013-2018, Imperial College London and others'
+copyright = u'2013-, Imperial College London and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
