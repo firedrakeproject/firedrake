6b622bcd14fb9191b5aee6b027ca2363ae363d8b
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-04-19 12:54:15 +0200

    Slate optimiser: Also handle already partially optimised expressions.

--- firedrake/slate/slac/optimise.py
+++ firedrake/slate/slac/optimise.py
@@ -351,9 +351,6 @@ class SwapController(object):
         yield ActionBag(state.coeff, swap_op, state.pick_op)
 
 def optimise(expr, tsfc_parameters):
-    from firedrake.slate.slate import BinaryOp, UnaryOp
-    if ((isinstance(expr, BinaryOp) and isinstance(expr.children, AssembledVector))
-        or isinstance(expr, UnaryOp)):
     # Optimise expression which is already partially optimised
     # by optimising a subexpression that is not optimised yet
     # the non optimised expression is a Mul
