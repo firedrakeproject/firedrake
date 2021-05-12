c50b2c4ab5406ecdaa65b8f68778ca8ed08c180e
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-05-12 09:42:26 +0200

    Moving the local matfree solve kernel to slate land.

--- firedrake/slate/slac/utils.py
+++ firedrake/slate/slac/utils.py
@@ -337,7 +337,7 @@ def topological_sort(exprs):
     return schedule
 
 
-def merge_loopy(slate_loopy, output_arg, builder, var2terminal,  wrapper_name, gem2pym, strategy="terminals_first", slate_expr = None, tsfc_parameters=None):
+def merge_loopy(slate_loopy, output_arg, builder, var2terminal,  wrapper_name, ctx_g2l, strategy="terminals_first", slate_expr = None, tsfc_parameters=None):
     """ Merges tsfc loopy kernels and slate loopy kernel into a wrapper kernel."""
 
     if strategy == "terminals_first":
