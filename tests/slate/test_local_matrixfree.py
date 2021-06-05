818cd90ff4024d9362db26fcceb774d0f7c43001
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-06-05 13:19:44 +0200

    Testing: set up a full hybridisaiton test.

--- tests/slate/test_local_matrixfree.py
+++ tests/slate/test_local_matrixfree.py
@@ -223,7 +223,7 @@ def test_blocks(block_expr):
     assert np.allclose(tmp.dat.data, tmp_opt.dat.data, atol=0.0001)
 
 
-def test_temporary_test_for_reallifeschur():
+def test_hybridisation():
     # Create a mesh
     mesh = UnitSquareMesh(6, 6)
     U = FunctionSpace(mesh, "RT", 1)
@@ -243,7 +243,6 @@ def test_temporary_test_for_reallifeschur():
     L = -inner(f, v) * dx + Constant(0.0) * dot(conj(tau), n) * (ds(3) + ds(4))
 
     # Compare hybridized solution with non-hybridized
-    w = Function(W)
     bc1 = DirichletBC(W[0], as_vector([0.0, -sin(5*x)]), 1)
     bc2 = DirichletBC(W[0], as_vector([0.0, sin(5*y)]), 2)
     bcs = [bc1, bc2]
@@ -270,34 +269,13 @@ def test_temporary_test_for_reallifeschur():
                                         'local_matfree': False,
                                         'throw_the_schur': True}}
 
-    import ufl.algorithms as ufl_alg
-    f = Function(W)
-    f.assign(Constant(2))
-    A = None
-    B = None
-    try:
-        # Solve the system in a local matrix-free manner
-        solve(a == L, w, bcs=bcs, solver_parameters=matfree_params)
-    except static_condensation.hybridization.CheckSchurComplement as e:
-        # local matrix-free form for schur complement is delivered by the exception
-        matfree_schur = e.expression.action
-        u, = matfree_schur._coefficients
-        # apply the LOCAL matrix-free schur action to a known function
-        matfree_schur_wv = ufl_alg.replace(matfree_schur, {u: f})
-        A = assemble(matfree_schur_wv)
-    try:
-        # Solve the system in a global only matrix-free manner
-        solve(a == L, w, bcs=bcs, solver_parameters=params)
-    except static_condensation.hybridization.CheckSchurComplement as e: 
-        # NON-LOCAL matrix-free form for schur complement is delivered by the exception
-        schur = e.expression.action
-        u, = schur._coefficients
-        # apply the NON-LOCAL matrix-free schur action to a known function
-        schur_wv = ufl_alg.replace(schur, {u: f})
-        B = assemble(schur_wv)
-        # compare local to non-local action of schur complement on a function
-    for a,b in zip(A.dat.data, B.dat.data):
-        assert np.allclose(a,b)
+    wA = Function(W)
+    wB = Function(W)
+    solve(a == L, wA, bcs=bcs, solver_parameters=params)
+    solve(a == L, wB, bcs=bcs, solver_parameters=matfree_params)
+    for a,b in zip([wA.dat.data], [wB.dat.data]):
+        for c,d in zip(a,b):
+            assert np.allclose(c,d)
 
 def test_hybridisation_bitbybit():
     mesh = UnitSquareMesh(6, 6)
