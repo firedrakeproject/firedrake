0a0bc8e2d697d737e564ef4da51db6fcfa592855
nbouziani <nacime.bouziani@gmail.com>
2021-07-07 14:42:15 +0100

    Add test for assembly_registry

--- tests/external_operators/test_abstract_external_operator.py
+++ tests/external_operators/test_abstract_external_operator.py
@@ -80,3 +80,43 @@ def test_add_dependencies(mesh):
     assert de2[0]._extop_master == e2
     assert de2[1]._extop_master == e2
     assert de2[2]._extop_master == e2
+
+
+def test_assemble_methods(mesh):
+
+    V = FunctionSpace(mesh, "CG", 1)
+    u = Function(V)
+    g = Function(V)
+
+    class MyExternalOperator1(AbstractExternalOperator):
+        def __init__(self, *args, **kwargs):
+            AbstractExternalOperator.__init__(self, *args, **kwargs)
+
+        @assemble_method((1, 0), (0, 1))
+        def dN_du(self, *args, **kwargs):
+            """Implements dN/du(u, m; uhat, v*), which is represented by derivs = (1, 0) and args = (0, 1)"""
+            pass
+
+    e1 = MyExternalOperator1(u, g, function_space=V)
+    assert e1._assembly_registry == MyExternalOperator1._assembly_registry
+    assert MyExternalOperator1._assembly_registry == {((1, 0), (0, 1)): MyExternalOperator1.dN_du}
+
+    class MyExternalOperator2(MyExternalOperator1):
+        def __init__(self, *args, **kwargs):
+            MyExternalOperator1.__init__(self, *args, **kwargs)
+
+        @assemble_method((0, 1), (1, 0))
+        def dN_dm_adjoint(self, *args, **kwargs):
+            """Implements dN/dm^{*}(u, m; v*, mhat), which is represented by derivs = (0, 1) and args = (1, 0)"""
+            pass
+
+        @assemble_method('jacobian')
+        def jacobian(self, *args, **kwargs):
+            """Implements the external operator jacobian: i.e. dN/dop with op in N's operands"""
+            pass
+
+    e2 = MyExternalOperator2(g, u*g, function_space=V)
+    assert e2._assembly_registry == MyExternalOperator2._assembly_registry
+    assert MyExternalOperator2._assembly_registry == {((1, 0), (0, 1)): MyExternalOperator1.dN_du,
+                                                      ((0, 1), (1, 0)): MyExternalOperator2.dN_dm_adjoint,
+                                                      ('jacobian',): MyExternalOperator2.jacobian}
