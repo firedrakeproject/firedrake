d3ea095d8f4e2282e2c2a52b2e971ef4fce817ae
Andrew Whitmell <awhitmell@gmail.com>
2021-08-03 14:03:44 +0100

    Create roofline.py

---
+++ tools/roofline.py
@@ -0,0 +1,31 @@
+"""
+The generation of a roofline plot given the flop count
+and arithmetic intensity for a given script.
+"""
+
+import numpy
+import datetime
+import matplotlib.pyplot as plt 
+
+flops = 123
+intensity = 0.25
+double_precision = 0.4
+msb = 11.23750
+mmb = 52.8
+time = datetime.datetime.now()
+fig_name = "Roofline plot {}".format(time.ctime())
+
+x_range = [-6, 10] #Probably need to auto generate this, or have large range then generate xlim & ylim?
+x = numpy.logspace(x_range[0], x_range[1], base=2, num = 1000)
+y = []
+for i in x: 
+    # The minimum of the memory streaming bandwidth and maximum memory bandwidth
+    # How to obtain msb & mmb?
+    y.append(min(i * msb, mmb))
+
+plt.loglog(x, y, c='black', label='Roofline')
+plt.loglog(intensity, double_precision, 'o', c='crimson', linewidth=0)
+#plt.savefig(figname)
+plt.show()
+
+# Annotate graph with regions, multiple roofs
