b5cbd86bc9c7d803db70386b79071ba94ceb408c
Andrew Whitmell <awhitmelL@gmail.com>
2021-08-09 13:50:17 +0100

    Implement use of PETSc performance logging

--- tools/roofline.py
+++ tools/roofline.py
@@ -7,30 +7,35 @@ import numpy
 import datetime
 import matplotlib.pyplot as plt 
 from firedrake.petsc import PETSc
-#from firedrake.PyOP2.pyop2.base import ParLoop
 
-print(PETSc.Log.getPerfInfoAllStages())
+def roofline():
+    flops = 0
+    bytes = 0
+    log = PETSc.Log.getPerfInfoAllStages()['Main Stage']
+    data = log.items()
 
-def roofline(kernel):
-    flops = kernel.flop_count
-    intensity = 0.25
-    double_precision = 0.4
-    msb = 11.23750
-    mmb = 52.8
+    for d in data:
+        flops += d[1]['flops']
+        bytes += d[1]['bytes']
+    
+    intensity = flops/bytes
+    streaming = 11.23750
+    maximum = 52.8
     time = datetime.datetime.now()
-    fig_name = "Roofline plot {}".format(time.ctime())
+    fig_name = "Roofline_plot_{}".format(time.ctime())
 
-    x_range = [-6, 10] #Probably need to auto generate this, or have large range then generate xlim & ylim?
-    x = numpy.logspace(x_range[0], x_range[1], base=2, num = 1000)
+    x_range = [-6, 10] 
+    x = numpy.logspace(x_range[0], x_range[1], base=2, num=1000)
     y = []
     for i in x: 
         # The minimum of the memory streaming bandwidth and maximum memory bandwidth
-        # How to obtain msb & mmb?
-        y.append(min(i * msb, mmb))
+        y.append(min(i * streaming, maximum))
 
     plt.loglog(x, y, c='black', label='Roofline')
-    plt.loglog(intensity, double_precision, 'o', c='crimson', linewidth=0)
-    #plt.savefig(figname)
+    plt.loglog(intensity, flops, 'o', c='crimson', linewidth=0)
+    plt.xlabel("Arithmetic Intensity [FLOPs/byte]")
+    plt.ylabel("Performance [FLOPs]")
     plt.show()
+    #plt.savefig(figname)
 
 # Annotate graph with regions, multiple roofs
