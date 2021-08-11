dd139d71f03d6935afcaa02554790bc380f3c05c
Daniel Shapero <shapero.daniel@gmail.com>
2021-08-11 10:44:30 -0700

    fix warning from loopy about language version (#2168)
    * Silence loopy warning over language version
    
    Also simplify some of the code paths in extrusion utilities -- hoist
    multiple calls to `loopy.make_function` from every branch of a
    conditional to the outside

--- firedrake/parloops.py
+++ firedrake/parloops.py
@@ -9,6 +9,7 @@ from ufl.domain import join_domains
 from pyop2 import READ, WRITE, RW, INC, MIN, MAX
 import pyop2
 import loopy
+from loopy.version import LOOPY_USE_LANGUAGE_VERSION_2018_2  # noqa: F401
 import coffee.base as ast
 
 from firedrake.logging import warning
@@ -127,8 +128,8 @@ def _form_loopy_kernel(kernel_domains, instructions, measure, args, **kwargs):
             raise KeyError("No cache")
     except KeyError:
         kargs.append(...)
-        knl = loopy.make_function(kernel_domains, instructions, kargs, seq_dependencies=True,
-                                  name="par_loop_kernel", silenced_warnings=["summing_if_branches_ops"], target=loopy.CTarget())
+        knl = loopy.make_function(kernel_domains, instructions, kargs, name="par_loop_kernel", target=loopy.CTarget(),
+                                  seq_dependencies=True, silenced_warnings=["summing_if_branches_ops"])
         knl = pyop2.Kernel(knl, "par_loop_kernel", **kwargs)
         if kernel_cache is not None:
             return kernel_cache.setdefault(key, knl)
