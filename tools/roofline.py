aa65ef712609000552864ad20cb95201cb62502c
Andrew Whitmell <awhitmelL@gmail.com>
2021-08-11 14:49:56 +0100

    Fix subscriptability error

--- tools/roofline.py
+++ tools/roofline.py
@@ -3,6 +3,7 @@ The generation of a roofline plot given the flop count
 and arithmetic intensity for a given script.
 """
 
+import firedrake
 import numpy
 import matplotlib.pyplot as plt 
 from firedrake.petsc import PETSc
@@ -13,48 +14,53 @@ from contextlib import contextmanager
 class Roofline:
     def __init__(self, streaming_limit, flop_limit):
         """ """
-        self.data = defaultdict(partial(defaultdict, float))
+        self.data = defaultdict(partial(defaultdict, partial(defaultdict, float)))
         self.streaming_limit = streaming_limit
         self.flop_limit = flop_limit
     
-    def start_collection(self, region_name=None):
+    def start_collection(self, event_type=None):
         """ """
         start = PETSc.Log.getPerfInfoAllStages()['Main Stage']
-        data = self.data[region_name]
+        data = self.data[event_type]
         for event, info in start.items():
             event_data = data[event]
-            for n in ('flops', 'bytes', 'time'):
+            for n in ('flops', 'bytes', 'time', 'count'):
                 event_data[n] -= info[n]
 
-    def stop_collection(self, region_name=None):
+    def stop_collection(self, event_type=None):
         """ """
         stop = PETSc.Log.getPerfInfoAllStages()['Main Stage']
-        data = self.data[region_name]
+        data = self.data[event_type]
         for event, info in stop.items():
             event_data = data[event]
-            for n in ('flops', 'bytes', 'time'):
+            for n in ('flops', 'bytes', 'time', 'count'):
                 event_data[n] += info[n]
+                if event_data['count'] == 0:
+                    print(data[event][0])
+        
+        if len(data) == 0:
+            firedrake.logging.warn("Requested event has 0 occurrences.")
 
     @contextmanager
-    def collecting(self, region_name=None):
+    def collecting(self, event_type=None):
         """ """
-        self.start_collection(region_name)
+        self.start_collection(event_type)
         try: 
             yield
         finally:
-            self.stop_collection(region_name)
+            self.stop_collection(event_type)
 
-    def roofline(self, region_name=None, event_name=None, axes=None):
+    def roofline(self, event_type=None, event_name=None, axes=None):
         """ """
         if axes is None:
             figure = plt.figure()
             axes = figure.add_subplot(111)
         
         if event_name is not None:
-            data = self.data[region_name][event_name]
+            data = self.data[event_type][event_name]
         else: 
             data = defaultdict(float)
-            for event in self.data[region_name].values():
+            for event in self.data[event_type].values():
                 data['flops'] += event['flops']
                 data['bytes'] += event['bytes'] 
                 data['time'] += event['time']
