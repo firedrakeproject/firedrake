4484ff8d36f277814fed7955b5a0a3593edf641d
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-09-14 11:22:52 +0200

    Fix arguments for standalone MOP.

--- firedrake/slate/slac/optimise.py
+++ firedrake/slate/slac/optimise.py
@@ -143,6 +143,7 @@ def _drop_double_transpose_transpose(expr, self):
 @_drop_double_transpose.register(Negative)
 @_drop_double_transpose.register(Add)
 @_drop_double_transpose.register(Mul)
+@_drop_double_transpose.register(Solve)
 def _drop_double_transpose_distributive(expr, self):
     return type(expr)(*map(self, expr.children))
 
