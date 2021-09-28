firedrakeproject/firedrake

commit 9cf9b75d1a961354e4738d5d5f045e5539759490

Melina97/adjoint (#2201)
* fix _ad_annotate_solve

* Fix cache problem

* fix _ad_annotate_solve

* Fix cache problem

* Fix cache problem

* Updated changes

* Updated changes

* Added _compute_replace_map

* Small fix

* fix _ad_annotate_solve

* context manager for assing in recompute_component

* context manager for assign in recompute_component

* tracking assing

* fixing flamegraph error

* add context manager to assign

* add context manager to assign

* Added context managers to assign

* added stages to assign

* commenting out context managers

* added context managers to assign

* commenting out context managers

* tracking adjoint

* commenting out context managers

* Delete unnecessary comments

* Delete unnecessary comments

* Added stages and events to adjoint

* Added stages and events to assign

* Added stages and events to adjoint

* Added dat_map

* Removed context managers

* Fix lint

* Removed context managers

* Added adj_F to block

* Added adj_F to block

* Remove cache fix with dat_map

* Small change

* Add prepare_evaluate_adj method

* Add comments

* Separate _create_F_form

* Add prepare_evaluate_tlm

* Move adjoint outside prepare_evaluate_adj

* Small changes

* Add evaluate_adj_component

* Moved methods to NonlinearVariationalSolveBlock

* Add context managers

* Add context managers

* Added Stages and Events

* Remove context manager

* Lift adjoint out of loop and build cache

* Lint fix

* Minor changes

* Added dFdm_cache

* Populate dFdm_cache

* Remove dat_map

* Revert unwanted changes

* Remove reference to firedrake.Expression

* Small correction

* Don't die when trying to differentiate SLATE

* Be slightly less enthusiastic about failing

* Replace u with its checkpoint in dFdm

The replace_map contains replacements for the input variables only, the

firedrake/adjoint/blocks.py
@@ -99,10 +99,12 @@ class SolveVarFormBlock(GenericSolveBlock):
 
 
 class NonlinearVariationalSolveBlock(GenericSolveBlock):
-    def __init__(self, equation, func, bcs, problem_J, solver_params, solver_kwargs, **kwargs):
+    def __init__(self, equation, func, bcs, adj_F, dFdm_cache, problem_J, solver_params, solver_kwargs, **kwargs):
         lhs = equation.lhs
         rhs = equation.rhs
 
+        self.adj_F = adj_F
+        self._dFdm_cache = dFdm_cache
         self.problem_J = problem_J
         self.solver_params = solver_params.copy()
         self.solver_kwargs = solver_kwargs
@@ -145,6 +147,77 @@ class NonlinearVariationalSolveBlock(GenericSolveBlock):
         problem = self._ad_nlvs._problem
         self._ad_assign_coefficients(problem.F)
         self._ad_assign_coefficients(problem.J)
+
+    def prepare_evaluate_adj(self, inputs, adj_inputs, relevant_dependencies):
+        dJdu = adj_inputs[0]
+
+        F_form = self._create_F_form()
+
+        dFdu_form = self.adj_F
+        dJdu = dJdu.copy()
+
+        # Replace the form coefficients with checkpointed values.
+        dFdu_form = self._replace_form(dFdu_form)
+
+        compute_bdy = self._should_compute_boundary_adjoint(relevant_dependencies)
+        adj_sol, adj_sol_bdy = self._assemble_and_solve_adj_eq(dFdu_form, dJdu, compute_bdy)
+        self.adj_sol = adj_sol
+        if self.adj_cb is not None:
+            self.adj_cb(adj_sol)
+        if self.adj_bdy_cb is not None and compute_bdy:
+            self.adj_bdy_cb(adj_sol_bdy)
+
+        r = {}
+        r["form"] = F_form
+        r["adj_sol"] = adj_sol
+        r["adj_sol_bdy"] = adj_sol_bdy
+        return r
+
+    def evaluate_adj_component(self, inputs, adj_inputs, block_variable, idx, prepared=None):
+        if not self.linear and self.func == block_variable.output:
+            # We are not able to calculate derivatives wrt initial guess.
+            return None
+        F_form = prepared["form"]
+        adj_sol = prepared["adj_sol"]
+        adj_sol_bdy = prepared["adj_sol_bdy"]
+        c = block_variable.output
+        c_rep = block_variable.saved_output
+
+        if isinstance(c, firedrake.Function):
+            trial_function = firedrake.TrialFunction(c.function_space())
+        elif isinstance(c, firedrake.Constant):
+            mesh = self.compat.extract_mesh_from_form(F_form)
+            trial_function = firedrake.TrialFunction(c._ad_function_space(mesh))
+        elif isinstance(c, firedrake.DirichletBC):
+            tmp_bc = self.compat.create_bc(c, value=self.compat.extract_subfunction(adj_sol_bdy, c.function_space()))
+            return [tmp_bc]
+        elif isinstance(c, self.compat.MeshType):
+            # Using CoordianteDerivative requires us to do action before
+            # differentiating, might change in the future.
+            F_form_tmp = firedrake.action(F_form, adj_sol)
+            X = firedrake.SpatialCoordinate(c_rep)
+            dFdm = firedrake.derivative(-F_form_tmp, X, firedrake.TestFunction(c._ad_function_space()))
+
+            dFdm = self.compat.assemble_adjoint_value(dFdm, **self.assemble_kwargs)
+            return dFdm
+
+        # dFdm_cache works with original variables, not block saved outputs.
+        if c in self._dFdm_cache:
+            dFdm = self._dFdm_cache[c]
+        else:
+            dFdm = -firedrake.derivative(self.lhs, c, trial_function)
+            dFdm = firedrake.adjoint(dFdm)
+            self._dFdm_cache[c] = dFdm
+
+        # Replace the form coefficients with checkpointed values.
+        replace_map = self._replace_map(dFdm)
+        replace_map[self.func] = self.get_outputs()[0].saved_output
+        dFdm = replace(dFdm, replace_map)
+
+        dFdm = dFdm * adj_sol
+        dFdm = self.compat.assemble_adjoint_value(dFdm, **self.assemble_kwargs)
+
+        return dFdm
 
 
 class ProjectBlock(SolveVarFormBlock):

firedrake/adjoint/variational_solver.py
@@ -12,11 +12,21 @@ class NonlinearVariationalProblemMixin:
         @no_annotations
         @wraps(init)
         def wrapper(self, *args, **kwargs):
+            from firedrake import derivative, adjoint, TrialFunction
             init(self, *args, **kwargs)
             self._ad_F = self.F
             self._ad_u = self.u
             self._ad_bcs = self.bcs
             self._ad_J = self.J
+            try:
+                # Some forms (e.g. SLATE tensors) are not currently
+                # differentiable.
+                dFdu = derivative(self.F,
+                                  self.u,
+                                  TrialFunction(self.u.function_space()))
+                self._ad_adj_F = adjoint(dFdu)
+            except TypeError:
+                self._ad_adj_F = None
             self._ad_kwargs = {'Jp': self.Jp, 'form_compiler_parameters': self.form_compiler_parameters, 'is_linear': self.is_linear}
             self._ad_count_map = {}
         return wrapper
@@ -37,6 +47,7 @@ class NonlinearVariationalSolverMixin:
             self._ad_args = args
             self._ad_kwargs = kwargs
             self._ad_nlvs = None
+            self._ad_dFdm_cache = {}
 
         return wrapper
 
@@ -59,6 +70,8 @@ class NonlinearVariationalSolverMixin:
                 block = NonlinearVariationalSolveBlock(problem._ad_F == 0,
                                                        problem._ad_u,
                                                        problem._ad_bcs,
+                                                       problem._ad_adj_F,
+                                                       dFdm_cache=self._ad_dFdm_cache,
                                                        problem_J=problem._ad_J,
                                                        solver_params=self.parameters,
                                                        solver_kwargs=self._ad_kwargs,

firedrake/ufl_expr.py
@@ -141,6 +141,10 @@ def derivative(form, u, du=None, coefficient_derivatives=None):
 
     See also :func:`ufl.derivative`.
     """
+    if isinstance(form, firedrake.slate.TensorBase):
+        raise TypeError(
+            f"Cannot take the derivative of a {type(form).__name__}"
+        )
     # TODO: What about Constant?
     u_is_x = isinstance(u, ufl.SpatialCoordinate)
     uc, = (u,) if u_is_x else extract_coefficients(u)

commit 0d34583b4b343c97bcb47e27cfaee34467516be6

Fix adjoint with consecutive nonlinear solves
Similar to fix in dF/dm term, also in dF/du the u that we are solving
for should be replaced by the checkpoint of the _solution_ (i.e. the
output) in the forward model not by the checkpoint of the initial guess.
This only becomes relevant when F is actually nonlinear in u.

Bug introduced in #2201 , which has broken Thetis adjoint

Added regression test that tests a sequence of actual nonlinear solves
which fails without the fix.

firedrake/adjoint/blocks.py
@@ -157,7 +157,9 @@ class NonlinearVariationalSolveBlock(GenericSolveBlock):
         dJdu = dJdu.copy()
 
         # Replace the form coefficients with checkpointed values.
-        dFdu_form = self._replace_form(dFdu_form)
+        replace_map = self._replace_map(dFdu_form)
+        replace_map[self.func] = self.get_outputs()[0].saved_output
+        dFdu_form = replace(dFdu_form, replace_map)
 
         compute_bdy = self._should_compute_boundary_adjoint(relevant_dependencies)
         adj_sol, adj_sol_bdy = self._assemble_and_solve_adj_eq(dFdu_form, dJdu, compute_bdy)

tests/regression/test_adjoint_operators.py
@@ -673,3 +673,24 @@ def test_copy_function():
     J = assemble(g*dx)
     rf = ReducedFunctional(J, Control(f))
     assert np.isclose(rf(interpolate(-one, V)), -J)
+
+
+@pytest.mark.skipcomplex  # Taping for complex-valued 0-forms not yet done
+def test_consecutive_nonlinear_solves():
+    from firedrake_adjoint import ReducedFunctional, Control, taylor_test
+    mesh = UnitSquareMesh(1, 1)
+    V = FunctionSpace(mesh, "CG", 1)
+    uic = Constant(2.0)
+    u1 = Function(V).assign(uic)
+    u0 = Function(u1)
+    v = TestFunction(V)
+    F = v * u1**2 * dx - v*u0 * dx
+    problem = NonlinearVariationalProblem(F, u1)
+    solver = NonlinearVariationalSolver(problem)
+    for i in range(3):
+        u0.assign(u1)
+        solver.solve()
+    J = assemble(u1**16*dx)
+    rf = ReducedFunctional(J, Control(uic))
+    h = Constant(0.01)
+    assert taylor_test(rf, uic, h) > 1.9