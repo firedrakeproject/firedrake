beb4336ee0e0b161176a5d2245e6a5f5897123a2
Sophia Vorderwuelbecke <sv2518@ic.ac.uk>
2021-11-29 15:01:19 +0000

    undo

--- firedrake/slate/slac/utils.py
+++ firedrake/slate/slac/utils.py
@@ -673,14 +673,14 @@ def update_wrapper_kernel(builder, insns, output_arg, tensor2temps, knl_list, sl
     for i, insn in enumerate(insns):
         if insn:
             if i == 0:
-                last_id = insns[0].id
-                dep = frozenset()
+                last_id=insns[0].id
+                new_insns.append(insn.copy(priority=len(insns)-i,
+                within_inames_is_final=True))
             else:
-                last_id = insn.id
-                dep = frozenset({last_id})
-            new_insns.append(insn.copy(depends_on=dep,
-                                       priority=len(insns)-i,
-                                       within_inames_is_final=True))
+                new_insns.append(insn.copy(depends_on=frozenset({last_id}),
+                priority=len(insns)-i,
+                within_inames_is_final=True))
+                last_id=insn.id
 
     # 2) Prepare the wrapper kernel: in particular args and tvs so that they match the new instructions,
     # which contain the calls to the action, solve and tensorshell kernels
