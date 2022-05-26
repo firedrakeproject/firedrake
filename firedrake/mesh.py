68005f60243af54fccabd926861dfec60a8aae30
David Ham <David.Ham@imperial.ac.uk>
2022-05-26 10:04:50 +1000

    put the comment in the right place

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -2106,6 +2106,10 @@ def VertexOnlyMesh(mesh, vertexcoords, missing_points_behaviour=None,
         that they have the same list of vertices (else the test is not
         possible): this operation scales with number of vertices and number of
         ranks.
+    :kwarg tolerance: the amount by which the local coordinates of a point are
+        allowed to fall outside the cell while still having the point count as
+        in the cell. Increase the default (1.0e-14) somewhat if vertices are
+        being lost in the :class:`VertexOnlyMesh` construction process.
 
     .. note::
 
@@ -2272,10 +2276,6 @@ def _pic_swarm_in_mesh(parent_mesh, coords, fields=None, tolerance=None):
         RealType)]``. All fields must have the same number of points. For more
         information see `the DMSWARM API reference
         <https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/DMSWARM/DMSWARM.html>_.
-    :kwarg tolerance: the amount by which the local coordinates of a point are
-        allowed to fall outside the cell while still having the point count as
-        in the cell. Increase the default (1.0e-14) somewhat if vertices are
-        being lost in the :class:`VertexOnlyMesh` construction process.
     :return: the immersed DMSwarm
 
     .. note::
