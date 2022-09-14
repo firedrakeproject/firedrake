f75359a25570fbd819161355e6862ee4e4723676
Pablo Brubeck <brubeck@protonmail.com>
2022-09-14 17:37:40 +0100

    support high order and facet elements

--- firedrake/preconditioners/hiptmair.py
+++ firedrake/preconditioners/hiptmair.py
@@ -1,59 +1,105 @@
-from firedrake.preconditioners.base import PCBase
 from firedrake.petsc import PETSc
-from firedrake.functionspace import FunctionSpace
-from firedrake.ufl_expr import TestFunction, TrialFunction
-from firedrake.interpolation import Interpolator
-from firedrake.dmhooks import get_function_space, get_appctx
+from firedrake.preconditioners.base import PCBase
+import firedrake.dmhooks as dmhooks
 from firedrake_citations import Citations
-from ufl import grad, curl, HCurl, HDiv, FiniteElement, TensorElement
-import firedrake
+from ufl import (grad, curl, div, zero, inner, dx, replace,
+                 FiniteElement, TensorElement, FacetElement)
+
 import numpy as np
 
 __all__ = ("HiptmairPC",)
 
 
 class HiptmairPC(PCBase):
-    def initialize(self, obj):
+
+    needs_python_pmat = False
+    _prefix = "hiptmair_"
+
+    def initialize(self, pc):
+
+        from firedrake import TestFunction, TrialFunction, FunctionSpace, parameters, Constant
+        from firedrake.assemble import allocate_matrix, TwoFormAssembler
+        from firedrake.interpolation import Interpolator
+        from firedrake.solving_utils import _SNESContext
+        from firedrake.matrix_free.operators import ImplicitMatrixContext
+        from ufl.algorithms.ad import expand_derivatives
 
         Citations().register("Hiptmair1998")
-        A, P = obj.getOperators()
-        appctx = self.get_appctx(obj)
-        # prefix = obj.getOptionsPrefix()
-        V = get_function_space(obj.getDM())
+        A, P = pc.getOperators()
+        appctx = self.get_appctx(pc)
+        fcp = appctx.get("form_compiler_parameters")
+
+        prefix = pc.getOptionsPrefix()
+        options_prefix = prefix + self._prefix
+        opts = PETSc.Options()
+
+        V = dmhooks.get_function_space(pc.getDM())
         mesh = V.mesh()
 
-        sobolev_space = V.ufl_element().sobolev_space()
-        degree = V.ufl_element().degree()
+        element = V.ufl_element()
+        variant = element.variant()
+        degree = element.degree()
         try:
             degree = max(degree)
         except TypeError:
             pass
 
-        if sobolev_space == HCurl:
+        formdegree = V.finat_element.formdegree
+        if formdegree == 1:
             dminus = grad
             cfamily = "Lagrange"
             G_callback = appctx.get("get_gradient", None)
-        elif sobolev_space == HDiv:
+        elif formdegree == 2:
             dminus = curl
             cfamily = "N1curl" if mesh.ufl_cell().is_simplex() else "NCE"
             G_callback = appctx.get("get_curl", None)
         else:
-            raise ValueError("Hiptmair decomposition not available in", sobolev_space)
+            raise ValueError("Hiptmair decomposition not available for", element)
 
-        celement = FiniteElement(cfamily, cell=mesh.ufl_cell(), degree=degree)
+        celement = FiniteElement(cfamily, cell=mesh.ufl_cell(), degree=degree, variant=variant)
+        if (not list(list(V.finat_element.entity_dofs().values())[-1].values())[0]) and (degree > 1):
+            celement = FacetElement(celement)
         if V.shape:
             celement = TensorElement(element, shape=V.shape)
 
         Vc = FunctionSpace(mesh, celement)
-
-        ctx = get_appctx(obj.getDM())
-        a = ctx.J
+        ctx = dmhooks.get_appctx(pc.getDM())
         bcs = ctx._problem.bcs
-        ac = a(dminus(TestFunction(Vc)), dminus(TrialFunction(Vc)), coefficients={})
-        cbcs = [bc.reconstruct(V=Vc, g=0) for bc in bcs]
+        a = ctx._problem.J
+
+        coarse_space = Vc
+        coarse_space_bcs = [bc.reconstruct(V=Vc, g=0) for bc in bcs]
+        test = TestFunction(Vc)
+        trial = TrialFunction(Vc)
+        coarse_operator = a(dminus(test), dminus(trial), coefficients={})
+
+        # FIXME
+        b = replace(expand_derivatives(a), {grad(t): zero(grad(t).ufl_shape) for t in a.arguments()})
+        coarse_operator = replace(b, {t: dminus(s) for t, s in zip(a.arguments(), [test, trial])})
+        if formdegree > 1 and degree > 1:
+            shift = appctx.get("hiptmair_shift", None)
+            if shift is not None:
+                quad_degree = 2*(degree+1)-1
+                coarse_operator += inner(test, shift*trial)*dx(degree=quad_degree)
+
+        # Handle the coarse operator
+        coarse_options_prefix = options_prefix + "mg_coarse_"
+        coarse_mat_type = opts.getString(coarse_options_prefix + "mat_type",
+                                         parameters["default_matrix_type"])
+
+        self.coarse_op = allocate_matrix(coarse_operator,
+                                         bcs=coarse_space_bcs,
+                                         form_compiler_parameters=fcp,
+                                         mat_type=coarse_mat_type,
+                                         options_prefix=coarse_options_prefix)
+        self._assemble_coarse_op = TwoFormAssembler(coarse_operator, tensor=self.coarse_op,
+                                                    form_compiler_parameters=fcp,
+                                                    bcs=coarse_space_bcs).assemble
+        self._assemble_coarse_op()
+        coarse_opmat = self.coarse_op.petscmat
 
         if G_callback is None:
-            G = Interpolator(dminus(TestFunction(Vc)), V).callable().handle
+            G = Interpolator(dminus(test), V).callable().handle
 
             # remove (near) zeros from sparsity pattern
             ai, aj, a = G.getValuesCSR()
@@ -65,45 +111,64 @@ class HiptmairPC(PCBase):
             G2.setPreallocationCSR((ai, aj, a))
             G2.assemble()
         else:
-            G2 = G_callback(V, Vc, bcs, cbcs)
-        self.interp = G2
-        self.xc = G2.createVecRight()
-        self.yc = G2.createVecRight()
+            G2 = G_callback(V, Vc, bcs, coarse_space_bcs)
+        interp_petscmat = G2
 
-        # TODO create PC objects to replace Jacobi and give them options
-        def get_jacobi_smoother(a, bcs, tensor=None):
-            tensor = firedrake.assemble(a, bcs=bcs, diagonal=True, tensor=tensor)
-            with tensor.dat.vec as diag:
-                diag.reciprocal()
-            return tensor
+        # We set up a PCMG object that uses the constructed interpolation
+        # matrix to generate the restriction/prolongation operators.
+        # This is a two-level multigrid preconditioner.
+        pcmg = PETSc.PC().create(comm=pc.comm)
+        pcmg.incrementTabLevel(1, parent=pc)
 
-        def apply_jacobi(diag, x, y):
-            with diag.dat.vec as d:
-                y.pointwiseMult(x, d)
+        pcmg.setType(pc.Type.MG)
+        pcmg.setOptionsPrefix(options_prefix)
+        pcmg.setMGLevels(2)
+        pcmg.setMGType(pc.MGType.ADDITIVE)
+        pcmg.setMGCycleType(pc.MGCycleType.V)
+        pcmg.setMGInterpolation(1, interp_petscmat)
+        pcmg.setOperators(A=A, P=P)
 
-        self.fdiag = None
-        self.cdiag = None
-        self._update_fdiag = lambda: get_jacobi_smoother(a, bcs, tensor=self.fdiag)
-        self._update_cdiag = lambda: get_jacobi_smoother(ac, cbcs, tensor=self.cdiag)
-        self.fdiag = self._update_fdiag()
-        self.cdiag = self._update_cdiag()
+        coarse_solver = pcmg.getMGCoarseSolve()
+        coarse_solver.setOperators(A=coarse_opmat, P=coarse_opmat)
+        # coarse space dm
+        coarse_dm = coarse_space.dm
+        coarse_solver.setDM(coarse_dm)
+        coarse_solver.setDMActive(False)
+        pcmg.setDM(pc.getDM())
+        pcmg.setFromOptions()
+        self.pc = pcmg
+        self._dm = coarse_dm
 
-        self.fine_apply = lambda x, y: apply_jacobi(self.fdiag, x, y)
-        self.coarse_apply = lambda x, y: apply_jacobi(self.cdiag, x, y)
+        prefix = coarse_solver.getOptionsPrefix()
+        # Create new appctx
+        self._ctx_ref = self.new_snes_ctx(pc,
+                                          coarse_operator,
+                                          coarse_space_bcs,
+                                          coarse_mat_type,
+                                          fcp,
+                                          options_prefix=prefix)
+
+        with dmhooks.add_hooks(coarse_dm, self,
+                               appctx=self._ctx_ref,
+                               save=False):
+            coarse_solver.setFromOptions()
 
     def update(self, pc):
-        self._update_fdiag()
-        self._update_cdiag()
+        self._assemble_coarse_op()
+        self.pc.setUp()
 
-    def apply(self, pc, x, y):
-        self.fine_apply(x, y)
+    def apply(self, pc, X, Y):
+        dm = self._dm
+        with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref):
+            self.pc.apply(X, Y)
 
-        self.interp.multTranspose(x, self.xc)
-        self.coarse_apply(self.xc, self.yc)
-        self.interp.multAdd(self.yc, y, y)
-
-    def applyTranspose(self, pc, x, y):
-        self.apply(pc, x, y)
+    def applyTranspose(self, pc, X, Y):
+        dm = self._dm
+        with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref):
+            self.pc.applyTranspose(X, Y)
 
     def view(self, pc, viewer=None):
-        super().view(pc, viewer)
+        super(HiptmairPC, self).view(pc, viewer)
+        if hasattr(self, "pc"):
+            viewer.printfASCII("PC using Hiptmair decomposition\n")
+            self.pc.view(viewer)
