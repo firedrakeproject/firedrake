9924d495bf266b446255373d0fb1aa24efee7f73
Pablo Brubeck <brubeck@protonmail.com>
2022-09-26 17:23:33 +0100

    HiptmairPC subclasses TwoLevelPC

--- firedrake/preconditioners/hiptmair.py
+++ firedrake/preconditioners/hiptmair.py
@@ -1,28 +1,24 @@
+import abc
+
 from firedrake.petsc import PETSc
 from firedrake.preconditioners.base import PCBase
 import firedrake.dmhooks as dmhooks
-from firedrake_citations import Citations
-import numpy as np
 
 
 __all__ = ("HiptmairPC",)
 
-
-class HiptmairPC(PCBase):
+class TwoLevelPC(PCBase):
 
     needs_python_pmat = False
-    _prefix = "hiptmair_"
+
+    @abc.abstractmethod
+    def coarsen(self, pc):
+        raise NotImplementedError
 
     def initialize(self, pc):
-
-        from firedrake import TestFunction, TrialFunction, FunctionSpace, parameters
+        from firedrake import parameters
         from firedrake.assemble import allocate_matrix, TwoFormAssembler
-        from firedrake.interpolation import Interpolator
-        from ufl.algorithms.ad import expand_derivatives
-        from ufl import (grad, curl, zero, replace,
-                         FiniteElement, TensorElement, FacetElement)
 
-        Citations().register("Hiptmair1998")
         A, P = pc.getOperators()
         appctx = self.get_appctx(pc)
         fcp = appctx.get("form_compiler_parameters")
@@ -31,52 +27,7 @@ class HiptmairPC(PCBase):
         options_prefix = prefix + self._prefix
         opts = PETSc.Options()
 
-        V = dmhooks.get_function_space(pc.getDM())
-        mesh = V.mesh()
-
-        element = V.ufl_element()
-        variant = element.variant()
-        degree = element.degree()
-        try:
-            degree = max(degree)
-        except TypeError:
-            pass
-
-        formdegree = V.finat_element.formdegree
-        if formdegree == 1:
-            dminus = grad
-            cfamily = "Lagrange"
-            G_callback = appctx.get("get_gradient", None)
-        elif formdegree == 2:
-            dminus = curl
-            cfamily = "N1curl" if mesh.ufl_cell().is_simplex() else "NCE"
-            G_callback = appctx.get("get_curl", None)
-        else:
-            raise ValueError("Hiptmair decomposition not available for", element)
-
-        celement = FiniteElement(cfamily, cell=mesh.ufl_cell(), degree=degree, variant=variant)
-        if (not list(list(V.finat_element.entity_dofs().values())[-1].values())[0]) and (degree > 1):
-            celement = FacetElement(celement)
-        if V.shape:
-            celement = TensorElement(element, shape=V.shape)
-
-        Vc = FunctionSpace(mesh, celement)
-        ctx = dmhooks.get_appctx(pc.getDM())
-        bcs = ctx._problem.bcs
-        J = ctx._problem.J
-
-        coarse_space = Vc
-        coarse_space_bcs = [bc.reconstruct(V=Vc, g=0) for bc in bcs]
-        test = TestFunction(Vc)
-        trial = TrialFunction(Vc)
-
-        # Get only the zero-th order term of the form
-        beta = replace(expand_derivatives(J), {grad(t): zero(grad(t).ufl_shape) for t in J.arguments()})
-        coarse_operator = beta(dminus(test), dminus(trial), coefficients={})
-        if formdegree > 1 and degree > 1:
-            shift = appctx.get("hiptmair_shift", None)
-            if shift is not None:
-                coarse_operator += beta(test, shift*trial, coefficients={})
+        coarse_operator, coarse_space_bcs, interp_petscmat = self.coarsen(pc)
 
         # Handle the coarse operator
         coarse_options_prefix = options_prefix + "mg_coarse_"
@@ -94,22 +45,6 @@ class HiptmairPC(PCBase):
         self._assemble_coarse_op()
         coarse_opmat = self.coarse_op.petscmat
 
-        if G_callback is None:
-            G = Interpolator(dminus(test), V).callable().handle
-
-            # remove (near) zeros from sparsity pattern
-            ai, aj, a = G.getValuesCSR()
-            a[np.abs(a) < 1e-10] = 0
-            G2 = PETSc.Mat().create()
-            G2.setType(PETSc.Mat.Type.AIJ)
-            G2.setSizes(G.sizes)
-            G2.setOption(PETSc.Mat.Option.IGNORE_ZERO_ENTRIES, True)
-            G2.setPreallocationCSR((ai, aj, a))
-            G2.assemble()
-        else:
-            G2 = G_callback(V, Vc, bcs, coarse_space_bcs)
-        interp_petscmat = G2
-
         # We set up a PCMG object that uses the constructed interpolation
         # matrix to generate the restriction/prolongation operators.
         # This is a two-level multigrid preconditioner.
@@ -122,14 +57,16 @@ class HiptmairPC(PCBase):
         pcmg.setMGType(pc.MGType.ADDITIVE)
         pcmg.setMGCycleType(pc.MGCycleType.V)
         pcmg.setMGInterpolation(1, interp_petscmat)
-        # The injection operator is not available
-        # Defaults to restriction of the solution times a zero vector
+        # FIXME the default for MGRScale is created with the wrong shape when dim(coarse) > dim(fine)
+        # FIXME there is no need for injection in a KSP context, probably this comes from the snes_ctx below
+        # as workaround define injection as the restriction of the solution times a zero vector
         pcmg.setMGRScale(1, interp_petscmat.createVecRight())
         pcmg.setOperators(A=A, P=P)
 
         coarse_solver = pcmg.getMGCoarseSolve()
         coarse_solver.setOperators(A=coarse_opmat, P=coarse_opmat)
         # coarse space dm
+        coarse_space = coarse_operator.arguments()[-1].function_space()
         coarse_dm = coarse_space.dm
         coarse_solver.setDM(coarse_dm)
         coarse_solver.setDMActive(False)
@@ -167,7 +104,88 @@ class HiptmairPC(PCBase):
             self.pc.applyTranspose(X, Y)
 
     def view(self, pc, viewer=None):
-        super(HiptmairPC, self).view(pc, viewer)
+        super(TwoLevelPC, self).view(pc, viewer)
         if hasattr(self, "pc"):
-            viewer.printfASCII("PC using Hiptmair decomposition\n")
+            viewer.printfASCII("Two level PC\n")
             self.pc.view(viewer)
+
+
+class HiptmairPC(TwoLevelPC):
+
+    _prefix = "hiptmair_"
+
+    def coarsen(self, pc):
+        import numpy
+        from firedrake_citations import Citations
+        from firedrake import FunctionSpace, TestFunction, TrialFunction
+        from firedrake.interpolation import Interpolator
+        from ufl.algorithms.ad import expand_derivatives
+        from ufl import (FiniteElement, TensorElement, FacetElement,
+                         replace, zero, grad, curl)
+
+        Citations().register("Hiptmair1998")
+        appctx = self.get_appctx(pc)
+        V = dmhooks.get_function_space(pc.getDM())
+        ctx = dmhooks.get_appctx(pc.getDM())
+        J = ctx._problem.J
+        bcs = ctx._problem.bcs
+
+        mesh = V.mesh()
+        formdegree = V.finat_element.formdegree
+        if formdegree == 1:
+            dminus = grad
+            cfamily = "Lagrange"
+            G_callback = appctx.get("get_gradient", None)
+        elif formdegree == 2:
+            dminus = curl
+            cfamily = "N1curl" if mesh.ufl_cell().is_simplex() else "NCE"
+            G_callback = appctx.get("get_curl", None)
+        else:
+            raise ValueError("Hiptmair decomposition not available for", element)
+
+        element = V.ufl_element()
+        variant = element.variant()
+        degree = element.degree()
+        try:
+            degree = max(degree)
+        except TypeError:
+            pass
+
+        celement = FiniteElement(cfamily, cell=mesh.ufl_cell(), degree=degree, variant=variant)
+        if degree > 1:
+            if not V.finat_element.entity_dofs()[V.finat_element.cell.get_dimension()][0]:
+                celement = FacetElement(celement)
+                # TODO provide statically-condensed form with SLATE
+        if V.shape:
+            celement = TensorElement(element, shape=V.shape)
+
+        coarse_space = FunctionSpace(mesh, celement)
+        coarse_space_bcs = [bc.reconstruct(V=coarse_space, g=0) for bc in bcs]
+
+        # Get only the zero-th order term of the form
+        beta = replace(expand_derivatives(J), {grad(t): zero(grad(t).ufl_shape) for t in J.arguments()})
+
+        test = TestFunction(coarse_space)
+        trial = TrialFunction(coarse_space)
+        coarse_operator = beta(dminus(test), dminus(trial), coefficients={})
+        if formdegree > 1 and degree > 1:
+            shift = appctx.get("hiptmair_shift", None)
+            if shift is not None:
+                coarse_operator += beta(test, shift*trial, coefficients={})
+
+        if G_callback is None:
+            G = Interpolator(dminus(test), V).callable().handle
+
+            # remove (near) zeros from sparsity pattern
+            ai, aj, a = G.getValuesCSR()
+            a[numpy.abs(a) < 1e-10] = 0
+            interp_petscmat = PETSc.Mat().create()
+            interp_petscmat.setType(PETSc.Mat.Type.AIJ)
+            interp_petscmat.setSizes(G.sizes)
+            interp_petscmat.setOption(PETSc.Mat.Option.IGNORE_ZERO_ENTRIES, True)
+            interp_petscmat.setPreallocationCSR((ai, aj, a))
+            interp_petscmat.assemble()
+        else:
+            interp_petscmat = G_callback(V, coarse_space, bcs, coarse_space_bcs)
+
+        return coarse_operator, coarse_space_bcs, interp_petscmat
