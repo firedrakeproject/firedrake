498ff7cf3dc2cfa2b302293a395869ba6c2a9aa8
Daniel Shapero <shapero.daniel@gmail.com>
2022-10-10 14:19:55 -0700

    Add --prefix option

--- scripts/firedrake-install-conda.sh
+++ scripts/firedrake-install-conda.sh
@@ -1,25 +1,37 @@
 #!/usr/bin/env bash
 
+# Parse command-line arguments
+while [ $# -gt 0 ]; do
+    if [[ $1 == "--"* ]]; then
+        name="${1/--/}"
+        declare "$name"="$2"
+        shift
+    fi
+    shift
+done
+
 # For some odd reason, on MacOS we need to tell it where to find `ar`,
 # otherwise it might try to find an `llvm-ar` that doesn't necessarily exist.
 if [[ $(uname -s) == Darwin ]]; then
     export AR=/usr/bin/ar
 fi
 
-# Make an environment variable for the directory where the conda environment
-# will live; this will depend on whether or not you're using `pyenv`.
-if [[ $(command -v pyenv) ]]; then
-    export DIRECTORY=$(pyenv root)/versions/$(pyenv version-name)/envs/firedrake
-else
-    export DIRECTORY=${HOME}/.conda/envs/firedrake
+# Set the directory where we will build Firedrake.
+if [[ -z $prefix ]]; then
+    if [[ $(command -v pyenv) ]]; then
+        declare prefix=$(pyenv root)/versions/$(pyenv version-name)/envs/firedrake
+    else
+        declare prefix=${HOME}/.conda/envs/firedrake
+    fi
 fi
+echo "Install location: $prefix"
 
 # Make a conda environment that excludes HDF5 & mpich (PETSc will build these)
 conda list --explicit | grep -v mpich | grep -v hdf5 | grep -v h5py | grep -v asn1crypto > spec-file.txt
-conda create --prefix ${DIRECTORY} --file spec-file.txt
+conda create --prefix $prefix --file spec-file.txt
 
 # Install Firedrake into a subdirectory of the conda env
-cd ${DIRECTORY}
+cd $prefix
 curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-install
 source activate firedrake
 python firedrake-install --no-package-manager --disable-ssh --venv-name=firedrake-venv
