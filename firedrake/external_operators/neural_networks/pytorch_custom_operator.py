e5643cf10ee9c66550f9af53481d951ec294f4b0
nbouziani <nacime.bouziani@gmail.com>
2022-12-01 13:54:00 +0000

    Handle backpropagation through Forms

--- firedrake/external_operators/neural_networks/pytorch_custom_operator.py
+++ firedrake/external_operators/neural_networks/pytorch_custom_operator.py
@@ -1,6 +1,8 @@
 import torch
 import torch.autograd as torch_ad
 
+from ufl.algorithms.ad import expand_derivatives
+from firedrake import adjoint, action, derivative
 from firedrake.assemble import assemble
 from firedrake.external_operators.neural_networks import NeuralNet
 
@@ -31,11 +33,11 @@ class CustomOperator(torch_ad.Function):
                 output = N.model_output.squeeze(0)
             else:
                 print('\n\n Form!!!')
-                output = convert_to_torch(assembled_N)
+                output = to_pytorch(assembled_N)
         ctx.save_for_backward(output)  # N.model_output.squeeze(0))
         return output.detach()  # convert_to_torch(assembled_N)
 
-# Dispatch strategy for forward depending on whether ExternalOperator or not. Concretely what changes is:    
+# Dispatch strategy for forward depending on whether ExternalOperator or not. Concretely what changes is:
 #    forward: form the tensor from assembled result instead of model_output
 #    Backward: call assemble on action(adjoint(derivative(...)), ...) and parameters should be updated
 #              then return their grad as you do it now
@@ -49,14 +51,23 @@ class CustomOperator(torch_ad.Function):
         """
         output, = ctx.saved_tensors
         N = ctx.metadata['N']
+        F = ctx.metadata.get('F')
         with torch.enable_grad():
             # uu, = N.operator_inputs()
             # xx = convert_to_torch(uu)
             # output = N.model(xx.unsqueeze(0)).squeeze(0)
-            if isinstance(N, NeuralNet):
+            if F is None:
                 output.backward(grad_output)
             else:
-                import ipdb; ipdb.set_trace()
+                ΔN = from_pytorch(grad_output, N.function_space())
+                dNs = [expand_derivatives(derivative(N, mi)) for mi in N.operator_params()]
+                adj_values = [action(adjoint(dNi), ΔN) for dNi in dNs]
+                with torch.enable_grad():
+                    Δθs = [assemble(Δθ) for i, Δθ in enumerate(adj_values)]
+                return None, *[to_pytorch(Δθ) for Δθ in Δθs]
+        # Return the adjoint value of each parameters of the model
+        # Independently of the object in the loss (ExternalOperator or Form):
+        #  -> the adjoint value is always backpropagated to the parameters
         return None, *[θ.grad for θ in N.model.parameters()]
         """
         # delta_N = convert_to_firedrake(y.grad)
@@ -68,3 +79,15 @@ class CustomOperator(torch_ad.Function):
         import ipdb; ipdb.set_trace()
         w = convert_to_torch(w)
         """
+
+
+def to_pytorch(*args, **kwargs):
+    # Avoid circular import
+    from firedrake.external_operators.neural_networks.backends import PytorchBackend
+    return PytorchBackend().to_ml_backend(*args, **kwargs)
+
+
+def from_pytorch(*args, **kwargs):
+    # Avoid circular import
+    from firedrake.external_operators.neural_networks.backends import PytorchBackend
+    return PytorchBackend().from_ml_backend(*args, **kwargs)
