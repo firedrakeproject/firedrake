2cbb7fdd76b902ecbf35b4b89f54b2bcebc00922
nbouziani <nacime.bouziani@gmail.com>
2022-12-01 00:13:56 +0000

    Restructure neural networks code

--- firedrake/external_operators/__init__.py
+++ firedrake/external_operators/__init__.py
@@ -1,4 +1,4 @@
 from firedrake.external_operators.abstract_external_operators import *
-from firedrake.external_operators.neural_network_operators import *
+from firedrake.external_operators.neural_networks import *
 from firedrake.external_operators.point_solve_operator import *
 from firedrake.external_operators.point_expr_operator import *
