949a228bb0711d29694c190bb33970a2b8e2cb1f
Reuben Nixon-Hill <ReubenHill@users.noreply.github.com>
2022-12-01 14:55:23 +0000

    Guarantee finding cell for point on boundary
    Only implemented for non-extruded meshes for now.

--- firedrake/locate.c
+++ firedrake/locate.c
@@ -17,6 +17,13 @@ int locate_cell(struct Function *f,
     if (f->sidx) {
         int64_t *ids = NULL;
         uint64_t nids = 0;
+        /* Assume that data_ is a ReferenceCoords object */
+        struct ReferenceCoords *ref_coords = (struct ReferenceCoords *) data_;
+        double closest_ref_coord = 0.0;
+        double current_closest_ref_coord = 0.5;
+        /* We treat our list of candidate cells (ids) from libspatialindex's
+            Index_Intersects_id  as our source of truth: the point must be in
+            one of the cells. */
         err = Index_Intersects_id(f->sidx, x, x, dim, &ids, &nids);
         if (err != RT_None) {
             fputs("ERROR: Index_Intersects_id failed in libspatialindex!", stderr);
@@ -25,8 +32,35 @@ int locate_cell(struct Function *f,
         if (f->extruded == 0) {
             for (uint64_t i = 0; i < nids; i++) {
                 if ((*try_candidate)(data_, f, ids[i], x)) {
+                    /* Found cell! */
                     cell = ids[i];
                     break;
+                }
+                else {
+                    /* Cell not found, but could be on cell boundary. We therefore look
+                       at our reference coordinates and find the point closest to being
+                       inside the reference cell. If we don't find a cell using try_candidate
+                       we assume that this process has found our cell. */
+                    /* First we must find the the reference coordinate which is closest to
+                       the interval [0, 1] without being in it (if all reference coordinates
+                       were in the interval we would have already found our cell!). We do
+                       this by storing the distance of each coordinate dimension from the
+                       interval. Note that ref_coords was updated as data_ by try_candidate. */
+                    current_closest_ref_coord = -1.0;
+                    for (uint64_t j = 0; j < dim; j++) {
+                        if(ref_coords->X[j] < 0.0 && current_closest_ref_coord < ref_coords->X[j]) {
+                            current_closest_ref_coord = ref_coords->X[j];
+                        }
+                        else if(1.0 - ref_coords->X[j] < 0.0 && current_closest_ref_coord < 1.0 - ref_coords->X[j]) {
+                            current_closest_ref_coord = 1.0 - ref_coords->X[j];
+                        }
+                    }
+                    /* Then, if it's lower than the last, we assume we must have found our
+                       cell until told otherwise */
+                    if(current_closest_ref_coord < closest_ref_coord){
+                        closest_ref_coord = current_closest_ref_coord;
+                        cell = ids[i];
+                    }
                 }
             }
         }
