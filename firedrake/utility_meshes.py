d0c03c8b5ef3e8914c5af44ad06f2e15aad3cfb0
ksagiyam <k.sagiyama@imperial.ac.uk>
2022-12-08 12:39:41 +0000

    mesh: add hexahedral option in BoxMesh, CubeMesh, UnitCubeMesh

--- firedrake/utility_meshes.py
+++ firedrake/utility_meshes.py
@@ -1382,6 +1382,7 @@ def BoxMesh(
     Lx,
     Ly,
     Lz,
+    hexahedral=False,
     reorder=None,
     distribution_parameters=None,
     diagonal="default",
@@ -1398,7 +1399,8 @@ def BoxMesh(
     :arg Lx: The extent in the x direction
     :arg Ly: The extent in the y direction
     :arg Lz: The extent in the z direction
-    :arg diagonal: Two ways of cutting hexadra, should be cut into 6
+    :kwarg hexahedral: (optional), creates hexahedral mesh, defaults to False
+    :kwarg diagonal: Two ways of cutting hexadra, should be cut into 6
         tetrahedra (``"default"``), or 5 tetrahedra thus less biased
         (``"crossed"``)
     :kwarg reorder: (optional), should the mesh be reordered?
@@ -1417,72 +1419,77 @@ def BoxMesh(
     for n in (nx, ny, nz):
         if n <= 0 or n % 1:
             raise ValueError("Number of cells must be a postive integer")
-
-    xcoords = np.linspace(0, Lx, nx + 1, dtype=np.double)
-    ycoords = np.linspace(0, Ly, ny + 1, dtype=np.double)
-    zcoords = np.linspace(0, Lz, nz + 1, dtype=np.double)
-    # X moves fastest, then Y, then Z
-    coords = (
-        np.asarray(np.meshgrid(xcoords, ycoords, zcoords)).swapaxes(0, 3).reshape(-1, 3)
-    )
-    i, j, k = np.meshgrid(
-        np.arange(nx, dtype=np.int32),
-        np.arange(ny, dtype=np.int32),
-        np.arange(nz, dtype=np.int32),
-    )
-    if diagonal == "default":
-        v0 = k * (nx + 1) * (ny + 1) + j * (nx + 1) + i
-        v1 = v0 + 1
-        v2 = v0 + (nx + 1)
-        v3 = v1 + (nx + 1)
-        v4 = v0 + (nx + 1) * (ny + 1)
-        v5 = v1 + (nx + 1) * (ny + 1)
-        v6 = v2 + (nx + 1) * (ny + 1)
-        v7 = v3 + (nx + 1) * (ny + 1)
-
-        cells = [
-            [v0, v1, v3, v7],
-            [v0, v1, v7, v5],
-            [v0, v5, v7, v4],
-            [v0, v3, v2, v7],
-            [v0, v6, v4, v7],
-            [v0, v2, v6, v7],
-        ]
-        cells = np.asarray(cells).reshape(-1, ny, nx, nz).swapaxes(0, 3).reshape(-1, 4)
-    elif diagonal == "crossed":
-        v0 = k * (nx + 1) * (ny + 1) + j * (nx + 1) + i
-        v1 = v0 + 1
-        v2 = v0 + (nx + 1)
-        v3 = v1 + (nx + 1)
-        v4 = v0 + (nx + 1) * (ny + 1)
-        v5 = v1 + (nx + 1) * (ny + 1)
-        v6 = v2 + (nx + 1) * (ny + 1)
-        v7 = v3 + (nx + 1) * (ny + 1)
-
-        # There are only five tetrahedra in this cutting of hexahedra
-        cells = [
-            [v0, v1, v2, v4],
-            [v1, v7, v5, v4],
-            [v1, v2, v3, v7],
-            [v2, v4, v6, v7],
-            [v1, v2, v7, v4],
-        ]
-        cells = np.asarray(cells).reshape(-1, ny, nx, nz).swapaxes(0, 3).reshape(-1, 4)
-        raise NotImplementedError(
-            "The crossed cutting of hexahedra has a broken connectivity issue for Pk (k>1) elements"
-        )
+    if hexahedral:
+        plex = PETSc.DMPlex().createBoxMesh((nx, ny, nz), lower=(0., 0., 0.), upper=(Lx, Ly, Lz), simplex=False, periodic=False, interpolate=True, comm=comm)
+        plex.removeLabel(dmcommon.FACE_SETS_LABEL)
+        nvert = 4  # num. vertices on faect
     else:
-        raise ValueError("Unrecognised value for diagonal '%r'", diagonal)
+        xcoords = np.linspace(0, Lx, nx + 1, dtype=np.double)
+        ycoords = np.linspace(0, Ly, ny + 1, dtype=np.double)
+        zcoords = np.linspace(0, Lz, nz + 1, dtype=np.double)
+        # X moves fastest, then Y, then Z
+        coords = (
+            np.asarray(np.meshgrid(xcoords, ycoords, zcoords)).swapaxes(0, 3).reshape(-1, 3)
+        )
+        i, j, k = np.meshgrid(
+            np.arange(nx, dtype=np.int32),
+            np.arange(ny, dtype=np.int32),
+            np.arange(nz, dtype=np.int32),
+        )
+        if diagonal == "default":
+            v0 = k * (nx + 1) * (ny + 1) + j * (nx + 1) + i
+            v1 = v0 + 1
+            v2 = v0 + (nx + 1)
+            v3 = v1 + (nx + 1)
+            v4 = v0 + (nx + 1) * (ny + 1)
+            v5 = v1 + (nx + 1) * (ny + 1)
+            v6 = v2 + (nx + 1) * (ny + 1)
+            v7 = v3 + (nx + 1) * (ny + 1)
 
-    plex = mesh.plex_from_cell_list(
-        3, cells, coords, comm, mesh._generate_default_mesh_topology_name(name)
-    )
+            cells = [
+                [v0, v1, v3, v7],
+                [v0, v1, v7, v5],
+                [v0, v5, v7, v4],
+                [v0, v3, v2, v7],
+                [v0, v6, v4, v7],
+                [v0, v2, v6, v7],
+            ]
+            cells = np.asarray(cells).reshape(-1, ny, nx, nz).swapaxes(0, 3).reshape(-1, 4)
+        elif diagonal == "crossed":
+            v0 = k * (nx + 1) * (ny + 1) + j * (nx + 1) + i
+            v1 = v0 + 1
+            v2 = v0 + (nx + 1)
+            v3 = v1 + (nx + 1)
+            v4 = v0 + (nx + 1) * (ny + 1)
+            v5 = v1 + (nx + 1) * (ny + 1)
+            v6 = v2 + (nx + 1) * (ny + 1)
+            v7 = v3 + (nx + 1) * (ny + 1)
 
+            # There are only five tetrahedra in this cutting of hexahedra
+            cells = [
+                [v0, v1, v2, v4],
+                [v1, v7, v5, v4],
+                [v1, v2, v3, v7],
+                [v2, v4, v6, v7],
+                [v1, v2, v7, v4],
+            ]
+            cells = np.asarray(cells).reshape(-1, ny, nx, nz).swapaxes(0, 3).reshape(-1, 4)
+            raise NotImplementedError(
+                "The crossed cutting of hexahedra has a broken connectivity issue for Pk (k>1) elements"
+            )
+        else:
+            raise ValueError("Unrecognised value for diagonal '%r'", diagonal)
+        plex = mesh.plex_from_cell_list(
+            3, cells, coords, comm, mesh._generate_default_mesh_topology_name(name)
+        )
+        nvert = 3  # num. vertices on faect
     # Apply boundary IDs
     plex.createLabel(dmcommon.FACE_SETS_LABEL)
     plex.markBoundaryFaces("boundary_faces")
     coords = plex.getCoordinates()
     coord_sec = plex.getCoordinateSection()
+    cdim = plex.getCoordinateDim()
+    assert cdim == 3
     if plex.getStratumSize("boundary_faces", 1) > 0:
         boundary_faces = plex.getStratumIS("boundary_faces", 1).getIndices()
         xtol = Lx / (2 * nx)
@@ -1490,44 +1497,19 @@ def BoxMesh(
         ztol = Lz / (2 * nz)
         for face in boundary_faces:
             face_coords = plex.vecGetClosure(coord_sec, coords, face)
-            if (
-                abs(face_coords[0]) < xtol
-                and abs(face_coords[3]) < xtol
-                and abs(face_coords[6]) < xtol
-            ):
+            if all([abs(face_coords[0 + cdim * i]) < xtol for i in range(nvert)]):
                 plex.setLabelValue(dmcommon.FACE_SETS_LABEL, face, 1)
-            if (
-                abs(face_coords[0] - Lx) < xtol
-                and abs(face_coords[3] - Lx) < xtol
-                and abs(face_coords[6] - Lx) < xtol
-            ):
+            if all([abs(face_coords[0 + cdim * i] - Lx) < xtol for i in range(nvert)]):
                 plex.setLabelValue(dmcommon.FACE_SETS_LABEL, face, 2)
-            if (
-                abs(face_coords[1]) < ytol
-                and abs(face_coords[4]) < ytol
-                and abs(face_coords[7]) < ytol
-            ):
+            if all([abs(face_coords[1 + cdim * i]) < ytol for i in range(nvert)]):
                 plex.setLabelValue(dmcommon.FACE_SETS_LABEL, face, 3)
-            if (
-                abs(face_coords[1] - Ly) < ytol
-                and abs(face_coords[4] - Ly) < ytol
-                and abs(face_coords[7] - Ly) < ytol
-            ):
+            if all([abs(face_coords[1 + cdim * i] - Ly) < ytol for i in range(nvert)]):
                 plex.setLabelValue(dmcommon.FACE_SETS_LABEL, face, 4)
-            if (
-                abs(face_coords[2]) < ztol
-                and abs(face_coords[5]) < ztol
-                and abs(face_coords[8]) < ztol
-            ):
+            if all([abs(face_coords[2 + cdim * i]) < ztol for i in range(nvert)]):
                 plex.setLabelValue(dmcommon.FACE_SETS_LABEL, face, 5)
-            if (
-                abs(face_coords[2] - Lz) < ztol
-                and abs(face_coords[5] - Lz) < ztol
-                and abs(face_coords[8] - Lz) < ztol
-            ):
+            if all([abs(face_coords[2 + cdim * i] - Lz) < ztol for i in range(nvert)]):
                 plex.setLabelValue(dmcommon.FACE_SETS_LABEL, face, 6)
     plex.removeLabel("boundary_faces")
-
     m = mesh.Mesh(
         plex,
         reorder=reorder,
@@ -1546,6 +1528,7 @@ def CubeMesh(
     ny,
     nz,
     L,
+    hexahedral=False,
     reorder=None,
     distribution_parameters=None,
     comm=COMM_WORLD,
@@ -1559,6 +1542,7 @@ def CubeMesh(
     :arg ny: The number of cells in the y direction
     :arg nz: The number of cells in the z direction
     :arg L: The extent in the x, y and z directions
+    :kwarg hexahedral: (optional), creates hexahedral mesh, defaults to False
     :kwarg reorder: (optional), should the mesh be reordered?
     :kwarg comm: Optional communicator to build the mesh on (defaults to
         COMM_WORLD).
@@ -1586,6 +1570,7 @@ def CubeMesh(
         L,
         L,
         L,
+        hexahedral=hexahedral,
         reorder=reorder,
         distribution_parameters=distribution_parameters,
         comm=comm,
@@ -1600,6 +1585,7 @@ def UnitCubeMesh(
     nx,
     ny,
     nz,
+    hexahedral=False,
     reorder=None,
     distribution_parameters=None,
     comm=COMM_WORLD,
@@ -1612,6 +1598,7 @@ def UnitCubeMesh(
     :arg nx: The number of cells in the x direction
     :arg ny: The number of cells in the y direction
     :arg nz: The number of cells in the z direction
+    :kwarg hexahedral: (optional), creates hexahedral mesh, defaults to False
     :kwarg reorder: (optional), should the mesh be reordered?
     :kwarg comm: Optional communicator to build the mesh on (defaults to
         COMM_WORLD).
@@ -1637,6 +1624,7 @@ def UnitCubeMesh(
         ny,
         nz,
         1,
+        hexahedral=hexahedral,
         reorder=reorder,
         distribution_parameters=distribution_parameters,
         comm=comm,
