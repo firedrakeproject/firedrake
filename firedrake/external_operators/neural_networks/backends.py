3b69479299ad19cb625ec8a8a2198822656f4eac
nbouziani <nacime.bouziani@gmail.com>
2022-12-14 14:48:05 +0000

    Update ML backend classes

--- firedrake/external_operators/neural_networks/backends.py
+++ firedrake/external_operators/neural_networks/backends.py
@@ -1,7 +1,7 @@
 from firedrake.function import Function
 from firedrake.cofunction import Cofunction
-
-from firedrake.external_operators.neural_networks.pytorch_custom_operator import CustomOperator
+from firedrake.vector import Vector
+from firedrake.constant import Constant
 
 import firedrake.utils as utils
 
@@ -17,6 +17,14 @@ class AbstractMLBackend(object):
     def from_ml_backend(self, x, V, cofunction=None):
         raise NotImplementedError
 
+    def get_function_space(self, x):
+        if isinstance(x, (Function, Cofunction)):
+            return x.function_space()
+        elif isinstance(x, float):
+            return None
+        else:
+            raise ValueError('Cannot infer the function space of %s' % x)
+
 
 class PytorchBackend(AbstractMLBackend):
 
@@ -30,21 +38,51 @@ class PytorchBackend(AbstractMLBackend):
 
     @utils.cached_property
     def custom_operator(self):
-        return CustomOperator().apply
+        from firedrake.external_operators.neural_networks.pytorch_custom_operator import CustomOperatorRF
+        # return CustomOperator().apply
+        return CustomOperatorRF().apply
 
     def to_ml_backend(self, x):
-        return self.backend.tensor(x.dat.data, requires_grad=True)
-
-    def from_ml_backend(self, x, V, cofunction=False):
-        if cofunction:
-            u = Cofunction(V.dual())
+        # Work out what's the right thing to do here ?
+        requires_grad = True
+        if isinstance(x, (Function, Cofunction, Vector)):
+            x_P = self.backend.tensor(x.dat.data, requires_grad=requires_grad)
+            # Default behaviour: unsqueeze after converting to PyTorch
+            # Shape: [1, x.dat.shape]
+            return x_P.unsqueeze(0)
+        # Add case subclass constant representing theta
+        # elif isinstance(x, ...):
+        elif isinstance(x, Constant):
+            return self.backend.tensor(x.values(), requires_grad=requires_grad)
+        elif isinstance(x, (float, int)):
+            # Covers pyadjoint AdjFloat as well
+            return self.backend.tensor(x, requires_grad=requires_grad)
         else:
+            raise ValueError("Cannot convert %s to the ML backend environment" % str(type(x)))
+
+    def from_ml_backend(self, x, V=None):
+        if V is None:
+            # Constant subclass
+            if isinstance(x, (list, tuple)):
+                # Is Parameter the right check ? What about backprop wrt variable (tensor) that is not Parameter ?
+                # if all([isinstance(θ, torch.nn.parameter.Parameter) for θ in x])
+                pass
+            else:
+                val = x.detach().numpy()
+                return Constant(val)
+        else:
+            # u is either a Function or Cofunction depending on whether V is a primal or dual space
             u = Function(V)
-        u.vector()[:] = x.detach().numpy()
-        return u
+            # Default behaviour: squeeze before converting to Firedrake
+            # This is motivated by the fact that assigning to numpy array to `u` will automatically squeeze
+            # the batch dimension behind the scenes
+            # Shape: [x.shape]
+            x = x.squeeze(0)
+            u.vector()[:] = x.detach().numpy()
+            return u
 
 
-def get_backend(backend_name):
+def get_backend(backend_name='pytorch'):
     if backend_name == 'pytorch':
         return PytorchBackend()
     else:
