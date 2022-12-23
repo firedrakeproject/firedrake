5dd69d0d8720fe40aa20d0f8d52206af7965a34c
nbouziani <nacime.bouziani@gmail.com>
2022-12-23 19:33:09 +0000

    Refactor assembly methods of NeuralNet using torch.autograd.functional API functions

--- firedrake/external_operators/neural_networks/neural_network_operators.py
+++ firedrake/external_operators/neural_networks/neural_network_operators.py
@@ -9,6 +9,8 @@ from firedrake.external_operators.neural_networks.backends import get_backend
 from firedrake.function import Function
 from firedrake.constant import Constant, PytorchParams
 from firedrake import utils
+from firedrake.petsc import PETSc
+from firedrake.matrix import AssembledMatrix
 
 from pyop2.datatypes import ScalarType
 
@@ -248,19 +250,12 @@ class PytorchOperator(NeuralNet):
         space = self.ufl_function_space()
         return self.ml_backend.from_ml_backend(y_P, space)
 
-    # --- Evaluation ---
+    # One could also extend assembly to hessian, hvp (hessian-vector product) and vhp (vector-hessian product)
+    # using `torch.autograd.functional.{hvp, hessian, vhp}`
 
-    def _evaluate_jacobian(self, x, **kwargs):
-        N = self._evaluate(model_tape=True)
-        N = N.squeeze(self.inputs_format)
-        if sum(self.derivatives[-self.nparams:]) > 0:
-            # When we want to compute: \frac{\partial{N}}{\partial{params_i}}
-            return self.backend.zeros(len(x))
+    # vjp faster than jvp since adjoint and not TLM
+    # vjp faster than backward and give you model output + vjp at same time in 1 traversal
 
-        gradient, = self.backend.autograd.grad(outputs=N, inputs=x,
-                                               grad_outputs=self.backend.ones_like(N),
-                                               retain_graph=True)
-        return gradient.squeeze(self.inputs_format)
 
     def _eval_update_weights(evaluate):
         """Check if we need to update the weights"""
@@ -285,8 +280,58 @@ class PytorchOperator(NeuralNet):
             return evaluate(self, *args, **kwargs)
         return wrapper
 
-    @_eval_update_weights
-    def _evaluate(self, *args, **kwargs):
+    # -- PyTorch routines for computing AD based quantities via `torch.autograd.functional` -- #
+
+    def _vjp(self, δy):
+        # What happens where more than one input: e.g. N(u1, u2, theta; v*) and want ((0, 1, 0), (0, None))
+        # Since users tell us how to map from u1 and u2 to a single model input.
+        # PyTorch bit can only provide jvp wrt that model input and then the rest depends on what users do
+        model = self.model
+        ops = self.operator_inputs()
+        x = self._pre_forward_callback(*ops)
+        δy_P = self._pre_forward_callback(δy)
+        _, vjp = self.backend.autograd.functional.vjp(lambda x: model(x), x, δy_P)
+        vjp_F = self._post_forward_callback(vjp)
+        return vjp_F
+
+    def _jvp(self, δx):
+        # What happens where more than one input: e.g. N(u1, u2, theta; v*) and want ((0, 1, 0), (0, None))
+        # Since users tell us how to map from u1 and u2 to a single model input.
+        # PyTorch bit can only provide jvp wrt that model input and then the rest depends on what users do
+        model = self.model
+        ops = self.operator_inputs()
+        x = self._pre_forward_callback(*ops)
+        δx_P = self._pre_forward_callback(δx)
+        _, jvp = self.backend.autograd.functional.jvp(lambda x: model(x), x, δx_P)
+        jvp_F = self._post_forward_callback(jvp)
+        return jvp_F
+
+    def _jac(self):
+        # Should we special case when the model acts locally on the inputs and therefore yields a diagonal
+        # matrix ?
+        #  -> Atm, PyTorch would produce that diagonal matrix but it might be possible to compute the local jacobian
+        #  -> However, another option is to compute the local Jacobian with PyTorch and then populate the diagonal of the PETSc matrix
+        # Both options rely on generated code so it is probably not that critical.
+        model = self.model
+        ops = self.operator_inputs()
+        # Don't unsqueeze so that we end up with a rank 2 tensor
+        x = self._pre_forward_callback(*ops, unsqueeze=False)
+        jac = self.backend.autograd.functional.jacobian(lambda x: model(x), x)
+
+        # For big matrices, assembling the Jacobian is not a good idea and one should instead
+        # look for the Jacobian action (e.g. via using matrix-free methods) which in turn will call `jvp`
+        n, m = jac.shape
+        J = PETSc.Mat().create()
+        J.setSizes([n, m])
+        J.setType("dense")
+        J.setUp()
+        # Set values using Jacobian computed by PyTorch
+        J.setValues(range(n), range(m), jac.numpy().flatten())
+        J.assemble()
+        return J
+
+    #@_eval_update_weights
+    def _forward(self):
         """
         Evaluate the neural network by performing a forward pass through the network
         The first argument is considered as the input of the network, if one want to correlate different
@@ -311,7 +356,7 @@ class PytorchOperator(NeuralNet):
             # Vectorized forward pass
             y_P = model(x_P)
 
-            # TODO: We should now remove the `model_tape` system as the tape is conserved in `model_output`
+            # Stash model output
             self.model_output = y_P
 
             # Post forward callback
@@ -319,9 +364,35 @@ class PytorchOperator(NeuralNet):
 
         return y_F
 
+    def _backprop(self, δy):
+        # Use stashed value and if not evaluate
+        stashed = False
+        if not stashed:
+            self._forward()
+        y_P = self.model_output
+        # Be careful here δy is Cofunction. As it is will access Cofunction.dat.data.
+        # Should we rather pass the underlying vector ?
+        δy_P = self._pre_forward_callback(δy)
+        # Backpropagate and accumulate adjoint values into graph leaves
+        y_P.backward(δy_P)
+        # How to collect parameters and adjoint value ? and how does that play with self.model.parameters()
+        # and PyTorchParams ?
+        # θ = PytorchParams(*[θi for θi in enumerate(self.model.parameters())])
+
+        # Should we return a Function in which case we need to be able to make fct space out
+        # of PytorchParams which involves making a dat while not needed ?
+        # We should extend type allowed to be return in ExternalOperator and return PytorchParams.
+        # Each PyTorch parameter has an adjoint value attribute that gets populated during backpropagation
+        # 1) Should we return PytorchParams whose values are adjoint values:
+        #     -> Make a new PytorchParams that won't get used by PyTorch (optimizer or whatever) but simply here
+        #        to return the result
+        # 2) Should we just keep PytorchParams in the operands which will get populated with adj value in which
+        #    case, the output returned will always be equal to model parameters
+
     def evaluate_backprop(self, x, params_idx, controls):
-        outputs = self._evaluate(model_tape=True)
+        outputs = self._forward(model_tape=True)
         params = list(p for i, p in enumerate(self.model.parameters()) if i in params_idx)
+        # This is adjoint action (vjp) and not jvp
         grad_W = self.backend.autograd.grad(outputs, params,
                                                grad_outputs=[self.backend.tensor(x.dat.data_ro)],
                                                retain_graph=True)
@@ -330,43 +401,55 @@ class PytorchOperator(NeuralNet):
         cst_fct_spaces = tuple(ctrl._ad_function_space(self.function_space().mesh()) for ctrl in controls)
         return tuple(Function(fct_space, val=grad_Wi).vector() for grad_Wi, fct_space in zip(grad_W, cst_fct_spaces))
 
+    # -- PyTorch operator assembly methods -- #
+
     @assemble_method(0, (0,))
     def assemble_model(self, *args, **kwargs):
-        return self._evaluate(*args, **kwargs)
+        return self._forward()
 
     @assemble_method(1, (0, 1))
     def assemble_jacobian(self, *args, assembly_opts, **kwargs):
-        result = self._evaluate()
-        integral_types = set(['cell'])
-        J = self._matrix_builder((), assembly_opts, integral_types)
-        with result.dat.vec as vec:
-            J.petscmat.setDiagonal(vec)
-        return J
+        # Get Jacobian using PyTorch AD
+        J = self._jac()
+        # Set bcs
+        bcs = ()
+        return AssembledMatrix(self, bcs, J)
+
+    @assemble_method(1, (1, 0))
+    def assemble_jacobian_adjoint(self, *args, assembly_opts, **kwargs):
+        # Get Jacobian using PyTorch AD
+        J = self._jac()
+        # Set bcs
+        bcs = ()
+        # Take the adjoint (Hermitian transpose)
+        J.hermitianTranspose()
+        return AssembledMatrix(self, bcs, J)
 
     @assemble_method(1, (0, None))
     def assemble_jacobian_action(self, *args, **kwargs):
+        if self.derivatives[-1] == 1:
+            # Jacobian action is being taken wrt model parameters
+            raise ValueError('')
         w = self.argument_slots()[-1]
-        idx, = [i for i, e in enumerate(self.derivatives) if e == 1]
-        res = self._evaluate_jacobian(val,  torch_op)
+        return self._jvp(w)
 
     @assemble_method(1, (None, 0))
     def assemble_jacobian_adjoint_action(self, *args, assembly_opts, **kwargs):
 
         w = self.argument_slots()[0]
-        idx, = [i for i, e in enumerate(self.derivatives) if e == 1]
-        n_inputs = len(self.operator_inputs())
-        if idx < n_inputs:
-            # Gradient with respect to inputs
-            pass
-        else:
-            # Gradient with respect to parameters
+        if self.derivatives[-1] == 1:
+            # Gradient with respect to parameters: ∂N(u, θ; w, v*)/∂θ
+
             # Work out the right thing to do for updating parameters
             # self._update_model_params()
-            res, = self.evaluate_backprop(w.vector(), (idx - n_inputs,), (self.ufl_operands[idx],))
+            res, = self._backprop(w.vector(), (idx - n_inputs,), (self.ufl_operands[idx],))
             # PyOP2 flattens out DataCarrier object by destructively modifying shape
             # This does the inverse of that operation to get the parameters of the N in the right format.
             res.dat.data.shape = res.ufl_shape
             return res.function
+        else:
+            # Gradient with respect to inputs: ∂N(u, θ; w, v*)/∂u
+            return self._vjp(w)
 
     # --- Update parameters ---
 
