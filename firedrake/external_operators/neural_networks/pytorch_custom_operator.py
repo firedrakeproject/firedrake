22f654c18d0b1b7cf7cc1e84a36e9483c2edd351
nbouziani <nacime.bouziani@gmail.com>
2023-02-11 00:12:43 +0000

    Update comments FiredrakeTorchOperator

--- firedrake/external_operators/neural_networks/pytorch_custom_operator.py
+++ firedrake/external_operators/neural_networks/pytorch_custom_operator.py
@@ -13,33 +13,29 @@ backend = get_backend('pytorch')
 
 class FiredrakeTorchOperator(torch_ad.Function):
     """
-    We can implement our own custom autograd Functions by subclassing
-    torch.autograd.Function and implementing the forward and backward passes
-    which operate on Tensors.
+    PyTorch custom operator representing a set of Firedrake operations expressed as a ReducedFunctional F.
+    `FiredrakeTorchOperator` is a wrapper around `torch.autograd.Function` that executes forward and backward
+    passes by directly calling the reduced functional F.
+
+    Inputs:
+        metadata: dictionary used to stash Firedrake objects.
+        *ω: PyTorch tensors representing the inputs to the Firedrake operator F
+
+    Outputs:
+        y: PyTorch tensor representing the output of the Firedrake operator F
     """
 
     # This method is wrapped by something cancelling annotation (probably 'with torch.no_grad()')
     @staticmethod
     def forward(ctx, metadata, *ω):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation. You can cache arbitrary
-        objects for use in the backward pass using the ctx.save_for_backward method.
-        """
+        """Forward pass of the PyTorch custom operator."""
         F = metadata['F']
         V = metadata['V_controls']
-        # w can be list/tuple of model parameters or firedrake type.
-        # Converter checks first firedrake type if not check if list/tuple check
-        # all elements are parameters type and then return Constant subclass (PyTorchParams)
         # Convert PyTorch input (i.e. controls) to Firedrake
         ω_F = [backend.from_ml_backend(ωi, Vi) for ωi, Vi in zip(ω, V)]
-
-        # Should we turn annotation pyadjoint also if not turned on ?
-
-        # Forward operator: `ReducedFunctional` recompute blocks on the tape
+        # Forward operator: Delegated to pyadjoint.ReducedFunctional which recomputes the blocks on the tape
         y_F = F(ω_F)
-        # Attach metadata to the PyTorch contextx
+        # Stash metadata to the PyTorch context
         ctx.metadata.update(metadata)
         # Convert Firedrake output to PyTorch
         y = backend.to_ml_backend(y_F)
@@ -47,19 +43,15 @@ class FiredrakeTorchOperator(torch_ad.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-        """
+        """Backward pass of the PyTorch custom operator."""
         F = ctx.metadata['F']
         V = ctx.metadata['V_output']
-
+        # Convert PyTorch gradient to Firedrake
         adj_input = backend.from_ml_backend(grad_output, V)
         if isinstance(adj_input, Function):
             adj_input = adj_input.vector()
 
-        # Compute adjoint model of the Firedrake operator `F` on `adj_input`
+        # Compute adjoint model of `F`: Delegated to pyadjoint.ReducedFunctional
         Δω = F.derivative(adj_input=adj_input)
 
         # Tuplify
