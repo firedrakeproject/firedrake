08ca5bb94eec84b8ade11691175e6a591ddb93ef
APaganini <aldapa@gmail.com>
2023-02-13 12:03:08 +0000

    revert to from.ufl_domain/form.ufl_domains

--- firedrake/ufl_expr.py
+++ firedrake/ufl_expr.py
@@ -2,7 +2,6 @@ import ufl
 import ufl.argument
 from ufl.split_functions import split
 from ufl.algorithms import extract_arguments, extract_coefficients
-from ufl.domain import extract_unique_domain
 
 import firedrake
 from firedrake import utils
@@ -151,7 +150,7 @@ def derivative(form, u, du=None, coefficient_derivatives=None):
         raise ValueError("Taking derivative of form wrt u, but form contains coefficients from u.subfunctions."
                          "\nYou probably meant to write split(u) when defining your form.")
 
-    mesh = extract_unique_domain(form)
+    mesh = form.ufl_domain()
     if not mesh:
         raise ValueError("Expression to be differentiated has no ufl domain."
                          "\nDo you need to add a domain to your Constant?")
