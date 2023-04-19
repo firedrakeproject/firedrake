c7fe7c38d179db74f1eadbe8f83013fee76ac751
Kaito Arai <kaito.arai19@imperial.ac.uk>
2023-04-19 12:50:00 +0100

    Change manifold point evaluation test

--- tests/test_immersed_manifold_point_eval.py
+++ tests/test_immersed_manifold_point_eval.py
@@ -1,32 +1,27 @@
 from firedrake import *
 import numpy as np
-
 import pytest
 
 
 def test_convergence_rate():
+    """Check points on immersed manifold projects to the correct point
+    on the mesh."""
     res = [2**i for i in range(4, 10)]
     error = []
 
     for r in res:
-        m = CircleManifoldMesh(ncells=r)
-        x = SpatialCoordinate(m)
-        test_coords = interpolate(
-            x,
-            functionspace.VectorFunctionSpace(m, "CG", 9)
-        ).dat.data[:]
-        V = FunctionSpace(m, 'Lagrange', 4)
-        f = interpolate(sin(2*pi*x[0])*sin(2*pi*x[1]), V)
-        sol1 = np.array(f.at(test_coords))
-        sol2 = (
-            np.sin(2*np.pi*test_coords[:, 0])
-            * np.sin(2*np.pi*test_coords[:, 1])
+        m = CircleManifoldMesh(ncells=r, radius=1e6)
+        test_coords = 1e6 * np.column_stack(
+            (np.cos(np.linspace(0, 2*np.pi, 3**6, endpoint=False)),
+             np.sin(np.linspace(0, 2*np.pi, 3**6, endpoint=False)))
         )
-        error += [np.sqrt(np.linalg.norm(sol1 - sol2)/(9*r))]
+        f = interpolate(SpatialCoordinate(m),
+                        VectorFunctionSpace(m, "Lagrange", 1))
+        sol = np.array(f.at(test_coords))
+        error += [np.linalg.norm(test_coords - sol)]
 
     convergence_rate = np.array(
         [np.log(error[i]/error[i+1])/np.log(res[i+1]/res[i])
          for i in range(len(res)-1)]
     )
-
-    assert (convergence_rate > 0.9 * 3).all()
+    assert (convergence_rate > 0.9 * 2).all()
