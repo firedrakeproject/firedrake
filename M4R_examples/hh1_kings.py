77bef4e01b2158ab9a675248a402e45f9b093288
yianzeng <57109024+yianzeng@users.noreply.github.com>
2023-04-24 15:45:28 +0100

    Update hh1_kings.py

--- M4R_examples/hh1_kings.py
+++ M4R_examples/hh1_kings.py
@@ -1,22 +1,23 @@
-# Define the right-hand side
+# Define the variational form
+a = (inner(grad(u), grad(v)) + inner(u, v)) * dx
+
+# Define the linear form for the RHS of the equation
 f = Function(V)
 x, y = SpatialCoordinate(mesh)
 f.interpolate(sin(x)*cos(y))
+L = inner(f, v) * dx 
 
-# define the bilinear and linear forms for the LHS and RHS of the equation
-a = (inner(grad(u), grad(v)) + inner(u, v)) * dx  # LHS
-L = inner(f, v) * dx  # RHS
+# Apply the boundary conditions
+bc = None # to be added
 
-params = {'ksp_type': 'preonly', 'pc_type': 'bjacobi', 'sub_pc_type': 'ilu'}# can get rid of params
-bcs = None  # CHANGE THIS TO APPLY BOUNDARY CONDITIONS
-
-u_soln = Function(V)  # function to hold the solution
+ # Create a function to hold the solution
+u_soln = Function(V) 
 
 # Create the Linear Variational Problem
 problem = LinearVariationalProblem(a, L, u_soln, bcs=bcs)
 
 # Create the Linear Variational Solver
-solver = LinearVariationalSolver(problem, solver_parameters=params)  
+solver = LinearVariationalSolver(problem)  
 
 # Solve the problem
 solver.solve()
