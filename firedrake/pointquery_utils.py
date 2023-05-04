ce08a67a0d0e40d3972bc496dde6ff1e3370ff8d
Connor Ward <c.ward20@imperial.ac.uk>
2023-05-04 11:39:59 +0100

    fixes

--- firedrake/pointquery_utils.py
+++ firedrake/pointquery_utils.py
@@ -154,9 +154,10 @@ def to_reference_coords_newton_step(ufl_coordinate_element, parameters, x0_dtype
     x0shape = (numpy.prod(x0_expr.shape, dtype=int),)
 
     loopy_args = [
-        lp.GlobalArg("C", dtype=ScalarType_c, shape=Cshape),
-        lp.GlobalArg("x0", dtype=ScalarType_c, shape=x0shape),
-    ]
+        lp.GlobalArg(
+            "C", dtype=ScalarType_c, shape=(numpy.prod(Cexpr.shape, dtype=int),)),
+        lp.GlobalArg(
+            "x0", dtype=ScalarType_c, shape=(numpy.prod(x0_expr.shape, dtype=int),))]
 
     dim = cell.topological_dimension()
     point = gem.Variable('X', (dim,))
