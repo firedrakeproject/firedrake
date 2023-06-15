711a9311aeeae1cc528f355b2d5c3d0026583964
David Ham <David.Ham@imperial.ac.uk>
2023-06-15 17:13:25 +0100

    flake8

--- tests/vertexonly/test_point_eval_immersed_manifold.py
+++ tests/vertexonly/test_point_eval_immersed_manifold.py
@@ -14,7 +14,7 @@ def vertex_only_mesh(function, point):
 
 
 @pytest.mark.parametrize("point_eval", [
-    at, 
+    at,
     pytest.param(vertex_only_mesh, marks=pytest.mark.xfail(
         reason="Immersed manifold VertexOnlyMesh not implemented."
     ))
