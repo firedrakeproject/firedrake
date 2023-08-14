c93c505df2ac5dde992d66997c2d22feeb5bea35
Jørgen Schartum Dokken <dokken92@gmail.com>
2023-08-14 09:41:40 +0000

    Add AMS comment

--- firedrake/preconditioners/hypre_ads.py
+++ firedrake/preconditioners/hypre_ads.py
@@ -26,7 +26,9 @@ class HypreADS(PCBase):
         except TypeError:
             pass
         if mesh.topological_dimension() != 3:
-            raise ValueError("Hypre ADS only works for meshes of topological dimension 3, (not dimension %d)" % (mesh.topological_dimension())) 
+            raise ValueError("Hypre ADS only works for meshes of topological dimension 3 ",
+                             " (not dimension %d)." % (mesh.topological_dimension()),
+                             " Did you mean Hypre AMS?")
         if formdegree != 2 or degree != 1:
             raise ValueError("Hypre ADS requires lowest order RT elements! (not %s of degree %d)" % (family, degree))
 
