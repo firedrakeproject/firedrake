487a02d8d810f07b3803a11e6518266ac06bc3e2
David Ham <David.Ham@imperial.ac.uk>
2023-08-14 19:18:02 +0200

    lint

--- firedrake/adjoint/__init__.py
+++ firedrake/adjoint/__init__.py
@@ -1,3 +1,4 @@
+
 """The public interface to Firedrake's adjoint.
 
 To start taping, run::
@@ -16,30 +17,32 @@ if 'backend' not in sys.modules:
 else:
     raise ImportError("'backend' module already exists?")
 
-from pyadjoint.tape import (Tape, set_working_tape, get_working_tape,
-                            pause_annotation, continue_annotation,
-                            stop_annotating, annotate_tape)
-from pyadjoint.reduced_functional import ReducedFunctional
-from firedrake.adjoint_utils.checkpointing import (
-    enable_disk_checkpointing, pause_disk_checkpointing,
-    continue_disk_checkpointing, stop_disk_checkpointing,
-    checkpointable_mesh
-)
-from pyadjoint.verification import taylor_test, taylor_to_dict
-from pyadjoint.drivers import compute_gradient, compute_hessian
-from pyadjoint.adjfloat import AdjFloat
-from pyadjoint.control import Control
-from pyadjoint import IPOPTSolver, ROLSolver, MinimizationProblem, InequalityConstraint, minimize
+from pyadjoint.tape import Tape, set_working_tape, get_working_tape, \
+                            pause_annotation, continue_annotation, \
+                            stop_annotating, annotate_tape  # noqa F401
+from pyadjoint.reduced_functional import ReducedFunctional  # noqa F401
+from firedrake.adjoint_utils.checkpointing import \
+    enable_disk_checkpointing, pause_disk_checkpointing, \
+    continue_disk_checkpointing, stop_disk_checkpointing, \
+    checkpointable_mesh   # noqa F401
 
-from firedrake.adjoint.ufl_constraints import UFLInequalityConstraint, UFLEqualityConstraint
-import numpy_adjoint
+from pyadjoint.verification import taylor_test, taylor_to_dict  # noqa F401
+from pyadjoint.drivers import compute_gradient, compute_hessian  # noqa F401
+from pyadjoint.adjfloat import AdjFloat  # noqa F401
+from pyadjoint.control import Control  # noqa F401
+from pyadjoint import IPOPTSolver, ROLSolver, MinimizationProblem, \
+    InequalityConstraint, minimize  # noqa F401
+
+from firedrake.adjoint.ufl_constraints import UFLInequalityConstraint, \
+    UFLEqualityConstraint  # noqa F401
+import numpy_adjoint  # noqa F401
 import firedrake.ufl_expr
 import types
 import sys
 
 
 # Work around the name clash of firedrake.adjoint vs ufl.adjoint.
-# This will eventually become cleaner once we can rely on users having 
+# This will eventually become cleaner once we can rely on users having
 # Python 3.12 (see PEP 713).
 class AdjointModule(types.ModuleType):
     def __call__(self, form):
