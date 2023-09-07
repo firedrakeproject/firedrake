13d3722aaecbc1f2479e8050dd57fc5562dfaf05
nbouziani <nacime.bouziani@gmail.com>
2023-09-07 11:56:38 +0100

    Extend assembly arguments type to ufl.Argument

--- firedrake/assemble.py
+++ firedrake/assemble.py
@@ -243,7 +243,7 @@ def base_form_assembly_visitor(expr, tensor, bcs, diagonal,
                                           options_prefix=options_prefix)
         else:
             raise TypeError("Mismatching FormSum shapes")
-    elif isinstance(expr, (ufl.Cofunction, ufl.Coargument, ufl.Matrix, ufl.ZeroBaseForm)):
+    elif isinstance(expr, (ufl.Cofunction, ufl.Coargument, ufl.Argument, ufl.Matrix, ufl.ZeroBaseForm)):
         return expr
     elif isinstance(expr, ufl.Coefficient):
         return expr
