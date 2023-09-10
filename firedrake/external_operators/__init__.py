9614f632342d05927e51be7a5d4c400d39251863
nbouziani <nacime.bouziani@gmail.com>
2023-09-10 21:48:51 +0100

    Remove point_solve operator (lifted to a different branch)

--- firedrake/external_operators/__init__.py
+++ firedrake/external_operators/__init__.py
@@ -1,3 +1,2 @@
 from firedrake.external_operators.abstract_external_operators import *
-from firedrake.external_operators.point_solve_operator import *
 from firedrake.external_operators.point_expr_operator import *
