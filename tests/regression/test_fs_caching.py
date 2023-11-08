03063e0597a19b7532546c44a27a4e7975aa2953
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-11-08 22:48:20 +0000

    Update tests/regression/test_fs_caching.py

--- tests/regression/test_fs_caching.py
+++ tests/regression/test_fs_caching.py
@@ -248,7 +248,7 @@ def test_extruded_mixed_fs_misses_cache():
 
     V0 = TensorProductElement(U0, U1)
 
-    V2 = FunctionSpace(e, HCurlElement(V0))
+    V2 = FunctionSpace(e, HCurl(V0))
 
     W1 = V1*V2
 
