0015a9df95d531e858d0a8b8e47dd54927c883fe
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2023-11-24 14:52:48 +0000

    Comments

--- firedrake/_deprecation.py
+++ firedrake/_deprecation.py
@@ -1,19 +1,34 @@
+""" Tools for deprecating parts of Firedrake functionality
+"""
+
+
 class _fake_module:
+    """ Object which behaves like a module
+
+    Parameters
+    ----------
+    new_location:
+        Where to find old functionality
+    functions:
+        List of functions that have moved
+
+    """
     def __init__(self, new_location, functions):
         self.new_location = new_location
         for f in functions:
-            setattr(self, f, self._import_error(self, f))
+            setattr(self, f, self._import_error(f))
 
-    def _import_error(self, fake_module, function_name):
+    def _import_error(self, function_name):
         def __call__(*args, **kwargs):
             raise ImportError(
                 f"The function `{function_name}` has moved to "
-                f"`{fake_module.new_location}`, use\n"
-                f"\t from {fake_module.new_location} import {function_name}"
+                f"`{self.new_location}`, update your code to use\n\t"
+                f"from {self.new_location} import {function_name}"
             )
         return __call__
 
 
+# Deprecate plotting in the global namespace
 plot = _fake_module(
     "firedrake.pyplot",
     [
