5e55488d3a7158c966564cc5f2d92ae3ade06606
Connor Ward <c.ward20@imperial.ac.uk>
2023-12-05 14:01:13 +0000

    Fix ordering of PETSc and pytest teardowns

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,3 +55,27 @@ def check_empty_tape(request):
             assert len(tape.get_blocks()) == 0
 
     request.addfinalizer(fin)
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_sessionfinish(session, exitstatus):
+    """Eagerly call PETSc finalize.
+
+    This is required because of a diabolical ordering issue between petsc2py and
+    pytest-xdist setup and teardown operations (see
+    https://github.com/firedrakeproject/firedrake/issues/3245). Without this
+    modification the ordering is:
+
+        pytest init -> PETSc init -> pytest finalize -> PETSc finalize
+
+    This is problematic because pytest finalize cleans up some state that causes
+    PETSc finalize to crash. To get around this we call PETSc finalize earlier
+    in the process resulting in:
+
+        pytest init -> PETSc init -> PETSc finalize -> pytest finalize
+
+    """
+    # import must be inside the function to avoid calling petsc2py initialize here
+    from petsc2py import PETSc
+
+    PETSc._finalize()
