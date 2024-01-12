2a8a6d1fd6c5f111f98cbd9582985703f3c6f676
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2024-01-12 19:01:41 +0000

    Move output to directory and remove hard VTK dependency

--- firedrake/__init__.py
+++ firedrake/__init__.py
@@ -93,7 +93,6 @@ from firedrake.exceptions import *
 from firedrake.function import *
 from firedrake.functionspace import *
 from firedrake.interpolation import *
-from firedrake.output import *
 from firedrake.linear_solver import *
 from firedrake.preconditioners import *
 from firedrake.mesh import *
@@ -128,7 +127,9 @@ set_log_level(WARNING)
 set_log_handlers(comm=COMM_WORLD)
 
 # Moved functionality
-from firedrake._deprecation import plot
+from firedrake._deprecation import output, plot
+sys.modules["firedrake.output"] = output
+from firedrake.output import *
 sys.modules["firedrake.plot"] = plot
 from firedrake.plot import *
 
