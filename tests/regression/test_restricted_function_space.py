a94b30a967734442188d9dff420e4752c8c07898
Emma Rothwell <emma.rothwell20@imperial.ac.uk>
2024-01-12 17:40:30 +0000

    Test RFS on other square, fail if CG1 pass if not

--- tests/regression/test_restricted_function_space.py
+++ tests/regression/test_restricted_function_space.py
@@ -38,3 +38,40 @@ def test_restricted_function_space_1_1_square(j):
     assert (restricted_fs_matrix.M.nrows == np.shape(normal_fs_matrix_reduced)[0])
     assert (restricted_fs_matrix.M.ncols == np.shape(normal_fs_matrix_reduced)[1])
     assert (np.array_equal(normal_fs_matrix_reduced, restricted_fs_matrix.M.values))
+
+
+@pytest.mark.parametrize("j", [1, 2, 5])
+def test_restricted_function_space_j_j_square(j):
+    mesh = UnitSquareMesh(j, j)
+    V = FunctionSpace(mesh, "CG", 3) # this fails if it's 1, fine for any other? 
+    x, y = SpatialCoordinate(mesh)
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    original_form = u * v * dx
+
+    bc = DirichletBC(V, 0, 2)
+    V_res = RestrictedFunctionSpace(V, name="Restricted", bcs=[bc])
+
+    u2 = TrialFunction(V_res)
+    v2 = TestFunction(V_res)
+    restricted_form = u2 * v2 * dx
+    restricted_fs_matrix = assemble(restricted_form) 
+    normal_fs_matrix = assemble(original_form, bcs=[bc])
+
+    identity = np.identity(normal_fs_matrix.M.nrows)
+    delete_rows = []
+    for i in range(normal_fs_matrix.M.nrows):
+        row = normal_fs_matrix.M.values[i, :]
+        comparison = row == identity[i, :]
+        if comparison.all():
+            delete_rows.append(i)
+
+    # Remove all rows/columns associated with boundaries (i.e. identity)
+    normal_fs_matrix_reduced = np.delete(normal_fs_matrix.M.values, delete_rows,
+                                         axis=0)
+    normal_fs_matrix_reduced = np.delete(normal_fs_matrix_reduced, delete_rows,
+                                         axis=1)
+
+    assert (restricted_fs_matrix.M.nrows == np.shape(normal_fs_matrix_reduced)[0])
+    assert (restricted_fs_matrix.M.ncols == np.shape(normal_fs_matrix_reduced)[1])
+    assert (np.array_equal(normal_fs_matrix_reduced, restricted_fs_matrix.M.values))
