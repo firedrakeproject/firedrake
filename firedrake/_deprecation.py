981b2783048cd51093ff350daa215e62c087f683
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2024-01-12 20:34:35 +0000

    try-except in deprecation

--- firedrake/_deprecation.py
+++ firedrake/_deprecation.py
@@ -28,9 +28,12 @@ class _fake_module:
         # This allows us to deprecate a file `output.py` and add its contents
         # to a new file `output/new.py`
         module_name = ".".join(new_location.split(".")[1:])
-        module = importlib.import_module(f".{module_name}", "firedrake")
-        for x in dir(module):
-            setattr(self, x, getattr(module, x))
+        try:
+            module = importlib.import_module(f".{module_name}", "firedrake")
+            for x in dir(module):
+                setattr(self, x, getattr(module, x))
+        except ImportError:
+            pass
 
     def _import_error(self, function_name, new_function_name=None):
         def __call__(*args, **kwargs):
