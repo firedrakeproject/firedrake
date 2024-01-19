1143f19981cb883800ada698e933319fe2bc1614
Umberto Zerbinati <zerbinati@maths.ox.ac.uk>
2024-01-19 02:15:58 +0000

    Added Netgen test in parallel
    Signed-off-by: Umberto Zerbinati <zerbinati@maths.ox.ac.uk>

--- tests/regression/test_netgen.py
+++ tests/regression/test_netgen.py
@@ -108,6 +108,13 @@ def test_firedrake_Poisson_netgen():
     print("convergence order:", conv)
     assert (np.array(conv) > 2.8).all()
 
+@pytest.mark.parallel
+def test_firedrake_Poisson_netgen_parallel():
+    diff = np.array([poisson(h)[0] for h in [1/2, 1/4, 1/8]])
+    print("l2 error norms:", diff)
+    conv = np.log2(diff[:-1] / diff[1:])
+    print("convergence order:", conv)
+    assert (np.array(conv) > 2.8).all()
 
 def test_firedrake_Poisson3D_netgen():
     diff = np.array([poisson3D(h) for h in [1, 1/2, 1/4]])
@@ -218,9 +225,141 @@ def test_firedrake_integral_sphere_high_order_netgen():
     f = Function(V).interpolate(1+0*x)
     assert abs(assemble(f * dx) - (4/3)*np.pi) < 1.e-4
 
+@pytest.mark.parallel
+def test_firedrake_integral_circle_high_order_netgen_parallel():
+    try:
+        from netgen.geom2d import CSG2d, Cricle
+        import netgen
+    except ImportError:
+        pytest.skip(reason="Netgen unavailable, skipping Netgen test.")
+
+    # Setting up Netgen geometry and mes
+    comm = COMM_WORLD
+    if comm.Get_rank() == 0:
+        geo = CSG2d() 
+        circle = Circle(center=(1,1), radius=0.1, bc="curve")
+        geo.Add(circle)
+        ngmesh = geo.GenerateMesh(maxh=0.3)
+    else:
+        ngmesh = netgen.libngpy._meshing.Mesh(3)
+
+    msh = Mesh(ngmesh)
+    homsh = Mesh(msh.curve_field(4))
+    V = FunctionSpace(homsh, "CG", 4)
+    x, y, z = SpatialCoordinate(homsh)
+    f = Function(V).interpolate(1+0*x)
+    assert abs(assemble(f * dx) - np.pi) < 1.e-4
 
 @pytest.mark.skipcomplex
 def test_firedrake_Adaptivity_netgen():
+    try:
+        from netgen.geom2d import SplineGeometry
+        import netgen
+    except ImportError:
+        pytest.skip(reason="Netgen unavailable, skipping Netgen test.")
+
+    try:
+        from slepc4py import SLEPc
+    except ImportError:
+        pytest.skip(reason="SLEPc unavailable, skipping adaptive test refinement.")
+
+    gc.collect()
+    comm = COMM_WORLD
+
+    def Solve(msh, labels):
+        V = FunctionSpace(msh, "CG", 2)
+        u = TrialFunction(V)
+        v = TestFunction(V)
+        a = inner(grad(u), grad(v))*dx
+        m = (u*v)*dx
+        uh = Function(V)
+        bc = DirichletBC(V, 0, labels)
+        A = assemble(a, bcs=bc)
+        M = assemble(m)
+        Asc, Msc = A.M.handle, M.M.handle
+        E = SLEPc.EPS().create()
+        E.setType(SLEPc.EPS.Type.ARNOLDI)
+        E.setProblemType(SLEPc.EPS.ProblemType.GHEP)
+        E.setDimensions(1, SLEPc.DECIDE)
+        E.setOperators(Asc, Msc)
+        ST = E.getST()
+        ST.setType(SLEPc.ST.Type.SINVERT)
+        PC = ST.getKSP().getPC()
+        PC.setType("lu")
+        PC.setFactorSolverType("mumps")
+        E.setST(ST)
+        E.solve()
+        vr, vi = Asc.getVecs()
+        with uh.dat.vec_wo as vr:
+            lam = E.getEigenpair(0, vr, vi)
+        return (lam, uh, V)
+
+    def Mark(msh, uh, lam):
+        W = FunctionSpace(msh, "DG", 0)
+        w = TestFunction(W)
+        R_T = lam.real*uh + div(grad(uh))
+        n = FacetNormal(V.mesh())
+        h = CellDiameter(msh)
+        R_dT = dot(grad(uh), n)
+        eta = assemble(h**2*R_T**2*w*dx + (h("+")+h("-"))*(R_dT("+")-R_dT("-"))**2*(w("+")+w("-"))*dS)
+        frac = .95
+        delfrac = .05
+        part = .2
+        mark = Function(W)
+        with mark.dat.vec as markedVec:
+            with eta.dat.vec as etaVec:
+                sum_eta = etaVec.sum()
+                if sum_eta < tolerance:
+                    return markedVec
+                eta_max = etaVec.max()[1]
+                sct, etaVec0 = PETSc.Scatter.toZero(etaVec)
+                markedVec0 = etaVec0.duplicate()
+                sct(etaVec, etaVec0)
+                if etaVec.getComm().getRank() == 0:
+                    eta = etaVec0.getArray()
+                    marked = np.zeros(eta.size, dtype='bool')
+                    sum_marked_eta = 0.
+                    while sum_marked_eta < part*sum_eta:
+                        new_marked = (~marked) & (eta > frac*eta_max)
+                        sum_marked_eta += sum(eta[new_marked])
+                        marked += new_marked
+                        frac -= delfrac
+                    markedVec0.getArray()[:] = 1.0*marked[:]
+                sct(markedVec0, markedVec, mode=PETSc.Scatter.Mode.REVERSE)
+        return mark
+    tolerance = 1e-16
+    max_iterations = 15
+    exact = 3.375610652693620492628**2
+    geo = SplineGeometry()
+    pnts = [(0, 0), (1, 0), (1, 1),
+            (0, 1), (-1, 1), (-1, 0),
+            (-1, -1), (0, -1)]
+    p1, p2, p3, p4, p5, p6, p7, p8 = [geo.AppendPoint(*pnt) for pnt in pnts]
+    curves = [[["line", p1, p2], "line"],
+              [["spline3", p2, p3, p4], "curve"],
+              [["spline3", p4, p5, p6], "curve"],
+              [["spline3", p6, p7, p8], "curve"],
+              [["line", p8, p1], "line"]]
+    [geo.Append(c, bc=bc) for c, bc in curves]
+    if comm.Get_rank() == 0:
+        ngmsh = geo.GenerateMesh(maxh=0.2)
+        labels = [i+1 for i, name in enumerate(ngmsh.GetRegionNames(codim=1)) if name == "line" or name == "curve"]
+    else:
+        ngmsh = netgen.libngpy._meshing.Mesh(2)
+        labels = None
+    labels = comm.bcast(labels, root=0)
+    msh = Mesh(ngmsh)
+    for i in range(max_iterations):
+        printf("level {}".format(i))
+        lam, uh, V = Solve(msh, labels)
+        mark = Mark(msh, uh, lam)
+        msh = msh.refine_marked_elements(mark)
+        File("Sol.pvd").write(uh)
+    assert (abs(lam-exact) < 1e-2)
+
+@pytest.mark.skipcomplex
+@pytest.mark.parallel
+def test_firedrake_Adaptivity_netgen_parallel():
     try:
         from netgen.geom2d import SplineGeometry
         import netgen
