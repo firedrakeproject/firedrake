485bd28a6eecfca716034999bbae6a601affa9f2
Connor Ward <c.ward20@imperial.ac.uk>
2024-02-12 16:55:03 +0000

    Use tags instead of line numbers to include code in docs

--- tests/vertexonly/test_vertex_only_manual.py
+++ tests/vertexonly/test_vertex_only_manual.py
@@ -1,12 +1,10 @@
 from firedrake import *
 from firedrake.__future__ import *
 import pytest
-# Ensure that the code shown in the manual runs without error. If you change
-# the code here make sure you update the .. literalinclude:: bits in the manual
-# too!
 
 
 def test_vertex_only_mesh_manual_example():
+    # [test_vertex_only_mesh_manual_example 1]
     parent_mesh = UnitSquareMesh(10, 10)
 
     V = FunctionSpace(parent_mesh, "CG", 2)
@@ -24,10 +22,13 @@ def test_vertex_only_mesh_manual_example():
     P0DG = FunctionSpace(vom, "DG", 0)
 
     # Interpolation performs point evaluation
+    # [test_vertex_only_mesh_manual_example 2]
     f_at_points = assemble(interpolate(f, P0DG))
 
     print(f_at_points.dat.data_ro)
+    # [test_vertex_only_mesh_manual_example 3]
 
+    # [test_vertex_only_mesh_manual_example 4]
     # Make a P0DG function on the input ordering vertex-only mesh again
     P0DG_input_ordering = FunctionSpace(vom.input_ordering, "DG", 0)
     f_at_input_points = Function(P0DG_input_ordering)
@@ -36,32 +37,41 @@ def test_vertex_only_mesh_manual_example():
     f_at_input_points.interpolate(f_at_points)
 
     print(f_at_input_points.dat.data_ro)  # will print the values at the input points
+    # [test_vertex_only_mesh_manual_example 5]
 
+    # [test_vertex_only_mesh_manual_example 6]
     import numpy as np
     f_at_input_points.dat.data_wo[:] = np.nan
     f_at_input_points.interpolate(f_at_points)
 
     print(f_at_input_points.dat.data_ro)  # any points not found will be NaN
+    # [test_vertex_only_mesh_manual_example 7]
 
 
 def test_vom_manual_points_outside_domain():
     for i in [0]:
         parent_mesh = UnitSquareMesh(100, 100, quadrilateral=True)
 
+        # [test_vom_manual_points_outside_domain 1]
         # point (1.1, 1.0) is outside the mesh
         points = [[0.1, 0.1], [0.2, 0.2], [1.1, 1.0]]
+        # [test_vom_manual_points_outside_domain 2]
 
     vom = True  # avoid flake8 unused variable warning
     with pytest.raises(VertexOnlyMeshMissingPointsError):
+        # [test_vom_manual_points_outside_domain 3]
         # This will raise a VertexOnlyMeshMissingPointsError
         vom = VertexOnlyMesh(parent_mesh, points, missing_points_behaviour='error')
+        # [test_vom_manual_points_outside_domain 4]
 
     def display_correct_indent():
+        # [test_vom_manual_points_outside_domain 5]
         # This will generate a warning and the point will be lost
         vom = VertexOnlyMesh(parent_mesh, points, missing_points_behaviour='warn')
 
         # This will cause the point to be silently lost
         vom = VertexOnlyMesh(parent_mesh, points, missing_points_behaviour=None)
+        # [test_vom_manual_points_outside_domain 6]
 
         assert vom  # Just here to shut up flake8 unused variable warning.
 
@@ -70,6 +80,7 @@ def test_vom_manual_points_outside_domain():
 
 
 def test_vom_manual_keyword_arguments():
+    # [test_vom_manual_keyword_arguments 1]
     omega = UnitSquareMesh(100, 100, quadrilateral=True)
 
     V = FunctionSpace(omega, "CG", 2)
@@ -86,11 +97,13 @@ def test_vom_manual_keyword_arguments():
     # the points
     f_at_points = assemble(interpolate(f, P0DG))
     expr = assemble(f_at_points * dx)
+    # [test_vom_manual_keyword_arguments 2]
 
     assert expr == 0.0
 
 
 def test_mesh_tolerance():
+    # [test_mesh_tolerance 1]
     parent_mesh = UnitSquareMesh(100, 100, quadrilateral=True)
 
     # point (1.1, 1.0) is outside the mesh
@@ -112,6 +125,7 @@ def test_mesh_tolerance():
     # Similarly .at will not generate an error
     V = FunctionSpace(parent_mesh, 'CG', 2)
     Function(V).at((1.1, 1.0))
+    # [test_mesh_tolerance 2]
 
     assert vom
 
@@ -121,6 +135,7 @@ def test_mesh_tolerance_change():
     points = [[0.1, 0.1], [0.2, 0.2], [1.1, 1.0]]
     V = FunctionSpace(parent_mesh, 'CG', 2)
 
+    # [test_mesh_tolerance_change 1]
     # The point (1.1, 1.0) will still be included in the vertex-only mesh
     vom = VertexOnlyMesh(parent_mesh, points, tolerance=30.0)
 
@@ -139,6 +154,7 @@ def test_mesh_tolerance_change():
     except PointNotInDomainError:
         # But the tolerance property has still been changed - this will print 1.0
         print(parent_mesh.tolerance)
+    # [test_mesh_tolerance_change 2]
 
     assert vom
 
@@ -157,6 +173,7 @@ def test_input_ordering_input():
         point_locations_from_elsewhere = np.array([]).reshape(0, 2)
         point_data_values_from_elsewhere = []
 
+    # [test_input_ordering_input 1]
     # We have a set of points with corresponding data from elsewhere which vary
     # from rank to rank
     vom = VertexOnlyMesh(parent_mesh, point_locations_from_elsewhere, redundant=False)
@@ -172,6 +189,7 @@ def test_input_ordering_input():
 
     # Interpolate puts this data onto the original vertex-only mesh
     point_data = assemble(interpolate(point_data_input_ordering, P0DG))
+    # [test_input_ordering_input 2]
 
     assert vom
     if point_data:  # in case point data is None for some reason - apparently it can be in complex mode without an error occuring?
