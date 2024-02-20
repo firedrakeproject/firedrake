1f149041b4a4c0f1e3704196dae9252d059c13bc
ksagiyam <k.sagiyama@imperial.ac.uk>
2024-02-20 16:21:14 +0000

    assemble: spell out tensor=... explicitly

--- tests/regression/test_assemble.py
+++ tests/regression/test_assemble.py
@@ -106,9 +106,9 @@ def test_assemble_with_tensor(mesh):
     L = conj(v) * dx
     f = Cofunction(V.dual())
     # Assemble a form into f
-    f = assemble(L, f)
+    f = assemble(L, tensor=f)
     # Assemble a different form into f
-    f = assemble(Constant(2)*L, f)
+    f = assemble(Constant(2)*L, tensor=f)
     # Make sure we get the result of the last assembly
     assert np.allclose(f.dat.data, 2*assemble(L).dat.data, rtol=1e-14)
 
@@ -120,7 +120,7 @@ def test_assemble_mat_with_tensor(mesh):
     a = inner(u, v) * dx
     M = assemble(a)
     # Assemble a different form into M
-    M = assemble(Constant(2)*a, M)
+    M = assemble(Constant(2)*a, tensor=M)
     # Make sure we get the result of the last assembly
     assert np.allclose(M.M.values, 2*assemble(a).M.values, rtol=1e-14)
 
