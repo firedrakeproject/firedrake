5e1c62a0cda3e6996c2a30534fc0b9079c185bd7
Pablo Brubeck <brubeck@protonmail.com>
2024-02-21 16:51:31 +0000

    FDMPC: generate pyop2 kernels (#3341)
    * FDMPC: generate pyop2 kernels
    
    ---------
    
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>

--- firedrake/preconditioners/pmg.py
+++ firedrake/preconditioners/pmg.py
@@ -577,8 +577,8 @@ def hash_fiat_element(element):
     return (family, element.ref_el, degree, restriction)
 
 
-def generate_key_evaluate_dual(source, target, alpha=tuple()):
-    return hash_fiat_element(source) + hash_fiat_element(target) + (alpha,)
+def generate_key_evaluate_dual(source, target, derivative=None):
+    return hash_fiat_element(source) + hash_fiat_element(target) + (derivative,)
 
 
 def get_readonly_view(arr):
@@ -588,19 +588,40 @@ def get_readonly_view(arr):
 
 
 @cached({}, key=generate_key_evaluate_dual)
-def evaluate_dual(source, target, alpha=tuple()):
+def evaluate_dual(source, target, derivative=None):
     """Evaluate the action of a set of dual functionals of the target element
-       on the (derivative of order alpha of the) basis functions of the source
-       element."""
+    on the (derivative of the) basis functions of the source element.
+
+    Parameters
+    ----------
+    source :
+        A :class:`FIAT.CiarletElement` to interpolate.
+    target :
+        A :class:`FIAT.CiarletElement` defining the interpolation space.
+    derivative : ``str`` or ``None``
+        An optional differential operator to apply on the source expression,
+        (currently only "grad" is supported).
+
+    Returns
+    -------
+    A read-only :class:`numpy.ndarray` with the evaluation of the target
+    dual basis on the (derivative of the) source primal basis.
+    """
     primal = source.get_nodal_basis()
     dual = target.get_dual_set()
     A = dual.to_riesz(primal)
     B = numpy.transpose(primal.get_coeffs())
-    if sum(alpha) != 0:
+    if derivative == "grad":
         dmats = primal.get_dmats()
+        assert len(dmats) == 1
+        alpha = (1,)
         for i in range(len(alpha)):
             for j in range(alpha[i]):
                 B = numpy.dot(dmats[i], B)
+    elif derivative in ("curl", "div"):
+        raise NotImplementedError(f"Dual evaluation of {derivative} is not yet implemented.")
+    elif derivative is not None:
+        raise ValueError(f"Invalid derivaitve type {derivative}.")
     return get_readonly_view(numpy.dot(A, B))
 
 
