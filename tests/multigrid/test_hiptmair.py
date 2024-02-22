584b51310048a445ababac61a0f04820aed9a60e
Pablo Brubeck <brubeck@protonmail.com>
2024-02-22 17:44:48 +0000

    ASMExtrudedStar: test edge/face-star equivalence with Jacobi on NCE(1)/NCF(1)

--- tests/multigrid/test_hiptmair.py
+++ tests/multigrid/test_hiptmair.py
@@ -3,9 +3,11 @@ from firedrake.preconditioners.fdm import tabulate_exterior_derivative
 import pytest
 
 
-def mesh_hierarchy(cell):
+@pytest.fixture(params=["tetrahedron", "hexahedron"])
+def mesh_hierarchy(request):
     nx = 5
     nlevels = 2
+    cell = request.param
     if cell == "tetrahedron":
         base = UnitCubeMesh(nx, nx, nx)
         return MeshHierarchy(base, nlevels)
@@ -15,12 +17,27 @@ def mesh_hierarchy(cell):
         return ExtrudedMeshHierarchy(basemh, height=1, base_layer=nx)
 
 
-def run_riesz_map(V, mat_type):
-    relax = {
+def run_riesz_map(V, mat_type, max_it):
+    jacobi = {
         "mat_type": mat_type,
         "ksp_type": "preonly",
         "pc_type": "jacobi",
     }
+    potential = jacobi
+    if V.mesh().extruded and mat_type == "aij":
+        # Test equivalence of Jacobi and ASM on edge/face-stars
+        formdegree = V.finat_element.formdegree
+        relax = {
+            "ksp_type": "preonly",
+            "pc_type": "python",
+            "pc_python_type": "firedrake.ASMExtrudedStarPC",
+            "pc_star_construct_dim": formdegree,
+            "pc_star_sub_sub_ksp_type": "preonly",
+            "pc_star_sub_sub_pc_type": "jacobi",
+        }
+    else:
+        relax = jacobi
+
     coarse = {
         "ksp_type": "preonly",
         "pc_type": "cholesky",
@@ -34,9 +51,11 @@ def run_riesz_map(V, mat_type):
         }
     parameters = {
         "mat_type": mat_type,
-        "snes_type": "ksponly",
+        "ksp_max_it": max_it,
         "ksp_type": "cg",
         "ksp_norm_type": "natural",
+        "ksp_monitor": None,
+        "ksp_convergence_test": "skip",
         "pc_type": "mg",
         "mg_coarse": coarse,
         "mg_levels": {
@@ -45,7 +64,7 @@ def run_riesz_map(V, mat_type):
             "pc_type": "python",
             "pc_python_type": "firedrake.HiptmairPC",
             "hiptmair_mg_levels": relax,
-            "hiptmair_mg_coarse": relax,
+            "hiptmair_mg_coarse": potential,
         },
     }
 
@@ -70,24 +89,32 @@ def run_riesz_map(V, mat_type):
     problem = LinearVariationalProblem(a, L, uh, bcs=bcs, form_compiler_parameters={"mode": "vanilla"})
     solver = LinearVariationalSolver(problem, solver_parameters=parameters, appctx=appctx)
     solver.solve()
-    return solver.snes.ksp.getIterationNumber()
+    return errornorm(u_exact, uh)
 
 
 @pytest.mark.skipcomplexnoslate
-@pytest.mark.parametrize(["family", "cell"],
-                         [("N1curl", "tetrahedron"), ("NCE", "hexahedron")])
-def test_hiptmair_hcurl(family, cell):
-    mesh = mesh_hierarchy(cell)[-1]
+@pytest.mark.parametrize("mat_type", ["aij", "matfree"])
+def test_hiptmair_hcurl(mesh_hierarchy, mat_type):
+    mesh = mesh_hierarchy[-1]
+    if mesh.ufl_cell().is_simplex():
+        family = "N1curl"
+        max_it = 14
+    else:
+        family = "NCE"
+        max_it = 5
     V = FunctionSpace(mesh, family, degree=1)
-    assert run_riesz_map(V, "aij") <= 15
-    assert run_riesz_map(V, "matfree") <= 15
+    assert run_riesz_map(V, mat_type, max_it) < 1E-6
 
 
 @pytest.mark.skipcomplexnoslate
-@pytest.mark.parametrize(["family", "cell"],
-                         [("RT", "tetrahedron"), ("NCF", "hexahedron")])
-def test_hiptmair_hdiv(family, cell):
-    mesh = mesh_hierarchy(cell)[-1]
+@pytest.mark.parametrize("mat_type", ["aij", "matfree"])
+def test_hiptmair_hdiv(mesh_hierarchy, mat_type):
+    mesh = mesh_hierarchy[-1]
+    if mesh.ufl_cell().is_simplex():
+        family = "N1div"
+        max_it = 14
+    else:
+        family = "NCF"
+        max_it = 7
     V = FunctionSpace(mesh, family, degree=1)
-    assert run_riesz_map(V, "aij") <= 12
-    assert run_riesz_map(V, "matfree") <= 12
+    assert run_riesz_map(V, mat_type, max_it) < 1E-6
