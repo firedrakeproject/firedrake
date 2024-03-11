1d2bc54cfb1f7e26639cbbad97300fdf0c268e6d
Pablo Brubeck <brubeck@protonmail.com>
2024-03-11 10:31:39 +0000

    ASMVankaPC: fix tests (#3435)
    * ASMVankaPC: fix tests
    
    * ASMVankaPC: Excluded subspaces get the star. Only sort the points in the star
    
    * Avoid zero pivot by setting -pc_factor_mat_ordering_type natural

--- tests/regression/test_star_pc.py
+++ tests/regression/test_star_pc.py
@@ -79,23 +79,23 @@ def test_star_equivalence(problem_type, backend):
                        "snes_type": "ksponly",
                        "ksp_type": "cg",
                        "pc_type": "mg",
-                       "pc_mg_type": "full",
+                       "pc_mg_type": "multiplicative",
+                       "pc_mg_cycle_type": "v",
                        "mg_levels_ksp_type": "richardson",
                        "mg_levels_ksp_richardson_scale": 1/4,
                        "mg_levels_ksp_max_it": 1,
                        "mg_levels_pc_type": "python",
                        "mg_levels_pc_python_type": "firedrake.ASMStarPC",
                        "mg_levels_pc_star_construct_dim": 1,
-                       "mg_coarse_pc_type": "python",
-                       "mg_coarse_pc_python_type": "firedrake.AssembledPC",
-                       "mg_coarse_assembled_pc_type": "lu",
-                       "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps"}
+                       "mg_coarse_pc_type": "lu",
+                       "mg_coarse_pc_factor_mat_solver_type": "mumps"}
 
         comp_params = {"mat_type": "aij",
                        "snes_type": "ksponly",
                        "ksp_type": "cg",
                        "pc_type": "mg",
-                       "pc_mg_type": "full",
+                       "pc_mg_type": "multiplicative",
+                       "pc_mg_cycle_type": "v",
                        "mg_levels_ksp_type": "richardson",
                        "mg_levels_ksp_richardson_scale": 1/4,
                        "mg_levels_ksp_max_it": 1,
@@ -134,24 +134,21 @@ def test_star_equivalence(problem_type, backend):
                        "ksp_type": "richardson",
                        "pc_type": "mg",
                        "pc_mg_type": "multiplicative",
-                       "pc_mg_cycles": "v",
+                       "pc_mg_cycle_type": "v",
                        "mg_levels_ksp_type": "chebyshev",
                        "mg_levels_ksp_max_it": 2,
                        "mg_levels_ksp_convergence_test": "skip",
                        "mg_levels_pc_type": "python",
                        "mg_levels_pc_python_type": "firedrake.ASMStarPC",
                        "mg_levels_pc_star_construct_dim": 0,
-                       "mg_levels_pc_star_sub_sub_pc_factor_shift_type": "nonzero",
-                       "mg_coarse_pc_type": "python",
-                       "mg_coarse_pc_python_type": "firedrake.AssembledPC",
-                       "mg_coarse_assembled_pc_type": "lu"}
+                       "mg_coarse_pc_type": "lu"}
 
         comp_params = {"mat_type": "aij",
                        "snes_type": "ksponly",
                        "ksp_type": "richardson",
                        "pc_type": "mg",
                        "pc_mg_type": "multiplicative",
-                       "pc_mg_cycles": "v",
+                       "pc_mg_cycle_type": "v",
                        "mg_levels_ksp_type": "chebyshev",
                        "mg_levels_ksp_max_it": 2,
                        "mg_levels_ksp_convergence_test": "skip",
@@ -168,6 +165,7 @@ def test_star_equivalence(problem_type, backend):
                        "mg_coarse_assembled_pc_type": "lu"}
 
     star_params["mg_levels_pc_star_backend"] = backend
+    star_params["mg_levels_pc_star_mat_ordering_type"] = "rcm"
     nvproblem = NonlinearVariationalProblem(a, u, bcs=bcs)
     star_solver = NonlinearVariationalSolver(nvproblem, solver_parameters=star_params, nullspace=nsp)
     star_solver.solve()
@@ -181,7 +179,6 @@ def test_star_equivalence(problem_type, backend):
     assert star_its == comp_its
 
 
-@pytest.mark.skipif(True, reason="This test seems to randomly fail CI")
 def test_vanka_equivalence(problem_type):
     distribution_parameters = {"partition": True,
                                "overlap_type": (DistributedMeshOverlapType.VERTEX, 2)}
@@ -204,24 +201,22 @@ def test_vanka_equivalence(problem_type):
                         "ksp_type": "richardson",
                         "pc_type": "mg",
                         "pc_mg_type": "multiplicative",
-                        "pc_mg_cycles": "v",
+                        "pc_mg_cycle_type": "v",
                         "mg_levels_ksp_type": "richardson",
                         "mg_levels_ksp_richardson_scale": 1/10,
                         "mg_levels_ksp_max_it": 1,
                         "mg_levels_pc_type": "python",
                         "mg_levels_pc_python_type": "firedrake.ASMVankaPC",
                         "mg_levels_pc_vanka_construct_codim": 0,
-                        "mg_coarse_pc_type": "python",
-                        "mg_coarse_pc_python_type": "firedrake.AssembledPC",
-                        "mg_coarse_assembled_pc_type": "lu",
-                        "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps"}
+                        "mg_coarse_pc_type": "lu",
+                        "mg_coarse_pc_factor_mat_solver_type": "mumps"}
 
         comp_params = {"mat_type": "aij",
                        "snes_type": "ksponly",
                        "ksp_type": "richardson",
                        "pc_type": "mg",
                        "pc_mg_type": "multiplicative",
-                       "pc_mg_cycles": "v",
+                       "pc_mg_cycle_type": "v",
                        "mg_levels_ksp_type": "richardson",
                        "mg_levels_ksp_richardson_scale": 1/10,
                        "mg_levels_ksp_max_it": 1,
@@ -264,10 +259,8 @@ def test_vanka_equivalence(problem_type):
                         "mg_levels_pc_type": "python",
                         "mg_levels_pc_python_type": "firedrake.ASMVankaPC",
                         "mg_levels_pc_vanka_construct_codim": 0,
-                        "mg_coarse_pc_type": "python",
-                        "mg_coarse_pc_python_type": "firedrake.AssembledPC",
-                        "mg_coarse_assembled_pc_type": "lu",
-                        "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps"}
+                        "mg_coarse_pc_type": "lu",
+                        "mg_coarse_pc_factor_mat_solver_type": "mumps"}
 
         comp_params = {"mat_type": "aij",
                        "snes_type": "ksponly",
@@ -302,7 +295,7 @@ def test_vanka_equivalence(problem_type):
         (z, p) = split(u)
         (v, q) = split(TestFunction(V))
 
-        a = inner(grad(z), grad(v))*dx - inner(p, div(v))*dx - inner(q, div(z))*dx
+        a = inner(grad(z), grad(v))*dx - inner(p, div(v))*dx - inner(div(z), q)*dx
 
         bcs = DirichletBC(V.sub(0), Constant((1., 0.)), "on_boundary")
         nsp = MixedVectorSpaceBasis(V, [V.sub(0), VectorSpaceBasis(constant=True)])
@@ -312,7 +305,7 @@ def test_vanka_equivalence(problem_type):
                         "ksp_type": "richardson",
                         "pc_type": "mg",
                         "pc_mg_type": "multiplicative",
-                        "pc_mg_cycles": "v",
+                        "pc_mg_cycle_type": "v",
                         "mg_levels_ksp_type": "chebyshev",
                         "mg_levels_ksp_max_it": 2,
                         "mg_levels_ksp_convergence_test": "skip",
@@ -320,19 +313,16 @@ def test_vanka_equivalence(problem_type):
                         "mg_levels_pc_python_type": "firedrake.ASMVankaPC",
                         "mg_levels_pc_vanka_construct_dim": 0,
                         "mg_levels_pc_vanka_exclude_subspaces": "1",
-                        "mg_levels_pc_vanka_sub_sub_pc_factor_shift_type": "nonzero",
-                        "mg_levels_pc_vanka_sub_sub_pc_factor_mat_solver_type": "mumps",
-                        "mg_coarse_pc_type": "python",
-                        "mg_coarse_pc_python_type": "firedrake.AssembledPC",
-                        "mg_coarse_assembled_pc_type": "lu",
-                        "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps"}
+                        "mg_levels_pc_vanka_sub_sub_pc_type": "cholesky",
+                        "mg_coarse_pc_type": "cholesky",
+                        "mg_coarse_pc_factor_mat_solver_type": "mumps"}
 
         comp_params = {"mat_type": "aij",
                        "snes_type": "ksponly",
                        "ksp_type": "richardson",
                        "pc_type": "mg",
                        "pc_mg_type": "multiplicative",
-                       "pc_mg_cycles": "v",
+                       "pc_mg_cycle_type": "v",
                        "mg_levels_ksp_type": "chebyshev",
                        "mg_levels_ksp_max_it": 2,
                        "mg_levels_ksp_convergence_test": "skip",
@@ -350,6 +340,7 @@ def test_vanka_equivalence(problem_type):
                        "mg_coarse_assembled_pc_type": "lu",
                        "mg_coarse_assembled_pc_factor_mat_solver_type": "mumps"}
 
+    vanka_params["mg_levels_pc_vanka_mat_ordering_type"] = "rcm"
     nvproblem = NonlinearVariationalProblem(a, u, bcs=bcs)
     star_solver = NonlinearVariationalSolver(nvproblem, solver_parameters=vanka_params, nullspace=nsp)
     star_solver.solve()
