5f75618d9f65862d6220a03a625422cfbade8dd7
Lilly Schweiger <lilly.schweiger23@imperial.ac.uk>
2024-03-12 13:03:03 +0000

    cusparse convert - not done

--- firedrake/preconditioners/offload.py
+++ firedrake/preconditioners/offload.py
@@ -9,12 +9,13 @@ from firedrake.dmhooks import get_function_space
 
 import petsc4py.PETSc # in firedrake.petsc?
 
-#outside: ksp.setOperators(A)
+# outside: ksp.setOperators(A)
+# todo: for densecuda later- now only cusparse
 
 __all__ = ("OffloadPC")
 
 
-class OffloadPC(PCBase): #still PETSc PC object?
+class OffloadPC(PCBase):
     """Offload to GPU as PC to solve.
 
     Internally this makes a PETSc PC object that can be controlled by
@@ -24,108 +25,81 @@ class OffloadPC(PCBase): #still PETSc PC object?
     _prefix = "offload_"
 
     def initialize(self, pc):
-        A, P = pc.getOperators() #P preconditioner
+        A, P = pc.getOperators()  # P preconditioner
 
         if pc.getType() != "assembled":
-            raise ValueError("Expecting PC type assembled") #correct type?
-        opc = pc #opc?
-        appctx = self.get_appctx(pc) 
-        fcp = appctx.get("form_compiler_parameters") 
+            raise ValueError("Expecting PC type assembled")  # correct type?
+        outer_pc = pc
+        appctx = self.get_appctx(pc)
+        fcp = appctx.get("form_compiler_parameters")
 
-        if P.type == "assembled": #not python value error - only assembled (preconditioner)
+        V = get_function_space(pc.getDM())
+        if len(V) == 1:
+            V = FunctionSpace(V.mesh(), V.ufl_element())
+        else:
+            V = MixedFunctionSpace([V_ for V_ in V])
+        test = TestFunction(V)
+        trial = TrialFunction(V)
+
+        (a, bcs) = self.form(pc, test, trial)
+
+        if P.type == "assembled":  # not python value error - only assembled (preconditioner)
             context = P.getPythonContext()
             # It only makes sense to preconditioner/invert a diagonal
             # block in general.  That's all we're going to allow.
-            if not context.on_diag: #still? diagonal block?
+            if not context.on_diag:  # still? diagonal block?
                 raise ValueError("Only makes sense to invert diagonal block")
-    
+
         prefix = pc.getOptionsPrefix()
         options_prefix = prefix + self._prefix
 
-        mat_type = PETSc.Options().getString(options_prefix + "mat_type", "aijcusparse") 
+        mat_type = PETSc.Options().getString(options_prefix + "mat_type", "cusparse")  # cuda?
 
-        (a, bcs) = self.form(pc, test, trial) 
-
-        self.P = allocate_matrix(a, bcs=bcs, #eventually change allocate matrix
-                                 form_compiler_parameters=fcp,
-                                 mat_type=mat_type,
-                                 options_prefix=options_prefix)
+        # eventually change allocate_matrix from assembled.py
 
         # Transfer nullspace over
-        Pmat = self.P.petscmat 
+        Pmat = self.P.petscmat
         Pmat.setNullSpace(P.getNullSpace())
         tnullsp = P.getTransposeNullSpace()
         if tnullsp.handle != 0:
             Pmat.setTransposeNullSpace(tnullsp)
         Pmat.setNearNullSpace(P.getNearNullSpace())
 
-        # Internally, we just set up a PC object that the user can configure
-        # however from the PETSc command line.  Since PC allows the user to specify
-        # a KSP, we can do iterative by -assembled_pc_type ksp.
-        #?
+        pc = PETSc.PC().create(comm=outer_pc.comm)
+        pc.incrementTabLevel(1, parent=outer_pc)
 
-#same - matrix here
-        pc = PETSc.PC().create(comm=opc.comm) 
-        pc.incrementTabLevel(1, parent=opc) #
-
-        # We set a DM and an appropriate SNESContext on the constructed PC so one
-        # can do e.g. multigrid or patch solves.
-        dm = opc.getDM()
-        self._ctx_ref = self.new_snes_ctx(opc, a, bcs, mat_type,
+        # We set a DM and an appropriate SNESContext on the constructed PC
+        # so one can do e.g. multigrid or patch solves.
+        dm = outer_pc.getDM()
+        self._ctx_ref = self.new_snes_ctx(outer_pc, a, bcs, mat_type,
                                           fcp=fcp, options_prefix=options_prefix)
 
-        pc.setDM(dm) 
+        pc.setDM(dm)
         pc.setOptionsPrefix(options_prefix)
 
-        #matrix to cuda
-        A_cu = petsc4py.PETSc.Mat()
-        A_cu.createDenseCUDA(A.petscmat.size)
-        A.petscmat.copy(A_cu)
-        A.petscmat = A_cu
-        self._offload_A = A.petscmat #fishy
+        # matrix to cuda
+        P_cu = P.petscmat.convert(mat_type='aijcusparse')
 
-        P_cu = petsc4py.PETSc.Mat()
-        P_cu.createDenseCUDA(A.petscmat.size)
-        Pmat.petscmat.copy(P_cu)
-        Pmat.petscmat = P_cu
-        self._offload_A = Pmat.petscmat #fishy
-
-        pc.setOperators(A_cu, P_cu)
+        pc.setOperators(A, P_cu)
         self.pc = pc
-        with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref, save=False): 
+        with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref, save=False):
             pc.setFromOptions()
 
     def update(self, pc):
-        self._offload_A()
+        _, P = pc.getOperators()
+        _, P_cu = self.pc.getOperators()
+        P.copy(P_cu)
 
- #   def form(self, pc, test, trial):
- #       _, P = pc.getOperators()
- #       if P.getType() == "python":
- #           context = P.getPythonContext()
- #           return (context.a, context.row_bcs)
- #       else:
- #           context = dmhooks.get_appctx(pc.getDM())
- #           return (context.Jp or context.J, context._problem.bcs)
-
-#vectors and solve
-    def apply(self, pc, x, y): #y=b?
+# vectors and solve
+    def apply(self, pc, x, y):  # y=b?
         dm = pc.getDM()
-        with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref), :
-            b_cu = PETSc.Vec() 
-            b_cu.createCUDAWithArrays(y)  
+        with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref):
+            b_cu = PETSc.Vec()
+            b_cu.createCUDAWithArrays(y)
             u = PETSc.Vec()
             u.createCUDAWithArrays(x)
-            self.pc.apply(x, y) #solve is here
-            u.getArray() #give vector back
-
- #   def applyTranspose(self, pc, x, y): #same but other side
- #       dm = pc.getDM()
- #       with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref):
- #           b_cu = PETSc.Vec()
- #           b_cu.createCUDAWithArrays(y)  
- #           u = PETSc.Vec()
- #           u.createCUDAWithArrays(x)
- #           self.pc.applyTranspose(x, y)
+            self.pc.apply(x, y)  # solve is here
+            u.getArray()  # return vector
 
     def view(self, pc, viewer=None):
         super().view(pc, viewer)
