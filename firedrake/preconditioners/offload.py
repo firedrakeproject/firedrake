660dfbe4ce6e54ca459cdf21088296ac7c7c6316
Lilly Schweiger <lilly.schweiger23@imperial.ac.uk>
2024-03-14 17:38:16 +0000

    last changes to offload

--- firedrake/preconditioners/offload.py
+++ firedrake/preconditioners/offload.py
@@ -12,7 +12,7 @@ import petsc4py.PETSc # in firedrake.petsc?
 # outside: ksp.setOperators(A)
 # todo: for densecuda later- now only cusparse
 
-__all__ = ("OffloadPC")
+__all__ = ("OffloadPC",)
 
 
 class OffloadPC(PCBase):
@@ -27,8 +27,6 @@ class OffloadPC(PCBase):
     def initialize(self, pc):
         A, P = pc.getOperators()  # P preconditioner
 
-        if pc.getType() != "assembled":
-            raise ValueError("Expecting PC type assembled")  # correct type?
         outer_pc = pc
         appctx = self.get_appctx(pc)
         fcp = appctx.get("form_compiler_parameters")
@@ -55,15 +53,15 @@ class OffloadPC(PCBase):
 
         mat_type = PETSc.Options().getString(options_prefix + "mat_type", "cusparse")  # cuda?
 
+        # matrix to cuda
+        P_cu = P.convert(mat_type='aijcusparse')
         # eventually change allocate_matrix from assembled.py
 
-        # Transfer nullspace over
-        Pmat = self.P.petscmat
-        Pmat.setNullSpace(P.getNullSpace())
+        P_cu.setNullSpace(P.getNullSpace())
         tnullsp = P.getTransposeNullSpace()
         if tnullsp.handle != 0:
-            Pmat.setTransposeNullSpace(tnullsp)
-        Pmat.setNearNullSpace(P.getNearNullSpace())
+            P_cu.setTransposeNullSpace(tnullsp)
+        P_cu.setNearNullSpace(P.getNearNullSpace())
 
         pc = PETSc.PC().create(comm=outer_pc.comm)
         pc.incrementTabLevel(1, parent=outer_pc)
@@ -71,15 +69,13 @@ class OffloadPC(PCBase):
         # We set a DM and an appropriate SNESContext on the constructed PC
         # so one can do e.g. multigrid or patch solves.
         dm = outer_pc.getDM()
-        self._ctx_ref = self.new_snes_ctx(outer_pc, a, bcs, mat_type,
-                                          fcp=fcp, options_prefix=options_prefix)
+        self._ctx_ref = self.new_snes_ctx(
+            outer_pc, a, bcs, mat_type,
+            fcp=fcp, options_prefix=options_prefix
+        )
 
         pc.setDM(dm)
         pc.setOptionsPrefix(options_prefix)
-
-        # matrix to cuda
-        P_cu = P.petscmat.convert(mat_type='aijcusparse')
-
         pc.setOperators(A, P_cu)
         self.pc = pc
         with dmhooks.add_hooks(dm, self, appctx=self._ctx_ref, save=False):
@@ -89,6 +85,15 @@ class OffloadPC(PCBase):
         _, P = pc.getOperators()
         _, P_cu = self.pc.getOperators()
         P.copy(P_cu)
+
+    def form(self, pc, test, trial):
+        _, P = pc.getOperators()
+        if P.getType() == "python":
+            context = P.getPythonContext()
+            return (context.a, context.row_bcs)
+        else:
+            context = dmhooks.get_appctx(pc.getDM())
+            return (context.Jp or context.J, context._problem.bcs)
 
 # vectors and solve
     def apply(self, pc, x, y):  # y=b?
@@ -101,8 +106,12 @@ class OffloadPC(PCBase):
             self.pc.apply(x, y)  # solve is here
             u.getArray()  # return vector
 
+    def applyTranspose(self, pc, X, Y):
+        raise NotImplementedError
+
     def view(self, pc, viewer=None):
         super().view(pc, viewer)
+        print("viewing PC")
         if hasattr(self, "pc"):
             viewer.printfASCII("PC to solve on GPU\n")
             self.pc.view(viewer)
