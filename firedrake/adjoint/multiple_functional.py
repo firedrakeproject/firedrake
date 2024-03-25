6d2ecad591212c68f3bdb052bf48a25ef7c1a543
Ig-dolci <daia.dolci@gmail.com>
2024-03-25 16:21:24 +0000

    first idea

--- firedrake/adjoint/multiple_functional.py
+++ firedrake/adjoint/multiple_functional.py
@@ -1,18 +1,47 @@
 from pyadjoint import ReducedFunctional
+import numpy as np
+from enum import Enum
+
+import firedrake
+
+
+class OutputGradientType(Enum):
+    NUMPYARRAY = 1
+    FUNCTION = 2
+
 
 class EnsembleReducedFunctional(ReducedFunctional):
-    def __init__(self, J, control, ensemble):
-        super().__init__(J, control, ...)
+    def __init__(self, J, control, ensemble,
+                 output_gradient_type=OutputGradientType.FUNCTION):
+        super().__init__(J, control)
         self.ensemble = ensemble
-    
+        self.output_gradient_type = output_gradient_type
+
     def __call__(self, values):
-        J_local = super().__call__(values)
-        # Be careful with the __call__ output type
-        J_total = self.ensemble.ensemble_comm.allreduce(J_local)
-        return J_total
-    
-    def derivative(self):
-        dJ_local = super().derivative(...)
-        # define the dJ_total correctly
-        self.ensemble.reduce(dJ_local, dJ_total)
-        return dJ_total
+        local_functional = None
+        if isinstance(values, np.ndarray):
+            updated_control = [control.copy_data() for control in self.controls]
+            for i, control in enumerate(self.controls):
+                updated_control[i], offset = control.assign_numpy(updated_control[i], values, offset)
+            local_functional = self.__call__(updated_control)
+        else:
+            raise NotImplementedError("This type of control is not supported.")
+        if isinstance(local_functional, float):
+            return self.ensemble.ensemble_comm.allreduce(np.array([local_functional]))[0]
+        else:
+            raise NotImplementedError("This type of functional is not supported.")
+
+    def derivative(self, adj_input=1.0, options=None):
+        dJdm_local = super().derivative(adj_input=adj_input, options=options)
+        if isinstance(dJdm_local, firedrake.Function):
+            dJdm_total = dJdm_local.copy(deepcopy=True)
+            dJdm_total = self.ensemble.allreduce(dJdm_local, dJdm_total)
+        else:
+            raise NotImplementedError("This type of gradient is not supported.")
+        
+        if self.output_gradient_type == OutputGradientType.FUNCTION:
+            return dJdm_total
+        elif self.output_gradient_type == OutputGradientType.NUMPYARRAY:
+            return dJdm_total.dat.data_wo[:]
+        else:
+            raise NotImplementedError("This output gradient type is not yet supported.")
