04edd4c6a7e4828f0a3796aeea609338bf01770b
Rob Kirby <robert.c.kirby@gmail.com>
2024-04-04 12:44:19 -0500

    Add bc's to projection

--- tests/macro/test_macro_interp_project.py
+++ tests/macro/test_macro_interp_project.py
@@ -14,13 +14,24 @@ from firedrake import *
 
 # compute h1 projection of f into u's function
 # space, store the result in u.
-def h1_proj(u, f):
+def h1_proj(u, f, bcs=None):
     v = TestFunction(u.function_space())
     F = (inner(grad(u-f), grad(v)) * dx
          + inner(u-f, v) * dx)
-    solve(F == 0, u, solver_parameters={"snes_type": "ksponly",
-                                        "ksp_type": "preonly",
-                                        "pc_type": "lu"})
+    solve(F == 0, u,
+          bcs=bcs,
+          solver_parameters={"snes_type": "ksponly",
+                             "ksp_type": "preonly",
+                             "pc_type": "lu"})
+
+
+def h1_proj_bc(u, f):
+    h1_proj(u, f, bcs=DirichletBC(u.function_space(),
+                                  f, "on_boundary"))
+
+
+def proj_bc(u, f):
+    u.project(f, bcs=DirichletBC(u.function_space(), f, "on_boundary"))
 
 
 def do_op(n, op, deg, variant):
@@ -42,7 +53,7 @@ def do_op(n, op, deg, variant):
 
 @pytest.mark.parametrize('op', (lambda x, y: x.interpolate(y),
                                 lambda x, y: x.project(y),
-                                h1_proj))
+                                proj_bc, h1_proj, h1_proj_bc))
 @pytest.mark.parametrize(('deg', 'variant', 'convrate'),
                          [(2, None, 2.7),
                           (2, 'alfeld', 2.8),
@@ -51,6 +62,5 @@ def do_op(n, op, deg, variant):
 def test_firedrake_projection_scalar_convergence(op, deg, variant, convrate):
     diff = np.array([do_op(i, op, deg, variant) for i in range(3, 5)])
     conv = np.log2(diff[:-1] / diff[1:])
-    print(np.array(conv))
     # test *eventual* convergence rate
     assert conv[-1] > convrate
