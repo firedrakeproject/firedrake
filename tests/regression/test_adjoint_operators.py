1b06a7d17e827586c9479a83d7808f76d6d6cc3a
Iglesia Dolci <daia.dolci@gmail.com>
2024-05-02 16:38:43 +0100

    testing

--- tests/regression/test_adjoint_operators.py
+++ tests/regression/test_adjoint_operators.py
@@ -886,3 +886,17 @@ def test_cofunction_subfunctions_with_adjoint():
     k.block_variable.tlm_value = Constant(1)
     get_working_tape().evaluate_tlm()
     assert taylor_test(J_hat, k, Constant(1.0), dJdm=J.block_variable.tlm_value) > 1.9
+
+@pytest.mark.skipcomplex  # Taping for complex-valued 0-forms not yet done
+def test_reduce_functional_derivative_space():
+    mesh = UnitIntervalMesh(1)
+    X = SpatialCoordinate(mesh)
+    space = FunctionSpace(mesh, "Lagrange", 1)
+    test = TestFunction(space)
+    u = Function(space).interpolate(X[0])
+    J = assemble((u ** 2) * dx)
+    rf = ReducedFunctional(J, Control(u))
+    assert isinstance(rf.derivative(), Function)
+    assert isinstance(rf.derivative(options={"derivative_space": "dual"}), Cofunction)
+
+
