851567a2a1bebc22bdb89310bb4b726e503a725a
Iglesia Dolci <daia.dolci@gmail.com>
2024-05-07 11:09:18 +0100

    Add checkpoint to clear

--- firedrake/adjoint_utils/function.py
+++ firedrake/adjoint_utils/function.py
@@ -63,15 +63,15 @@ class FunctionMixin(FloatingType):
                 output = subfunctions(self, *args, **kwargs)
 
             if annotate:
-                output = tuple(type(self)(output[i].function_space(),
-                                          output[i],
-                                          block_class=SubfunctionBlock,
-                                          _ad_floating_active=True,
-                                          _ad_args=[self, i],
-                                          _ad_output_args=[i],
-                                          output_block_class=FunctionMergeBlock,
-                                          _ad_outputs=[self],
-                                          ad_block_tag=ad_block_tag)
+                output = tuple(firedrake.Function(output[i].function_space(),
+                                                  output[i],
+                                                  block_class=SubfunctionBlock,
+                                                  _ad_floating_active=True,
+                                                  _ad_args=[self, i],
+                                                  _ad_output_args=[i],
+                                                  output_block_class=FunctionMergeBlock,
+                                                  _ad_outputs=[self],
+                                                  ad_block_tag=ad_block_tag)
                                for i in range(len(output)))
             return output
         return wrapper
@@ -277,6 +277,18 @@ class FunctionMixin(FloatingType):
     def _ad_convert_type(self, value, options=None):
         # `_ad_convert_type` is not annoated unlike to `_ad_convert_riesz`
         return self._ad_convert_riesz(value, options=options)
+
+    def _ad_checkpoint_to_clear(self, to_keep=None):
+        if to_keep:
+            for bv in to_keep:
+                if isinstance(self, type(bv.output)):
+                    checkpoint = bv._checkpoint
+                    while isinstance(checkpoint, DelegatedFunctionCheckpoint):
+                        checkpoint = checkpoint._ad_checkpoint_to_clear()
+                    if self == checkpoint:
+                        # keep this checkpoint, since it is delegated.
+                        return None
+        return self
 
     def _ad_restore_at_checkpoint(self, checkpoint):
         if isinstance(checkpoint, CheckpointBase):
