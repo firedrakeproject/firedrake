7ca0885a30493e15e1eec47369e0a67dd12bd9c4
Iglesia Dolci <daia.dolci@gmail.com>
2024-05-22 17:57:29 +0100

    get derivative instead gradient in fem_operotor

--- firedrake/ml/pytorch/fem_operator.py
+++ firedrake/ml/pytorch/fem_operator.py
@@ -81,9 +81,8 @@ class FiredrakeTorchOperator(torch.autograd.Function):
         if isinstance(adj_input, Constant) and adj_input.ufl_shape == ():
             # This will later on result in an `AdjFloat` adjoint input instead of a Constant
             adj_input = float(adj_input)
-
         # Compute adjoint model of `F`: delegated to pyadjoint.ReducedFunctional
-        adj_output = F.derivative(adj_input=adj_input)
+        adj_output = F.derivative(adj_input=adj_input, options={"riesz_representation": None})
 
         # Tuplify adjoint output
         adj_output = (adj_output,) if not isinstance(adj_output, collections.abc.Sequence) else adj_output
