43f9d5fe44c0342ecbe670597d7987c3f1aa5bce
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2024-05-31 18:12:31 +0100

    Incorperate TinyASM into Firedrake

---
+++ tinyasm/matinvert.cpp
@@ -0,0 +1,13 @@
+#include <petsc.h>
+#include <petscblaslapack.h>
+
+#include <numeric>
+#include <vector>
+using namespace std;
+
+PetscErrorCode mymatinvert(PetscInt* n, PetscScalar* mat, PetscInt* piv, PetscInt* info, PetscScalar* work) {
+    PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(n,n,mat,n,piv,info));
+    PetscCallBLAS("LAPACKgetri",LAPACKgetri_(n,mat, n, piv,work,n,info));
+    return 0;
+}
+
