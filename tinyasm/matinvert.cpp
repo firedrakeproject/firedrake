f77091f266bf0580619af86f71cb455edd0041fb
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-06-04 11:00:25 +0100

    Update tinyasm/matinvert.cpp

--- tinyasm/matinvert.cpp
+++ tinyasm/matinvert.cpp
@@ -1,9 +1,6 @@
 #include <petsc.h>
 #include <petscblaslapack.h>
 
-#include <numeric>
-#include <vector>
-using namespace std;
 
 PetscErrorCode mymatinvert(PetscInt* n, PetscScalar* mat, PetscInt* piv, PetscInt* info, PetscScalar* work) {
     PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(n,n,mat,n,piv,info));
