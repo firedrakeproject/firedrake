71060eb16b3af5e1ed775fbf44d17a0f27b0084f
ksagiyam <k.sagiyama@imperial.ac.uk>
2024-06-16 22:13:16 +0100

    benchmark

--- benchmark.py
+++ benchmark.py
@@ -643,11 +643,16 @@ elif case in ["FSI1_2", "FSI2_2", "FSI3_2"]:
             #fname_checkpoint = f"dumbdata/fsi3_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S"
             #fname_FD = f"time_series_FD_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S.dat"
             #fname_FL = f"time_series_FL_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S.dat"
-            fname_checkpoint = f"dumbdata/fsi3_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparam1_S"
-            fname_FD = f"time_series_FD_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparam1_S.dat"
-            fname_FL = f"time_series_FL_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparam1_S.dat"
-            fname_ux = f"time_series_ux_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparam1_S.dat"
-            fname_uy = f"time_series_uy_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparam1_S.dat"
+            fname_checkpoint = f"dumbdata/fsi3_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparamvar1_S"
+            fname_FD = f"time_series_FD_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparamvar1_S.dat"
+            fname_FL = f"time_series_FL_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparamvar1_S.dat"
+            fname_ux = f"time_series_ux_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparamvar1_S.dat"
+            fname_uy = f"time_series_uy_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_{slip_bc}_ALEparamvar1_S.dat"
+            fname_checkpoint = f"dumbdata/fsi3_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S"
+            fname_FD = f"time_series_FD_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S.dat"
+            fname_FL = f"time_series_FL_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S.dat"
+            fname_ux = f"time_series_ux_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S.dat"
+            fname_uy = f"time_series_uy_Q4_Q3_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamvar1_S.dat"
         else:
             fname_checkpoint = f"dumbdata/fsi3_P4_P2_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamtest1"
             fname_FD = f"time_series_FD_P4_P2_nref{nref}_0.002_shift{CNshift}_{elast}_{linear_elast}_ALEparamtest1.dat"
@@ -683,8 +688,8 @@ elif case in ["FSI1_2", "FSI2_2", "FSI3_2"]:
     lambda_s = nu_s * E_s / (1 + nu_s) / (1 - 2 * nu_s)
     # ALE constants
     decay_r = Constant(0.5)  # decay ratio
-    #nu_ale = Constant(0.49) * (1. - decay_r * abs(x_f - pointA[0]) / (L - pointA[0])) * (1. - decay_r * abs(y_f - pointA[1]) / (L - pointA[0]))
-    nu_ale = Constant(0.49)
+    nu_ale = Constant(0.49) * (1. - decay_r * abs(x_f - pointA[0]) / (L - pointA[0])) * (1. - decay_r * abs(y_f - pointA[1]) / (L - pointA[0]))
+    #nu_ale = Constant(0.49)
     mu_ale = Constant(float(mu_s))
     #mu_ale = Constant(4 * float(mu_s))
     E_ale = mu_ale * 2 * (1 + nu_ale)
@@ -895,9 +900,9 @@ elif case in ["FSI1_2", "FSI2_2", "FSI3_2"]:
     print("num cells = ", mesh.comm.allreduce(mesh.cell_set.size), flush=True)
     print("num DoFs = ", V.dim(), flush=True)
     if mesh.comm.rank == 0:
-        print(f"nu_ale = {float(nu_ale)}")
+        #print(f"nu_ale = {float(nu_ale)}")
         print(f"mu_ale = {float(mu_ale)}")
-        print(f"labmda_ale = {float(lambda_ale)}")
+        #print(f"labmda_ale = {float(lambda_ale)}")
     #v_f_ = solution.subfunctions[0]
     F_f_, J_f_, _, _ = compute_elast_tensors(dim, u_f, lambda_s, mu_s)
     sigma_f_ = - p * Identity(dim) + rho_f * nu_f * 2 * sym(dot(grad(v_f), inv(F_f_)))
@@ -906,11 +911,12 @@ elif case in ["FSI1_2", "FSI2_2", "FSI3_2"]:
             steps, indices = chk.get_timesteps()
             t.assign(steps[-1])
             iplot = indices[-1]
-            if False:
+            if True:
                 iplot = 0
                 while True:
                     #if abs(steps[iplot] - 3.96) < 1.e-6:
-                    if abs(steps[iplot] - 3.75) < 1.e-6:
+                    #if abs(steps[iplot] - 3.75) < 1.e-6:
+                    if abs(steps[iplot] - 3.76) < 1.e-6:
                         t.assign(steps[iplot])
                         break
                     else:
@@ -931,7 +937,7 @@ elif case in ["FSI1_2", "FSI2_2", "FSI3_2"]:
                  outfile.write("t val" + "\n")
             with open(fname_uy, 'w') as outfile:
                  outfile.write("t val" + "\n")
-    if False:
+    if True:
         coords = mesh_f.coordinates.dat.data_with_halos
         uplot = solution.subfunctions[2]
         uplot = Function(V_0).project(solution.subfunctions[2])
