95ce2649ee99694d36db623a9df0a169172ee3b1
Iglesia Dolci <daia.dolci@gmail.com>
2024-07-06 21:07:35 +0100

    wip

--- firedrake/adjoint_utils/function.py
+++ firedrake/adjoint_utils/function.py
@@ -293,18 +293,6 @@ class FunctionMixin(FloatingType):
         else:
             return checkpoint
 
-    def _ad_checkpoint_to_clear(self, to_keep=None):
-        if to_keep:
-            for bv in to_keep:
-                if isinstance(self, type(bv.output)):
-                    checkpoint = bv._checkpoint
-                    while isinstance(checkpoint, DelegatedFunctionCheckpoint):
-                        checkpoint = checkpoint._ad_checkpoint_to_clear()
-                    if self == checkpoint:
-                        # keep this checkpoint, since it is delegated.
-                        return None
-        return self
-
     def _ad_restore_at_checkpoint(self, checkpoint):
         if isinstance(checkpoint, CheckpointBase):
             return checkpoint.restore()
