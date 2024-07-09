09dd0fba4e833e7f6436e20b464af469fe5eadd9
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2024-07-09 16:21:22 +0100

    No need to convert piv

--- tinyasm/matinvert.cpp
+++ tinyasm/matinvert.cpp
@@ -1,14 +1,29 @@
 #include <petsc.h>
 #include <petscblaslapack.h>
+#include <assert.h>
 
 
 PetscErrorCode mymatinvert(PetscInt* n, PetscScalar* mat, PetscInt* piv, PetscInt* info, PetscScalar* work) {
-    PetscBLASInt n_blas, piv_blas, info_blas;
+    // Declare BLAS compatible variables
+    PetscBLASInt n_blas, info_blas;
+    PetscBLASInt *piv_blas;
+
+    // Convert function arguments to BLAS compatible ints
     PetscCall(PetscBLASIntCast(*n, &n_blas));
-    PetscCall(PetscBLASIntCast(*piv, &piv_blas));
+    // piv is OUT for getrf and IN for getri so we don't need to check
+    // whether the cast is safe
+    piv_blas = (PetscBLASInt*) piv;
     PetscCall(PetscBLASIntCast(*info, &info_blas));
-    PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(&n_blas, &n_blas, mat, &n_blas, &piv_blas, &info_blas));
-    PetscCallBLAS("LAPACKgetri",LAPACKgetri_(&n_blas, mat, &n_blas, &piv_blas, work, &n_blas, &info_blas));
+
+    assert(*n == n_blas);
+
+    //~ printf("n %" PetscInt_FMT " \n", *n);
+    //~ printf("n_blas %" PetscBLASInt_FMT " \n", n_blas);
+    //~ printf("piv %" PetscInt_FMT " \n", *piv);
+    //~ printf("piv_blas %" PetscBLASInt_FMT " \n", piv_blas);
+
+    PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(&n_blas, &n_blas, mat, &n_blas, piv_blas, &info_blas));
+    PetscCallBLAS("LAPACKgetri",LAPACKgetri_(&n_blas, mat, &n_blas, piv_blas, work, &n_blas, &info_blas));
     return 0;
 }
 
