5bb6c18d279f3e72793efa028f230346d62c5c08
Jack Betteridge <J.Betteridge@imperial.ac.uk>
2024-07-09 14:38:13 +0100

    Pointers

--- tinyasm/matinvert.cpp
+++ tinyasm/matinvert.cpp
@@ -3,12 +3,12 @@
 
 
 PetscErrorCode mymatinvert(PetscInt* n, PetscScalar* mat, PetscInt* piv, PetscInt* info, PetscScalar* work) {
-    PetscBLASInt *n_blas, *piv_blas, *info_blas;
-    PetscCall(PetscBLASIntCast(*n, n_blas));
-    PetscCall(PetscBLASIntCast(*piv, piv_blas));
-    PetscCall(PetscBLASIntCast(*info, info_blas));
-    PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(n_blas, n_blas, mat, n_blas, piv_blas, info_blas));
-    PetscCallBLAS("LAPACKgetri",LAPACKgetri_(n_blas, mat, n_blas, piv_blas, work, n_blas, info_blas));
+    PetscBLASInt n_blas, piv_blas, info_blas;
+    PetscCall(PetscBLASIntCast(*n, &n_blas));
+    PetscCall(PetscBLASIntCast(*piv, &piv_blas));
+    PetscCall(PetscBLASIntCast(*info, &info_blas));
+    PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(&n_blas, &n_blas, mat, &n_blas, &piv_blas, &info_blas));
+    PetscCallBLAS("LAPACKgetri",LAPACKgetri_(&n_blas, mat, &n_blas, &piv_blas, work, &n_blas, &info_blas));
     return 0;
 }
 
