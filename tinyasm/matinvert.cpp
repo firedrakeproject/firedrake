13d570d90b47535dee1e1a110acbf6628037715a
Jack Betteridge <43041811+JDBetteridge@users.noreply.github.com>
2024-07-16 15:14:54 +0100

    Fix some TinyASM types (#3665)
    * Fix some TinyASM types to work with int64 indices in PETSc

--- tinyasm/matinvert.cpp
+++ tinyasm/matinvert.cpp
@@ -1,10 +1,11 @@
 #include <petsc.h>
 #include <petscblaslapack.h>
 
-
-PetscErrorCode mymatinvert(PetscInt* n, PetscScalar* mat, PetscInt* piv, PetscInt* info, PetscScalar* work) {
-    PetscCallBLAS("LAPACKgetrf",LAPACKgetrf_(n,n,mat,n,piv,info));
-    PetscCallBLAS("LAPACKgetri",LAPACKgetri_(n,mat, n, piv,work,n,info));
-    return 0;
+PetscErrorCode mymatinvert(PetscBLASInt* n, PetscScalar* mat, PetscBLASInt* piv, PetscBLASInt* info, PetscScalar* work) {
+    PetscCallBLAS("LAPACKgetrf", LAPACKgetrf_(n, n, mat, n, piv, info));
+    PetscCheck(!(*info), PETSC_COMM_SELF, PETSC_ERR_LIB, "TinyASM error calling ?getrf in mymatinvert");
+    PetscCallBLAS("LAPACKgetri", LAPACKgetri_(n, mat, n, piv, work, n, info));
+    PetscCheck(!(*info), PETSC_COMM_SELF, PETSC_ERR_LIB, "TinyASM error calling ?getri in mymatinvert");
+    return PETSC_SUCCESS;
 }
 
