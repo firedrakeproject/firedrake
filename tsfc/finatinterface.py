162bde1703675105176d1ba0133e0c6825b286bb
Rob Kirby <robert.c.kirby@gmail.com>
2024-07-19 12:27:33 -0500

    add PS12 to finat interface

--- tsfc/finatinterface.py
+++ tsfc/finatinterface.py
@@ -54,6 +54,7 @@ supported_elements = {
     "Argyris": finat.Argyris,
     "Hsieh-Clough-Tocher": finat.HsiehCloughTocher,
     "QuadraticPowellSabin6": finat.QuadraticPowellSabin6,
+    "QuadraticPowellSabin12": finat.QuadraticPowellSabin12,
     "Reduced-Hsieh-Clough-Tocher": finat.ReducedHsiehCloughTocher,
     "Mardal-Tai-Winther": finat.MardalTaiWinther,
     "Morley": finat.Morley,
