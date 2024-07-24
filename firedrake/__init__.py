306792a79c92a31da7537f11b4a9d72ca63b364e
Jack Betteridge <43041811+JDBetteridge@users.noreply.github.com>
2024-07-24 14:05:57 +0100

    Namespace FML (#3701)
    * Namespace FML
    
    * Update the FML regression test

--- firedrake/__init__.py
+++ firedrake/__init__.py
@@ -119,7 +119,6 @@ from firedrake.ensemble import *
 from firedrake.randomfunctiongen import *
 from firedrake.external_operators import *
 from firedrake.progress_bar import ProgressBar  # noqa: F401
-from firedrake.fml import *
 
 from firedrake.logging import *
 # Set default log level
