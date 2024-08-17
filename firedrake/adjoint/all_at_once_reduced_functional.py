7315a4e1989dd8d6a4b78602df5af103399c7f49
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2024-08-17 15:29:43 +0100

    aaorf: separate error calculations and inner products for observations and background

--- firedrake/adjoint/all_at_once_reduced_functional.py
+++ firedrake/adjoint/all_at_once_reduced_functional.py
@@ -1,60 +1,102 @@
 from pyadjoint import ReducedFunctional, stop_annotating, Control, \
     overloaded_type
 from pyadjoint.enlisting import Enlist
-from firedrake import norm
+from firedrake import assemble, inner, dx
+
+__all__ = ['AllAtOnceReducedFunctional']
+
+
+def l2prod(x):
+    return assemble(inner(x, x)*dx)
 
 
 class AllAtOnceReducedFunctional(ReducedFunctional):
 
-    def __init__(self, control: Control, background_fn=None, observation_fn=None, weak_constraint=True):
+    def __init__(self, control: Control, background_iprod=None,
+                 observation_err=None, observation_iprod=None,
+                 weak_constraint=True):
         """
         :arg control: initial guess at initial condition of timeseries (background)
-        :arg background_fn: function that takes in the initial conditions and returns the background functional. Only required if using strong constraint.
-        :arg observation_fn: function that takes in a state and returns the observation functional at time 0. Optional.
-        :arg weak_constraint: whether to use the weak or strong constraint 4DVar formulation
+        :arg background_iprod: Inner product callable for background error (possibly including error covariance).
+        :arg observation_err: callable mapping state to the initial observation error. Optional.
+        :arg observation_iprod: Inner product callable for initial observation error (possibly including error covariance). Optional.
+        :arg weak_constraint: whether to use the weak or strong constraint 4DVar formulation.
         """
         self.weak_constraint = weak_constraint
-        self.initial_observations = observation_fn is not None
+        self.initial_observations = observation_err is not None
+
+        # default to l2 inner products for all functionals
+        background_iprod = background_iprod or l2prod
+        observation_iprod = observation_iprod or l2prod
+
+        # We need a copy for the prior, but this shouldn't be part of the tape
+        with stop_annotating():
+            self.background = control.copy_data()
+
         if self.weak_constraint:
-            self.controls = [control.block_variable]  # The solution at the beginning of each time-chunk
-            self.states = [control.block_variable]    # The model propogation at the end of each time-chunk
-            self.forward_model_stages = []            # ReducedFunctional for each model propogation
-            self.observations = []                    # ReducedFunctional for each observation set
+            self.background_iprod = background_iprod  # Inner product for background error (possibly including error covariance)
+            self.controls = [control]                 # The solution at the beginning of each time-chunk
+            self.states = []                          # The model propogation at the end of each time-chunk
+            self.forward_model_stages = []            # ReducedFunctional for each model propogation (returns state)
+            self.forward_model_iprods = []            # Inner product for model errors (possibly including error covariance)
+            self.observations = []                    # ReducedFunctional for each observation set (returns observation error)
+            self.observation_iprods = []              # Inner product for observation errors (possibly including error covariance)
+
             if self.initial_observations:
-                self.observations.append(ReducedFunctional(observation_fn(control), control))
+                self.observations.append(ReducedFunctional(observation_err(control), control))
+                self.observation_iprods.append(observation_iprod)
+
         else:
-            self.controls = Enlist(control)  # initial conditions guess to be updated
+            # initial conditions guess to be updated
+            self.controls = Enlist(control)
 
             # Strong constraint functional to be converted to ReducedFunctional later
-            self.functional = background_fn(control.control)  # penalty for straying from prior
-            if self.initial_observations:
-                self.functional += observation_fn(control.control)  # penalty for not hitting observations
 
-    def set_observation(self, state: overloaded_type, observation_fn):
+            # penalty for straying from prior
+            self.functional = background_iprod(control.control - self.background)
+
+            # penalty for not hitting observations at initial time
+            if self.initial_observations:
+                self.functional += observation_iprod(observation_err(control.control))
+
+    def set_observation(self, state: overloaded_type, observation_err,
+                        observation_iprod=None, forward_model_iprod=None):
         """
         :arg state: state at current observation time.
-        :arg observation_fn: function that takes in a state and returns the observation functional at the current time.
+        :arg observation_err: function that takes in a state and returns the observation error at the current time.
         """
+        observation_iprod = observation_iprod or l2prod
         if self.weak_constraint:
+            forward_model_iprod = forward_model_iprod or l2prod
+
             self.states.append(state.block_variable)
+            self.forward_model_iprods.append(forward_model_iprod)
+
+            # Cut the tape into seperate time-chunks.
+            # State is output from previous control i.e. forward model
+            # propogation over previous time-chunk.
             with stop_annotating(modifies=state):
                 self.forward_model_stages.append(
                     ReducedFunctional(state,
-                                      controls=Control(self.controls[-1]))
+                                      controls=self.controls[-1])
                 )
-            self.controls.append(state.block_variable)
-            # should the control for the observations be the state at the end of the previous time-chunk
-            # or the state at the beginning of the next time-chunk? These won't be equal once we start
-            # the optimisation iterations.
+            # Beginning of next time-chunk is the control for this observation
+            # and the state at the end of the next time-chunk.
+            next_control = Control(state)
+            self.controls.append(next_control)
+
+            # Observations after tape cut because this is now a control, not a state
             self.observations.append(
-                ReducedFunctional(observation_fn(state), Control(state))
+                ReducedFunctional(observation_err(state), next_control)
             )
+            self.observation_iprods.append(observation_iprod)
 
         else:
+
             if hasattr(self, "strong_reduced_functional"):
                 msg = "Cannot add observations once strong constraint ReducedFunctional instantiated"
                 raise ValueError(msg)
-            self.functional += observation_fn(state)
+            self.functional += observation_iprod(observation_err(state))
 
     def setup_strong_constraint(self):
         if not hasattr(self, "strong_reduced_functional"):
@@ -62,30 +104,50 @@ class AllAtOnceReducedFunctional(ReducedFunctional):
                 self.functional, self.controls)
 
     def __call__(self, control_value):
-        if self.weak_constraint:
-            J = 0.0
-            # Assumes control_value is iterable over observation stages.
-            for i in range(len(self.forward_model_stages)+1):  # +1 for the initial time
-                if i == 0:
-                    # background 'error' - how far from the prior ic?
-                    J += norm(control_value[0] - self.controls[0])
-                    # observation error - do we match the 'real world'?
-                    if self.initial_observations:
-                        J += norm(self.observations[0](control_value[0]))
-                else:
-                    # model error - does propogation from last control match this control?
-                    stage = self.forward_model_stages[i-1]
-                    end_state = stage(control_value[i-1])
-                    J += norm(control_value[i] - end_state)
+        # update controls so derivative etc is evaluated at correct point
+        for old, new in zip(self.controls, control_value):
+            old.update(new)
 
-                    # observation error - do we match the 'real world'?
-                    if self.initial_observations:
-                        J += norm(self.observations[i](control_value[i]))
-                    else:
-                        J += norm(self.observations[i-1](control_value[i]))
+        if self.weak_constraint:
+
+            controls = self.controls
+
+            # Shift lists so indexing matches standard nomenclature:
+            # index 0 is initial condition. Model i propogates from i-1 to i.
+
+            forward_models = [None, *self.forward_model_stages]
+            model_iprods = [None, *self.forward_model_iprods]
+
+            observations = (self.observations if self.initial_observations
+                            else [None, *self.observations])
+            observation_iprods = (self.observation_iprods if self.initial_observations
+                                  else [None, *self.observation_iprods])
+
+            # Initial condition functionals
+            J = self.background_iprod(controls[0].control - self.background)
+
+            if self.initial_observations:
+                J += observation_iprods[0](observations[0](controls[0]))
+
+            for i in range(1, len(forward_models)):
+                # Propogate forward over previous time-chunk
+                end_state = forward_models[i](controls[i-1])
+
+                # Cache end state here so we can reuse it in other functions
+                self.states[i-1] = end_state.block_variable
+
+                # Model error - does propogation from last control match this control?
+                model_err = end_state - controls[i].control
+                J += model_iprods[i](model_err)
+
+                # observation error - do we match the 'real world'?
+                obs_err = observations[i](controls[i])
+                J += observation_iprods[i](obs_err)
+
             return J
 
         else:
+
             self.setup_strong_constraint()
             return self.strong_reduced_functional(control_value)
 
