806b9bea5dfe3762ab1659596cf844540ccc7f9a
Connor <c.ward20@imperial.ac.uk>
2024-10-18 11:20:18 +0100

    cleanup test

--- tests/regression/test_locate_cell.py
+++ tests/regression/test_locate_cell.py
@@ -45,12 +45,38 @@ def test_locate_cell_not_found(meshdata):
 
 @pytest.mark.parallel(2)
 def test_owned_cells_preferred_over_ghost_cells():
+    """Test that points are preferentially allocated into owned cells.
+
+    Specifically this test takes the partitioned global mesh
+
+           p0   |   p1
+        o-----o | -----o
+                |
+
+    and tries to locate a point (x) onto it
+
+           p0   |   p1
+        o-----o | -----o
+                |  x
+
+    Since points should be allocated preferentially to owned cells, both
+    ranks (p0 and p1) should claim that x lives in their owned cell, which
+    is always numbered 0.
+
+    This behaviour is important to ensure that points do not get lost between
+    processes, where p0 would believe p1 to own the point and p1 believe that
+    it is owned by p0 (e.g. https://github.com/firedrakeproject/firedrake/issues/3151).
+
+    """
     mesh = UnitIntervalMesh(2)
     mesh.init()
 
+    # each process owns one cell and sees both
     assert mesh.cell_set.size == 1
     assert mesh.cell_set.total_size == 2
 
-    coord = (0.6,)
-    cell = mesh.locate_cell(coord, tolerance=0.5)
-    assert cell == 0
+    # points near the boundary should be claimed by both cells
+    coords = [(0.4,), (0.6,)]
+    for coord in coords:
+        cell = mesh.locate_cell(coord, tolerance=0.5)
+        assert cell == 0
