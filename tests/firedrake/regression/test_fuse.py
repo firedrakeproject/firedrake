dbf22d4998405837b13b65e68167c18b2ebb5569
India Marsden <imm1117@ic.ac.uk>
2025-01-22 10:58:12 +0000

    Add small subset of tests for easy temporary fuse testing

--- tests/firedrake/regression/test_fuse.py
+++ tests/firedrake/regression/test_fuse.py
@@ -13,9 +13,12 @@ import numpy as np
 def test_poisson_analytic(params, degree, quadrilateral):
     assert (run_test(2, degree, parameters=params, quadrilateral=False) < 1.e-9)
 
-def test_helmholtz():
-    diff = np.array([helmholtz(i)[0] for i in range(3, 6)])
+@pytest.mark.parametrize(['conv_num', 'degree'],
+                         [(p, d)
+                          for p,d in zip([1.8, 2.8, 3.8],[1, 2, 3])])
+def test_helmholtz(conv_num, degree):
+    diff = np.array([helmholtz(i, degree=degree)[0] for i in range(3, 6)])
     print("l2 error norms:", diff)
     conv = np.log2(diff[:-1] / diff[1:])
     print("convergence order:", conv)
-    assert (np.array(conv) > 2.8).all()
+    assert (np.array(conv) > conv_num).all()
