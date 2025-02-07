21db7fff709c1dc5009b395861e398a9930f436c
Pablo Brubeck <brubeck@protonmail.com>
2025-02-07 14:17:46 +0000

    Update firedrake/preconditioners/base.py

--- firedrake/preconditioners/base.py
+++ firedrake/preconditioners/base.py
@@ -110,7 +110,27 @@ class PCSNESBase(object, metaclass=abc.ABCMeta):
 
     @staticmethod
     def new_snes_ctx(pc, op, bcs, mat_type, fcp=None, options_prefix=None, pre_apply_bcs=True):
-        """ Create a new SNES contex for nested preconditioning
+        """Create a new `_SNESContext` for nested (linear) preconditioning
+        
+        Parameters
+        ----------
+        pc : PETSc.PC
+             The PC object.
+        op : ufl.BaseForm
+             A bilinear form.
+        bcs : DirichletBC[]
+             The boundary conditions.
+        mat_type : str
+             The matrix type for the assembly of ``op``.
+        options_prefix : str
+             The PETSc options prefix for the new `_SNESContext`.
+        pre_apply_bcs : bool
+             If ``True``, the ``bcs`` are pre applied on the solution before the solve, 
+             otherwise the residual of the ``bcs`` is included in the linear system.
+        
+        Returns
+        -------
+        A new `_SNESContext`
         """
         from firedrake.variational_solver import LinearVariationalProblem
         from firedrake.function import Function
