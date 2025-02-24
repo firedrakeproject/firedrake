ee02ea808cf0dfa2e4eca768c705daed2e72cf87
Connor Ward <c.ward20@imperial.ac.uk>
2025-02-24 15:07:39 -0500

    Add demo (remove later)

---
+++ demo.py
@@ -0,0 +1,24 @@
+from firedrake import *
+
+
+# plex = PETSc.DMPlex().createBoxMesh(
+#     (2, 2, 2),
+#     lower=(0, 0, 0),
+#     upper=(1, 1, 1),
+#     simplex=False,
+#     periodic=True,
+#     interpolate=True,
+#     sparseLocalize=False,
+# )
+# mesh = Mesh(plex)
+
+mesh = UnitSquareMesh(2, 2)
+
+V = FunctionSpace(mesh, "DG", 1)
+
+x, _ = SpatialCoordinate(mesh)
+
+assert np.allclose(assemble(1 * dx(domain=mesh)), 1)
+assert np.allclose(assemble(x * dx(domain=mesh)), 0.5)
+
+print("Success! Yay!")
