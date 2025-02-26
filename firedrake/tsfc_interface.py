ac9a9710203db6f1620c4d4c89c3b281635830f8
India Marsden <imm1117@ic.ac.uk>
2025-02-26 11:25:17 +0000

    clean up

--- firedrake/tsfc_interface.py
+++ firedrake/tsfc_interface.py
@@ -216,7 +216,6 @@ def compile_form(form, name, parameters=None, split=True, interface=None, diagon
             numbering[c] for c in extract_firedrake_constants(f)
         )
         prefix = name + "".join(map(str, (i for i in idx if i is not None)))
-        breakpoint()
         tsfc_kernel = TSFCKernel(
             f,
             prefix,
