f10b66aed1f20325af368b82e8d0052f34868edf
India Marsden <imm1117@ic.ac.uk>
2025-02-27 13:42:05 +0000

    Fix small issue in interpolation

--- firedrake/interpolation.py
+++ firedrake/interpolation.py
@@ -1281,7 +1281,9 @@ def rebuild_dg(element, expr, rt_var_name):
     #             weights.append(w)
     # assert len(weights) == num_points
     # but for now we just fix the values to what we know works:
-    if element.degree != 0 or not isinstance(element.cell, FIAT.reference_element.Point):
+    
+    # not isinstance(element.cell, FIAT.reference_element.Point)
+    if element.degree != 0 or element.cell.get_dimension() != 0:
         raise NotImplementedError("Cross mesh interpolation only implemented for P0DG on vertex cells.")
     num_points = 1
     weights = [1.]*num_points
