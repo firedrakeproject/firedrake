e265f0a77ef12124d2f9558324343eb96d99e1d1
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2025-03-21 12:09:59 +0000

    initial python type FieldsplitSNES implementation

---
+++ firedrake/preconditioners/fieldsplit_snes.py
@@ -0,0 +1,82 @@
+from firedrake.preconditioners.base import SNESBase
+from firedrake.petsc import PETSc
+from firedrake.dmhooks import get_appctx
+from firedrake.function import Function
+
+__all__ = ("FieldsplitSNES",)
+
+
+class FieldsplitSNES(SNESBase):
+    prefix = "fieldsplit_"
+
+    # TODO:
+    #   - Allow setting field grouping/ordering like fieldsplit
+
+    def initialize(self, snes):
+        from firedrake.variational_solver import NonlinearVariationalSolver  # ImportError if we do this at file level
+        ctx = get_appctx(snes.dm)
+        self.sol = ctx._problem.u_restrict
+
+        # buffer to save solution to outer problem during solve
+        self.sol_outer = Function(self.sol.function_space())
+
+        # buffers for shuffling solutions during solve
+        self.sol_current = Function(self.sol.function_space())
+        self.sol_new = Function(self.sol.function_space())
+
+        # options for setting up the fieldsplit
+        snes_prefix = snes.getOptionsPrefix() + 'snes_' + self.prefix
+        # options for each field
+        sub_prefix = snes.getOptionsPrefix() + self.prefix
+
+        snes_options = PETSc.Options(snes_prefix)
+        self.fieldsplit_type = snes_options.getString('type', 'additive')
+        if self.fieldsplit_type not in ('additive', 'multiplicative'):
+            raise ValueError(
+                'FieldsplitSNES option snes_fieldsplit_type must be'
+                ' "additive" or "multiplicative"')
+
+        split_ctxs = ctx.split([(i,) for i in range(len(self.sol))])
+
+        self.solvers = tuple(
+            NonlinearVariationalSolver(
+                ctx._problem, appctx=ctx.appctx,
+                options_prefix=sub_prefix+str(i))
+            for i, ctx in enumerate(split_ctxs)
+        )
+
+    def update(self, snes):
+        pass
+
+    def step(self, snes, x, f, y):
+        # store current value of outer solution
+        self.sol_outer.assign(self.sol)
+
+        # the full form in ctx now has the most up to date solution
+        with self.sol_current.dat.vec_wo as vec:
+            x.copy(vec)
+        self.sol.assign(self.sol_current)
+
+        # The current snes solution x is held in sol_current, and we
+        # will place the new solution in sol_new.
+        # The solvers evaluate forms containing sol, so for each
+        # splitting type sol needs to hold:
+        #   - additive: all fields need to hold sol_current values
+        #   - multiplicative: fields need to hold sol_current before
+        #       they are are solved for, and keep the updated sol_new
+        #       values afterwards.
+        for solver, u, ucurr, unew in zip(self.solvers,
+                                          self.sol.subfunctions,
+                                          self.sol_current.subfunctions,
+                                          self.sol_new.subfunctions):
+            solver.solve()
+            unew.assign(u)
+            if self.fieldsplit_type == 'additive':
+                u.assign(ucurr)
+
+        with self.sol_new.dat.vec_ro as vec:
+            vec.copy(y)
+            y.aypx(-1, x)
+
+        # restore outer solution
+        self.sol.assign(self.sol_outer)
