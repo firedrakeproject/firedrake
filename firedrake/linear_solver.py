310804820ac214b1c81a19a18a590f2446283174
Pablo Brubeck <brubeck@protonmail.com>
2025-03-21 21:44:40 +0000

    LinearSolver: fix zero initial guess and update after error

--- firedrake/linear_solver.py
+++ firedrake/linear_solver.py
@@ -4,6 +4,8 @@ from firedrake.matrix import MatrixBase
 from firedrake.petsc import PETSc
 from pyop2.mpi import internal_comm
 from firedrake.variational_solver import LinearVariationalProblem, LinearVariationalSolver
+from firedrake.solving_utils import ConvergenceError
+
 
 __all__ = ["LinearSolver"]
 
@@ -83,7 +85,14 @@ class LinearSolver(LinearVariationalSolver):
         if b.function_space() != self.b.function_space():
             raise ValueError(f"b must be a Cofunction in {self.b.function_space()}.")
 
-        self.x.assign(x)
         self.b.assign(b)
-        super().solve()
-        x.assign(self.x)
+        if self.ksp.getInitialGuessNonzero():
+            self.x.assign(x)
+        else:
+            self.x.zero()
+        try:
+            super().solve()
+            x.assign(self.x)
+        except ConvergenceError as e:
+            x.assign(self.x)
+            raise e
