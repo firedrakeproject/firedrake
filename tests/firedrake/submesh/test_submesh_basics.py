764428c3fe2396d754f8c2ce4ee43577b2560f62
Pablo Brubeck <brubeck@protonmail.com>
2025-04-07 11:10:13 +0100

    Fix Submesh.parent

---
+++ tests/firedrake/submesh/test_submesh_basics.py
@@ -0,0 +1,16 @@
+from firedrake import *
+
+
+def test_submesh_parent():
+    mesh = UnitIntervalMesh(2)
+
+    M = FunctionSpace(mesh, "DG", 0)
+    m = Function(M)
+    m.dat.data[0] = 1
+
+    cell_marker = 100
+    parent = RelabeledMesh(mesh, [m], [cell_marker])
+
+    submesh = Submesh(parent, parent.geometric_dimension(), cell_marker)
+    assert submesh.topology.submesh_parent is parent.topology
+    assert submesh.submesh_parent is parent
