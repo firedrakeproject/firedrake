de5f5ee5f9843ba1d9c96d31c1238b82f3482c0a
Pablo Brubeck <brubeck@protonmail.com>
2025-04-09 10:52:02 +0100

    More extensive tests

--- tests/firedrake/regression/test_interpolate_zany.py
+++ tests/firedrake/regression/test_interpolate_zany.py
@@ -75,13 +75,59 @@ def test_interpolate_zany_into_cg(V, mesh, which, expect, tolerance):
 
 @pytest.fixture
 def vom(mesh):
-    return VertexOnlyMesh(mesh, [(0.5, 0.5)])
+    return VertexOnlyMesh(mesh, [(0.5, 0.5), (0.31, 0.72)])
 
 
-def test_interpolate_zany_into_vom(V, vom):
+def test_interpolate_zany_into_vom(V, mesh, which, vom):
+    degree = V.ufl_element().degree()
+    x, y = SpatialCoordinate(mesh)
+    expr = (x + y)**degree
+
+    f = Function(V)
+    f.project(expr, solver_parameters={"ksp_type": "preonly",
+                                       "pc_type": "lu"})
+    fexpr = f
+    vexpr = TestFunction(V)
     P0 = FunctionSpace(vom, "DG", 0)
-    vvom = TestFunction(P0)
-    Fvom = inner(1, vvom) * dx
+    if which == "coefficient":
+        P0 = FunctionSpace(vom, "DG", 0)
+    elif which == "grad":
+        fexpr = grad(fexpr)
+        vexpr = grad(vexpr)
+        expr = ufl.algorithms.expand_derivatives(grad(expr))
+        P0 = VectorFunctionSpace(vom, "DG", 0)
 
-    v = TestFunction(V)
-    assemble(Interpolate(v, Fvom))
+    expected = Function(P0)
+    point = Constant([0]*mesh.geometric_dimension())
+    expr_at_pt = ufl.replace(expr, {SpatialCoordinate(mesh): point})
+    for i, pt in enumerate(vom.coordinates.dat.data_ro):
+        point.assign(pt)
+        expected.dat.data[i] = numpy.asarray(expr_at_pt, dtype=float)
+
+    # Interpolate a Function into P0(vom)
+    f_at_vom = assemble(Interpolate(fexpr, P0))
+    assert numpy.allclose(f_at_vom.dat.data_ro, expected.dat.data_ro)
+
+    # Construct a Cofunction on P0(vom)*
+    Fvom = Cofunction(P0.dual()).assign(1)
+    expected = assemble(action(Fvom, expected))
+
+    # Interpolate a Function into Fvom
+    f_at_vom = assemble(Interpolate(fexpr, Fvom))
+    assert numpy.allclose(f_at_vom, expected)
+
+    # Interpolate a TestFunction into Fvom
+    expr_vom = assemble(Interpolate(vexpr, Fvom))
+    f_at_vom = assemble(action(expr_vom, f))
+    assert numpy.allclose(f_at_vom, expected)
+
+
+def test_high_order_mesh_cell_sizes():
+    msh1 = UnitSquareMesh(2, 2)
+    h1 = msh1.cell_sizes
+
+    P2 = msh1.coordinates.function_space().reconstruct(degree=2)
+    msh2 = Mesh(Function(P2).interpolate(msh1.coordinates))
+    h2 = msh2.cell_sizes
+
+    assert numpy.allclose(h1.dat.data, h2.dat.data)
