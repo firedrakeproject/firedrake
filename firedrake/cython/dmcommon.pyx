d43d152e04db65b1b46e0e098fdf2af4095f1e97
ksagiyam <k.sagiyama@imperial.ac.uk>
2025-04-28 03:06:43 +0100

    createCoordianteSpace() test

--- firedrake/cython/dmcommon.pyx
+++ firedrake/cython/dmcommon.pyx
@@ -1913,6 +1913,7 @@ def _set_dg_coordinates(PETSc.DM dm,
         PetscScalar *dg_coords
         const PetscScalar *firedrake_dg_coords
         PetscInt n, gdim, cStart, cEnd, c, offset, firedrake_offset, i, j, coord_size, ndof
+        PETSc.FE coord_fe
 
     gdim = firedrake_dg_coord_vec.getBlockSize()
     coord_dm = dm.getCoordinateDM()
@@ -1945,6 +1946,7 @@ def _set_dg_coordinates(PETSc.DM dm,
                                           dg_coord_vec,
                                           True)
     dm.setCellCoordinatesLocal(dg_coord_vec)
+    dm.createCoordinateSpace(1, True, False)
 
 
 @cython.boundscheck(False)
