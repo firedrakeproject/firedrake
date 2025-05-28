16e086edde9df2cb47ed6189a884cbede65b6721
Colin J Cotter <colin.cotter@imperial.ac.uk>
2025-05-28 17:00:09 +0100

    Vlasov demo (#4320)
    * Vlasov demo
    ---------
    
    Co-authored-by: David Ham <David.Ham@imperial.ac.uk>
    Co-authored-by: Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>

--- tests/firedrake/demos/test_demos_run.py
+++ tests/firedrake/demos/test_demos_run.py
@@ -46,6 +46,7 @@ SERIAL_DEMOS = [
     Demo(("patch", "hdiv_riesz_star"), []),
     Demo(("quasigeostrophy_1layer", "qg_1layer_wave"), ["hypre", "vtk"]),
     Demo(("saddle_point_pc", "saddle_point_systems"), ["hypre", "mumps"]),
+    Demo(('vlasov_poisson_1d', 'vp1d'), []),
 ]
 PARALLEL_DEMOS = [
     Demo(("full_waveform_inversion", "full_waveform_inversion"), ["adjoint"]),
