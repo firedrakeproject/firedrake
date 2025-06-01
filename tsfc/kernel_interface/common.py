8ed0f41ccee3aa73c6911f8f131800ace78a9bd6
ksagiyam <k.sagiyama@imperial.ac.uk>
2025-06-02 00:54:48 +0100

    use ufl CoefficientSplitter
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>

--- tsfc/kernel_interface/common.py
+++ tsfc/kernel_interface/common.py
@@ -14,7 +14,7 @@ from gem.node import traversal
 from gem.optimise import constant_fold_zero
 from gem.optimise import remove_componenttensors as prune
 from numpy import asarray
-from tsfc import fem, ufl_utils
+from tsfc import fem
 from finat.element_factory import as_fiat_cell, create_element
 from finat.ufl import MixedElement
 from tsfc.kernel_interface import KernelInterface
@@ -132,9 +132,6 @@ class KernelBuilderMixin(object):
 
         See :meth:`create_context` for typical calling sequence.
         """
-        # Split Coefficients
-        if self.coefficient_split:
-            integrand = ufl_utils.split_coefficients(integrand, self.coefficient_split)
         # Compile: ufl -> gem
         info = self.integral_data_info
         functions = [*info.arguments, self.coordinate(info.domain), *info.coefficients]
