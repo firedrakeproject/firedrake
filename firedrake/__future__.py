491dac35ccc5031c024294b922c9db9f81cc2bb1
Leo <leocollins511@gmail.com>
2025-06-02 16:03:51 +0100

    lint

--- firedrake/__future__.py
+++ firedrake/__future__.py
@@ -1,8 +1,9 @@
 from firedrake.interpolation import interpolate as interpolate_default, Interpolator as Interpolator_default
 from warnings import deprecated
 
-@deprecated("""The symbolic `interpolate` has been moved into `firedrake` 
-            and is now the default method for interpolating in Firedrake. The need to 
+
+@deprecated("""The symbolic `interpolate` has been moved into `firedrake`
+            and is now the default method for interpolating in Firedrake. The need to
             `from firedrake.__future__ import interpolate` is now deprecated.""")
 def interpolate(expr, V, *args, **kwargs):
     return interpolate_default(expr, V, *args, **kwargs)
