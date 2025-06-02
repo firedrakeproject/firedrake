bf748e2a9eaa95410651815230d3f398417a9186
Leo <leocollins511@gmail.com>
2025-06-02 14:01:22 +0100

    wrap `Interpolate` class in `__future__` with deprecation warning

--- firedrake/__future__.py
+++ firedrake/__future__.py
@@ -1,4 +1,4 @@
-from firedrake.interpolation import interpolate as interpolate_default
+from firedrake.interpolation import interpolate as interpolate_default, Interpolator as Interpolator_default
 from warnings import deprecated
 
 @deprecated("""The symbolic `interpolate` has been moved into `firedrake.interpolation` 
@@ -6,3 +6,11 @@ from warnings import deprecated
             `from firedrake.__future__ import interpolate` is now unnecessary.""")
 def interpolate(expr, V, *args, **kwargs):
     return interpolate_default(expr, V, *args, **kwargs)
+
+
+@deprecated("""The symbolic `Interpolator` has been moved into `firedrake.interpolation` 
+            and is now the default method for interpolating in Firedrake. The need to 
+            `from firedrake.__future__ import Interpolator` is now unnecessary.""")
+class Interpolator(Interpolator_default):
+    def __new__(cls, *args, **kwargs):
+        return Interpolator_default(*args, **kwargs)
