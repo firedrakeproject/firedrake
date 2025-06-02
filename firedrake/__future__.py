a892edaa9dceb955822eaa26aab76009beca5fac
Leo <leocollins511@gmail.com>
2025-06-02 12:52:50 +0100

    add warning to `__future__.interpolate`

--- firedrake/__future__.py
+++ firedrake/__future__.py
@@ -1,5 +1,9 @@
 from firedrake.interpolation import interpolate
 from functools import wraps
+import warnings
 
 def interpolate(expr, V, *args, **kwargs):
+    warnings.warn("""The symbolic `interpolate` has been moved into `firedrake.interpolation` 
+                  and is now the default method for interpolating in Firedrake. The need to 
+                  `from firedrake.__future__ import interpolate` is now unnecessary.""", FutureWarning)
     return interpolate(expr, V, *args, **kwargs)
