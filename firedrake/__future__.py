74686c8d774f88a0de1d3574aca280e37abb4a90
Leo <leocollins511@gmail.com>
2025-06-03 10:00:28 +0100

    use `warnings.warn` instead of `warnings.deprecated` `warnings.deprecated` was added in Python 3.13.

--- firedrake/__future__.py
+++ firedrake/__future__.py
@@ -1,16 +1,16 @@
 from firedrake.interpolation import interpolate as interpolate_default, Interpolator as Interpolator_default
-from warnings import deprecated
+import warnings
 
 
-@deprecated("""The symbolic `interpolate` has been moved into `firedrake`
-            and is now the default method for interpolating in Firedrake. The need to
-            `from firedrake.__future__ import interpolate` is now deprecated.""")
 def interpolate(expr, V, *args, **kwargs):
+    warnings.warn("""The symbolic `interpolate` has been moved into `firedrake` 
+                  and is now the default method for interpolating in Firedrake. The need to 
+                  `from firedrake.__future__ import interpolate` is now deprecated.""", FutureWarning)
     return interpolate_default(expr, V, *args, **kwargs)
 
 
-@deprecated("""The symbolic `Interpolator` has been moved into `firedrake`.
-            The need to `from firedrake.__future__ import Interpolator` is now deprecated.""")
 class Interpolator(Interpolator_default):
     def __new__(cls, *args, **kwargs):
+        warnings.warn("""The symbolic `Interpolator` has been moved into `firedrake`. 
+                      The need to `from firedrake.__future__ import Interpolator` is now deprecated.""", FutureWarning)
         return Interpolator_default(*args, **kwargs)
