a48fd7c3afa50e2e0855bace27acc95ed434a862
Leo <leocollins511@gmail.com>
2025-06-03 10:02:50 +0100

    lint

--- firedrake/__future__.py
+++ firedrake/__future__.py
@@ -3,14 +3,14 @@ import warnings
 
 
 def interpolate(expr, V, *args, **kwargs):
-    warnings.warn("""The symbolic `interpolate` has been moved into `firedrake` 
-                  and is now the default method for interpolating in Firedrake. The need to 
+    warnings.warn("""The symbolic `interpolate` has been moved into `firedrake`
+                  and is now the default method for interpolating in Firedrake. The need to
                   `from firedrake.__future__ import interpolate` is now deprecated.""", FutureWarning)
     return interpolate_default(expr, V, *args, **kwargs)
 
 
 class Interpolator(Interpolator_default):
     def __new__(cls, *args, **kwargs):
-        warnings.warn("""The symbolic `Interpolator` has been moved into `firedrake`. 
+        warnings.warn("""The symbolic `Interpolator` has been moved into `firedrake`.
                       The need to `from firedrake.__future__ import Interpolator` is now deprecated.""", FutureWarning)
         return Interpolator_default(*args, **kwargs)
