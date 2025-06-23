f8849f9ee33cc62f6075673885e6acf429246d41
David Ham <David.Ham@imperial.ac.uk>
2025-06-23 11:42:26 +0100

    lint

--- tests/firedrake/extrusion/test_change_coordinates.py
+++ tests/firedrake/extrusion/test_change_coordinates.py
@@ -6,7 +6,7 @@ import numpy as np
 def test_extruded_change_coordinates():
     # This test exists to ensure the code in the manual works.
 
-    # start extruded change coordinates
+    # start extruded change coordinates
     base_mesh = IntervalMesh(16, 0, 2*pi)
     # Make a height 1 mesh.
     unit_extruded_mesh = ExtrudedMesh(base_mesh, layers=10)
@@ -20,7 +20,7 @@ def test_extruded_change_coordinates():
     # Now we transfer the bathymetry field into a depth-averaged field.
     extruded_element = FiniteElement("R", "interval", 0)
     extruded_space = FunctionSpace(unit_extruded_mesh,
-                                TensorProductElement(base_fs.ufl_element(),
+                                   TensorProductElement(base_fs.ufl_element(),
                                                         extruded_element))
     extruded_bathymetry = Function(extruded_space)
     extruded_bathymetry.dat.data_wo[:] = bathymetry.dat.data_ro[:]
@@ -35,8 +35,6 @@ def test_extruded_change_coordinates():
     )
     # Finally build the mesh you are actually after.
     mesh = Mesh(new_coordinates)
-    # end extruded change coordinates
+    # end extruded change coordinates
 
-    f = Function(FunctionSpace(mesh, "CG", 1))
-
-    assert np.allclose(mesh.coordinates.dat.data_ro[:,1].min(), -0.2)
+    assert np.allclose(mesh.coordinates.dat.data_ro[:, 1].min(), -0.2)
