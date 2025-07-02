7f1df3ff711d17bb2a9a8c7e1f56b5157f7c7dba
India Marsden <imm1117@ic.ac.uk>
2025-07-02 15:32:42 +0100

    switch to cupy streams

--- firedrake/device.py
+++ firedrake/device.py
@@ -1,33 +1,48 @@
 import abc
 import contextlib
-from cuda.core.experimental import Device, Stream
+from cuda.core.experimental import Device as CuDevice, Stream
+import cupy as cp
+from cuda.bindings.driver import CUstream
+#import cutlass.cute as cute
+#import cutlass
 
 class ComputeDevice(abc.ABC):
     pass
 
 class CPUDevice(ComputeDevice):
-    pass
+
+    def context_manager(self):
+        yield
 
 class GPUDevice(ComputeDevice):
 
     def __init__(self, num_threads=32):
         self.num_threads=num_threads
+        self.stream = cp.cuda.Stream(non_blocking=True)
+
+    def context_manager(self):    
+        with self.stream:
+            self.stream.begin_capture()
+            yield
+            g = self.stream.end_capture()
+        g.launch(self.stream)
+        self.stream.synchronize()
 
 
-
-class gpu_assembly:
-
-    def __init__(self):
-        self.device = Device(0)
-
-    def __enter__(self):
-        self.gb = self.device.create_graph_builder()
-        self.gb.begin_building()
-        return self.gb.stream.handle
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.gb.end_building()
-        graph = self.gb.complete()
-
-        graph.launch(self.device.default_stream)
-        self.device.sync()
+@contextlib.contextmanager
+def device(type=None):
+    print(type)
+    if type=="gpu":
+        gpu = GPUDevice()
+        yield from gpu.context_manager()
+    else:
+        cpu = CPUDevice()
+        yield from cpu.context_manager()
+        #stream = cp.cuda.Stream(non_blocking=True)
+        #with stream:
+        #    stream.begin_capture()
+        #    yield
+        #    g =stream.end_capture()
+        #g.launch(stream)
+        #stream.synchronize()
+    
