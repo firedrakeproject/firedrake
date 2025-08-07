2d88ba545893d4bc68c10f96206efc0c2defd59a
Leo <leocollins511@gmail.com>
2025-08-07 13:21:44 +0100

    add test

--- tests/firedrake/regression/test_point_location.py
+++ tests/firedrake/regression/test_point_location.py
@@ -6,14 +6,14 @@ def test_high_order_location():
     f = Function(V)
     f.interpolate(mesh.coordinates)
 
-    def warp(x):
-        return 5.0*x*(2*x - 1)
+    def warp(x, p):
+        return p * x * (2 * x - 1)
 
     warp_indices = np.where((f.dat.data[:, 0] > 0.0) & (f.dat.data[:, 0] < 0.5) & (f.dat.data[:, 1] == 0.0))[0]
-    f.dat.data[warp_indices, 1] = warp(f.dat.data[warp_indices, 0])
+    f.dat.data[warp_indices, 1] = warp(f.dat.data[warp_indices, 0], 5.0)
 
     mesh = Mesh(f)
-    
+
     # The point (0.25, -0.6) *is* in the mesh, but falls outside the Lagrange bounding box
     # The below used to return (None, None), but projecting to Bernstein coordinates
     # allows us to locate the cell.
@@ -21,3 +21,10 @@ def test_high_order_location():
     # The point (0.25, -0.7) is outside the mesh, but inside the Bernstein bounding box.
     # This should return (None, None).
     assert mesh.locate_cell([0.25, -0.7], tolerance=0.001) is None
+
+    # Change mesh coordinates to check that the bounding box is recalculated
+    mesh.coordinates.dat.data_wo[warp_indices, 1] = warp(mesh.coordinates.dat.data_ro[warp_indices, 0], 8.0)
+    assert mesh.locate_cell([0.25, -0.6], tolerance=0.0001) is not None
+    assert mesh.locate_cell([0.25, -0.7], tolerance=0.0001) is not None
+    assert mesh.locate_cell([0.25, -0.95], tolerance=0.0001) is not None
+    assert mesh.locate_cell([0.25, -1.05], tolerance=0.0001) is None
