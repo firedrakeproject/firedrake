ef54973b7b10bdcf5b5d42d2c6a241a3f007fd6d
ksagiyam <k.sagiyama@imperial.ac.uk>
2025-08-15 21:38:35 +0100

    k

--- tests/firedrake/submesh/test_submesh_assign.py
+++ tests/firedrake/submesh/test_submesh_assign.py
@@ -112,10 +112,10 @@ def test_submesh_assign_2_quads_2_processes_no_overlap():
     assert abs(e) < 1.e-15
 
 
-@pytest.mark.parallel(nprocs=8)
+@pytest.mark.parallel(nprocs=5)
 @pytest.mark.parametrize('simplex', [True, False])
 @pytest.mark.parametrize('distribution_parameters', [None, {"overlap_type": (DistributedMeshOverlapType.NONE, 0)}])
-def test_submesh_assign_unstructured_8_processes(simplex, distribution_parameters):
+def test_submesh_assign_unstructured_5_processes(simplex, distribution_parameters):
     if not simplex and distribution_parameters == {"overlap_type": (DistributedMeshOverlapType.NONE, 0)}:
         pytest.skip(reason="quad orientation bug; see https://github.com/firedrakeproject/firedrake/issues/4476")
     left = 111
