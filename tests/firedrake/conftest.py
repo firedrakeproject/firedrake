44fc32c6589b062cfb1932a76dc57166f04be638
Connor Ward <c.ward20@imperial.ac.uk>
2025-09-24 16:55:29 +0100

    Make VTK a hard dependency again (#4565)
    * Make VTK a hard dependency again
    
    This should streamline things for users and is now possible because VTK
    now provide ARM wheels.
    
    * Remove vtk from install instructions
    
    * Don't conditionally skip VTK tests

--- tests/firedrake/conftest.py
+++ tests/firedrake/conftest.py
@@ -67,14 +67,6 @@ def _skip_test_dependency(dependency):
         except ImportError:
             return skip
 
-    elif dependency == "vtk":
-        try:
-            from firedrake.output import VTKFile  # noqa: F401
-            del VTKFile
-            return not skip
-        except ImportError:
-            return skip
-
     elif dependency in ("mumps", "hypre"):
         return dependency not in get_external_packages()
 
@@ -90,7 +82,6 @@ dependency_skip_markers_and_reasons = (
     ("jax", "skipjax", "JAX is not installed"),
     ("matplotlib", "skipplot", "Matplotlib is not installed"),
     ("netgen", "skipnetgen", "Netgen and ngsPETSc are not installed"),
-    ("vtk", "skipvtk", "VTK is not installed")
 )
 
 
@@ -125,10 +116,6 @@ def pytest_configure(config):
     config.addinivalue_line(
         "markers",
         "skipslepc: mark as skipped if slepc4py is not installed"
-    )
-    config.addinivalue_line(
-        "markers",
-        "skipvtk: mark as skipped if vtk is not installed"
     )
     config.addinivalue_line(
         "markers",
