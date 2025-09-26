fd9a3f60b51483343c17b52b245a2e289036d5b5
Pablo Brubeck <brubeck@protonmail.com>
2025-09-26 12:30:45 +0100

    Update firedrake/interpolation.py

--- firedrake/interpolation.py
+++ firedrake/interpolation.py
@@ -1008,6 +1008,9 @@ def _interpolator(V, tensor, expr, subset, arguments, access, bcs=None):
     if access == op2.INC:
         callables += (tensor.zero,)
 
+    # For the matfree adjoint 1-form and the 0-form, the cellwise kernel will add multiple
+    # contributions from the facet DOFs of the dual argument.
+    # The incoming Cofunction needs to be weighted by the reciprocal of the DOF multiplicity.
     needs_weight = isinstance(dual_arg, ufl.Cofunction) and not to_element.is_dg()
     if needs_weight:
         # Compute the reciprocal of the DOF multiplicity
