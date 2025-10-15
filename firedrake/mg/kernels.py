26b26c56bc5cfb936c60d7515ab119c2ebc91ad1
Pablo Brubeck <brubeck@protonmail.com>
2025-10-15 08:58:24 +0100

    BUG: fix multigrid transfers for symmetric tensors

--- firedrake/mg/kernels.py
+++ firedrake/mg/kernels.py
@@ -30,6 +30,8 @@ from tsfc import fem, ufl_utils, spectral
 from tsfc.driver import TSFCIntegralDataInfo
 from tsfc.kernel_interface.common import lower_integral_type
 from tsfc.parameters import default_parameters
+from tsfc.ufl_utils import apply_mapping, simplify_abs
+
 from finat.element_factory import create_element
 from finat.quadrature import make_quadrature
 from firedrake.pointquery_utils import dX_norm_square, X_isub_dX, init_X, inside_check, is_affine, celldist_l1_c_expr
@@ -116,10 +118,7 @@ def compile_element(expression, dual_space=None, parameters=None,
         arg, = extract_coefficients(expression)
         argument_multiindices = ()
         coefficient = True
-        if expression.ufl_shape:
-            tensor_indices = tuple(gem.Index() for s in expression.ufl_shape)
-        else:
-            tensor_indices = ()
+        tensor_indices = None
     except ValueError:
         arg, = extract_arguments(expression)
         finat_elem = create_element(arg.ufl_element())
@@ -150,8 +149,12 @@ def compile_element(expression, dual_space=None, parameters=None,
                   scalar_type=parameters["scalar_type"])
     context = tsfc.fem.GemPointContext(**config)
 
+    # Map into reference values
+    expression = apply_mapping(expression, arg.ufl_element(), domain)
+    if tensor_indices is None:
+        tensor_indices = tuple(gem.Index() for s in expression.ufl_shape)
     # Abs-simplification
-    expression = tsfc.ufl_utils.simplify_abs(expression, complex_mode)
+    expression = simplify_abs(expression, complex_mode)
 
     # Translate UFL -> GEM
     if coefficient:
@@ -177,7 +180,7 @@ def compile_element(expression, dual_space=None, parameters=None,
         return_variable = gem.Indexed(gem.Variable('R', finat_elem.index_shape), argument_multiindex)
         result = gem.Indexed(result, tensor_indices)
         if dual_space:
-            value_shape = dual_space.value_shape
+            value_shape = dual_space.ufl_element().reference_value_shape
             if value_shape:
                 var = gem.Indexed(gem.Variable("b", value_shape), tensor_indices)
                 b_arg = [lp.GlobalArg("b", dtype=ScalarType, shape=value_shape)]
@@ -219,7 +222,7 @@ def prolong_kernel(expression):
         assert hierarchy._meshes[int(idx)].cell_set._extruded
     V = expression.function_space()
     key = (("prolong",)
-           + V.value_shape
+           + (V.block_size,)
            + entity_dofs_key(V.finat_element.complex.get_topology())
            + entity_dofs_key(V.finat_element.entity_dofs())
            + entity_dofs_key(coordinates.function_space().finat_element.entity_dofs()))
@@ -283,7 +286,7 @@ def prolong_kernel(expression):
                "evaluate": eval_code,
                "spacedim": element.cell.get_spatial_dimension(),
                "ncandidate": hierarchy.fine_to_coarse_cells[levelf].shape[1],
-               "Rdim": V.value_size,
+               "Rdim": V.block_size,
                "inside_cell": inside_check(element.cell, eps=1e-8, X="Xref"),
                "celldist_l1_c_expr": celldist_l1_c_expr(element.cell, X="Xref"),
                "Xc_cell_inc": coords_element.space_dimension(),
@@ -301,7 +304,7 @@ def restrict_kernel(Vf, Vc):
     if Vf.extruded:
         assert Vc.extruded
     key = (("restrict",)
-           + Vf.value_shape
+           + (Vf.block_size,)
            + entity_dofs_key(Vf.finat_element.complex.get_topology())
            + entity_dofs_key(Vc.finat_element.complex.get_topology())
            + entity_dofs_key(Vf.finat_element.entity_dofs())
@@ -389,7 +392,7 @@ def inject_kernel(Vf, Vc):
     else:
         level_ratio = 1
     key = (("inject", level_ratio)
-           + Vf.value_shape
+           + (Vf.block_size,)
            + entity_dofs_key(Vc.finat_element.complex.get_topology())
            + entity_dofs_key(Vf.finat_element.complex.get_topology())
            + entity_dofs_key(Vc.finat_element.entity_dofs())
@@ -464,7 +467,7 @@ def inject_kernel(Vf, Vc):
             "celldist_l1_c_expr": celldist_l1_c_expr(Vc.finat_element.cell, X="Xref"),
             "tdim": Vc.mesh().topological_dimension(),
             "ncandidate": ncandidate,
-            "Rdim": numpy.prod(Vf.value_shape),
+            "Rdim": Vf.block_size,
             "Xf_cell_inc": coords_element.space_dimension(),
             "f_cell_inc": Vf_element.space_dimension()
         }
@@ -608,7 +611,8 @@ def dg_injection_kernel(Vf, Vc, ncell):
     # Coarse basis function evaluated at fine quadrature points
     phi_c = fem.fiat_to_ufl(Vce.point_evaluation(0, X_a, (Vce.cell.get_dimension(), 0)), 0)
 
-    tensor_indices = tuple(gem.Index(extent=d) for d in f.ufl_shape)
+    index_shape = f.ufl_element().reference_value_shape
+    tensor_indices = tuple(gem.Index(extent=d) for d in index_shape)
 
     phi_c = gem.Indexed(phi_c, argument_multiindex + tensor_indices)
     fexpr = gem.Indexed(fexpr, tensor_indices)
