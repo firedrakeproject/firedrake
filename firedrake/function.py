53eaec45a9f373401ff3460dd08a3d4c6dfbe8e2
Leo Collins <collins.leo@proton.me>
2025-11-10 11:30:29 +0000

    cast to numpy array

--- firedrake/function.py
+++ firedrake/function.py
@@ -549,6 +549,7 @@ class Function(ufl.Coefficient, FunctionMixin):
         # Called by UFL when evaluating expressions at coordinates
         if component or index_values:
             raise NotImplementedError("Unsupported arguments when attempting to evaluate Function.")
+        coord = np.asarray(coord, dtype=utils.ScalarType)
         evaluator = PointEvaluator(self.function_space().mesh(), coord)
         result = evaluator.evaluate(self)
         if len(coord.shape) == 1:
