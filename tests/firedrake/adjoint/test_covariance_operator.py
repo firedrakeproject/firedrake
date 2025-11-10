bfa69da5e5ea12302068ad74f0b0eec8b71f0891
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2025-11-10 11:22:19 +0000

    White noise generators and AR covariance operators
    Co-authored-by: Jack Betteridge <j.betteridge@imperial.ac.uk>

---
+++ tests/firedrake/adjoint/test_covariance_operator.py
@@ -0,0 +1,187 @@
+import pytest
+import numpy as np
+from scipy.sparse import csr_matrix
+from firedrake import *
+from firedrake.adjoint import *
+
+
+def petsc2numpy_vec(petsc_vec):
+    """Allgather a PETSc.Vec."""
+    gvec = petsc_vec
+    gather, lvec = PETSc.Scatter().toAll(gvec)
+    gather(gvec, lvec, addv=PETSc.InsertMode.INSERT_VALUES)
+    return lvec.array_r.copy()
+
+
+def petsc2numpy_mat(petsc_mat):
+    """Allgather a PETSc.Mat."""
+    comm = petsc_mat.getComm()
+    local_mat = petsc_mat.getRedundantMatrix(
+        comm.size, PETSc.COMM_SELF)
+    return csr_matrix(
+        local_mat.getValuesCSR()[::-1],
+        shape=local_mat.getSize()
+    ).todense()
+
+
+@pytest.mark.parallel([1, 2])
+@pytest.mark.parametrize("degree", (1, 2), ids=["degree1", "degree2"])
+@pytest.mark.parametrize("dim", (0, 1, 2), ids=["scalar", "vec1", "vec2"])
+@pytest.mark.parametrize("family", ("CG", "DG"))
+@pytest.mark.parametrize("mesh_type", ("interval", "square"))
+@pytest.mark.parametrize("backend", ("pyop2", "petsc"))
+def test_white_noise(family, degree, mesh_type, dim, backend):
+    """Test that white noise generator converges to a mass matrix covariance.
+    """
+    if backend == "petsc" and COMM_WORLD.size > 1:
+        pytest.skip(
+            "petsc backend for noise generation not implemented in parallel.")
+
+    nx = 10
+    # Mesh dimension
+    if mesh_type == 'interval':
+        mesh = UnitIntervalMesh(nx)
+    elif mesh_type == 'square':
+        mesh = UnitSquareMesh(nx, nx)
+    elif mesh_type == 'cube':
+        mesh = UnitCubeMesh(nx, nx, nx)
+
+    # Variable rank
+    if dim > 0:
+        V = VectorFunctionSpace(mesh, family, degree, dim=dim)
+    else:
+        V = FunctionSpace(mesh, family, degree)
+
+    # Finite element white noise has mass matrix covariance
+    M = inner(TrialFunction(V), TestFunction(V))*dx
+    covmat = petsc2numpy_mat(
+        assemble(M, mat_type='aij').petscmat)
+
+    rng = RandomGenerator(PCG64(seed=13))
+
+    generator = WhiteNoiseGenerator(
+        V, backend=NoiseBackend(backend), rng=rng)
+
+    # Test convergence as sample size increases
+    nsamples = [50, 100, 200, 400, 800]
+
+    samples = np.empty((V.dim(), nsamples[-1]))
+    for i in range(nsamples[-1]):
+        with generator.sample().dat.vec_ro as bv:
+            samples[:, i] = petsc2numpy_vec(bv)
+
+    covariances = [np.cov(samples[:, :ns]) for ns in nsamples]
+
+    # Covariance matrix should converge at a rate of sqrt(n)
+    errors = [np.linalg.norm(cov-covmat) for cov in covariances]
+    normalised_errors = [err*sqrt(n) for err, n in zip(errors, nsamples)]
+    normalised_errors /= normalised_errors[-1]
+
+    # Loose tolerance because RNG
+    tol = 0.2
+    assert (1 - tol) < np.max(normalised_errors) < (1 + tol)
+
+
+@pytest.mark.parallel([1, 2])
+@pytest.mark.parametrize("m", (0, 2, 4))
+@pytest.mark.parametrize("degree", (1, 2), ids=["degree1", "degree2"])
+@pytest.mark.parametrize("dim", (0, 1, 2), ids=["scalar", "vector1", "vector2"])
+@pytest.mark.parametrize("family", ("CG", "DG"))
+@pytest.mark.parametrize("mesh_type", ("interval", "square"))
+@pytest.mark.parametrize("backend", ("pyop2", "petsc"))
+def test_covariance_inverse_action(m, family, degree, mesh_type, dim, backend):
+    """Test that correlated noise generator has the right covariance matrix.
+    """
+    if backend == "petsc" and COMM_WORLD.size > 1:
+        pytest.skip(
+            "petsc backend for noise generation not implemented in parallel.")
+
+    nx = 16
+    if mesh_type == 'interval':
+        mesh = UnitIntervalMesh(nx)
+        x, = SpatialCoordinate(mesh)
+        wexpr = cos(2*pi*x)
+    elif mesh_type == 'square':
+        mesh = UnitSquareMesh(nx, nx)
+        x, y = SpatialCoordinate(mesh)
+        wexpr = cos(2*pi*x)*cos(4*pi*x)
+    elif mesh_type == 'cube':
+        mesh = UnitCubeMesh(nx, nx, nx)
+        x, y, z = SpatialCoordinate(mesh)
+        wexpr = cos(2*pi*x)*cos(4*pi*y)*cos(pi*z)
+    if dim > 0:
+        V = VectorFunctionSpace(mesh, family, degree, dim=dim)
+        wexpr = as_vector([-1**(j+1)*wexpr for j in range(dim)])
+    else:
+        V = FunctionSpace(mesh, family, degree)
+
+    rng = WhiteNoiseGenerator(
+        V, backend=NoiseBackend(backend),
+        rng=RandomGenerator(PCG64(seed=13)))
+
+    L = 0.1
+    sigma = 0.9
+
+    solver_parameters = {
+        'ksp_type': 'preonly',
+        'pc_type': 'lu',
+        'pc_factor_mat_solver_type': 'mumps'
+    }
+
+    B = GaussianCovarianceOperator(
+        V, L, sigma, m, rng=rng,
+        solver_parameters=solver_parameters, options_prefix="",
+        form=DiffusionFormulation(family))
+
+    w = Function(V).project(wexpr)
+    wcheck = B.apply_inverse(B.apply_action(w))
+
+    tol = 1e-8
+    # Particularly sensitive tests??
+    if mesh_type == 'square' and family == 'DG' and degree == 2 and m == 4:
+        tol = 5e-4
+
+    assert errornorm(w, wcheck) < tol
+
+
+@pytest.mark.parallel([1, 2])
+@pytest.mark.parametrize("m", (0, 2, 4))
+@pytest.mark.parametrize("family", ("CG", "DG"))
+@pytest.mark.parametrize("backend", ("pyop2", "petsc"))
+def test_covariance_adjoint_norm(m, family, backend):
+    """Test that correlated noise generator has the right covariance matrix.
+    """
+    if backend == "petsc" and COMM_WORLD.size > 1:
+        pytest.skip(
+            "petsc backend for noise generation not implemented in parallel.")
+    nx = 20
+    L = 0.2
+    sigma = 0.1
+
+    mesh = UnitIntervalMesh(nx)
+    x, = SpatialCoordinate(mesh)
+
+    V = FunctionSpace(mesh, family, 1)
+
+    u = Function(V).project(sin(2*pi*x))
+    v = Function(V).project(2 - 0.5*sin(6*pi*x))
+
+    B = GaussianCovarianceOperator(
+        V, L, sigma, m,
+        form=DiffusionFormulation(family))
+
+    continue_annotation()
+    with set_working_tape() as tape:
+        w = Function(V).project(u**4 + v)
+        J = B.norm(w)
+        Jhat = ReducedFunctional(J, Control(u), tape=tape)
+    pause_annotation()
+
+    m = Function(V).project(sin(2*pi*(x+0.2)))
+    h = Function(V).project(sin(4*pi*(x-0.2)))
+
+    taylor = taylor_to_dict(Jhat, m, h)
+
+    assert min(taylor['R0']['Rate']) > 0.95, taylor['R0']
+    assert min(taylor['R1']['Rate']) > 1.95, taylor['R1']
+    assert min(taylor['R2']['Rate']) > 2.95, taylor['R2']
