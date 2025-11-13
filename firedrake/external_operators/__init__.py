7169415ae300be6ff0e1ecbd437a3556177555f1
Leo Collins <collins.leo@proton.me>
2025-11-13 09:54:46 +0000

    remove most star imports

--- firedrake/external_operators/__init__.py
+++ firedrake/external_operators/__init__.py
@@ -1,3 +1,7 @@
-from firedrake.external_operators.abstract_external_operators import *     # noqa: F401
-from firedrake.external_operators.point_expr_operator import *             # noqa: F401
-from firedrake.external_operators.ml_operator import *             # noqa: F401
+from firedrake.external_operators.abstract_external_operators import (  # noqa F401
+    AbstractExternalOperator, assemble_method
+)
+from firedrake.external_operators.point_expr_operator import (  # noqa F401
+    PointexprOperator, point_expr
+)
+from firedrake.external_operators.ml_operator import MLOperator  # noqa F401
