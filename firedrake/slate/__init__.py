3166f59f5f5fc283130d5762de27faea0526512a
Leo Collins <collins.leo@proton.me>
2025-11-13 10:30:56 +0000

    remove more star imports

--- firedrake/slate/__init__.py
+++ firedrake/slate/__init__.py
@@ -3,4 +3,6 @@ from firedrake.slate.slate import (  # noqa: F401
     Transpose, Negative, Add, Mul, Solve, BlockAssembledVector,
     DiagonalTensor, Reciprocal
 )
-from firedrake.slate.static_condensation import *  # noqa: F401
+from firedrake.slate.static_condensation import (  # noqa: F401
+    HybridizationPC, SchurComplementBuilder, SCPC
+)
