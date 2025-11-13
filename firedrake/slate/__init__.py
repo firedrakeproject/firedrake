562b51f94ce66e2a43541d0d09b68de9c4fb24b3
Leo Collins <collins.leo@proton.me>
2025-11-13 11:14:39 +0000

    add TensorBase to slate API

--- firedrake/slate/__init__.py
+++ firedrake/slate/__init__.py
@@ -1,7 +1,7 @@
 from firedrake.slate.slate import (  # noqa: F401
     AssembledVector, Block, Factorization, Tensor, Inverse,
     Transpose, Negative, Add, Mul, Solve, BlockAssembledVector,
-    DiagonalTensor, Reciprocal, TensorOp
+    DiagonalTensor, Reciprocal, TensorOp, TensorBase
 )
 from firedrake.slate.static_condensation import (  # noqa: F401
     HybridizationPC, SchurComplementBuilder, SCPC
