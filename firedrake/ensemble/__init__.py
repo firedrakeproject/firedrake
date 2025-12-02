6c961ad55177a3a042f887bf9b2d7fa88a272f9e
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2025-12-02 11:10:19 +0000

    remove import * from ensemble module

--- firedrake/ensemble/__init__.py
+++ firedrake/ensemble/__init__.py
@@ -1,5 +1,5 @@
-from firedrake.ensemble.ensemble import *  # noqa: F401
-from firedrake.ensemble.ensemble_function import *  # noqa: F401
-from firedrake.ensemble.ensemble_functionspace import *  # noqa: F401
-from firedrake.ensemble.ensemble_mat import *  # noqa: F401
-from firedrake.ensemble.ensemble_pc import *  # noqa: F401
+from firedrake.ensemble.ensemble import Ensemble  # noqa: F401
+from firedrake.ensemble.ensemble_function import EnsembleFunction, EnsembleCofunction  # noqa: F401
+from firedrake.ensemble.ensemble_functionspace import EnsembleFunctionSpace, EnsembleDualSpace  # noqa: F401
+from firedrake.ensemble.ensemble_mat import EnsembleBlockDiagonalMat, EnsembleBlockDiagonalMatrix  # noqa: F401
+from firedrake.ensemble.ensemble_pc import EnsembleBJacobiPC  # noqa: F401
