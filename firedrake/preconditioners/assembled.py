234ee715c1eb3c136f0c020916d80eda839344b0
Pablo Brubeck <brubeck@protonmail.com>
2026-02-16 13:46:18 +0000

    _SNESContext.reconstruct

--- firedrake/preconditioners/assembled.py
+++ firedrake/preconditioners/assembled.py
@@ -62,12 +62,10 @@ class AssembledPC(PCBase):
         # We set a DM and an appropriate SNESContext on the constructed PC so one
         # can do e.g. multigrid or patch solves.
         dm = opc.getDM()
-        ctx = dmhooks.get_appctx(dm)
         self._ctx_ref = self.new_snes_ctx(opc, a, bcs, mat_type,
                                           fcp=fcp,
                                           sub_mat_type=sub_mat_type,
-                                          options_prefix=options_prefix,
-                                          transfer_manager=ctx._transfer_manager)
+                                          options_prefix=options_prefix)
 
         pc.setDM(dm)
         pc.setOptionsPrefix(options_prefix)
