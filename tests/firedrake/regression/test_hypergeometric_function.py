1b736b7e0f37b57711b1e3b00d662e7467ec769f
Pablo Brubeck <brubeck@protonmail.com>
2026-03-04 17:19:34 +0000

    WIP

--- tests/firedrake/regression/test_hypergeometric_function.py
+++ tests/firedrake/regression/test_hypergeometric_function.py
@@ -6,9 +6,9 @@ import pytest
 
 @pytest.mark.skipif(utils.complex_mode, reason="Complex bessel functions are not implemented.")
 def test_hypergeometric_function():
-
+    sp_hyp2f1 = scipy.special.hyp2f1
     a = 1
-    b = 2
+    b = 1
     c = 1
     mesh = UnitDiskMesh(3)
     V = FunctionSpace(mesh, "CG", 1)
@@ -18,14 +18,14 @@ def test_hypergeometric_function():
     expr = x / 10
     uexact = hyp2f1(a, b, c, expr)
     assert np.allclose(assemble(Function(V).interpolate(uexact)).dat.data,
-                       scipy.hypf1(a, b, c, assemble(Function(V).interpolate(expr)).dat.data))
+                       sp_hyp2f1(a, b, c, assemble(Function(V).interpolate(expr)).dat.data))
 
     expr = sqrt(x**2+y**2) / 10
     uexact = hyp2f1(a, b, c, expr)
     assert np.allclose(assemble(Function(V).interpolate(uexact)).dat.data,
-                       scipy.hypf1(a, b, c, assemble(Function(V).interpolate(expr)).dat.data))
+                       sp_hyp2f1(a, b, c, assemble(Function(V).interpolate(expr)).dat.data))
 
     expr = sqrt(x*x + y*y) / 10
     uexact = hyp2f1(a, b, c, expr)
     assert np.allclose(assemble(Function(V).interpolate(uexact)).dat.data,
-                       scipy.hypf1(a, b, c, assemble(Function(V).interpolate(expr)).dat.data))
+                       sp_hyp2f1(a, b, c, assemble(Function(V).interpolate(expr)).dat.data))
--- tsfc/loopy.py
+++ tsfc/loopy.py
@@ -449,13 +449,8 @@ def _expression_power(expr, ctx):
 
 @_expression.register(gem.MathFunction)
 def _expression_mathfunction(expr, ctx):
-    if expr.name == "hyp2f1":
-        assert isinstance(ctx.target, lp.target.c.CWithGNULibcTarget)
-        # Generate right functions calls to gnulibc hypergeometric function
-        name = "hyperg_2F1"
-        return p.Variable(name)(*(expression(c, ctx) for c in expr.children))
 
-    elif expr.name.startswith('cyl_bessel_'):
+    if expr.name.startswith('cyl_bessel_'):
         # Bessel functions
         if is_complex(ctx.scalar_type):
             raise NotImplementedError("Bessel functions for complex numbers: "
@@ -486,7 +481,11 @@ def _expression_mathfunction(expr, ctx):
                 else:
                     return p.Variable(f"{name}n")(nu_, arg_)
     else:
-        if expr.name == "ln":
+        if expr.name == "hyp2f1":
+            assert isinstance(ctx.target, lp.target.c.CWithGNULibcTarget)
+            # Generate right functions calls to gsl hypergeometric function
+            name = "hyperg_2F1"
+        elif expr.name == "ln":
             name = "log"
         else:
             name = expr.name
