c8db3e20e5e65c85293e0c00b189e452a088c4da
SamSJackson <s2020962@ed.ac.uk>
2026-05-06 18:07:27 +0100

    test cases for gpu - no fixtures due to bug

--- tests/pyop3/unit/test_gpu_context.py
+++ tests/pyop3/unit/test_gpu_context.py
@@ -11,10 +11,6 @@ import pyop3 as op3
 from firedrake import Function, FunctionSpace, UnitSquareMesh
 
 
-# ---------------------------------------------------------------------------
-# Fixtures
-# ---------------------------------------------------------------------------
-
 HOST = op3.HOST_DEVICE
 CUDAGPU = op3.CUDAGPU()
 
@@ -22,21 +18,21 @@ STATE_NOT_CREATED = -1
 STATE_UNTOUCHED = 0
 STATE_MODIFIED = 1
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def mesh():
     return UnitSquareMesh(3, 3)
 
-@pytest.fixture(scope="module")
-def function_space(mesh):
+@pytest.fixture()
+def V(mesh):
     return FunctionSpace(mesh, "P", 2)
 
-@pytest.fixture(scope="module")
-def f(function_space):
-    return Function(function_space)
+@pytest.fixture()
+def f(V):
+    return Function(V)
 
-@pytest.fixture(scope="module")
-def g(function_space):
-    return Function(function_space)
+@pytest.fixture()
+def g(V):
+    return Function(V)
 
 def state(func, device):
     """Shorthand for reading buffer state on a given device."""
@@ -55,6 +51,8 @@ class TestInitialState:
         """CUDAGPU buffer should not exist before any offloading."""
         assert state(f, CUDAGPU) == STATE_NOT_CREATED
 
+# NOTE: `pytest.fixture`s not used for Offloading GPU tests due to segfault
+# Unsure what is causing but we are leaving for now.
 class TestOffloadingArrayTypes:
     """Inside op3.offloading, data array type should be GPU array types"""
 
@@ -78,15 +76,48 @@ class TestOffloadingArrayTypes:
             assert not isinstance(k.dat.data_ro, np.ndarray)
 
 class TestOffloadingAssignmentState:
-    
-    @pytest.fixture(autouse=True)
-    def _run_gpu_assign(self, f, g):
+
+    def test_host_state_untouched_after_gpu_assign(self):
+        """g was not modified on host"""
+        mesh = UnitSquareMesh(3, 3)
+        V = FunctionSpace(mesh, "P", 2)
+
+        f = Function(V).assign(10)
+        g = Function(V)
         with op3.offloading(CUDAGPU):
             g.dat.assign(2 * f.dat + 3, eager=True, eager_strategy="array")
-
-    def test_host_state_untouched_after_gpu_assign(self, g):
-        """g was not modified on host"""
         assert state(g, HOST) == 0 
 
-    def test_g_gpu_state_modified_after_assign(self, g):
+    def test_gpu_state_modified_after_assign(self):
+        mesh = UnitSquareMesh(3, 3)
+        V = FunctionSpace(mesh, "P", 2)
+
+        f = Function(V).assign(10)
+        g = Function(V)
+        with op3.offloading(CUDAGPU):
+            g.dat.assign(2 * f.dat + 3, eager=True, eager_strategy="array")
         assert state(g, CUDAGPU) == 1
+
+class TestOffloadingArraysUpdated:
+
+    def test_gpu_array_modified(self):
+        '''Data on GPU is updated in GPU context'''
+        mesh = UnitSquareMesh(3, 3)
+        V = FunctionSpace(mesh, "P", 2)
+
+        f = Function(V).assign(10)
+        g = Function(V)
+        with op3.offloading(CUDAGPU):
+            g.dat.assign(2 * f.dat + 3, eager=True, eager_strategy="array")
+            assert (g.dat.data_ro == 23).all()
+
+    def test_gpu_array_modified(self):
+        ''' Data on CPU is updated when in CPU context'''
+        mesh = UnitSquareMesh(3, 3)
+        V = FunctionSpace(mesh, "P", 2)
+
+        f = Function(V).assign(10)
+        g = Function(V)
+        with op3.offloading(CUDAGPU):
+            g.dat.assign(2 * f.dat + 3, eager=True, eager_strategy="array")
+        assert (g.dat.data_ro == 23).all()
