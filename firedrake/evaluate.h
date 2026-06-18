70f78df3bcb639dacac00dbc1589b8d626f52859
Leo Collins <collins.leo@proton.me>
2026-06-18 13:13:39 +0100

    merge bulk-query
    use bulk query in locate.c

--- firedrake/evaluate.h
+++ firedrake/evaluate.h
@@ -45,7 +45,7 @@ typedef PetscReal (*ref_cell_l1_dist_xtr)(void *data_,
 				int layer,
 				double *x);
 
-extern int locate_cell(struct Function *f,
+extern int locate_cell_from_candidates(struct Function *f,
 		       double *x,
 		       int dim,
 		       ref_cell_l1_dist try_candidate,
@@ -53,6 +53,8 @@ extern int locate_cell(struct Function *f,
 		       void *temp_ref_coords,
 		       void *found_ref_coords,
 		       double *found_ref_cell_dist_l1,
+			   size_t nids,
+			   size_t *ids,
 			   size_t ncells_ignore,
 			   int* cells_ignore);
 
