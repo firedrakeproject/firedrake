39930f6f318acd25d5520e3a091f65b68ed91da6
SamSJackson <s2020962@ed.ac.uk>
2026-06-19 12:39:01 +0100

    demo to satisfy

---
+++ gpu_offloading_demo.py
@@ -0,0 +1,19 @@
+from firedrake import *
+import pyop3 as op3
+import numpy as np, cupy as cp
+
+mesh = UnitSquareMesh(3,3)
+V = FunctionSpace(mesh, "CG", 1)
+f = Function(V).assign(10)
+g = Function(V)
+
+gpu = op3.CUDAGPU()
+
+with op3.offloading(gpu):
+	g.dat.assign(2 * f.dat, eager=True, eager_strategy="compile")
+	assert isinstance(g.dat.data_ro, cp.ndarray) # Device
+	assert (g.dat.data_ro == 20).all()
+	
+assert isinstance(g.dat.data_ro, np.ndarray) # Host
+assert (g.dat.data_ro == 20).all()
+
