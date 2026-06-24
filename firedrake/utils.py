a2667fe1aa708460f60c28fa6e0afe52c5d6cb97
Leo Collins <collins.leo@proton.me>
2026-06-24 11:43:24 +0100

    wording

--- firedrake/utils.py
+++ firedrake/utils.py
@@ -272,7 +272,7 @@ def cached_property_until(key: Callable[[Self], Hashable]):
     """Decorator for a property that is cached until some value changes.
     
     For example, the `expensive_property` below will be cached until `self.value` changes,
-    and will be recomputed with the new `self.value` when accessed again.
+    and will be recomputed with the new `self.value` and cached when accessed again.
 
     ```
     class MyClass:
