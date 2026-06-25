49d0e3c566a417437fca798037c82e7c49e4d202
Dale Roberts <dale.roberts1@anu.edu.au>
2026-06-25 22:26:59 +1000

    Changes to test_no_offload_A.py in response to review

--- tests/firedrake/offload/test_no_offload_A.py
+++ tests/firedrake/offload/test_no_offload_A.py
@@ -1,9 +1,9 @@
 from firedrake import *
-import numpy as np
 
 
-def run_stokes_mini(n):
+def test_matfree_A_does_not_offload():
     length = 10
+    n = 3
     mesh = RectangleMesh(2**n, 2**n, length, 1)
 
     P1 = FiniteElement("CG", cell=mesh.ufl_cell(), degree=1)
@@ -25,17 +25,17 @@ def run_stokes_mini(n):
     # No-slip velocity boundary condition on top and bottom,
     # y == 0 and y == 1
     noslip = Constant((0, 0))
-    bc0 = [DirichletBC(W[0], noslip, (3, 4))]
+    bc0 = DirichletBC(W[0], noslip, (3, 4))
 
     # Parabolic inflow y(1-y) at x = 0 in positive x direction
     x = SpatialCoordinate(W.mesh())
     inflow = as_vector((x[1]*(1 - x[1]), 0.0))
     bc1 = DirichletBC(W[0], inflow, 1)
 
-    # Zero pressure at outlow at x = 1
+    # Zero pressure at outflow at x = 1
     bc2 = DirichletBC(W[1], 0.0, 2)
 
-    bcs = bc0 + [bc1, bc2]
+    bcs = [bc0, bc1, bc2]
 
     w = Function(W)
 
@@ -62,7 +62,7 @@ def run_stokes_mini(n):
                         "ksp_converged_reason": None,
                         "ksp_type": "cg",
                         "ksp_rtol": 1e-5,
-                        "ksp_max_it": 1000,
+                        "ksp_max_it": 50,
                     }
                 }
             }
@@ -76,31 +76,9 @@ def run_stokes_mini(n):
     solver = LinearVariationalSolver(problem, solver_parameters=iterative_stokes_solver_parameters)
     solver.solve()
 
-    # ksp0 = solver.snes.ksp.pc.getFieldSplitSchurGetSubKSP()[0]
-    # sub_pc = ksp0.pc.???
-    # offload_python_context = sub_pc.getPythonContext()
-    # assert offload_python_context.pc.P.type == "seqaijcusparse"
-    # assert offload_python_context.pc.A.type == "python"
-
-    # We've set up Poiseuille flow, so we expect a parabolic velocity
-    # field and a linearly decreasing pressure.
-    x, y = SpatialCoordinate(mesh)
-    uexact = as_vector([y*(1 - y), 0])
-    pexact = 2*(length - x)
-
-    return errornorm(uexact, u, degree_rise=0), errornorm(pexact, p, degree_rise=0)
-
-
-def test_matfree_A_does_not_offload():
-    u_err = []
-    p_err = []
-
-    for n in range(3, 6):
-        errs = run_stokes_mini(n)
-        u_err.append(errs[0])
-        p_err.append(errs[1])
-
-    u_err = np.asarray(u_err)
-    p_err = np.asarray(p_err)
-    assert (np.log2(u_err[:-1] / u_err[1:]) > 2).all()
-    assert (np.log2(p_err[:-1] / p_err[1:]) > 1.5).all()
+    ksp0 = solver.snes.ksp.pc.getFieldSplitSchurGetSubKSP()[0]
+    assembled_ctx = ksp0.pc.getPythonContext()
+    offload_ctx = assembled_ctx.pc.getPythonContext()
+    A, P = offload_ctx.pc.getOperators()
+    assert P.type == "seqaijcusparse"
+    assert A.type == "python"
