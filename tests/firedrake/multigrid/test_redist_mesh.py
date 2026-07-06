6ad926ed33ba55922b696337375c20fa1a34157c
Pablo Brubeck <brubeck@protonmail.com>
2026-07-06 16:54:41 +0100

    test second level

--- tests/firedrake/multigrid/test_redist_mesh.py
+++ tests/firedrake/multigrid/test_redist_mesh.py
@@ -18,35 +18,35 @@ def test_redistributed_hierarchy_transfers_no_empty_ranks():
     base = UnitSquareMesh(1, 1, distribution_parameters=dparams)
     mh = MeshHierarchy(base, 2)
 
-    for m in mh[1:]:
+    for l, m in enumerate(mh[1:]):
         assert m.cell_set.size > 0
         for k, v in dparams.items():
             assert m._distribution_parameters.get(k, None) == v
 
-    Vc = FunctionSpace(mh[0], "CG", 1)
-    Vf = FunctionSpace(mh[1], "CG", 1)
+        Vc = FunctionSpace(mh[l], "CG", 1)
+        Vf = FunctionSpace(mh[l+1], "CG", 1)
 
-    xc, yc = SpatialCoordinate(mh[0])
-    xf, yf = SpatialCoordinate(mh[1])
-    coarse_expr = xc + 2*yc
-    fine_expr = xf + 2*yf
+        xc, yc = SpatialCoordinate(mh[l])
+        xf, yf = SpatialCoordinate(mh[l+1])
+        coarse_expr = xc + 2*yc
+        fine_expr = xf + 2*yf
 
-    coarse = Function(Vc).interpolate(coarse_expr)
-    fine = Function(Vf)
-    prolong(coarse, fine)
-    assert errornorm(fine_expr, fine) < 1e-12
+        coarse = Function(Vc).interpolate(coarse_expr)
+        fine = Function(Vf)
+        prolong(coarse, fine)
+        assert errornorm(fine_expr, fine) < 1e-12
 
-    one_coarse = Function(Vc).assign(1)
-    one_fine = Function(Vf)
-    prolong(one_coarse, one_fine)
+        one_coarse = Function(Vc).assign(1)
+        one_fine = Function(Vf)
+        prolong(one_coarse, one_fine)
 
-    fine_dual = assemble(conj(TestFunction(Vf))*dx)
-    coarse_dual = Cofunction(Vc.dual())
-    restrict(fine_dual, coarse_dual)
+        fine_dual = assemble(conj(TestFunction(Vf))*dx)
+        coarse_dual = Cofunction(Vc.dual())
+        restrict(fine_dual, coarse_dual)
 
-    assert np.allclose(
-        assemble(action(coarse_dual, one_coarse)),
-        assemble(action(fine_dual, one_fine)),
-        rtol=1e-12,
-        atol=1e-12,
-    )
+        assert np.allclose(
+            assemble(action(coarse_dual, one_coarse)),
+            assemble(action(fine_dual, one_fine)),
+            rtol=1e-12,
+            atol=1e-12,
+        )
