03dc3316c2c4b7ef2d1216964f2417fa436bb075
India Marsden <imm1117@ic.ac.uk>
2026-07-15 14:53:31 +0100

    allow any element on hex if fuse enabled

--- firedrake/functionspaceimpl.py
+++ firedrake/functionspaceimpl.py
@@ -49,7 +49,7 @@ from firedrake.petsc import PETSc
 from firedrake.utils import IntType, deprecated
 
 
-def check_element(element, top=True):
+def check_element(element, top=True, use_fuse=False):
     """Run some checks on the provided element.
 
     The :class:`finat.ufl.mixedelement.VectorElement` and
@@ -82,7 +82,7 @@ def check_element(element, top=True):
        type(element) is not finat.ufl.MixedElement:
         raise ValueError("MixedElement modifier must be outermost")
     if element.cell.cellname == "hexahedron" and \
-       element.family() not in ["Q", "DQ", "Real", "IT"]:
+       element.family() not in ["Q", "DQ", "Real", "IT"] and not use_fuse:
         raise NotImplementedError("Currently can only use 'Q', 'DQ', and/or 'Real' elements on hexahedral meshes, not", element.family())
     if type(element) in (finat.ufl.BrokenElement, finat.ufl.RestrictedElement,
                          finat.ufl.HDivElement, finat.ufl.HCurlElement):
@@ -99,7 +99,7 @@ def check_element(element, top=True):
     else:
         inner = ()
     for e in inner:
-        check_element(e, top=False)
+        check_element(e, top=False, use_fuse=use_fuse)
 
 
 def create_element(ufl_element, use_fuse=False):
@@ -499,7 +499,7 @@ class WithGeometryBase:
             if isinstance(mesh, MeshSequenceGeometry):
                 raise TypeError(f"mesh must not be MeshSequenceGeometry: got {mesh}")
             # Check that any Vector/Tensor/Mixed modifiers are outermost.
-            check_element(element)
+            check_element(element, use_fuse=mesh._use_fuse)
             if element.family() == "Real":
                 new = RealFunctionSpace(topology, element, name=name, **kwargs)
             else:
