cbbcc7197deb3bd17175c0d5e811780fd62aed28
Iglesia Dolci <daia.dolci@gmail.com>
2026-07-15 10:53:52 -0300

    Renew deprecation warning and review some docs

--- firedrake/checkpointing.py
+++ firedrake/checkpointing.py
@@ -23,7 +23,7 @@ import os
 import h5py
 
 
-__all__ = ["HDF5File", "FILE_READ", "FILE_CREATE", "FILE_UPDATE", "CheckpointFile"]
+__all__ = ["DumbCheckpoint", "HDF5File", "FILE_READ", "FILE_CREATE", "FILE_UPDATE", "CheckpointFile"]
 
 
 FILE_READ = PETSc.Viewer.Mode.READ
@@ -59,6 +59,295 @@ r"""The prefix attached to the attributes associated with timestepping history."
 distribution_parameters_noop = {"partition": False,
                                 "overlap_type": (DistributedMeshOverlapType.NONE, 0)}
 reorder_noop = None
+
+
+class DumbCheckpoint:
+
+    r"""A very dumb checkpoint object.
+
+    This checkpoint object is capable of writing :class:`~.Function`\s
+    to disk in parallel (using HDF5) and reloading them on the same
+    number of processes and a :func:`~.Mesh` constructed identically.
+
+    :arg basename: the base name of the checkpoint file.
+    :arg single_file: Should the checkpoint object use only a single
+         on-disk file (irrespective of the number of stored
+         timesteps)?  See :meth:`~.DumbCheckpoint.new_file` for more
+         details.
+    :arg mode: the access mode (one of :data:`~.FILE_READ`,
+         :data:`~.FILE_CREATE`, or :data:`~.FILE_UPDATE`)
+    :arg comm: (optional) communicator the writes should be collective
+         over.
+
+    This object can be used in a context manager (in which case it
+    closes the file when the scope is exited).
+
+    .. note::
+
+       This object contains both a PETSc ``Viewer``, used for storing
+       and loading :class:`~.Function` data, and an :class:`h5py.File`
+       opened on the same file handle.  *DO NOT* call
+       :meth:`h5py.File.close` on the latter, this will cause
+       breakages.
+
+    .. warning::
+
+       ``DumbCheckpoint`` is deprecated and is scheduled for removal in the
+       first major Firedrake release after 1 January 2027.  Use
+       :class:`~.CheckpointFile` instead where possible.
+
+    """
+    def __init__(self, basename, single_file=True,
+                 mode=FILE_UPDATE, comm=None):
+        import warnings
+        with warnings.catch_warnings():
+            warnings.simplefilter('always', DeprecationWarning)
+            warnings.warn("DumbCheckpoint is deprecated and is scheduled for removal in the "
+                          "first major Firedrake release after 1 January 2027; use CheckpointFile instead.",
+                          DeprecationWarning, stacklevel=2)
+        self.comm = comm or COMM_WORLD
+        self.mode = mode
+
+        self._single = single_file
+        self._made_file = False
+        self._basename = basename
+        self._time = None
+        self._tidx = -1
+        self._fidx = 0
+        self.new_file()
+
+    @PETSc.Log.EventDecorator()
+    def set_timestep(self, t, idx=None):
+        r"""Set the timestep for output.
+
+        :arg t: The timestep value.
+        :arg idx: An optional timestep index to use, otherwise an
+             internal index is used, incremented by 1 every time
+             :meth:`set_timestep` is called.
+        """
+        if idx is not None:
+            self._tidx = idx
+        else:
+            self._tidx += 1
+        self._time = t
+        if self.mode == FILE_READ:
+            return
+        indices = self.read_attribute("/", "stored_time_indices", [])
+        new_indices = np.concatenate((indices, [self._tidx]))
+        self.write_attribute("/", "stored_time_indices", new_indices)
+        steps = self.read_attribute("/", "stored_time_steps", [])
+        new_steps = np.concatenate((steps, [self._time]))
+        self.write_attribute("/", "stored_time_steps", new_steps)
+
+    @PETSc.Log.EventDecorator()
+    def get_timesteps(self):
+        r"""Return all the time steps (and time indices) in the current
+        checkpoint file.
+
+        This is useful when reloading from a checkpoint file that
+        contains multiple timesteps and one wishes to determine the
+        final available timestep in the file."""
+        indices = self.read_attribute("/", "stored_time_indices", [])
+        steps = self.read_attribute("/", "stored_time_steps", [])
+        return steps, indices
+
+    @PETSc.Log.EventDecorator()
+    def new_file(self, name=None):
+        r"""Open a new on-disk file for writing checkpoint data.
+
+        :arg name: An optional name to use for the file, an extension
+             of ``.h5`` is automatically appended.
+
+        If ``name`` is not provided, a filename is generated from the
+        ``basename`` used when creating the :class:`~.DumbCheckpoint`
+        object.  If ``single_file`` is ``True``, then we write to
+        ``BASENAME.h5`` otherwise, each time
+        :meth:`~.DumbCheckpoint.new_file` is called, we create a new
+        file with an increasing index.  In this case the files created
+        are::
+
+            BASENAME_0.h5
+            BASENAME_1.h5
+            ...
+            BASENAME_n.h5
+
+        with the index incremented on each invocation of
+        :meth:`~.DumbCheckpoint.new_file` (whenever the custom name is
+        not provided).
+        """
+        self.close()
+        if name is None:
+            if self._single:
+                if self._made_file:
+                    raise ValueError("Can't call new_file without name with 'single_file'")
+                name = "%s.h5" % (self._basename)
+                self._made_file = True
+            else:
+                name = "%s_%s.h5" % (self._basename, self._fidx)
+            self._fidx += 1
+        else:
+            name = "%s.h5" % name
+
+        import os
+        exists = os.path.exists(name)
+        if self.mode == FILE_READ and not exists:
+            raise IOError("File '%s' does not exist, cannot be opened for reading" % name)
+        mode = self.mode
+        if mode == FILE_UPDATE and not exists:
+            mode = FILE_CREATE
+        self._vwr = PETSc.ViewerHDF5().create(name, mode=mode,
+                                              comm=self.comm)
+        if self.mode == FILE_READ:
+            nprocs = self.read_attribute("/", "nprocs")
+            if nprocs != self.comm.size:
+                raise ValueError("Process mismatch: written on %d, have %d" %
+                                 (nprocs, self.comm.size))
+        else:
+            self.write_attribute("/", "nprocs", self.comm.size)
+
+    @property
+    def vwr(self):
+        r"""The PETSc Viewer used to store and load function data."""
+        if hasattr(self, '_vwr'):
+            return self._vwr
+        self.new_file()
+        return self._vwr
+
+    @property
+    def h5file(self):
+        r"""An h5py File object pointing at the open file handle."""
+        if hasattr(self, '_h5file'):
+            return self._h5file
+        self._h5file = h5i.get_h5py_file(self.vwr)
+        return self._h5file
+
+    @PETSc.Log.EventDecorator()
+    def close(self):
+        r"""Close the checkpoint file (flushing any pending writes)"""
+        if hasattr(self, "_vwr"):
+            self._vwr.destroy()
+            del self._vwr
+        if hasattr(self, "_h5file"):
+            self._h5file.flush()
+            del self._h5file
+
+    def _get_data_group(self):
+        r"""Return the group name for function data.
+
+        If a timestep is set, this incorporates the current timestep
+        index.  See :meth:`.set_timestep`."""
+        if self._time is not None:
+            return "/fields/%d" % self._tidx
+        return "/fields"
+
+    def _write_timestep_attr(self, group):
+        r"""Write the current timestep value (if it exists) to the
+        specified group."""
+        if self._time is not None:
+            self.h5file.require_group(group)
+            self.write_attribute(group, "timestep", self._time)
+
+    @PETSc.Log.EventDecorator()
+    def store(self, function, name=None):
+        r"""Store a function in the checkpoint file.
+
+        :arg function: The function to store.
+        :arg name: an (optional) name to store the function under.  If
+             not provided, uses ``function.name()``.
+
+        This function is timestep-aware and stores to the appropriate
+        place if :meth:`set_timestep` has been called.
+        """
+        if self.mode is FILE_READ:
+            raise IOError("Cannot store to checkpoint opened with mode 'FILE_READ'")
+        if not isinstance(function, firedrake.Function):
+            raise ValueError("Can only store functions")
+        name = name or function.name()
+        group = self._get_data_group()
+        self._write_timestep_attr(group)
+        with function.dat.vec_ro as v:
+            self.vwr.pushGroup(group)
+            oname = v.getName()
+            v.setName(name)
+            v.view(self.vwr)
+            v.setName(oname)
+            self.vwr.popGroup()
+
+    @PETSc.Log.EventDecorator()
+    def load(self, function, name=None):
+        r"""Store a function from the checkpoint file.
+
+        :arg function: The function to load values into.
+        :arg name: an (optional) name used to find the function values.  If
+             not provided, uses ``function.name()``.
+
+        This function is timestep-aware and reads from the appropriate
+        place if :meth:`set_timestep` has been called.
+        """
+        if not isinstance(function, firedrake.Function):
+            raise ValueError("Can only load functions")
+        name = name or function.name()
+        group = self._get_data_group()
+        with function.dat.vec_wo as v:
+            self.vwr.pushGroup(group)
+            # PETSc replaces the array in the Vec, which screws things
+            # up for us, so read into temporary Vec.
+            tmp = v.duplicate()
+            tmp.setName(name)
+            tmp.load(self.vwr)
+            tmp.copy(v)
+            tmp.destroy()
+            self.vwr.popGroup()
+
+    def write_attribute(self, obj, name, val):
+        r"""Set an HDF5 attribute on a specified data object.
+
+        :arg obj: The path to the data object.
+        :arg name: The name of the attribute.
+        :arg val: The attribute value.
+
+        Raises :exc:`AttributeError` if writing the attribute fails.
+        """
+        try:
+            self.h5file[obj].attrs[name] = val
+        except KeyError:
+            raise AttributeError("Object '%s' not found" % obj)
+
+    def read_attribute(self, obj, name, default=None):
+        r"""Read an HDF5 attribute on a specified data object.
+
+        :arg obj: The path to the data object.
+        :arg name: The name of the attribute.
+        :arg default: Optional default value to return.  If not
+             provided an :exc:`AttributeError` is raised if the
+             attribute does not exist.
+        """
+        try:
+            return self.h5file[obj].attrs[name]
+        except KeyError:
+            if default is not None:
+                return default
+            raise AttributeError("Attribute '%s' on '%s' not found" % (name, obj))
+
+    def has_attribute(self, obj, name):
+        r"""Check for existance of an HDF5 attribute on a specified data object.
+
+        :arg obj: The path to the data object.
+        :arg name: The name of the attribute.
+        """
+        try:
+            return (name in self.h5file[obj].attrs)
+        except KeyError:
+            return False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
+    def __del__(self):
+        self.close()
 
 
 class HDF5File:
---
+++ tests/firedrake/output/test_dumb_checkpoint.py
@@ -0,0 +1,159 @@
+import pytest
+from firedrake import *
+import numpy as np
+
+
+@pytest.fixture(scope="module",
+                params=[False, True],
+                ids=["simplex", "quad"])
+def mesh(request):
+    return UnitSquareMesh(2, 2, quadrilateral=request.param)
+
+
+@pytest.fixture(params=range(1, 4))
+def degree(request):
+    return request.param
+
+
+@pytest.fixture(params=["CG"])
+def fs(request):
+    return request.param
+
+
+@pytest.fixture(scope="module")
+def f():
+    m = UnitSquareMesh(2, 2)
+    V = FunctionSpace(m, 'CG', 1)
+    f = Function(V, name="f")
+    x = SpatialCoordinate(m)
+    f.interpolate(x[0]*x[1])
+    return f
+
+
+def test_deprecation_warning(dumpfile):
+    match = "scheduled for removal in the first major Firedrake release after 1 January 2027"
+    with pytest.warns(DeprecationWarning, match=match):
+        with DumbCheckpoint(dumpfile, mode=FILE_CREATE):
+            pass
+
+
+def run_store_load(mesh, fs, degree, dumpfile):
+
+    V = FunctionSpace(mesh, fs, degree)
+
+    f = Function(V, name="f")
+    x = SpatialCoordinate(mesh)
+
+    f.interpolate(x[0]*x[1])
+
+    f2 = Function(V, name="f")
+
+    dumpfile = mesh.comm.bcast(dumpfile, root=0)
+    with DumbCheckpoint(dumpfile, mode=FILE_CREATE) as chk:
+        chk.store(f)
+        chk.load(f2)
+
+    assert np.allclose(f.dat.data_ro, f2.dat.data_ro)
+
+
+def test_store_load(mesh, fs, degree, dumpfile):
+    run_store_load(mesh, fs, degree, dumpfile)
+
+
+@pytest.mark.parallel(nprocs=2)
+def test_store_load_parallel(mesh, fs, degree, dumpfile):
+    run_store_load(mesh, fs, degree, dumpfile)
+
+
+@pytest.mark.parallel(nprocs=2)
+def test_serial_checkpoint_parallel_load_fails(f, dumpfile):
+    # Write on COMM_SELF (size == 1)
+    with DumbCheckpoint("%s.%d" % (dumpfile, f.comm.rank),
+                        mode=FILE_CREATE, comm=COMM_SELF) as chk:
+        chk.store(f)
+    # Make sure it's written, and broadcast rank-0 name to all processes
+    fname = f.comm.bcast("%s.0" % dumpfile, root=0)
+    with pytest.raises(ValueError):
+        with DumbCheckpoint(fname, mode=FILE_READ, comm=f.comm) as chk:
+            # Written on 1 process, loading on 2 should raise ValueError
+            chk.load(f)
+
+
+def test_checkpoint_fails_for_non_function(dumpfile):
+    with DumbCheckpoint(dumpfile, mode=FILE_CREATE) as chk:
+        with pytest.raises(ValueError):
+            chk.store(np.arange(10))
+
+
+def test_checkpoint_read_not_exist_ioerror(dumpfile):
+    with pytest.raises(IOError):
+        with DumbCheckpoint(dumpfile, mode=FILE_READ):
+            pass
+
+
+def test_attributes(f, dumpfile):
+    mesh = f.function_space().mesh()
+    with DumbCheckpoint(dumpfile, mode=FILE_CREATE, comm=mesh.comm) as chk:
+        with pytest.raises(AttributeError):
+            chk.write_attribute("/foo", "nprocs", 1)
+        with pytest.raises(AttributeError):
+            chk.read_attribute("/bar", "nprocs")
+
+        chk.store(mesh.coordinates, name="coords")
+
+        assert chk.read_attribute("/", "nprocs") == 1
+
+        chk.write_attribute("/fields/coords", "dimension",
+                            mesh.coordinates.dat.cdim)
+
+        assert chk.read_attribute("/fields/coords", "dimension") == \
+            mesh.coordinates.dat.cdim
+
+
+def test_store_read_only_ioerror(f, dumpfile):
+    # Make file
+    with DumbCheckpoint(dumpfile, mode=FILE_CREATE) as chk:
+        pass
+    with DumbCheckpoint(dumpfile, mode=FILE_READ) as chk:
+        with pytest.raises(IOError):
+            chk.store(f)
+
+
+def test_multiple_timesteps(f, dumpfile):
+    with DumbCheckpoint(dumpfile, mode=FILE_CREATE) as chk:
+        chk.set_timestep(0.1)
+        chk.store(f)
+        chk.set_timestep(0.2)
+        chk.store(f)
+
+        steps, indices = chk.get_timesteps()
+
+        assert np.allclose(steps, [0.1, 0.2])
+        assert np.allclose(indices, [0, 1])
+
+
+def test_new_file(f, dumpfile):
+    custom_name = "%s_custom" % dumpfile
+    with DumbCheckpoint(dumpfile, single_file=False, mode=FILE_CREATE) as chk:
+        chk.store(f)
+        chk.new_file()
+        chk.store(f)
+        chk.new_file(name=custom_name)
+        chk.store(f)
+
+    with DumbCheckpoint("%s_1" % dumpfile, mode=FILE_READ) as chk:
+        g = Function(f.function_space(), name=f.name())
+        chk.load(g)
+        assert np.allclose(g.dat.data_ro, f.dat.data_ro)
+
+    with DumbCheckpoint(custom_name, mode=FILE_READ) as chk:
+        g = Function(f.function_space(), name=f.name())
+        chk.load(g)
+        assert np.allclose(g.dat.data_ro, f.dat.data_ro)
+
+
+def test_new_file_valueerror(f, dumpfile):
+    with DumbCheckpoint(dumpfile, single_file=True, mode=FILE_CREATE) as chk:
+        chk.store(f)
+        with pytest.raises(ValueError):
+            chk.new_file()
