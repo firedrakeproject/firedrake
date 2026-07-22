1cc941d39b5626ae368783dae296db42d201897d
Connor Ward <c.ward20@imperial.ac.uk>
2026-07-22 17:22:46 +0100

    Empty __future__ module

--- firedrake/__future__.py
+++ firedrake/__future__.py
@@ -1,16 +1,6 @@
-from firedrake.interpolation import interpolate as interpolate_default, Interpolator as Interpolator_default
-import warnings
+"""Module that may be imported to enable 'future' features of Firedrake.
 
+This is inspired by the __future__ module in Python
+(https://docs.python.org/3/library/__future__.html).
 
-def interpolate(expr, V, *args, **kwargs):
-    warnings.warn("""The symbolic `interpolate` has been moved into `firedrake`
-                  and is now the default method for interpolating in Firedrake. The need to
-                  `from firedrake.__future__ import interpolate` is now deprecated.""", FutureWarning)
-    return interpolate_default(expr, V, *args, **kwargs)
-
-
-class Interpolator(Interpolator_default):
-    def __new__(cls, *args, **kwargs):
-        warnings.warn("""The symbolic `Interpolator` has been moved into `firedrake`.
-                      The need to `from firedrake.__future__ import Interpolator` is now deprecated.""", FutureWarning)
-        return Interpolator_default(*args, **kwargs)
+"""
