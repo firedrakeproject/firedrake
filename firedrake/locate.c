6635283cb4995216029b480b868bb6bce41e0021
Leo Collins <leocollins511@gmail.com>
2026-07-22 10:14:45 +0100

    Petsc 64 bit int fixes (#5252)
    * function struct use PetscInt
    
    * use correct petsc types
    
    * fix header
    
    * fix to legacy .at
    
    fix
    
    * use PetscInt
    
    fix
    
    * use correct mpi type for permutation
    
    * use blas int
    
    * use dtype
    
    * type fixes
    
    use size_t for ncells_ignore
    use size_t for loops over ignored cells
    locator now returns int since it is an error code
    
    * relax assertions in adjoint test
    
    This was very flaky
    Add additional assertions to test that the transformed reduced
    functional converges faster
    
    * Update firedrake/mesh.py
    
    Co-authored-by: Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
    
    ---------
    
    Co-authored-by: Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>

--- firedrake/locate.c
+++ firedrake/locate.c
@@ -4,19 +4,19 @@
 #include <float.h>
 #include <evaluate.h>
 
-int locate_cell(struct Function *f,
+PetscInt locate_cell(struct Function *f,
         double *x,
         int dim,
         ref_cell_l1_dist try_candidate,
         ref_cell_l1_dist_xtr try_candidate_xtr,
         void *temp_ref_coords,
         void *found_ref_coords,
-        double *found_ref_cell_dist_l1,
+        PetscReal *found_ref_cell_dist_l1,
         size_t ncells_ignore,
-        int* cells_ignore)
+        PetscInt* cells_ignore)
 {
     RTError err;
-    int cell = -1;
+    PetscInt cell = -1;
     int cell_ignore_found = 0;
     /* NOTE: temp_ref_coords and found_ref_coords are actually of type
     struct ReferenceCoords but can't be declared as such in the function
@@ -25,8 +25,8 @@ int locate_cell(struct Function *f,
     surrounds this is declared in pointquery_utils.py. We cast when we use the
     ref_coords_copy function and trust that the underlying memory which the
     pointers refer to is updated as necessary. */
-    double ref_cell_dist_l1 = DBL_MAX;
-    double current_ref_cell_dist_l1 =  -0.5;
+    PetscReal ref_cell_dist_l1 = PETSC_MAX_REAL;
+    PetscReal current_ref_cell_dist_l1 =  -0.5;
     /* NOTE: `tolerance`, which is used throughout this funciton, is a static
        variable defined outside this function when putting together all the C
        code that needs to be compiled - see pointquery_utils.py */
@@ -45,7 +45,7 @@ int locate_cell(struct Function *f,
         if (f->extruded == 0) {
             for (uint64_t i = 0; i < nids; i++) {
                 current_ref_cell_dist_l1 = (*try_candidate)(temp_ref_coords, f, ids[i], x);
-                for (uint64_t j = 0; j < ncells_ignore; j++) {
+                for (size_t j = 0; j < ncells_ignore; j++) {
                     if (ids[i] == cells_ignore[j]) {
                         cell_ignore_found = 1;
                         break;
@@ -76,11 +76,11 @@ int locate_cell(struct Function *f,
         }
         else {
             for (uint64_t i = 0; i < nids; i++) {
-                int nlayers = f->n_layers;
-                int c = ids[i] / nlayers;
-                int l = ids[i] % nlayers;
+                PetscInt nlayers = f->n_layers;
+                PetscInt c = ids[i] / nlayers;
+                PetscInt l = ids[i] % nlayers;
                 current_ref_cell_dist_l1 = (*try_candidate_xtr)(temp_ref_coords, f, c, l, x);
-                for (uint64_t j = 0; j < ncells_ignore; j++) {
+                for (size_t j = 0; j < ncells_ignore; j++) {
                     if (ids[i] == cells_ignore[j]) {
                         cell_ignore_found = 1;
                         break;
@@ -112,9 +112,9 @@ int locate_cell(struct Function *f,
         free(ids);
     } else {
         if (f->extruded == 0) {
-            for (int c = 0; c < f->n_cols; c++) {
+            for (PetscInt c = 0; c < f->n_cols; c++) {
                 current_ref_cell_dist_l1 = (*try_candidate)(temp_ref_coords, f, c, x);
-                for (uint64_t j = 0; j < ncells_ignore; j++) {
+                for (size_t j = 0; j < ncells_ignore; j++) {
                     if (c == cells_ignore[j]) {
                         cell_ignore_found = 1;
                         break;
@@ -144,10 +144,10 @@ int locate_cell(struct Function *f,
             }
         }
         else {
-            for (int c = 0; c < f->n_cols; c++) {
-                for (int l = 0; l < f->n_layers; l++) {
+            for (PetscInt c = 0; c < f->n_cols; c++) {
+                for (PetscInt l = 0; l < f->n_layers; l++) {
                     current_ref_cell_dist_l1 = (*try_candidate_xtr)(temp_ref_coords, f, c, l, x);
-                    for (uint64_t j = 0; j < ncells_ignore; j++) {
+                    for (size_t j = 0; j < ncells_ignore; j++) {
                         if (l == cells_ignore[j]) {
                             cell_ignore_found = 1;
                             break;
