601d63a3f6aafbbfac5b290f530f0a6380d4a109
Anastasia Chanbour <69864747+achanbour@users.noreply.github.com>
2026-07-22 16:42:29 +0100

    Write VOM data to VTKFile (#5244)
    * added support for writing VOM functions in a VTKFile
    
    * added an internal persistent particle ID field to the VOM which is then read by the VTK writer
    * added tests + doc
    
    ---------
    
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -2053,7 +2053,9 @@ class VertexOnlyMeshTopology(AbstractMeshTopology):
                                          "overlap_type": (DistributedMeshOverlapType.NONE, 0)}
         self.input_ordering_swarm = input_ordering_swarm
         self._parent_mesh = parentmesh
+
         super().__init__(swarm, name, reorder, None, perm_is, distribution_name, permutation_name, parentmesh.comm)
+        self._init_particle_ids()
 
     def _distribute(self):
         pass
@@ -2070,6 +2072,18 @@ class VertexOnlyMeshTopology(AbstractMeshTopology):
             # all entities as pyop2 core, which mark_entity_classes will do.
             assert isinstance(self._parent_mesh, VertexOnlyMeshTopology)
             dmcommon.mark_entity_classes(self.topology_dm)
+
+    def _init_particle_ids(self):
+        from firedrake.functionspace import FunctionSpace
+        from firedrake.function import CoordinatelessFunction
+
+        # Attach persistent IDs to VOM points
+        P0 = FunctionSpace(self, "DG", 0)
+        pid = CoordinatelessFunction(P0, dtype=IntType, name="firedrake_particle_ids")
+        n_owned = self.cell_set.size
+        offset = self.comm.scan(n_owned) - n_owned
+        pid.dat.data_wo[:] = np.arange(offset, offset + n_owned, dtype=IntType)
+        self._particle_ids = pid
 
     @cached_property
     def _ufl_cell(self):
@@ -3709,6 +3723,7 @@ def VertexOnlyMesh(mesh, vertexcoords, reorder=None, missing_points_behaviour='e
     )
     vmesh_out = make_vom_from_vom_topology(topology, name, tolerance)
     vmesh_out._parent_mesh = mesh
+
     return vmesh_out
 
 
