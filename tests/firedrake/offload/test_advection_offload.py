12312957dc11e43ca3399ddd725c3233f5900cb6
dsroberts <115434035+dsroberts@users.noreply.github.com>
2026-08-21 19:39:12 +1000

    Minor updates to OffloadPC (#5164)
    * Minor updates to OffloadPC
    
    * Add check for silent P matrix conversion
    
    * Update firedrake/preconditioners/offload.py
    
    Co-authored-by: Josh Hope-Collins <jhc.jss@gmail.com>
    
    * Respond to review comments. Add new tests.
    
    Add comments describing the no-offload matrix types. Add a test to
    ensure value-only copying is happening correctly. Add start of test to
    ensure A is not being offloaded if it is a no-offload type matrix.
    
    * Update tests/firedrake/offload/test_advection_offload.py
    
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>
    
    * Update tests/firedrake/offload/test_advection_offload.py
    
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>
    
    * Update tests/firedrake/offload/test_advection_offload.py
    
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>
    
    * Update tests/firedrake/offload/test_no_offload_A.py
    
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>
    
    * Set structure argument in P.copy/A.copy with kwarg, use PETSc MatStructure enum for value
    
    * Changes to test_no_offload_A.py in response to review
    
    * Make solve step in test_no_offload_A a no-op. Slacken steady-state tolerance in test_advection_offload to reduce test time.
    
    * Address review comment. Add skipnogpu marker to no_offload_A test
    
    ---------
    
    Co-authored-by: Josh Hope-Collins <jhc.jss@gmail.com>
    Co-authored-by: Connor Ward <c.ward20@imperial.ac.uk>

---
+++ tests/firedrake/offload/test_advection_offload.py
@@ -0,0 +1,64 @@
+
+from firedrake import *
+import pytest
+import numpy as np
+
+
+@pytest.mark.skipnogpu
+@pytest.mark.parametrize(
+    "ksp_type, pc_type",
+    [("cg", "sor"), ("cg", "gamg"), ("preonly", "lu")]
+)
+@pytest.mark.parallel([1, 2])
+def test_advection_offload(ksp_type, pc_type):
+    mesh = PeriodicIntervalMesh(100, length=2)
+    x = SpatialCoordinate(mesh)[0]
+    u_init = sin(2*pi*x)
+
+    nested_parameters = {
+        "pc_type": "ksp",
+        "ksp": {
+            "ksp_type": ksp_type,
+            "ksp_max_it": 50,
+            "ksp_rtol": 1e-5,
+            "ksp_monitor": None,
+            "pc_type": pc_type,
+            'ksp_converged_reason': None,
+        },
+    }
+    parameters = {
+        "snes_type": "ksponly",
+        "ksp_type": "preonly",
+        "pc_type": "python",
+        "pc_python_type": "firedrake.OffloadPC",
+        "offload": nested_parameters,
+    }
+
+    nu = Constant(1e-2)
+
+    V = FunctionSpace(mesh, "Lagrange", 2)
+
+    u_n1 = Function(V, name="u^{n+1}")
+    u_n = Function(V, name="u^{n}")
+    v = TestFunction(V)
+
+    u_n.interpolate(u_init)
+    dt = 0.01
+
+    F = (((u_n1 - u_n)/dt) * v + u_n1 * u_n1.dx(0) * v + nu*u_n1.dx(0)*v.dx(0))*dx
+
+    problem = NonlinearVariationalProblem(F, u_n1)
+    solver = NonlinearVariationalSolver(problem, solver_parameters=parameters)
+
+    t = 0
+    steps = 600
+    t_end = steps * dt
+
+    while t <= t_end:
+        solver.solve()
+        maxchange = sqrt(assemble((u_n - u_n1)**2 * dx))
+        u_n.assign(u_n1)
+        t += dt
+        if maxchange < 1e-4 or np.isnan(maxchange):
+            break
+    assert maxchange < 1e-4
