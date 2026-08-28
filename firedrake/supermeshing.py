c724685bf61f82d2ad86dd8d590ffe6d63281ac4
Connor Ward <c.ward20@imperial.ac.uk>
2026-08-28 14:23:41 +0100

    add rtree dep

--- firedrake/supermeshing.py
+++ firedrake/supermeshing.py
@@ -4,6 +4,7 @@ import ctypes
 import pathlib
 import libsupermesh
 import petsctools
+import rtree
 
 from firedrake.cython.supermeshimpl import assemble_mixed_mass_matrix as ammm, intersection_finder
 from firedrake.mg.utils import get_level
@@ -468,6 +469,7 @@ each supermesh cell.
         *[f"-Wl,-rpath,{d}/lib" for d in dirs],
         "-lpetsc",
         "-lsupermesh",
+        str(pathlib.Path(rtree.core.rt._name).absolute()),  # libspatialindex.so
     ]
     dll = load(
         supermesh_kernel_str, "c",
