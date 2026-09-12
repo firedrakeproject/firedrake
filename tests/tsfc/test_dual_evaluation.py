77a5dfc030ad195758c6b2610b24890c162ea3d7
Pablo Brubeck <brubeck@protonmail.com>
2026-09-12 14:38:58 +0100

    add missing ExpressionKernel attributes back to Kernel

--- tests/tsfc/test_dual_evaluation.py
+++ tests/tsfc/test_dual_evaluation.py
@@ -5,11 +5,7 @@ import ufl
 import finat.ufl
 from finat.element_factory import create_element
 from tsfc import compile_expression_dual_evaluation
-from tsfc.kernel_args import CoordinatesKernelArg, OutputKernelArg
-
-
-def has_external_coordinates(kernel):
-    return any(isinstance(arg, CoordinatesKernelArg) for arg in kernel.arguments)
+from tsfc.kernel_args import OutputKernelArg
 
 
 def test_ufl_only_simple():
@@ -19,7 +15,7 @@ def test_ufl_only_simple():
     expr = ufl.inner(v, v)
     W = V
     kernel = compile_expression_dual_evaluation(expr, W.ufl_element())
-    assert not has_external_coordinates(kernel)
+    assert kernel.needs_external_coordinates is False
 
 
 def test_ufl_only_nested_interpolate():
@@ -32,7 +28,7 @@ def test_ufl_only_nested_interpolate():
 
     kernel = compile_expression_dual_evaluation(expression, X.ufl_element())
 
-    assert has_external_coordinates(kernel)
+    assert kernel.needs_external_coordinates is True
 
 
 def test_ufl_only_spatialcoordinate():
@@ -42,7 +38,7 @@ def test_ufl_only_spatialcoordinate():
     expr = x*y - y**2 + x
     W = V
     kernel = compile_expression_dual_evaluation(expr, W.ufl_element())
-    assert has_external_coordinates(kernel)
+    assert kernel.needs_external_coordinates is True
 
 
 def test_ufl_only_from_contravariant_piola():
@@ -52,7 +48,7 @@ def test_ufl_only_from_contravariant_piola():
     expr = ufl.inner(v, v)
     W = ufl.FunctionSpace(mesh, finat.ufl.FiniteElement("P", ufl.triangle, 2))
     kernel = compile_expression_dual_evaluation(expr, W.ufl_element())
-    assert has_external_coordinates(kernel)
+    assert kernel.needs_external_coordinates is True
 
 
 def test_ufl_only_to_contravariant_piola():
@@ -62,7 +58,7 @@ def test_ufl_only_to_contravariant_piola():
     expr = ufl.as_vector([v, v])
     W = ufl.FunctionSpace(mesh, finat.ufl.FiniteElement("RT", ufl.triangle, 1))
     kernel = compile_expression_dual_evaluation(expr, W.ufl_element())
-    assert has_external_coordinates(kernel)
+    assert kernel.needs_external_coordinates is True
 
 
 def test_ufl_only_shape_mismatch():
